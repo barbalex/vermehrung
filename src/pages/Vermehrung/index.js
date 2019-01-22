be850711e6c5bda05af18b81502024cd47df7124
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-22 15:30:54 +0100

    work on vermehrung

--- src/pages/Vermehrung/index.js
+++ src/pages/Vermehrung/index.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 
-import AuthorList from '../../components/AuthorList'
+const Container = styled.div`
+  margin-top: 64px;
+  min-height: calc(100vh - 64px);
+`
+const StyledReflexContainer = styled(ReflexContainer)`
+  height: calc(100vh - 64px) !important;
+`
+
+//import AuthorList from '../../components/AuthorList'
 import ErrorBoundary from '../../components/ErrorBoundary'
 import Layout from '../../components/Layout'
 
-const Index = ({ data }) => (
-  <ErrorBoundary>
-    <Layout>
-      <div>
-        <h1>Vermehrung</h1>
-        <AuthorList authors={data.hasura.person} />
-      </div>
-    </Layout>
-  </ErrorBoundary>
-)
+const Index = ({ data }) => {
+  return (
+    <ErrorBoundary>
+      <Layout>
+        <Container>
+          <StyledReflexContainer orientation="vertical">
+            <ReflexElement
+              flex={0.3}
+              propagateDimensions={true}
+              renderOnResizeRate={200}
+              renderOnResize={true}
+            >
+              <p>Tree</p>
+            </ReflexElement>
+            <ReflexSplitter />
+            <ReflexElement
+              propagateDimensions={true}
+              renderOnResizeRate={200}
+              renderOnResize={true}
+            >
+              <p>Form</p>
+            </ReflexElement>
+          </StyledReflexContainer>
+        </Container>
+      </Layout>
+    </ErrorBoundary>
+  )
+}
 
 export default Index
 
