b6c9d47d8dd5d82d56f6132857e31b7ce8c0ef34
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-26 20:58:38 +0100

    simplify

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,17 @@
 import Typography from 'typography'
 
-import theme from './theme'
-
-const typography = new Typography(theme)
+const typography = new Typography({
+  title: 'Homegrown',
+  baseFontSize: '16px',
+  headerFontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+  fontFeatureSettings: 'normal',
+  googleFonts: [
+    {
+      name: 'Roboto',
+      styles: ['300', '400', '500', '600', '700'],
+    },
+  ],
+})
 
 export default typography
