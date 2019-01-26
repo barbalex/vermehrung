6402d9a7ba179133135c0e404b3c6acd8b551a87
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-26 23:22:16 +0100

    clean

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback } from 'react'
 import Typography from '@material-ui/core/Typography'
 import { graphql, navigate } from 'gatsby'
 import styled from 'styled-components'
