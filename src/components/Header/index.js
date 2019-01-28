e2648c11a22310501f100966b364d238a7343349
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-28 11:22:41 +0100

    lint

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
