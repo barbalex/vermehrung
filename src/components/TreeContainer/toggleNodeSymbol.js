bf72593f15e9954c9ef57a191ee3f9598ef3a1ed
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-30 10:40:19 +0100

    squash bug

--- src/components/TreeContainer/toggleNodeSymbol.js
+++ src/components/TreeContainer/toggleNodeSymbol.js
@@ -1,10 +1,13 @@
 import isEqual from 'lodash/isEqual'
+import { getSnapshot } from 'mobx-state-tree'
 
 import isNodeOpen from './isNodeOpen'
 
-export default ({ node, store }) => {
+export default ({ node: nodeRaw, store }) => {
+  const node = getSnapshot(nodeRaw)
   if (!node.url) throw new Error('passed node has no url')
-  const { setOpenNodes, openNodes } = store.tree
+  const { setOpenNodes, openNodes: openNodesRaw } = store.tree
+  const openNodes = getSnapshot(openNodesRaw)
 
   let newOpenNodes = [...openNodes]
   if (isNodeOpen(openNodes, node.url)) {
