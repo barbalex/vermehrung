c50ed2e1d487dd22a848560dc818966273484f0d
Alexander Gabriel <alex.barbalex@gmail.com>
2019-04-18 16:28:37 +0200

    optimize

---
+++ src/pages/Technische-Dokumentation.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+
+import Layout from '../components/Layout'
+import Sidebar from '../templates/Sidebar'
+import ErrorBoundary from '../components/ErrorBoundary'
+
+const Container = styled.div`
+  margin-top: 64px;
+  height: calc(100vh - 64px);
+  display: flex;
+`
+const Doku = styled.div`
+  width: 100%;
+  padding: 25px;
+  overflow-y: auto;
+  ul {
+    margin-top: 0;
+  }
+  p,
+  li {
+    margin-bottom: 0;
+  }
+  h1,
+  h3,
+  ol {
+    margin-bottom: 10px;
+  }
+  h2 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+`
+
+const Template = ({ data }) => {
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+
+  return (
+    <ErrorBoundary>
+      <Layout>
+        <Container>
+          <Sidebar
+            title="Technische Dokumentation"
+            titleLink="/Technische-Dokumentation/"
+            edges={edges}
+          />
+          <Doku>
+            <p>Hier erfahren Sie, wie vermehrung.apflora.ch funktioniert</p>
+          </Doku>
+        </Container>
+      </Layout>
+    </ErrorBoundary>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___sort] }
+      filter: { fileAbsolutePath: { regex: "/(/technischeDoku)/.*.md$/" } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD.MM.YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Template
