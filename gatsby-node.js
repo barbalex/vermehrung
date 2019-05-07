57bfb4933813483aa6775301db288966d8dd783b
Alexander Gabriel <alex.barbalex@gmail.com>
2019-05-07 09:29:52 +0200

    initialize auth0

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 const path = require('path')
 
-exports.createPages = ({ actions, graphql }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
 
   const technDokuTemplate = path.resolve(`src/templates/technDokuTemplate.js`)
@@ -15,7 +15,7 @@ exports.createPages = ({ actions, graphql }) => {
     `src/templates/benutzerDokuTemplate.js`,
   )
 
-  return graphql(`
+  const result = await graphql(`
     {
       allMarkdownRemark(
         sort: { order: ASC, fields: [frontmatter___sort] }
@@ -31,29 +31,51 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+  `)
 
-    const { edges } = result.data.allMarkdownRemark
-    edges.forEach(({ node }) => {
-      if (node.frontmatter.typ === 'technDoku') {
-        return createPage({
-          path: node.frontmatter.path,
-          component: technDokuTemplate,
-        })
-      } else if (node.frontmatter.typ === 'benutzerDoku') {
-        createPage({
-          path: node.frontmatter.path,
-          component: benutzerDokuTemplate,
-        })
-      } else {
-        console.log(
-          'gatsby-node, node: NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!',
-          node,
-        )
-      }
-    })
+  if (result.errors) {
+    return Promise.reject(result.errors)
+  }
+
+  const { edges } = result.data.allMarkdownRemark
+  edges.forEach(({ node }) => {
+    if (node.frontmatter.typ === 'technDoku') {
+      return createPage({
+        path: node.frontmatter.path,
+        component: technDokuTemplate,
+      })
+    } else if (node.frontmatter.typ === 'benutzerDoku') {
+      createPage({
+        path: node.frontmatter.path,
+        component: benutzerDokuTemplate,
+      })
+    } else {
+      console.log(
+        'gatsby-node, node: NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!',
+        node,
+      )
+    }
   })
+}
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    /*
+     * During the build step, `auth0-js` will break because it relies on
+     * browser-specific APIs. Fortunately, we don’t need it during the build.
+     * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+     * during the build. (See `src/utils/auth.js` to see how we prevent this
+     * from breaking the app.)
+     */
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /auth0-js/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
 }
