555426fdfe8bab4eac2da44333fbe958cbdfef4a
Alexander Gabriel <alex.barbalex@gmail.com>
2019-05-24 18:20:41 +0200

    update material ui

--- src/utils/materialTheme.js
+++ src/utils/materialTheme.js
@@ -1,15 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import purple from '@material-ui/core/colors/purple'
-//import red from '@material-ui/core/colors/red'
 
 const theme = createMuiTheme({
   palette: {
     primary: { main: purple[900] },
-    /*error: {
-      main: red[800],
-    },*/
   },
-  typography: { useNextVariants: true },
 })
 
 export default theme
