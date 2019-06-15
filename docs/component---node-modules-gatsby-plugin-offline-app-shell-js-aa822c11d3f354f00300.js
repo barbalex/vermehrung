162f13b2240a76e175dd48d77606a4e37d0b8053
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-15 21:55:53 +0200

    remove old docs

--- docs/component---node-modules-gatsby-plugin-offline-app-shell-js-aa822c11d3f354f00300.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{291:function(e,t,n){"use strict";var u=n(11);t.__esModule=!0,t.default=void 0;var o=u(n(9)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
-//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-aa822c11d3f354f00300.js.map
