914107513ee89e2fac391b887b82adb7b343d423
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-17 18:12:01 +0200

    show inactive

--- src/components/Header/Vermehrung/index.js
+++ src/components/Header/Vermehrung/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext, useCallback } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
+import Switch from '@material-ui/core/Switch'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
+import { observer } from 'mobx-react-lite'
+
 import Account from './Account'
 //import More from './More'
 import ErrorBoundary from '../../ErrorBoundary'
 import Search from './Search'
+import storeContext from '../../../storeContext'
+import Label from '../../shared/Label'
 
 const SiteTitle = styled(Button)`
   display: none;
@@ -26,7 +31,6 @@ const SiteTitle = styled(Button)`
 const Spacer = styled.div`
   flex-grow: 1;
 `
-
 const NavButton = styled(Button)`
   color: white !important;
   border-color: rgba(255, 255, 255, 0.5) !important;
@@ -37,28 +41,67 @@ const NavButton = styled(Button)`
     border-width: 1px !important;
   }
 `
+const HideActiveDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 5px;
+  min-width: 40px;
+  padding-left: 10px;
+  > div {
+    color: white !important;
+  }
+`
+const StyledSwitch = styled(Switch)`
+  margin-left: -13px;
+  margin-top: -18px;
+`
 
-const HeaderVermehrung = () => (
-  <ErrorBoundary>
-    <AppBar position="fixed">
-      <Toolbar>
-        <SiteTitle variant="outlined" component={Link} to="/" title="Home">
-          Vermehrung
-        </SiteTitle>
-        <Spacer />
-        <NavButton
-          variant="outlined"
-          component={Link}
-          to="/Dokumentation/Benutzer/"
-        >
-          Dokumentation
-        </NavButton>
-        <Search />
-        <Account />
-        {/*<More />*/}
-      </Toolbar>
-    </AppBar>
-  </ErrorBoundary>
-)
+const HeaderVermehrung = () => {
+  const store = useContext(storeContext)
+  const { setHideInactive, hideInactive } = store
 
-export default HeaderVermehrung
+  const onClickHideActive = useCallback(() => setHideInactive(!hideInactive), [
+    hideInactive,
+  ])
+
+  return (
+    <ErrorBoundary>
+      <AppBar position="fixed">
+        <Toolbar>
+          <SiteTitle variant="outlined" component={Link} to="/" title="Home">
+            Vermehrung
+          </SiteTitle>
+          <Spacer />
+          <NavButton
+            variant="outlined"
+            component={Link}
+            to="/Dokumentation/Benutzer/"
+          >
+            Dokumentation
+          </NavButton>
+          <Search />
+          <HideActiveDiv>
+            <Label
+              label="Inaktive"
+              title={`Inaktive Personen, Gärten und Kulturen ${
+                hideInactive ? 'anzeigen' : 'verbergen'
+              }`}
+            />
+            <StyledSwitch
+              checked={hideInactive}
+              onChange={onClickHideActive}
+              color="secondary"
+              title={`Inaktive Personen, Gärten und Kulturen ${
+                hideInactive ? 'anzeigen' : 'verbergen'
+              }`}
+            />
+          </HideActiveDiv>
+          <Account />
+          {/*<More />*/}
+        </Toolbar>
+      </AppBar>
+    </ErrorBoundary>
+  )
+}
+
+export default observer(HeaderVermehrung)
--- src/utils/materialTheme.js
+++ src/utils/materialTheme.js
@@ -1,9 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import purple from '@material-ui/core/colors/purple'
+import amber from '@material-ui/core/colors/amber'
 
 const theme = createMuiTheme({
   palette: {
     primary: { main: purple[900] },
+    secondary: { main: amber[800] },
   },
 })
 
