87dec92bb0018657b9ec10061d49d0f49c2919f9
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-25 01:06:19 +0200

    remove loadable

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -1,47 +1,46 @@
 import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import loadable from '@loadable/component'
 
 import storeContext from '../../storeContext'
+import Art from './Art'
+import Herkunft from './Herkunft'
+import Sammlung from './Sammlung'
+import Garten from './Garten'
+import Kultur from './Kultur'
+import Lieferung from './Lieferung'
+import Person from './Person'
+import Zaehlung from './Zaehlung'
+import Event from './Event'
 
 const Data = () => {
   const { activeForm } = useContext(storeContext)
 
   switch (activeForm) {
     case 'art': {
-      const Art = loadable(() => import('./Art'))
       return <Art />
     }
     case 'herkunft': {
-      const Herkunft = loadable(() => import('./Herkunft'))
       return <Herkunft />
     }
     case 'sammlung': {
-      const Sammlung = loadable(() => import('./Sammlung'))
       return <Sammlung />
     }
     case 'garten': {
-      const Garten = loadable(() => import('./Garten'))
       return <Garten />
     }
     case 'kultur': {
-      const Kultur = loadable(() => import('./Kultur'))
       return <Kultur />
     }
     case 'lieferung': {
-      const Lieferung = loadable(() => import('./Lieferung'))
       return <Lieferung />
     }
     case 'person': {
-      const Person = loadable(() => import('./Person'))
       return <Person />
     }
     case 'zaehlung': {
-      const Zaehlung = loadable(() => import('./Zaehlung'))
       return <Zaehlung />
     }
     case 'event': {
-      const Event = loadable(() => import('./Event'))
       return <Event />
     }
     default:
