2ff666c41edfae1f6e6f31c4f15e5a2f5eee8067
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-25 23:52:26 +0200

    optimize

--- src/components/TreeContainer/openNode.js
+++ src/components/TreeContainer/openNode.js
@@ -2,15 +2,15 @@ import { getSnapshot } from 'mobx-state-tree'
 import isNodeOpen from './isNodeOpen'
 
 export default ({ node: nodeRaw, store }) => {
-  const { setOpenNodes, openNodes: openNodesRaw, addNode } = store.tree
+  const { addOpenNodes, openNodes: openNodesRaw, addNode } = store.tree
   const openNodes = getSnapshot(openNodesRaw)
   const node = getSnapshot(nodeRaw)
   // make sure this node's url is not yet contained
   // otherwise same nodes will be added multiple times!
   if (isNodeOpen(openNodes, node.url)) return
 
-  let newOpenNodes = [...openNodes, node.url]
-  setOpenNodes(newOpenNodes)
+  console.log('openNode opening:', node.url)
+  addOpenNodes([node.url])
   // now add a loading node at the right position
   // to tell the user what is going on
   const loadingNode = {
