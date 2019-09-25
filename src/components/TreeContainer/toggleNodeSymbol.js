2975acfb9da968433d9a3ee46299f8b57655b62b
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-25 23:52:59 +0200

    optimize

--- src/components/TreeContainer/toggleNodeSymbol.js
+++ src/components/TreeContainer/toggleNodeSymbol.js
@@ -6,15 +6,16 @@ import isNodeOpen from './isNodeOpen'
 export default ({ node: nodeRaw, store }) => {
   const node = getSnapshot(nodeRaw)
   if (!node.url) throw new Error('passed node has no url')
-  const { setOpenNodes, openNodes: openNodesRaw } = store.tree
+  const { setOpenNodes, addOpenNodes, openNodes: openNodesRaw } = store.tree
   const openNodes = getSnapshot(openNodesRaw)
 
+  store.filter.setShow(false)
   let newOpenNodes = [...openNodes]
   if (isNodeOpen(openNodes, node.url)) {
     newOpenNodes = newOpenNodes.filter(n => !isEqual(n, node.url))
+    setOpenNodes(newOpenNodes)
   } else {
-    newOpenNodes.push(node.url)
+    //newOpenNodes.push(node.url)
+    addOpenNodes([node.url])
   }
-  store.filter.setShow(false)
-  setOpenNodes(newOpenNodes)
 }
