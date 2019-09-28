3b220a67a5c34ff7875c0aee9891086170e5e3b8
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-28 23:09:56 +0200

    query data for art timeline

--- src/components/Data/Art/artQuery.js
+++ src/components/Data/Art/artQuery.js
@@ -1,11 +1,45 @@
 import gql from 'graphql-tag'
 
-import { art as artFragment } from '../../../utils/fragments'
+import {
+  art as artFragment,
+  lieferung as lieferungFragment,
+  sammlung as sammlungFragment,
+} from '../../../utils/fragments'
 
 export default gql`
   query ArtQuery($id: bigint!, $filter: art_bool_exp!, $isFiltered: Boolean!) {
     art(where: { id: { _eq: $id } }) {
       ...ArtFields
+      sammlungsDone: sammlungs(
+        where: { geplant: { _eq: false }, datum: { _is_null: false } }
+        order_by: { datum: asc }
+      ) {
+        ...SammlungFields
+      }
+      sammlungsPlanned: sammlungs(
+        where: { geplant: { _eq: true }, datum: { _is_null: false } }
+        order_by: { datum: asc }
+      ) {
+        ...SammlungFields
+      }
+      lieferungsDone: lieferungs(
+        where: {
+          nach_ausgepflanzt: { _eq: true }
+          geplant: { _eq: false }
+          datum: { _is_null: false }
+        }
+      ) {
+        ...LieferungFields
+      }
+      lieferungsPlanned: lieferungs(
+        where: {
+          nach_ausgepflanzt: { _eq: true }
+          geplant: { _eq: true }
+          datum: { _is_null: false }
+        }
+      ) {
+        ...LieferungFields
+      }
     }
     rowsUnfiltered: art @include(if: $isFiltered) {
       id
@@ -15,4 +49,6 @@ export default gql`
     }
   }
   ${artFragment}
+  ${lieferungFragment}
+  ${sammlungFragment}
 `
