e3eb52eb2c09f4b90f294e9f5aaad3620d999a81
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-05 18:51:15 +0200

    work on qk

--- src/components/Data/Art/QK/query.js
+++ src/components/Data/Art/QK/query.js
@@ -1,17 +1,18 @@
 import gql from 'graphql-tag'
 
-import { art as artFragment } from '../../../utils/fragments'
+import { kultur } from '../../../utils/fragments'
 
 export default gql`
   uery ArtQkQuery($artId: bigint!, $startYear: date!, $startNextYear: date!) {
   art(where: {id: {_eq: $artId}}) {
     id
     kultursWithoutVonAnzahlIndividuen: kulturs(where: {von_anzahl_individuen: {_is_null: true}}) {
-      id
+      ...KulturFields
     }
     kultursWithoutZaehlungInYear: kulturs(where: {_not: {zaehlungs: {_and: [{datum: {_gte: $startYear}}, {datum: {_lt: $startNextYear}}]}}}) {
-      id
+      ...KulturFields
     }
   }
 }
+${kultur}
 `
