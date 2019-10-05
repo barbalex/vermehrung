39105a54b86fcdca457f33b5ca67d79f40aaf9c3
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-05 21:56:02 +0200

    lint

--- src/components/Data/Art/QK/QkContent.js
+++ src/components/Data/Art/QK/QkContent.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import { useQuery } from '@apollo/react-hooks'
