ad38bafd0bac7cbeed2ec1d9e47c7aca4e46300c
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-17 11:42:46 +0200

    trigger qks

--- src/sql/createTriggers.sql
+++ src/sql/createTriggers.sql
@@ -38,12 +38,11 @@ CREATE TRIGGER kultur_has_felder AFTER INSERT ON kultur
 
 -- in case this trigger was not working
 -- add kultur where they are missing
-insert into kultur_felder (kultur_id)
-select kultur.id from kultur
-left join kultur_felder
-on kultur_felder.kultur_id = kultur.id
-where kultur_felder.kultur_id is null;
-
+--insert into kultur_felder (kultur_id)
+--select kultur.id from kultur
+--left join kultur_felder
+--on kultur_felder.kultur_id = kultur.id
+--where kultur_felder.kultur_id is null;
 
 DROP TRIGGER IF EXISTS person_has_felder ON person cascade;
 DROP FUNCTION IF EXISTS person_has_felder() cascade;
@@ -67,6 +66,28 @@ left join person_felder
 on person_felder.person_id = person.id
 where person_felder.person_id is null;
 
+DROP TRIGGER IF EXISTS kultur_has_qk_choosen ON kultur_qk_choosen cascade;
+DROP FUNCTION IF EXISTS kultur_has_qk_choosen() cascade;
+CREATE FUNCTION kultur_has_qk_choosen() RETURNS trigger AS $kultur_has_qk_choosen$
+BEGIN
+  insert into kultur_qk_choosen(kultur_id, qk_name)
+  select distinct kultur.id, kultur_qk.name from kultur, kultur_qk where kultur.id = NEW.id;
+  RETURN NEW;
+END;
+$kultur_has_qk_choosen$ LANGUAGE plpgsql;
 
+CREATE TRIGGER kultur_has_qk_choosen AFTER INSERT ON kultur
+  FOR EACH ROW EXECUTE PROCEDURE kultur_has_qk_choosen();
 
+DROP TRIGGER IF EXISTS art_has_qk_choosen ON art_qk_choosen cascade;
+DROP FUNCTION IF EXISTS art_has_qk_choosen() cascade;
+CREATE FUNCTION art_has_qk_choosen() RETURNS trigger AS $art_has_qk_choosen$
+BEGIN
+  insert into art_qk_choosen(art_id, qk_name)
+  select distinct art.id, art_qk.name from art, art_qk where art.id = NEW.id;
+  RETURN NEW;
+END;
+$art_has_qk_choosen$ LANGUAGE plpgsql;
 
+CREATE TRIGGER art_has_qk_choosen AFTER INSERT ON art
+  FOR EACH ROW EXECUTE PROCEDURE art_has_qk_choosen();
