a4c965e507b4bdd4ed07188c661a62d8e42094fe
Alexander Gabriel <alex.barbalex@gmail.com>
2019-12-06 12:00:30 +0100

    add to export

--- src/components/Data/Kultur/download.js
+++ src/components/Data/Kultur/download.js
@@ -20,6 +20,7 @@ export default async ({ client, store, kultur_id }) => {
         query GetKulturForKulturDownload($id: bigint!) {
           kultur(where: { id: { _eq: $id } }) {
             id
+            art_id
             art {
               id
               art_ae_art {
@@ -58,8 +59,8 @@ export default async ({ client, store, kultur_id }) => {
     })
   }
   const kultur = { ...get(kulturResult, 'data.kultur[0]') }
-  kultur.art_id = get(kultur, 'art.art_ae_art.id', '')
-  kultur.art_name = get(kultur, 'art.art_ae_art.name', '')
+  kultur.art_ae_id = get(kultur, 'art.art_ae_art.id', '')
+  kultur.art_ae_name = get(kultur, 'art.art_ae_art.name', '')
   kultur.herkunft_id = get(kultur, 'herkunft.id', '')
   kultur.herkunft_nr = get(kultur, 'herkunft.nr', '')
   kultur.garten_id = get(kultur, 'garten.id', '')
@@ -164,8 +165,292 @@ export default async ({ client, store, kultur_id }) => {
     data: zaehlungen,
   })
   // 3. Get Teil-Zählungen
+  let teilzaehlungResult
+  try {
+    teilzaehlungResult = await client.query({
+      query: gql`
+        query GetTeilzaehlungForKulturDownload($id: bigint!) {
+          teilzaehlung(where: { zaehlung: { kultur_id: { _eq: $id } } }) {
+            id
+            zaehlung_id
+            teilkultur_id
+            teilkultur {
+              id
+              name
+            }
+            anzahl_pflanzen
+            anzahl_auspflanzbereit
+            anzahl_mutterpflanzen
+            andere_menge
+            auspflanzbereit_beschreibung
+            bemerkungen
+          }
+        }
+      `,
+      variables: {
+        id: kultur_id,
+      },
+    })
+  } catch (error) {
+    return enqueNotification({
+      message: error.message,
+      options: {
+        variant: 'error',
+      },
+    })
+  }
+  const teilzaehlungenArray = get(teilzaehlungResult, 'data.teilzaehlung') || []
+  const teilzaehlungen = teilzaehlungenArray.map(z => {
+    z.teilkultur_name = get(z, 'teilkultur.name', '')
+    delete z.teilkultur
+    delete z.__typename
+    return z
+  })
+  addWorksheetToExceljsWorkbook({
+    workbook,
+    title: 'Teilzaehlungen',
+    data: teilzaehlungen,
+  })
   // 4. Get An-Lieferungen
+  let anlieferungResult
+  try {
+    anlieferungResult = await client.query({
+      query: gql`
+        query GetAnlieferungForKulturDownload($id: bigint!) {
+          lieferung(where: { nach_kultur_id: { _eq: $id } }) {
+            id
+            sammel_lieferung_id
+            art_id
+            art {
+              id
+              art_ae_art {
+                id
+                name
+              }
+            }
+            person_id
+            person {
+              id
+              name
+            }
+            von_sammlung_id
+            von_sammlung: sammlung {
+              id
+              datum
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+              person_id
+              person {
+                id
+                name
+              }
+            }
+            von_kultur_id
+            kulturByVonKulturId {
+              id
+              garten_id
+              garten {
+                id
+                name
+              }
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+            }
+            datum
+            nach_kultur_id
+            kulturByNachKulturId {
+              id
+              garten_id
+              garten {
+                id
+                name
+              }
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+            }
+            nach_ausgepflanzt
+            von_anzahl_individuen
+            anzahl_pflanzen
+            anzahl_auspflanzbereit
+            gramm_samen
+            andere_menge
+            geplant
+            bemerkungen
+          }
+        }
+      `,
+      variables: {
+        id: kultur_id,
+      },
+    })
+  } catch (error) {
+    return enqueNotification({
+      message: error.message,
+      options: {
+        variant: 'error',
+      },
+    })
+  }
+  const anlieferungenArray = get(anlieferungResult, 'data.lieferung') || []
+  const anlieferungen = anlieferungenArray.map(z => {
+    z.art_ae_id = get(z, 'art.art_ae_art.id', '')
+    z.art_ae_name = get(z, 'art.art_ae_art.name', '')
+    delete z.art
+    z.person_name = get(z, 'person.name', '')
+    delete z.person
+    z.von_sammlung_datum = get(z, 'von_sammlung.datum', '')
+    z.von_sammlung_herkunft_id = get(z, 'von_sammlung.herkunft_id', '')
+    z.von_sammlung_herkunft_nr = get(z, 'von_sammlung.herkunft.nr', '')
+    z.von_sammlung_person_id = get(z, 'von_sammlung.person_id', '')
+    z.von_sammlung_person_name = get(z, 'von_sammlung.person.name', '')
+    delete z.von_sammlung
+    z.von_kultur_garten_id = get(z, 'kulturByVonKulturId.garten_id', '')
+    z.von_kultur_garten_name = get(z, 'kulturByVonKulturId.garten.name', '')
+    z.von_kultur_herkunft_id = get(z, 'kulturByVonKulturId.herkunft_id', '')
+    z.von_kultur_herkunft_nr = get(z, 'kulturByVonKulturId.herkunft.nr', '')
+    delete z.kulturByVonKulturId
+    z.nach_kultur_garten_id = get(z, 'kulturByNachKulturId.garten_id', '')
+    z.nach_kultur_garten_name = get(z, 'kulturByNachKulturId.garten.name', '')
+    z.nach_kultur_herkunft_id = get(z, 'kulturByNachKulturId.herkunft_id', '')
+    z.nach_kultur_herkunft_nr = get(z, 'kulturByNachKulturId.herkunft.nr', '')
+    delete z.kulturByNachKulturId
+    delete z.__typename
+    return z
+  })
+  addWorksheetToExceljsWorkbook({
+    workbook,
+    title: 'Anlieferungen',
+    data: anlieferungen,
+  })
   // 5. Get Aus-Lieferungen
+  let auslieferungResult
+  try {
+    auslieferungResult = await client.query({
+      query: gql`
+        query GetAuslieferungForKulturDownload($id: bigint!) {
+          lieferung(where: { von_kultur_id: { _eq: $id } }) {
+            id
+            sammel_lieferung_id
+            art_id
+            art {
+              id
+              art_ae_art {
+                id
+                name
+              }
+            }
+            person_id
+            person {
+              id
+              name
+            }
+            von_sammlung_id
+            von_sammlung: sammlung {
+              id
+              datum
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+              person_id
+              person {
+                id
+                name
+              }
+            }
+            von_kultur_id
+            kulturByVonKulturId {
+              id
+              garten_id
+              garten {
+                id
+                name
+              }
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+            }
+            datum
+            nach_kultur_id
+            kulturByNachKulturId {
+              id
+              garten_id
+              garten {
+                id
+                name
+              }
+              herkunft_id
+              herkunft {
+                id
+                nr
+              }
+            }
+            nach_ausgepflanzt
+            von_anzahl_individuen
+            anzahl_pflanzen
+            anzahl_auspflanzbereit
+            gramm_samen
+            andere_menge
+            geplant
+            bemerkungen
+          }
+        }
+      `,
+      variables: {
+        id: kultur_id,
+      },
+    })
+  } catch (error) {
+    return enqueNotification({
+      message: error.message,
+      options: {
+        variant: 'error',
+      },
+    })
+  }
+  const auslieferungenArray = get(auslieferungResult, 'data.lieferung') || []
+  const auslieferungen = auslieferungenArray.map(z => {
+    z.art_ae_id = get(z, 'art.art_ae_art.id', '')
+    z.art_ae_name = get(z, 'art.art_ae_art.name', '')
+    delete z.art
+    z.person_name = get(z, 'person.name', '')
+    delete z.person
+    z.von_sammlung_datum = get(z, 'von_sammlung.datum', '')
+    z.von_sammlung_herkunft_id = get(z, 'von_sammlung.herkunft_id', '')
+    z.von_sammlung_herkunft_nr = get(z, 'von_sammlung.herkunft.nr', '')
+    z.von_sammlung_person_id = get(z, 'von_sammlung.person_id', '')
+    z.von_sammlung_person_name = get(z, 'von_sammlung.person.name', '')
+    delete z.von_sammlung
+    z.von_kultur_garten_id = get(z, 'kulturByVonKulturId.garten_id', '')
+    z.von_kultur_garten_name = get(z, 'kulturByVonKulturId.garten.name', '')
+    z.von_kultur_herkunft_id = get(z, 'kulturByVonKulturId.herkunft_id', '')
+    z.von_kultur_herkunft_nr = get(z, 'kulturByVonKulturId.herkunft.nr', '')
+    delete z.kulturByVonKulturId
+    z.nach_kultur_garten_id = get(z, 'kulturByNachKulturId.garten_id', '')
+    z.nach_kultur_garten_name = get(z, 'kulturByNachKulturId.garten.name', '')
+    z.nach_kultur_herkunft_id = get(z, 'kulturByNachKulturId.herkunft_id', '')
+    z.nach_kultur_herkunft_nr = get(z, 'kulturByNachKulturId.herkunft.nr', '')
+    delete z.kulturByNachKulturId
+    delete z.__typename
+    return z
+  })
+  addWorksheetToExceljsWorkbook({
+    workbook,
+    title: 'Auslieferungen',
+    data: auslieferungen,
+  })
   // 6. Get Events
 
   let buffer
