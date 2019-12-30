b460570b0d457b58edffc419b1ee9357df79b55f
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-12-30 17:36:14 +0100

    ensure list query refetches

--- src/components/TreeContainer/Tree/delete.js
+++ src/components/TreeContainer/Tree/delete.js
@@ -1,13 +1,21 @@
 import gql from 'graphql-tag'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
 import tableFromTitleHash from '../../../utils/tableFromTitleHash'
 
 export default async ({ node, store, client }) => {
-  const { activeNodeArray, setActiveNodeArray, refetch } = store.tree
+  const { activeNodeArray, setActiveNodeArray } = store.tree
   // get table and id from url
   const title = node.url.slice(-2)[0]
   const id = node.url.slice(-1)[0]
   const table = tableFromTitleHash[title]
+
+  const refetchQueries = ['TreeQueryForTreeContainer']
+  // ensure list form's query is refetched
+  const tableExtract = upperFirst(camelCase(table))
+  const listQueryName = `${tableExtract}QueryFor${tableExtract}s`
+  refetchQueries.push(listQueryName)
 
   const mutation = gql`
     mutation deleteDatasetInTree {
@@ -21,10 +29,10 @@ export default async ({ node, store, client }) => {
   try {
     await client.mutate({
       mutation,
+      refetchQueries,
     })
   } catch (error) {
     return console.log('Error deleting dataset', error.message)
   }
   setActiveNodeArray(activeNodeArray.slice(0, -1))
-  refetch()
 }
