e782dadd1bc8e9784a46ef4b435e394b4d6fec59
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-12 19:04:18 +0100

    correct possible error

--- backend/auth/server.js
+++ backend/auth/server.js
@@ -147,7 +147,7 @@ async function start() {
               'x-hasura-default-role': user_role,
               'x-hasura-allowed-roles': [user_role],
               // beware: hasura expects strings
-              'x-hasura-user-id': `"${id}"`,
+              'x-hasura-user-id': `${id.toString ? id.toString() : id}`,
             },
           }
 
