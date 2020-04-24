310a6cc615e086a21f230b6456a41c3dc3a22c40
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-24 12:50:08 +0200

    update db

---
+++ backend/db/backup.sh
@@ -0,0 +1,11 @@
+# dumped apflora
+# stellt dem Filenamen das Datum voran
+# kopiert das File auf die dropbox
+# entfernt das File
+echo "creating dump file..."
+FILENAME=$(date +"%Y-%m-%d_%H-%M-%S_vermehrung.backup")
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U postgres -h localhost --file=/sik_data/$FILENAME -Fc -Z9 postgres
+echo "uploading dump file..."
+/dropbox_uploader.sh -f dropbox_uploader.conf upload /sik_data/$FILENAME $FILENAME
+rm /sik_data/$FILENAME
+echo "backed up $FILENAME" >> /var/log/cron.log 2>&1
