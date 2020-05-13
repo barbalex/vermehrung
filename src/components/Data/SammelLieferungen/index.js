e86e307742e3a86ef3d67a25a80522555f3460cf
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-13 11:08:02 +0200

    use mst

--- src/components/Data/SammelLieferungen/index.js
+++ src/components/Data/SammelLieferungen/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useCallback, useReducer } from 'react'
 import { observer } from 'mobx-react-lite'
-import { useApolloClient, useQuery } from '@apollo/react-hooks'
+import { useApolloClient } from '@apollo/react-hooks'
 import styled from 'styled-components'
 import get from 'lodash/get'
 import { FaPlus } from 'react-icons/fa'
@@ -8,8 +8,10 @@ import IconButton from '@material-ui/core/IconButton'
 import gql from 'graphql-tag'
 import { FixedSizeList } from 'react-window'
 import ReactResizeDetector from 'react-resize-detector'
+import { v1 as uuidv1 } from 'uuid'
+import md5 from 'blueimp-md5'
 
-import { StoreContext } from '../../../models/reactUtils'
+import { useQuery, StoreContext } from '../../../models/reactUtils'
 import FormTitle from '../../shared/FormTitle'
 import FilterTitle from '../../shared/FilterTitle'
 import queryFromTable from '../../../utils/queryFromTable'
@@ -100,9 +102,14 @@ function sizeReducer(state, action) {
 const SammelLieferungen = ({ filter: showFilter }) => {
   const client = useApolloClient()
   const store = useContext(StoreContext)
-  const { filter } = store
+  const { filter, addSammelLieferung, addQueuedQuery } = store
   const { isFiltered: runIsFiltered } = filter
-  const { activeNodeArray } = store.tree
+  const {
+    activeNodeArray,
+    setActiveNodeArray,
+    addOpenNodes,
+    refetch: refetchTree,
+  } = store.tree
   const isFiltered = runIsFiltered()
 
   const sammelLieferungFilter = queryFromTable({
@@ -118,9 +125,43 @@ const SammelLieferungen = ({ filter: showFilter }) => {
   const filteredNr = rows.length
 
   const add = useCallback(() => {
-    const node = { nodeType: 'folder', url: activeNodeArray }
-    createNew({ node, store, client })
-  }, [activeNodeArray, client, store])
+    const id = uuidv1()
+    const _rev = `1-${md5({ id, _deleted: false }.toString())}`
+    const _depth = 1
+    const _revisions = `{"${_rev}"}`
+    const newObject = { id, _rev, _depth, _revisions }
+    addQueuedQuery({
+      name: 'mutateInsert_sammel_lieferung_rev',
+      variables: JSON.stringify({
+        objects: [newObject],
+        on_conflict: {
+          constraint: 'sammel_lieferung_rev_pkey',
+          update_columns: ['id'],
+        },
+      }),
+      callbackQuery: 'querySammel_lieferung',
+      callbackQueryVariables: JSON.stringify({
+        where: { id: { _eq: id } },
+      }),
+    })
+    // optimistically update store
+    addSammelLieferung(newObject)
+    setTimeout(() => {
+      // will be unnecessary once tree is converted to mst
+      refetchTree()
+      // update tree status
+      const newActiveNodeArray = [...activeNodeArray, id]
+      setActiveNodeArray(newActiveNodeArray)
+      addOpenNodes([newActiveNodeArray])
+    })
+  }, [
+    activeNodeArray,
+    addOpenNodes,
+    addQueuedQuery,
+    addSammelLieferung,
+    refetchTree,
+    setActiveNodeArray,
+  ])
 
   const [sizeState, sizeDispatch] = useReducer(sizeReducer, {
     width: 0,
