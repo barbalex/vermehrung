8f00403b8ee8f46e27c9c618a5d38acd419d0340
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-14 22:37:23 +0200

    clean

--- src/components/Data/Art/index.js
+++ src/components/Data/Art/index.js
@@ -109,7 +109,6 @@ const Art = ({
   const saveToDb = useCallback(
     async (event) => {
       const field = event.target.name
-      // TODO: still necessary?
       let value = ifIsNumericAsNumber(event.target.value)
       if (event.target.value === undefined) value = null
       if (event.target.value === '') value = null
