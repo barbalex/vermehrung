9d9af95d662b43de849bcc1a2c70290939741fa9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-15 17:13:24 +0200

    clean

--- src/components/Data/Herkunft/index.js
+++ src/components/Data/Herkunft/index.js
@@ -6,7 +6,6 @@ import get from 'lodash/get'
 import IconButton from '@material-ui/core/IconButton'
 import { IoMdInformationCircleOutline } from 'react-icons/io'
 import md5 from 'blueimp-md5'
-import moment from 'moment'
 
 import { useQuery, StoreContext } from '../../../models/reactUtils'
 import toPgArray from '../../../utils/toPgArray'
