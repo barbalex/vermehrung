62a6ccdac585b30cab57f3bfead5fe793f41830d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-17 15:38:30 +0200

    work on conflicts

--- src/components/Data/Herkunft/Conflict.js
+++ src/components/Data/Herkunft/Conflict.js
@@ -1,41 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import ConflictExplainer from '../../shared/ConflictExplainer'
+
 const Container = styled.div`
   padding: 10px;
 `
 const Title = styled.h4`
   margin-bottom: 10px;
 `
-const ExplainText = styled.p`
-  margin-bottom: 10px;
+const Rev = styled.span`
+  font-weight: normal;
+  padding-left: 7px;
 `
 
 const Conflict = ({ rev }) => {
   return (
     <Container>
-      <Title>Widersprüchliche Version</Title>
-      <ExplainText>
-        Es gibt einen Konflikt zwischen der aktuellen Version der Herkunft und
-        dieser widersprüchlichen Version.
-      </ExplainText>
-      <ExplainText>
-        Konflikte können entstehen, wenn zwei Personen gleichzeitig oder offline
-        arbeiten. Oder eine Person an verschiedenen Geräten.
-      </ExplainText>
-      <ExplainText>Um den Konflikt zu bereinigen:</ExplainText>
-      <ul>
-        <li>
-          Passen Sie die aktuelle Version an (wenn nötig) und verwerfen Sie
-          anschliessend die widersprüchliche Version
-        </li>
-        <li>
-          Oder: Übernehmen Sie diese widersprüchliche Version. Sie wird damit
-          zur aktuellen Version, die bisher aktuelle Version wird zur
-          widersprüchlichen. Anschliessend können Sie die (neu) aktuelle Version
-          wenn nötig anpassen und die widersprüchliche verwerfen
-        </li>
-      </ul>
+      <Title>
+        Widersprüchliche Version<Rev>{rev}</Rev>
+      </Title>
+      <ConflictExplainer name="Herkunft" />
       <div>{`Konflikt ${rev}`}</div>
     </Container>
   )
