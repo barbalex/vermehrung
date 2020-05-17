c5ce1ecdfc7d8ca0a03e59495d286407e3f024a1
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-17 12:59:08 +0200

    work on conflicts

--- src/components/Data/Herkunft/Conflict.js
+++ src/components/Data/Herkunft/Conflict.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding: 10px;
+`
 
 const Conflict = ({ rev }) => {
-  return <div>{`Konflikt ${rev}`}</div>
+  return (
+    <Container>
+      <div>{`Konflikt ${rev}`}</div>
+    </Container>
+  )
 }
 
 export default Conflict
