6e0e6d38b7779ca5d273a756bc0c0786b467a661
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-17 10:46:46 +0200

    clean

--- src/models/HerkunftModel.js
+++ src/models/HerkunftModel.js
@@ -1,5 +1,4 @@
 import { types } from 'mobx-state-tree'
-import { reaction, flow } from 'mobx'
 
 import { herkunftModelBase } from './herkunftModel.base'
 
@@ -17,20 +16,9 @@ export const herkunftModel = herkunftModelBase
   .props({
     nr: types.union(types.undefined, types.null, types.integer, types.string),
   })
-  .actions((self) => {
-    /*reaction(
-      () => self._conflicts,
-      () => {
-        // TODO: Can we detect when a conflict arrives and query it?
-        if (self._conflicts.length) {
-          console.log('CONFLICT detected:', self._conflicts)
-        }
-      },
-    )*/
-    return {
-      // This is an auto-generated example action.
-      log() {
-        console.log(JSON.stringify(self))
-      },
-    }
-  })
+  .actions((self) => ({
+    // This is an auto-generated example action.
+    log() {
+      console.log(JSON.stringify(self))
+    },
+  }))
