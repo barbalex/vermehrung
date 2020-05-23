d533a9c475649692c2a492b720569ff968477a2e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-23 14:32:26 +0200

    work on conflict

---
+++ src/components/Data/Lieferung/Lieferung/Conflict.js
@@ -0,0 +1,245 @@
+import React, { useCallback, useContext } from 'react'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
+import { observer } from 'mobx-react-lite'
+import gql from 'graphql-tag'
+
+import { useQuery, StoreContext } from '../../../../models/reactUtils'
+import Conflict from '../../../shared/Conflict'
+
+const lieferungRevQuery = gql`
+  query lieferungRevForConflictQuery($id: uuid!, $rev: String!) {
+    lieferung_rev(where: { lieferung_id: { _eq: $id }, _rev: { _eq: $rev } }) {
+      id
+      __typename
+      lieferung_id
+      sammel_lieferung_id
+      art_id
+      art {
+        id
+        __typename
+        art_ae_art {
+          id
+          __typename
+          name
+        }
+      }
+      person_id
+      person {
+        id
+        __typename
+        name
+      }
+      von_sammlung_id
+      bemerkungen
+      changed
+      changed_by
+      _rev
+      _parent_rev
+      _depth
+    }
+  }
+`
+
+const LieferungConflict = ({
+  id,
+  rev,
+  row,
+  callbackAfterVerwerfen,
+  callbackAfterUebernehmen,
+  setActiveConflict,
+}) => {
+  const store = useContext(StoreContext)
+  const { user, addNotification } = store
+
+  // need to use this query to ensure that the person's name is queried
+  const { error, loading } = useQuery(lieferungRevQuery, {
+    variables: {
+      rev,
+      id,
+    },
+  })
+
+  // need to grab store object to ensure this remains up to date
+  const revRow =
+    [...store.lieferung_revs.values()].find(
+      (v) => v._rev === rev && v.lieferung_id === id,
+    ) || {}
+
+  const dataArray = [
+    {
+      valueInRow: row?.art?.art_ae_art?.name,
+      valueInRev: revRow?.art?.art_ae_art?.name,
+      label: 'Art',
+    },
+    {
+      valueInRow: row?.person?.name,
+      valueInRev: revRow?.person?.name,
+      label: 'Person',
+    },
+    {
+      valueInRow: row?.strasse,
+      valueInRev: revRow?.strasse,
+      label: 'Von Sammlung',
+    },
+    {
+      valueInRow: row?.plz,
+      valueInRev: revRow?.plz,
+      label: 'PLZ',
+    },
+    { valueInRow: row?.ort, valueInRev: revRow?.ort, label: 'Ort' },
+    {
+      valueInRow: row?.geom_point?.coordinates,
+      valueInRev: revRow?.geom_point?.coordinates,
+      label: 'Längen- und Breitengrad',
+    },
+    {
+      valueInRow: row?.aktiv == true,
+      valueInRev: revRow?.aktiv == true,
+      label: 'aktiv',
+    },
+    {
+      valueInRow: row?.bemerkungen,
+      valueInRev: revRow?.bemerkungen,
+      label: 'bemerkungen',
+    },
+    {
+      valueInRow: row?.changed,
+      valueInRev: revRow?.changed,
+      label: 'geändert',
+    },
+    {
+      valueInRow: row?.changed_by,
+      valueInRev: revRow?.changed_by,
+      label: 'geändert von',
+    },
+  ]
+
+  const onClickVerwerfen = useCallback(async () => {
+    const newDepth = revRow._depth + 1
+    const newObject = {
+      lieferung_id: revRow.lieferung_id,
+      art_id: revRow.art_id,
+      person_id: revRow.person_id,
+      von_sammlung_id: revRow.von_sammlung_id,
+      plz: revRow.plz,
+      ort: revRow.ort,
+      geom_point: revRow.geom_point,
+      aktiv: revRow.aktiv,
+      bemerkungen: revRow.bemerkungen,
+      changed_by: user.email,
+      _parent_rev: revRow._rev,
+      _depth: newDepth,
+      _deleted: true,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject.changed = new window.Date().toISOString()
+    //console.log('Lieferung Conflict', { row, revRow, newObject })
+    try {
+      await store.mutateInsert_lieferung_rev_one({
+        object: newObject,
+        on_conflict: {
+          constraint: 'lieferung_rev_pkey',
+          update_columns: ['id'],
+        },
+      })
+    } catch (error) {
+      addNotification({
+        message: error.message,
+      })
+    }
+    callbackAfterVerwerfen()
+  }, [
+    addNotification,
+    callbackAfterVerwerfen,
+    revRow._depth,
+    revRow._rev,
+    revRow.aktiv,
+    revRow.bemerkungen,
+    revRow.lieferung_id,
+    revRow.geom_point,
+    revRow.name,
+    revRow.ort,
+    revRow.person_id,
+    revRow.plz,
+    revRow.strasse,
+    store,
+    user.email,
+  ])
+  const onClickUebernehmen = useCallback(async () => {
+    // need to attach to the winner, that is row
+    // otherwise risk to still have lower depth and thus loosing
+    const newDepth = row._depth + 1
+    const newObject = {
+      lieferung_id: revRow.lieferung_id,
+      art_id: revRow.art_id,
+      person_id: revRow.person_id,
+      von_sammlung_id: revRow.von_sammlung_id,
+      plz: revRow.plz,
+      ort: revRow.ort,
+      geom_point: revRow.geom_point,
+      aktiv: revRow.aktiv,
+      bemerkungen: revRow.bemerkungen,
+      changed_by: user.email,
+      _parent_rev: row._rev,
+      _depth: newDepth,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject.changed = new window.Date().toISOString()
+    //console.log('Lieferung Conflict', { row, revRow, newObject })
+    try {
+      await store.mutateInsert_lieferung_rev_one({
+        object: newObject,
+        on_conflict: {
+          constraint: 'lieferung_rev_pkey',
+          update_columns: ['id'],
+        },
+      })
+    } catch (error) {
+      addNotification({
+        message: error.message,
+      })
+    }
+    callbackAfterUebernehmen()
+  }, [
+    addNotification,
+    callbackAfterUebernehmen,
+    revRow.aktiv,
+    revRow.bemerkungen,
+    revRow.lieferung_id,
+    revRow.geom_point,
+    revRow.name,
+    revRow.ort,
+    revRow.person_id,
+    revRow.plz,
+    revRow.strasse,
+    row._depth,
+    row._rev,
+    store,
+    user.email,
+  ])
+  const onClickSchliessen = useCallback(() => setActiveConflict(null), [
+    setActiveConflict,
+  ])
+
+  //console.log('Lieferung Conflict', { dataArray, row, revRow })
+
+  return (
+    <Conflict
+      name="Lieferung"
+      rev={rev}
+      dataArray={dataArray}
+      loading={loading}
+      error={error}
+      onClickVerwerfen={onClickVerwerfen}
+      onClickUebernehmen={onClickUebernehmen}
+      onClickSchliessen={onClickSchliessen}
+    />
+  )
+}
+
+export default observer(LieferungConflict)
