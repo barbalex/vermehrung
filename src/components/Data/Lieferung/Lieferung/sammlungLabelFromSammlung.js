204fcb26311e32a958af8ae495859f5d260c5832
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-23 14:58:43 +0200

    work on conflict

--- src/components/Data/Lieferung/Lieferung/sammlungLabelFromSammlung.js
+++ src/components/Data/Lieferung/Lieferung/sammlungLabelFromSammlung.js
@@ -1,5 +1,7 @@
+import moment from 'moment'
+
 export default (s) => {
-  const datum = s?.datum ?? '(kein Datum)'
+  const datum = s?.datum ? moment(s.datum).format('YYYY.MM.DD') : '(kein Datum)'
   const nr = s?.herkunft?.nr ?? '(keine Nr)'
   const person = s?.person?.name ?? '(kein Name)'
   const label = `${datum}: Herkunft ${nr}; ${person}`
