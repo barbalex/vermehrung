51e1ef8263f683920986bca5e9b582a200f71ef9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-26 14:35:20 +0200

    use mst

--- src/models/PersonModel.js
+++ src/models/PersonModel.js
@@ -1,16 +1,163 @@
-import { personModelBase } from "./personModel.base"
+import { getParent } from 'mobx-state-tree'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
 
+import { personModelBase } from './personModel.base'
+import toPgArray from '../utils/toPgArray'
+import toStringIfPossible from '../utils/toStringIfPossible'
 
 /* A graphql query fragment builders for personModel */
-export { selectFromperson, personModelPrimitives, personModelSelector } from "./personModel.base"
+export {
+  selectFromperson,
+  personModelPrimitives,
+  personModelSelector,
+} from './personModel.base'
 
 /**
  * personModel
  */
-export const personModel = personModelBase
-  .actions(self => ({
-    // This is an auto-generated example action.
-    log() {
-      console.log(JSON.stringify(self))
+export const personModel = personModelBase.actions((self) => ({
+  edit({ field, value }) {
+    const store = getParent(self, 2)
+    const { addQueuedQuery, user, upsertPersonModel, tree } = store
+
+    // first build the part that will be revisioned
+    const depth = self._depth + 1
+    const newObject = {
+      person_id: self.id,
+      nr: field === 'nr' ? toStringIfPossible(value) : self.nr,
+      name: field === 'name' ? toStringIfPossible(value) : self.name,
+      adresszusatz:
+        field === 'adresszusatz'
+          ? toStringIfPossible(value)
+          : self.adresszusatz,
+      strasse: field === 'strasse' ? toStringIfPossible(value) : self.strasse,
+      plz: field === 'plz' ? value : self.plz,
+      ort: field === 'ort' ? toStringIfPossible(value) : self.ort,
+      telefon_privat:
+        field === 'telefon_privat'
+          ? toStringIfPossible(value)
+          : self.telefon_privat,
+      telefon_geschaeft:
+        field === 'telefon_geschaeft'
+          ? toStringIfPossible(value)
+          : self.telefon_geschaeft,
+      telefon_mobile:
+        field === 'telefon_mobile'
+          ? toStringIfPossible(value)
+          : self.telefon_mobile,
+      email: field === 'email' ? toStringIfPossible(value) : self.email,
+      kein_email: field === 'kein_email' ? value : self.kein_email,
+      bemerkungen:
+        field === 'bemerkungen' ? toStringIfPossible(value) : self.bemerkungen,
+      account_id:
+        field === 'account_id' ? toStringIfPossible(value) : self.account_id,
+      user_role:
+        field === 'user_role' ? toStringIfPossible(value) : self.user_role,
+      kommerziell: field === 'kommerziell' ? value : self.kommerziell,
+      info: field === 'info' ? value : self.info,
+      aktiv: field === 'aktiv' ? value : self.aktiv,
+      changed: new window.Date().toISOString(),
+      changed_by: user.email,
+      _parent_rev: self._rev,
+      _depth: depth,
     }
-  }))
+    const rev = `${depth}-${md5(JSON.stringify(newObject))}`
+    // DO NOT include id in rev - or revs with same data will conflict
+    newObject.id = uuidv1()
+    newObject._rev = rev
+    const newObjectForStore = { ...newObject }
+    // convert to string as hasura does not support arrays yet
+    // https://github.com/hasura/graphql-engine/pull/2243
+    newObject._revisions = self._revisions
+      ? toPgArray([rev, ...self._revisions])
+      : toPgArray([rev])
+    // do not stringify revisions for store
+    // as _that_ is a real array
+    newObjectForStore._revisions = self._revisions
+      ? [rev, ...self._revisions]
+      : [rev]
+    addQueuedQuery({
+      name: 'mutateInsert_person_rev_one',
+      variables: JSON.stringify({
+        object: newObject,
+        on_conflict: {
+          constraint: 'person_rev_pkey',
+          update_columns: ['id'],
+        },
+      }),
+      callbackQuery: 'queryPerson',
+      callbackQueryVariables: JSON.stringify({
+        where: { id: { _eq: self.id } },
+      }),
+    })
+    // optimistically update store
+    upsertPersonModel(newObjectForStore)
+    setTimeout(() => {
+      if (['name'].includes(field)) tree.refetch()
+    }, 50)
+  },
+  setDeleted() {
+    const store = getParent(self, 2)
+    const { addQueuedQuery, user } = store
+
+    // build new object
+    const newDepth = self._depth + 1
+    const newObject = {
+      person_id: self.id,
+      nr: self.nr,
+      name: self.name,
+      adresszusatz: self.adresszusatz,
+      strasse: self.strasse,
+      plz: self.plz,
+      ort: self.ort,
+      telefon_privat: self.telefon_privat,
+      telefon_geschaeft: self.telefon_geschaeft,
+      telefon_mobile: self.telefon_mobile,
+      email: self.email,
+      kein_email: self.kein_email,
+      bemerkungen: self.bemerkungen,
+      account_id: self.account_id,
+      user_role: self.user_role,
+      kommerziell: self.kommerziell,
+      info: self.info,
+      aktiv: self.aktiv,
+      changed: new window.Date().toISOString(),
+      changed_by: user.email,
+      _parent_rev: self._rev,
+      _depth: newDepth,
+      _deleted: true,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject._revisions = self._revisions
+      ? toPgArray([rev, ...self._revisions])
+      : toPgArray([rev])
+
+    addQueuedQuery({
+      name: 'mutateInsert_person_rev_one',
+      variables: JSON.stringify({
+        object: newObject,
+        on_conflict: {
+          constraint: 'person_rev_pkey',
+          update_columns: ['id'],
+        },
+      }),
+      callbackQuery: 'queryPerson',
+      callbackQueryVariables: JSON.stringify({
+        where: { id: { _eq: self.id } },
+      }),
+    })
+  },
+  delete() {
+    const store = getParent(self, 2)
+    const { tree, deletePersonModel } = store
+
+    self.setDeleted()
+    deletePersonModel({ id: self.id })
+    setTimeout(() => {
+      tree.refetch()
+    }, 50)
+  },
+}))
--- src/models/person_revModel.js
+++ src/models/person_revModel.js
@@ -1,18 +1,75 @@
-import { person_revModelBase } from "./person_revModel.base"
+import { getParent } from 'mobx-state-tree'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
 
+import { person_revModelBase } from './person_revModel.base'
+import toPgArray from '../utils/toPgArray'
 
 /* A graphql query fragment builders for person_revModel */
-export { selectFromperson_rev, person_revModelPrimitives, person_revModelSelector } from "./person_revModel.base"
+export {
+  selectFromperson_rev,
+  person_revModelPrimitives,
+  person_revModelSelector,
+} from './person_revModel.base'
 
 /**
  * person_revModel
  *
  * columns and relationships of "person_rev"
  */
-export const person_revModel = person_revModelBase
-  .actions(self => ({
-    // This is an auto-generated example action.
-    log() {
-      console.log(JSON.stringify(self))
+export const person_revModel = person_revModelBase.actions((self) => ({
+  setDeleted() {
+    const store = getParent(self, 2)
+    const { addQueuedQuery, user, deletePersonRevModel } = store
+
+    // build new object
+    const newDepth = self._depth + 1
+    const newObject = {
+      person_id: self.person_id,
+      nr: self.nr,
+      name: self.name,
+      adresszusatz: self.adresszusatz,
+      strasse: self.strasse,
+      plz: self.plz,
+      ort: self.ort,
+      telefon_privat: self.telefon_privat,
+      telefon_geschaeft: self.telefon_geschaeft,
+      telefon_mobile: self.telefon_mobile,
+      email: self.email,
+      kein_email: self.kein_email,
+      bemerkungen: self.bemerkungen,
+      account_id: self.account_id,
+      user_role: self.user_role,
+      kommerziell: self.kommerziell,
+      info: self.info,
+      aktiv: self.aktiv,
+      changed: new window.Date().toISOString(),
+      changed_by: user.email,
+      _parent_rev: self._rev,
+      _depth: newDepth,
+      _deleted: true,
     }
-  }))
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject._revisions = self._revisions
+      ? toPgArray([rev, ...self._revisions])
+      : toPgArray([rev])
+
+    addQueuedQuery({
+      name: 'mutateInsert_person_rev_one',
+      variables: JSON.stringify({
+        object: newObject,
+        on_conflict: {
+          constraint: 'person_rev_pkey',
+          update_columns: ['id'],
+        },
+      }),
+      callbackQuery: 'queryPerson',
+      callbackQueryVariables: JSON.stringify({
+        where: { id: { _eq: self.id } },
+      }),
+    })
+    deletePersonRevModel({ id: self.id })
+  },
+}))
