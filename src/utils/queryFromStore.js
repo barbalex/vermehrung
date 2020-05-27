69b8053120e28bee91ded1ea38f3b2ab732a1b3d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-27 13:13:31 +0200

    filter better

--- src/utils/queryFromStore.js
+++ src/utils/queryFromStore.js
@@ -2,24 +2,29 @@ import types from '../models/Filter/simpleTypes'
 
 export default ({ store, table }) => {
   const { filter: storeFilter } = store
-  //console.log('queryFromTable', { storeFilter, table })
 
   if (!store[`${table}s`]) throw new Error(`no store found for table ${table}`)
-  let filteredValues = [...store[`${table}s`].values()]
-  console.log('queryFromStore', { table, filteredValues1: [...filteredValues] })
 
-  if (!Object.entries(storeFilter[table]).length) return filteredValues
   const filterValues = Object.entries(storeFilter[table]).filter(
     (e) => e[1] || e[1] === 0,
   )
-  filterValues.forEach(([key, value]) => {
-    const type = types[table][key] || 'string'
-    if (type === 'string') {
-      filteredValues = filteredValues.filter((v) => v[key].includes(value))
-    } else {
-      filteredValues = filteredValues.filter((v) => (v[key] = value))
-    }
-  })
+  const values = [...store[`${table}s`].values()]
+  if (!filterValues.length) return values
+
+  console.log('queryFromStore', { table, filterValues, values })
+
+  const test = (val) => {
+    const testArray = filterValues.map(([key, value]) => {
+      const type = types[table][key] || 'string'
+      if (type === 'string' && val[key]) {
+        return val[key].includes(value)
+      }
+      return val[key] === value
+    })
+    return !testArray.includes(false)
+  }
+
+  const filteredValues = values.filter((v) => test(v))
 
   return filteredValues
 }
