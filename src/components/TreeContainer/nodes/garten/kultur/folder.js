f650e2321d53c1105dab2931c9d0fd8c838996ee
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:12:23 +0200

    optimize

--- src/components/TreeContainer/nodes/garten/kultur/folder.js
+++ src/components/TreeContainer/nodes/garten/kultur/folder.js
@@ -1,18 +1,20 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ url, nodes, data, loading }) => {
   const gartenId = url[1]
-  const gartenNodes = nodes.filter(n => n.parentId === 'gartenFolder')
-  const gartenIndex = findIndex(gartenNodes, n => n.id === `garten${gartenId}`)
+  const gartenNodes = nodes.filter((n) => n.parentId === 'gartenFolder')
+  const gartenIndex = findIndex(
+    gartenNodes,
+    (n) => n.id === `garten${gartenId}`,
+  )
 
-  const gaerten = get(data, 'garten') || []
-  const garten = gaerten.find(a => a.id === gartenId)
-  const kulturen = get(garten, 'kulturs') || []
+  const gaerten = data?.garten ?? []
+  const garten = gaerten.find((a) => a.id === gartenId)
+  const kulturen = garten?.kulturs ?? []
   const nr = loading && !kulturen.length ? '...' : kulturen.length
 
   // only return if parent exists
-  if (!nodes.map(n => n.id).includes(`garten${gartenId}`)) return []
+  if (!nodes.map((n) => n.id).includes(`garten${gartenId}`)) return []
 
   return [
     {
