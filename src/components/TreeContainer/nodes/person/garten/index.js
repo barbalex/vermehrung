9e8a63f9bdad02d92a2a8616edfffb8e366190c5
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:34:19 +0200

    optimize

--- src/components/TreeContainer/nodes/person/garten/index.js
+++ src/components/TreeContainer/nodes/person/garten/index.js
@@ -1,28 +1,30 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ nodes, data, url }) => {
   const personId = url[1]
-  const personen = get(data, 'person') || []
-  const person = personen.find(p => p.id === personId)
-  const gaerten = get(person, 'gartens') || []
+  const personen = data?.person ?? []
+  const person = personen.find((p) => p.id === personId)
+  const gaerten = person?.gartens ?? []
 
-  const personNodes = nodes.filter(n => n.parentId === 'personFolder')
-  const personIndex = findIndex(personNodes, n => n.id === `person${personId}`)
+  const personNodes = nodes.filter((n) => n.parentId === 'personFolder')
+  const personIndex = findIndex(
+    personNodes,
+    (n) => n.id === `person${personId}`,
+  )
 
   return (
     gaerten
       // only show if parent node exists
       .filter(() =>
-        nodes.map(n => n.id).includes(`person${personId}GartenFolder`),
+        nodes.map((n) => n.id).includes(`person${personId}GartenFolder`),
       )
-      .map(el => ({
+      .map((el) => ({
         nodeType: 'table',
         menuTitle: 'Garten',
         table: 'garten',
         id: `person${personId}Garten${el.id}`,
         parentId: `person${personId}GartenFolder`,
-        label: el.name || `(${get(el, 'person.name') || 'kein Name'})`,
+        label: el.name || `(${el?.person?.name ?? 'kein Name'})`,
         url: ['Personen', personId, 'Gaerten', el.id],
         hasChildren: true,
       }))
