269468d455bbff4f5ab6b2527b56fe57a4e3fe37
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 16:45:14 +0200

    optimize

--- src/components/TreeContainer/nodes/garten/folder.js
+++ src/components/TreeContainer/nodes/garten/folder.js
@@ -1,7 +1,5 @@
-import get from 'lodash/get'
-
 export default ({ data, loading }) => {
-  const gaerten = get(data, 'garten') || []
+  const gaerten = data?.garten ?? []
   const nr = loading && !gaerten.length ? '...' : gaerten.length
 
   return [
