aa5e56aa909b832f3acd2168e274f5e0a833bd8b
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:27:39 +0200

    optimize

--- src/components/TreeContainer/nodes/kultur/teilkultur/folder.js
+++ src/components/TreeContainer/nodes/kultur/teilkultur/folder.js
@@ -1,21 +1,23 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ url, nodes, data, loading }) => {
   const kulturId = url[1]
-  const kulturen = get(data, 'kultur') || []
-  const kultur = kulturen.find(k => k.id === kulturId)
-  const tk = get(kultur, 'kultur_option.tk')
+  const kulturen = data?.kultur ?? []
+  const kultur = kulturen.find((k) => k.id === kulturId)
+  const tk = kultur?.kultur_option?.tk
   if (!tk) return []
 
-  const teilkulturs = get(kultur, 'teilkulturs') || []
+  const teilkulturs = kultur?.teilkulturs ?? []
   const nr = loading && !teilkulturs.length ? '...' : teilkulturs.length
 
-  const kulturNodes = nodes.filter(n => n.parentId === `kulturFolder`)
-  const kulturIndex = findIndex(kulturNodes, n => n.id === `kultur${kulturId}`)
+  const kulturNodes = nodes.filter((n) => n.parentId === `kulturFolder`)
+  const kulturIndex = findIndex(
+    kulturNodes,
+    (n) => n.id === `kultur${kulturId}`,
+  )
 
   // only return if parent exists
-  if (!nodes.map(n => n.id).includes(`kultur${kulturId}`)) return []
+  if (!nodes.map((n) => n.id).includes(`kultur${kulturId}`)) return []
 
   return [
     {
