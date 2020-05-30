cbee5c33072b7581f3c0d83a206cdfea11307e4e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:32:03 +0200

    optimize

--- src/components/TreeContainer/nodes/lieferung/index.js
+++ src/components/TreeContainer/nodes/lieferung/index.js
@@ -1,23 +1,18 @@
-import get from 'lodash/get'
 import moment from 'moment'
 
-import exists from '../../../../utils/exists'
-
 export default ({ nodes, data }) => {
-  const lieferungen = get(data, 'lieferung') || []
+  const lieferungen = data?.lieferung ?? []
 
   return (
     lieferungen
       // only show if parent node exists
-      .filter(() => nodes.map(n => n.id).includes('lieferungFolder'))
-      .map(el => {
+      .filter(() => nodes.map((n) => n.id).includes('lieferungFolder'))
+      .map((el) => {
         const datum = el.datum
           ? moment(el.datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
           : 'kein Datum'
-        const anz = exists(el.anzahl_pflanzen) ? el.anzahl_pflanzen : '_'
-        const anzAb = exists(el.anzahl_auspflanzbereit)
-          ? el.anzahl_auspflanzbereit
-          : '_'
+        const anz = el.anzahl_pflanzen ?? '_'
+        const anzAb = el.anzahl_auspflanzbereit ?? '_'
         const numbers = `${anz
           .toString()
           .padStart(3, '_')}/${anzAb.toString().padStart(3, '_')}`
--- src/components/TreeContainer/nodes/person/index.js
+++ src/components/TreeContainer/nodes/person/index.js
@@ -1,19 +1,17 @@
-import get from 'lodash/get'
-
 export default ({ nodes, data }) =>
-  (get(data, 'person') || [])
+  (data?.person ?? [])
     // only show if parent node exists
-    .filter(() => nodes.map(n => n.id).includes('personFolder'))
-    .map(el => ({
+    .filter(() => nodes.map((n) => n.id).includes('personFolder'))
+    .map((el) => ({
       nodeType: 'table',
       menuTitle: 'Person',
       table: 'person',
       id: `person${el.id}`,
       parentId: 'personFolder',
-      label: get(el, 'name') || '(kein Name)',
+      label: el?.name ?? '(kein Name)',
       url: ['Personen', el.id],
       hasChildren: true,
-      accountId: el.account_id || '',
+      accountId: el?.account_id ?? '',
     }))
     .map((el, index) => {
       el.sort = [11, index]
