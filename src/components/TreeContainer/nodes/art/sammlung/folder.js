4ad53f4bbb2a6c918cf814fde49009786b3f5924
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 16:43:01 +0200

    optimize

--- src/components/TreeContainer/nodes/art/sammlung/folder.js
+++ src/components/TreeContainer/nodes/art/sammlung/folder.js
@@ -1,18 +1,17 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ url, nodes, data, loading }) => {
   const artId = url[1]
-  const artNodes = nodes.filter(n => n.parentId === 'artFolder')
-  const artIndex = findIndex(artNodes, n => n.id === `art${artId}`)
+  const artNodes = nodes.filter((n) => n.parentId === 'artFolder')
+  const artIndex = findIndex(artNodes, (n) => n.id === `art${artId}`)
 
-  const arten = get(data, 'art') || []
-  const art = arten.find(a => a.id === artId)
-  const sammlungen = get(art, 'sammlungs') || []
+  const arten = data?.art ?? []
+  const art = arten.find((a) => a.id === artId)
+  const sammlungen = art?.sammlungs ?? []
   const nr = loading && !sammlungen.length ? '...' : sammlungen.length
 
   // only return if parent exists
-  if (!nodes.map(n => n.id).includes(`art${artId}`)) return []
+  if (!nodes.map((n) => n.id).includes(`art${artId}`)) return []
 
   return [
     {
--- src/components/TreeContainer/nodes/art/sammlung/index.js
+++ src/components/TreeContainer/nodes/art/sammlung/index.js
@@ -1,30 +1,30 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 import moment from 'moment'
 
 export default ({ nodes, data, url }) => {
   const artId = url[1]
-  const arten = get(data, 'art') || []
-  const art = arten.find(a => a.id === artId)
-  const sammlungen = get(art, 'sammlungs') || []
-  const artNodes = nodes.filter(n => n.parentId === 'artFolder')
-  const artIndex = findIndex(artNodes, n => n.id === `art${artId}`) || 0
+  const arten = data?.art ?? []
+  const art = arten.find((a) => a.id === artId)
+  const sammlungen = art?.sammlungs ?? []
+  const artNodes = nodes.filter((n) => n.parentId === 'artFolder')
+  const artIndex = findIndex(artNodes, (n) => n.id === `art${artId}`) || 0
 
   return (
     sammlungen
       // only show if parent node exists
-      .filter(() => nodes.map(n => n.id).includes(`art${artId}SammlungFolder`))
-      .map(el => {
+      .filter(() =>
+        nodes.map((n) => n.id).includes(`art${artId}SammlungFolder`),
+      )
+      .map((el) => {
         const datum = el.datum
           ? moment(el.datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
           : 'kein Datum'
         const geplant = el.geplant ? ' (geplant)' : ''
-        const herkunftId = get(el, 'herkunft.id')
+        const herkunftId = el?.herkunft?.id
         const herkunft = herkunftId
-          ? `${get(el, 'herkunft.gemeinde') || '(keine Gemeinde)'}, ${get(
-              el,
-              'herkunft.nr',
-            ) || '(keine Nr.)'}`
+          ? `${el?.herkunft?.gemeinde ?? '(keine Gemeinde)'}, ${
+              el?.herkunft?.nr ?? '(keine Nr.)'
+            }`
           : 'keine Herkunft'
         const label = `${datum}: ${herkunft}${geplant}`
 
