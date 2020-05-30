c36573e10497b8bf41aa13e95b310b93a0c047e0
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:32:03 +0200

    optimize

--- src/components/TreeContainer/nodes/person/folder.js
+++ src/components/TreeContainer/nodes/person/folder.js
@@ -1,7 +1,5 @@
-import get from 'lodash/get'
-
 export default ({ data, loading }) => {
-  const personen = get(data, 'person') || []
+  const personen = data?.person ?? []
   const nr = loading && !personen.length ? '...' : personen.length
 
   return [
--- src/components/TreeContainer/nodes/person/garten/folder.js
+++ src/components/TreeContainer/nodes/person/garten/folder.js
@@ -1,18 +1,20 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ data, loading, url, nodes }) => {
   const personId = url[1]
-  const personen = get(data, 'person') || []
-  const person = personen.find(p => p.id === personId)
-  const gaerten = get(person, 'gartens') || []
+  const personen = data?.person ?? []
+  const person = personen.find((p) => p.id === personId)
+  const gaerten = person?.gartens ?? []
   const nr = loading && !gaerten.length ? '...' : gaerten.length
 
-  const personNodes = nodes.filter(n => n.parentId === 'personFolder')
-  const personIndex = findIndex(personNodes, n => n.id === `person${personId}`)
+  const personNodes = nodes.filter((n) => n.parentId === 'personFolder')
+  const personIndex = findIndex(
+    personNodes,
+    (n) => n.id === `person${personId}`,
+  )
 
   // only return if parent exists
-  if (!nodes.map(n => n.id).includes(`person${personId}`)) return []
+  if (!nodes.map((n) => n.id).includes(`person${personId}`)) return []
 
   return [
     {
