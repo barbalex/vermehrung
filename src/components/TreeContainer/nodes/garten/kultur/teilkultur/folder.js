9e805ca27efd96ea98d658e014d76ddad7891367
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:16:08 +0200

    optimize

--- src/components/TreeContainer/nodes/garten/kultur/teilkultur/folder.js
+++ src/components/TreeContainer/nodes/garten/kultur/teilkultur/folder.js
@@ -1,30 +1,32 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ url, nodes, data, loading }) => {
   const gartenId = url[1]
   const kulturId = url[3]
-  const gaerten = get(data, 'garten') || []
-  const garten = gaerten.find(a => a.id === gartenId)
-  const kulturen = get(garten, 'kulturs') || []
-  const kultur = kulturen.find(k => k.id === kulturId)
-  const tk = get(kultur, 'kultur_option.tk')
+  const gaerten = data?.garten ?? []
+  const garten = gaerten.find((a) => a.id === gartenId)
+  const kulturen = garten?.kulturs ?? []
+  const kultur = kulturen.find((k) => k.id === kulturId)
+  const tk = kultur?.kultur_option?.tk
   if (!tk) return []
-  const teilkulturen = get(kultur, 'teilkulturs') || []
+  const teilkulturen = kultur?.teilkulturs ?? []
   const nr = loading && !teilkulturen.length ? '...' : teilkulturen.length
 
-  const gartenNodes = nodes.filter(n => n.parentId === 'gartenFolder')
-  const gartenIndex = findIndex(gartenNodes, n => n.id === `garten${gartenId}`)
+  const gartenNodes = nodes.filter((n) => n.parentId === 'gartenFolder')
+  const gartenIndex = findIndex(
+    gartenNodes,
+    (n) => n.id === `garten${gartenId}`,
+  )
   const kulturNodes = nodes.filter(
-    n => n.parentId === `garten${gartenId}KulturFolder`,
+    (n) => n.parentId === `garten${gartenId}KulturFolder`,
   )
   const kulturIndex = findIndex(
     kulturNodes,
-    n => n.id === `garten${gartenId}Kultur${kulturId}`,
+    (n) => n.id === `garten${gartenId}Kultur${kulturId}`,
   )
 
   // only return if parent exists
-  if (!nodes.map(n => n.id).includes(`garten${gartenId}Kultur${kulturId}`))
+  if (!nodes.map((n) => n.id).includes(`garten${gartenId}Kultur${kulturId}`))
     return []
 
   return [
