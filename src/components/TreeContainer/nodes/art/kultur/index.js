d71186ff98fa0ac491613759d5aadb31a2d08579
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 16:25:39 +0200

    optimize

--- src/components/TreeContainer/nodes/art/kultur/index.js
+++ src/components/TreeContainer/nodes/art/kultur/index.js
@@ -1,23 +1,21 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 
 export default ({ nodes, data, url }) => {
   const artId = url[1]
-  const arten = get(data, 'art') || []
-  const art = arten.find(a => a.id === artId)
-  const kulturen = get(art, 'kulturs') || []
-  const artNodes = nodes.filter(n => n.parentId === 'artFolder')
-  const artIndex = findIndex(artNodes, n => n.id === `art${artId}`) || 0
+  const arten = data?.art ?? []
+  const art = arten.find((a) => a.id === artId)
+  const kulturen = art?.kulturs ?? []
+  const artNodes = nodes.filter((n) => n.parentId === 'artFolder')
+  const artIndex = findIndex(artNodes, (n) => n.id === `art${artId}`) || 0
 
   return (
     kulturen
       // only show if parent node exists
-      .filter(() => nodes.map(n => n.id).includes(`art${artId}KulturFolder`))
-      .map(el => {
+      .filter(() => nodes.map((n) => n.id).includes(`art${artId}KulturFolder`))
+      .map((el) => {
         const garten =
-          get(el, 'garten.name') ||
-          `(${get(el, 'garten.person.name') || 'kein Name'})`
-        const herkunft = get(el, 'herkunft.nr') || '(Herkunft ohne Nr)'
+          el?.garten?.name ?? `(${el?.garten?.person?.name ?? 'kein Name'})`
+        const herkunft = el?.herkunft?.nr ?? '(Herkunft ohne Nr)'
         const label = `von: ${herkunft}, in: ${garten}`
 
         return {
