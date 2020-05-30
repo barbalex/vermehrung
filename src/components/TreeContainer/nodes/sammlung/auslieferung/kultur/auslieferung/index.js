8fa7e99cd420a09a41f5993b37c5d08fef577fce
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-30 17:52:40 +0200

    optimize

--- src/components/TreeContainer/nodes/sammlung/auslieferung/kultur/auslieferung/index.js
+++ src/components/TreeContainer/nodes/sammlung/auslieferung/kultur/auslieferung/index.js
@@ -1,40 +1,37 @@
-import get from 'lodash/get'
 import findIndex from 'lodash/findIndex'
 import moment from 'moment'
-
-import exists from '../../../../../../../utils/exists'
 
 export default ({ nodes, data, url }) => {
   const sammlungId = url[1]
   const lieferungId = url[3]
   const kulturId = url[5]
 
-  const sammlungen = get(data, 'sammlung', [])
-  const sammlung = sammlungen.find(p => p.id === sammlungId)
-  const lieferungen = get(sammlung, 'lieferungs', [])
-  const lieferung = lieferungen.find(p => p.id === lieferungId)
-  const kultur = get(lieferung, 'kulturByNachKulturId')
-  const auslieferungen = get(kultur, 'lieferungsByVonKulturId') || []
+  const sammlungen = data?.sammlung ?? []
+  const sammlung = sammlungen.find((p) => p.id === sammlungId)
+  const lieferungen = sammlung?.lieferungs ?? []
+  const lieferung = lieferungen.find((p) => p.id === lieferungId)
+  const kultur = lieferung?.kulturByNachKulturId
+  const auslieferungen = kultur?.lieferungsByVonKulturId ?? []
 
-  const sammlungNodes = nodes.filter(n => n.parentId === 'sammlungFolder')
+  const sammlungNodes = nodes.filter((n) => n.parentId === 'sammlungFolder')
   const sammlungIndex = findIndex(
     sammlungNodes,
-    n => n.id === `sammlung${sammlungId}`,
+    (n) => n.id === `sammlung${sammlungId}`,
   )
   const lieferungNodes = nodes.filter(
-    n => n.parentId === `sammlung${sammlungId}LieferungFolder`,
+    (n) => n.parentId === `sammlung${sammlungId}LieferungFolder`,
   )
   const lieferungIndex = findIndex(
     lieferungNodes,
-    n => n.id === `sammlung${sammlungId}Lieferung${lieferungId}`,
+    (n) => n.id === `sammlung${sammlungId}Lieferung${lieferungId}`,
   )
   const kulturNodes = nodes.filter(
-    n =>
+    (n) =>
       n.parentId === `sammlung${sammlungId}Lieferung${lieferungId}KulturFolder`,
   )
   const kulturIndex = findIndex(
     kulturNodes,
-    n =>
+    (n) =>
       n.id === `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}`,
   )
 
@@ -43,19 +40,17 @@ export default ({ nodes, data, url }) => {
       // only show if parent node exists
       .filter(() =>
         nodes
-          .map(n => n.id)
+          .map((n) => n.id)
           .includes(
             `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}AusLieferungFolder`,
           ),
       )
-      .map(el => {
+      .map((el) => {
         const datum = el.datum
           ? moment(el.datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
           : 'kein Datum'
-        const anz = exists(el.anzahl_pflanzen) ? el.anzahl_pflanzen : '_'
-        const anzAb = exists(el.anzahl_auspflanzbereit)
-          ? el.anzahl_auspflanzbereit
-          : '_'
+        const anz = el.anzahl_pflanzen ?? '_'
+        const anzAb = el.anzahl_auspflanzbereit ?? '_'
         const numbers = `${anz
           .toString()
           .padStart(3, '_')}/${anzAb.toString().padStart(3, '_')}`
