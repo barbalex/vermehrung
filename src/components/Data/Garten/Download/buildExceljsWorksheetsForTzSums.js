0402224a561ea46c5a3281eefee07b79caa6d129
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-31 12:44:34 +0200

    work on exports

--- src/components/Data/Garten/Download/buildExceljsWorksheetsForTzSums.js
+++ src/components/Data/Garten/Download/buildExceljsWorksheetsForTzSums.js
@@ -1,36 +1,23 @@
-import gql from 'graphql-tag'
-
 import addWorksheetToExceljsWorkbook from '../../../../utils/addWorksheetToExceljsWorkbook'
-import { gartenTeilzaehlungSums as gartenTeilzaehlungSumsFragment } from '../../../../utils/fragments'
 
 /**
  * this function cann be used from higher up
  * that is why it receives a workbook and _can_ recieve calledFromHigherUp
  */
-export default async ({ client, store, garten_id, workbook }) => {
+export default async ({ store, garten_id, workbook }) => {
   const { addNotification } = store
 
   let gaertenResult
   try {
-    gaertenResult = await client.query({
-      query: gql`
-        query GetGartenTzSums($id: uuid!) {
-          garten_teilzaehlung_sums(where: { garten_id: { _eq: $id } }) {
-            ...GartenTeilzaehlungSumsFields
-          }
-        }
-        ${gartenTeilzaehlungSumsFragment}
-      `,
-      variables: {
-        id: garten_id,
-      },
+    gaertenResult = await store.queryGarten_teilzaehlung_sums({
+      where: { garten_id: { _eq: garten_id } },
     })
   } catch (error) {
     return addNotification({
       message: error.message,
     })
   }
-  const data = gaertenResult?.data?.garten_teilzaehlung_sums ?? []
+  const data = gaertenResult?.garten_teilzaehlung_sums ?? []
   for (let row of data) {
     delete row.__typename
   }
