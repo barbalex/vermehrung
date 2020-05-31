57f7258f2a8b3e92b75b424ed653284518dc9cfe
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-31 22:30:46 +0200

    use mst

--- src/components/TreeContainer/nodes/art/index.js
+++ src/components/TreeContainer/nodes/art/index.js
@@ -1,5 +1,5 @@
-export default ({ nodes, data }) =>
-  (data?.art ?? [])
+export default ({ nodes, store }) =>
+  store.artFiltered
     // only show if parent node exists
     .filter(() => nodes.map((n) => n.id).includes('artFolder'))
     .map((n) => ({
