0b8257da376596c9db205c9eb2bb37ea4c8bc0ff
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-02 13:23:02 +0200

    work on deletions

--- src/components/Data/Art/Conflict.js
+++ src/components/Data/Art/Conflict.js
@@ -86,6 +86,7 @@ const ArtConflict = ({
       changed_by: user.email,
       _parent_rev: row._rev,
       _depth: newDepth,
+      _conflicts: [],
       _deleted: revRow._deleted,
     }
     const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
--- src/components/Data/Garten/Conflict.js
+++ src/components/Data/Garten/Conflict.js
@@ -125,6 +125,7 @@ const GartenConflict = ({
       changed_by: user.email,
       _parent_rev: row._rev,
       _depth: newDepth,
+      _conflicts: [],
       _deleted: revRow._deleted,
     }
     const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
