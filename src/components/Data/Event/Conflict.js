6ac613aa5be0c048f247a50dbd69bd388aea944f
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-02 13:23:02 +0200

    work on deletions

--- src/components/Data/Event/Conflict.js
+++ src/components/Data/Event/Conflict.js
@@ -149,6 +149,7 @@ const EventConflict = ({
       changed_by: user.email,
       _parent_rev: row._rev,
       _depth: newDepth,
+      _conflicts: [],
       _deleted: revRow._deleted,
     }
     const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
