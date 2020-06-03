4f7051ef65c10b32110f832c4127b54d4f94a13e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-03 17:16:59 +0200

    use mst

--- src/components/TreeContainer/nodes/art/index.js
+++ src/components/TreeContainer/nodes/art/index.js
@@ -1,27 +1,27 @@
 import isEqual from 'lodash/isEqual'
+import sortBy from 'lodash/sortBy'
 
 export default ({ store }) => {
-  if (!store.tree.showArt) return []
+  const { showArt, openNodes } = store.tree
+  if (!showArt) return []
 
-  return (
-    store.artsFiltered
-      // only show if parent node exists
-      .filter(() =>
-        store.tree.openNodes.some((node) => isEqual(['Arten'], node)),
-      )
-      .map((n) => ({
-        nodeType: 'table',
-        menuTitle: 'Art',
-        table: 'art',
-        id: `art${n.id}`,
-        parentId: 'artFolder',
-        label: n?.art_ae_art?.name ?? '(keine Art gewählt)',
-        url: ['Arten', n.id],
-        hasChildren: true,
-      }))
-      .map((n, index) => {
-        n.sort = [1, index]
-        return n
-      })
-  )
+  const nodes = store.artsFiltered
+    // only show if parent node exists
+    .filter(() => openNodes.some((node) => isEqual(['Arten'], node)))
+    .map((n) => ({
+      nodeType: 'table',
+      menuTitle: 'Art',
+      table: 'art',
+      id: n.id,
+      parentId: 'artFolder',
+      label: n?.art_ae_art?.name ?? '(keine Art gewählt)',
+      url: ['Arten', n.id],
+      hasChildren: true,
+    }))
+    .map((n, index) => {
+      n.sort = [1, index]
+      return n
+    })
+
+  return sortBy(nodes, 'sort')
 }
