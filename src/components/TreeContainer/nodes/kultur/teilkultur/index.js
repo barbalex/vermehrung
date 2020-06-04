6e264238a149fc19c2b1d24b7e9d609c6788e4c8
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-04 18:05:15 +0200

    use mst

--- src/components/TreeContainer/nodes/kultur/teilkultur/index.js
+++ src/components/TreeContainer/nodes/kultur/teilkultur/index.js
@@ -1,24 +1,23 @@
-import findIndex from 'lodash/findIndex'
+export default ({ store }) => {
+  const { showKultur, visibleOpenNodes, kulturKultur } = store.tree
+  if (!showKultur) return []
 
-export default ({ nodes, store, url }) => {
-  const kulturId = url[1]
-
-  const teilkulturs = store.teilkultursFiltered.filter(
-    (z) => z.kultur_id === kulturId,
+  const parentNodes = visibleOpenNodes.filter(
+    (node) =>
+      node.length === 3 && node[0] === 'Kulturen' && node[2] === 'Teilkulturen',
   )
 
-  const kulturNodes = nodes.filter((n) => n.parentId === `kulturFolder`)
-  const kulturIndex = findIndex(
-    kulturNodes,
-    (n) => n.id === `kultur${kulturId}`,
-  )
+  if (!parentNodes.length) return []
 
-  return (
-    teilkulturs
-      // only show if parent node exists
-      .filter(() =>
-        nodes.map((n) => n.id).includes(`kultur${kulturId}TeilkulturFolder`),
-      )
+  return parentNodes.flatMap((node) => {
+    const kulturId = node[1]
+    const kulturIndex = kulturKultur.findIndex((a) => a.id === kulturId)
+
+    const teilkulturs = store.teilkultursFiltered.filter(
+      (z) => z.kultur_id === kulturId,
+    )
+
+    return teilkulturs
       .map((el) => {
         const label = el.name ?? '(kein Name)'
 
@@ -26,8 +25,7 @@ export default ({ nodes, store, url }) => {
           nodeType: 'table',
           menuTitle: 'Teilkultur',
           table: 'teilkultur',
-          id: `kultur${kulturId}Teilkultur${el.id}`,
-          parentId: `kultur${kulturId}TeilkulturFolder`,
+          id: el.id,
           label,
           url: ['Kulturen', kulturId, 'Teilkulturen', el.id],
           hasChildren: false,
@@ -37,5 +35,5 @@ export default ({ nodes, store, url }) => {
         el.sort = [5, kulturIndex, 1, index]
         return el
       })
-  )
+  })
 }
