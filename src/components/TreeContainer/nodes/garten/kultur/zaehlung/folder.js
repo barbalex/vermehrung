a9366008162c0dbd85471b57a0869a314e71ce0e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-04 14:15:51 +0200

    use mst

--- src/components/TreeContainer/nodes/garten/kultur/zaehlung/folder.js
+++ src/components/TreeContainer/nodes/garten/kultur/zaehlung/folder.js
@@ -1,40 +1,37 @@
-import findIndex from 'lodash/findIndex'
+export default ({ store }) => {
+  const {
+    showGarten,
+    visibleOpenNodes,
+    loading,
+    gartenGarten,
+    gartenKultur,
+  } = store.tree
+  if (!showGarten) return []
 
-export default ({ url, nodes, store, loading }) => {
-  const gartenId = url[1]
-  const kulturId = url[3]
-  const zaehlungen = store.zaehlungsFiltered.filter(
-    (z) => z.kultur_id === kulturId,
-  )
-  const nr = loading && !zaehlungen.length ? '...' : zaehlungen.length
-
-  const gartenNodes = nodes.filter((n) => n.parentId === 'gartenFolder')
-  const gartenIndex = findIndex(
-    gartenNodes,
-    (n) => n.id === `garten${gartenId}`,
-  )
-  const kulturNodes = nodes.filter(
-    (n) => n.parentId === `garten${gartenId}KulturFolder`,
-  )
-  const kulturIndex = findIndex(
-    kulturNodes,
-    (n) => n.id === `garten${gartenId}Kultur${kulturId}`,
+  const parentNodes = visibleOpenNodes.filter(
+    (node) =>
+      node.length === 4 && node[0] === 'Gaerten' && node[2] === 'Kulturen',
   )
 
-  // only return if parent exists
-  if (!nodes.map((n) => n.id).includes(`garten${gartenId}Kultur${kulturId}`))
-    return []
+  return parentNodes.map((node) => {
+    const gartenId = node[1]
+    const gartenIndex = gartenGarten.findIndex((a) => a.id === gartenId)
+    const kulturId = node[3]
+    const kulturIndex = gartenKultur.findIndex((a) => a.id === kulturId)
+    const zaehlungen = store.zaehlungsFiltered.filter(
+      (z) => z.kultur_id === kulturId,
+    )
+    const nr = loading && !zaehlungen.length ? '...' : zaehlungen.length
 
-  return [
-    {
+    return {
       nodeType: 'folder',
       menuTitle: 'Zählungen',
-      id: `garten${gartenId}Kultur${kulturId}ZaehlungFolder`,
+      id: `${kulturId}ZaehlungFolder`,
       label: `Zählungen (${nr})`,
       url: ['Gaerten', gartenId, 'Kulturen', kulturId, 'Zaehlungen'],
       sort: [4, gartenIndex, 1, kulturIndex, 2],
       hasChildren: true,
       childrenCount: nr,
-    },
-  ]
+    }
+  })
 }
