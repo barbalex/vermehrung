e2ca09137452d02b38b64613d2d4b2f3cc169599
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-04 21:00:55 +0200

    use mst

--- src/components/TreeContainer/nodes/person/lieferung/folder.js
+++ src/components/TreeContainer/nodes/person/lieferung/folder.js
@@ -1,32 +1,29 @@
-import findIndex from 'lodash/findIndex'
+export default ({ store }) => {
+  const { showPerson, visibleOpenNodes, loading, personPerson } = store.tree
+  if (!showPerson) return []
 
-export default ({ store, nodes, loading, url }) => {
-  const personId = url[1]
-
-  const lieferungen = store.lieferungsFiltered.filter(
-    (s) => s.person_id === personId,
-  )
-  const nr = loading && !lieferungen.length ? '...' : lieferungen.length
-
-  const personNodes = nodes.filter((n) => n.parentId === 'personFolder')
-  const personIndex = findIndex(
-    personNodes,
-    (n) => n.id === `person${personId}`,
+  const parentNodes = visibleOpenNodes.filter(
+    (node) => node.length === 2 && node[0] === 'Personen',
   )
 
-  // only return if parent exists
-  if (!nodes.map((n) => n.id).includes(`person${personId}`)) return []
+  return parentNodes.map((node) => {
+    const personId = node[1]
+    const personIndex = personPerson.findIndex((a) => a.id === personId)
 
-  return [
-    {
+    const lieferungen = store.lieferungsFiltered.filter(
+      (s) => s.person_id === personId,
+    )
+    const nr = loading && !lieferungen.length ? '...' : lieferungen.length
+
+    return {
       nodeType: 'folder',
       menuTitle: 'Lieferungen',
-      id: `person${personId}LieferungFolder`,
+      id: `${personId}LieferungFolder`,
       label: `Lieferungen (${nr})`,
       url: ['Personen', personId, 'Lieferungen'],
       sort: [11, personIndex, 3],
       hasChildren: true,
       childrenCount: nr,
-    },
-  ]
+    }
+  })
 }
