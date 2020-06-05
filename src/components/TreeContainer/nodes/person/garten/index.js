79abf6fbbe8098c0d036642d4381450cd6668dcc
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-05 11:15:07 +0200

    use mst

--- src/components/TreeContainer/nodes/person/garten/index.js
+++ src/components/TreeContainer/nodes/person/garten/index.js
@@ -1,5 +1,5 @@
 export default ({ store }) => {
-  const { showPerson, visibleOpenNodes, personPerson } = store.tree
+  const { showPerson, visibleOpenNodes, person } = store.tree
   if (!showPerson) return []
 
   const parentNodes = visibleOpenNodes.filter(
@@ -11,7 +11,7 @@ export default ({ store }) => {
 
   return parentNodes.flatMap((node) => {
     const personId = node[1]
-    const personIndex = personPerson.findIndex((a) => a.id === personId)
+    const personIndex = person.findIndex((a) => a.id === personId)
 
     const gaerten = store.gartensFiltered.filter(
       (s) => s.person_id === personId,
