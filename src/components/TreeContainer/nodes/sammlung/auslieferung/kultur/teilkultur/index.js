7340968d3288e363d8a1f052ef973dace21ab266
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-05 15:02:33 +0200

    use mst

--- src/components/TreeContainer/nodes/sammlung/auslieferung/kultur/teilkultur/index.js
+++ src/components/TreeContainer/nodes/sammlung/auslieferung/kultur/teilkultur/index.js
@@ -6,7 +6,9 @@ export default ({ nodes, store, url }) => {
   const lieferungId = url[3]
   const kulturId = url[5]
 
-  const events = store.eventsFiltered.filter((z) => z.kultur_id === kulturId)
+  const anlieferungen = store.lieferungsFiltered.filter(
+    (z) => z.nach_kultur_id === kulturId,
+  )
 
   const sammlungNodes = nodes.filter((n) => n.parentId === 'sammlungFolder')
   const sammlungIndex = findIndex(
@@ -31,29 +33,33 @@ export default ({ nodes, store, url }) => {
   )
 
   return (
-    events
+    anlieferungen
       // only show if parent node exists
       .filter(() =>
         nodes
           .map((n) => n.id)
           .includes(
-            `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}EventFolder`,
+            `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}AnLieferungFolder`,
           ),
       )
       .map((el) => {
         const datum = el.datum
           ? moment(el.datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
-          : null
+          : 'kein Datum'
+        const anz = el.anzahl_pflanzen ?? '_'
+        const anzAb = el.anzahl_auspflanzbereit ?? '_'
+        const numbers = `${anz
+          .toString()
+          .padStart(3, '_')}/${anzAb.toString().padStart(3, '_')}`
         const geplant = el.geplant ? ' (geplant)' : ''
-        const event = `${el?.beschreibung ?? '(nicht beschrieben)'}${geplant}`
-        const label = `${datum ?? '(kein Datum)'}: ${event}`
+        const label = `${datum}: ${numbers}${geplant}`
 
         return {
           nodeType: 'table',
-          menuTitle: 'Event',
-          table: 'event',
-          id: `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}Event${el.id}`,
-          parentId: `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}EventFolder`,
+          menuTitle: 'An-Lieferung',
+          table: 'lieferung',
+          id: `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}Lieferung${el.id}`,
+          parentId: `sammlung${sammlungId}Lieferung${lieferungId}Kultur${kulturId}AnLieferungFolder`,
           label,
           url: [
             'Sammlungen',
@@ -62,10 +68,11 @@ export default ({ nodes, store, url }) => {
             lieferungId,
             'Kulturen',
             kulturId,
-            'Events',
+            'An-Lieferungen',
             el.id,
           ],
           hasChildren: false,
+          mono: true,
         }
       })
       .map((el, index) => {
@@ -76,7 +83,7 @@ export default ({ nodes, store, url }) => {
           lieferungIndex,
           1,
           kulturIndex,
-          5,
+          1,
           index,
         ]
         return el
