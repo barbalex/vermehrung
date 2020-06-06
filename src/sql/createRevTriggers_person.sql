121ec9525cadc12025dec4468ec0272f0c8c496c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-06 13:14:45 +0200

    work on conflicts

--- src/sql/createRevTriggers_person.sql
+++ src/sql/createRevTriggers_person.sql
@@ -2,120 +2,168 @@ create or replace function person_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into person (
-        id,
-        nr,
-        name,
-        adresszusatz,
-        strasse,
-        plz,
-        ort,
-        telefon_privat,
-        telefon_geschaeft,
-        telefon_mobile,
-        email,
-        kein_email,
-        bemerkungen,
-        changed,
-        changed_by,
-        account_id,
-        user_role,
-        kommerziell,
-        info,
-        aktiv,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
+    select
+      person_id,
+      _rev,
+      _depth
+    from
+      person_rev
+    where
+      not exists (
+        select
+          person_id
+        from
+          person_rev as t
+        where
+          t.person_id = new.person_id
+          and t._parent_rev = person_rev._rev)
+        and _deleted = false
+        and person_id = new.person_id
+    ),
+    max_depths as (
       select
-        person_id,
-        _rev,
-        _depth
+        max(_depth) as max_depth
       from
-        person_rev
-      where
-        not exists (
-          select
-            person_id
-          from
-            person_rev as t
-          where
-            t.person_id = new.person_id
-            and t._parent_rev = person_rev._rev)
-          and _deleted = false
-          and person_id = new.person_id
-      )
+        leaves
+    ),
+    winning_revisions as (
       select
-        person_rev.person_id,
-        person_rev.nr,
-        person_rev.name,
-        person_rev.adresszusatz,
-        person_rev.strasse,
-        person_rev.plz,
-        person_rev.ort,
-        person_rev.telefon_privat,
-        person_rev.telefon_geschaeft,
-        person_rev.telefon_mobile,
-        person_rev.email,
-        person_rev.kein_email,
-        person_rev.bemerkungen,
-        person_rev.changed,
-        person_rev.changed_by,
-        person_rev.account_id,
-        person_rev.user_role,
-        person_rev.kommerziell,
-        person_rev.info,
-        person_rev.aktiv,
-        person_rev._rev,
-        person_rev._revisions,
-        person_rev._parent_rev,
-        person_rev._depth,
-        person_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> person_rev._rev
-            and _rev <> ANY(person_rev._revisions)
-        )) as _conflicts
+        max(leaves._rev) as _rev
       from
-        person_rev
-        where id = new.id
-      on conflict on constraint person_pkey do update set
-        -- do not update the id = pkey
-        nr = excluded.nr,
-        name = excluded.name,
-        adresszusatz = excluded.adresszusatz,
-        strasse = excluded.strasse,
-        plz = excluded.plz,
-        ort = excluded.ort,
-        telefon_privat = excluded.telefon_privat,
-        telefon_geschaeft = excluded.telefon_geschaeft,
-        telefon_mobile = excluded.telefon_mobile,
-        email = excluded.email,
-        kein_email = excluded.kein_email,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        account_id = excluded.account_id,
-        user_role = excluded.user_role,
-        kommerziell = excluded.kommerziell,
-        info = excluded.info,
-        aktiv = excluded.aktiv,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from person_rev
+    join winning_revisions on person_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into person (
+      id,
+      nr,
+      name,
+      adresszusatz,
+      strasse,
+      plz,
+      ort,
+      telefon_privat,
+      telefon_geschaeft,
+      telefon_mobile,
+      email,
+      kein_email,
+      bemerkungen,
+      changed,
+      changed_by,
+      account_id,
+      user_role,
+      kommerziell,
+      info,
+      aktiv,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      person_id,
+      _rev,
+      _depth
+    from
+      person_rev
+    where
+      not exists (
+        select
+          person_id
+        from
+          person_rev as t
+        where
+          t.person_id = new.person_id
+          and t._parent_rev = person_rev._rev)
+        and _deleted = false
+        and person_id = new.person_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      person_rev.person_id,
+      person_rev.nr,
+      person_rev.name,
+      person_rev.adresszusatz,
+      person_rev.strasse,
+      person_rev.plz,
+      person_rev.ort,
+      person_rev.telefon_privat,
+      person_rev.telefon_geschaeft,
+      person_rev.telefon_mobile,
+      person_rev.email,
+      person_rev.kein_email,
+      person_rev.bemerkungen,
+      person_rev.changed,
+      person_rev.changed_by,
+      person_rev.account_id,
+      person_rev.user_role,
+      person_rev.kommerziell,
+      person_rev.info,
+      person_rev.aktiv,
+      person_rev._rev,
+      person_rev._revisions,
+      person_rev._parent_rev,
+      person_rev._depth,
+      person_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> person_rev._rev
+          and _rev <> ANY(person_rev._revisions)
+      )) as _conflicts
+    from
+      person_rev
+      join winning_revisions on person_rev._rev = winning_revisions._rev
+    on conflict on constraint person_pkey do update set
+      -- do not update the id = pkey
+      nr = excluded.nr,
+      name = excluded.name,
+      adresszusatz = excluded.adresszusatz,
+      strasse = excluded.strasse,
+      plz = excluded.plz,
+      ort = excluded.ort,
+      telefon_privat = excluded.telefon_privat,
+      telefon_geschaeft = excluded.telefon_geschaeft,
+      telefon_mobile = excluded.telefon_mobile,
+      email = excluded.email,
+      kein_email = excluded.kein_email,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      account_id = excluded.account_id,
+      user_role = excluded.user_role,
+      kommerziell = excluded.kommerziell,
+      info = excluded.info,
+      aktiv = excluded.aktiv,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into person (
         id,
         nr,
@@ -160,7 +208,7 @@ begin
           where
             t.person_id = new.person_id
             and t._parent_rev = person_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and person_id = new.person_id
       ),
       max_depths as (
--- src/sql/createRevTriggers_person_option.sql
+++ src/sql/createRevTriggers_person_option.sql
@@ -2,126 +2,174 @@ create or replace function person_option_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into person_option (
-        id,
-        ar_name_deutsch,
-        ga_strasse,
-        ga_plz,
-        ga_ort,
-        ga_geom_point,
-        ga_lat_lng,
-        ga_aktiv,
-        ga_bemerkungen,
-        hk_kanton,
-        hk_land,
-        hk_bemerkungen,
-        hk_geom_point,
-        li_show_sl_felder,
-        li_show_sl,
-        sl_show_empty_when_next_to_li,
-        sl_auto_copy_edits,
-        tree_kultur,
-        tree_teilkultur,
-        tree_zaehlung,
-        tree_lieferung,
-        tree_event,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
+    select
+      person_id,
+      _rev,
+      _depth
+    from
+      person_option_rev
+    where
+      not exists (
+        select
+          person_id
+        from
+          person_option_rev as t
+        where
+          t.person_id = new.person_id
+          and t._parent_rev = person_option_rev._rev)
+        and _deleted = false
+        and person_id = new.person_id
+    ),
+    max_depths as (
       select
-        person_id,
-        _rev,
-        _depth
+        max(_depth) as max_depth
       from
-        person_option_rev
-      where
-        not exists (
-          select
-            person_id
-          from
-            person_option_rev as t
-          where
-            t.person_id = new.person_id
-            and t._parent_rev = person_option_rev._rev)
-          and _deleted = false
-          and person_id = new.person_id
-      )
+        leaves
+    ),
+    winning_revisions as (
       select
-        person_option_rev.person_id,
-        person_option_rev.ar_name_deutsch,
-        person_option_rev.ga_strasse,
-        person_option_rev.ga_plz,
-        person_option_rev.ga_ort,
-        person_option_rev.ga_geom_point,
-        person_option_rev.ga_lat_lng,
-        person_option_rev.ga_aktiv,
-        person_option_rev.ga_bemerkungen,
-        person_option_rev.hk_kanton,
-        person_option_rev.hk_land,
-        person_option_rev.hk_bemerkungen,
-        person_option_rev.hk_geom_point,
-        person_option_rev.li_show_sl_felder,
-        person_option_rev.li_show_sl,
-        person_option_rev.sl_show_empty_when_next_to_li,
-        person_option_rev.sl_auto_copy_edits,
-        person_option_rev.tree_kultur,
-        person_option_rev.tree_teilkultur,
-        person_option_rev.tree_zaehlung,
-        person_option_rev.tree_lieferung,
-        person_option_rev.tree_event,
-        person_option_rev._rev,
-        person_option_rev._revisions,
-        person_option_rev._parent_rev,
-        person_option_rev._depth,
-        person_option_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> person_option_rev._rev
-            and _rev <> ANY(person_option_rev._revisions)
-        )) as _conflicts
+        max(leaves._rev) as _rev
       from
-        person_option_rev
-        where id = new.id
-      on conflict on constraint person_option_person_id_key do update set
-        -- do not update the id = pkey
-        ar_name_deutsch = excluded.ar_name_deutsch,
-        ga_strasse = excluded.ga_strasse,
-        ga_plz = excluded.ga_plz,
-        ga_ort = excluded.ga_ort,
-        ga_geom_point = excluded.ga_geom_point,
-        ga_lat_lng = excluded.ga_lat_lng,
-        ga_aktiv = excluded.ga_aktiv,
-        ga_bemerkungen = excluded.ga_bemerkungen,
-        hk_kanton = excluded.hk_kanton,
-        hk_land = excluded.hk_land,
-        hk_bemerkungen = excluded.hk_bemerkungen,
-        hk_geom_point = excluded.hk_geom_point,
-        li_show_sl_felder = excluded.li_show_sl_felder,
-        li_show_sl = excluded.li_show_sl,
-        sl_show_empty_when_next_to_li = excluded.sl_show_empty_when_next_to_li,
-        sl_auto_copy_edits = excluded.sl_auto_copy_edits,
-        tree_kultur = excluded.tree_kultur,
-        tree_teilkultur = excluded.tree_teilkultur,
-        tree_zaehlung = excluded.tree_zaehlung,
-        tree_lieferung = excluded.tree_lieferung,
-        tree_event = excluded.tree_event,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from person_option_rev
+    join winning_revisions on person_option_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into person_option (
+      id,
+      ar_name_deutsch,
+      ga_strasse,
+      ga_plz,
+      ga_ort,
+      ga_geom_point,
+      ga_lat_lng,
+      ga_aktiv,
+      ga_bemerkungen,
+      hk_kanton,
+      hk_land,
+      hk_bemerkungen,
+      hk_geom_point,
+      li_show_sl_felder,
+      li_show_sl,
+      sl_show_empty_when_next_to_li,
+      sl_auto_copy_edits,
+      tree_kultur,
+      tree_teilkultur,
+      tree_zaehlung,
+      tree_lieferung,
+      tree_event,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      person_id,
+      _rev,
+      _depth
+    from
+      person_option_rev
+    where
+      not exists (
+        select
+          person_id
+        from
+          person_option_rev as t
+        where
+          t.person_id = new.person_id
+          and t._parent_rev = person_option_rev._rev)
+        and _deleted = false
+        and person_id = new.person_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      person_option_rev.person_id,
+      person_option_rev.ar_name_deutsch,
+      person_option_rev.ga_strasse,
+      person_option_rev.ga_plz,
+      person_option_rev.ga_ort,
+      person_option_rev.ga_geom_point,
+      person_option_rev.ga_lat_lng,
+      person_option_rev.ga_aktiv,
+      person_option_rev.ga_bemerkungen,
+      person_option_rev.hk_kanton,
+      person_option_rev.hk_land,
+      person_option_rev.hk_bemerkungen,
+      person_option_rev.hk_geom_point,
+      person_option_rev.li_show_sl_felder,
+      person_option_rev.li_show_sl,
+      person_option_rev.sl_show_empty_when_next_to_li,
+      person_option_rev.sl_auto_copy_edits,
+      person_option_rev.tree_kultur,
+      person_option_rev.tree_teilkultur,
+      person_option_rev.tree_zaehlung,
+      person_option_rev.tree_lieferung,
+      person_option_rev.tree_event,
+      person_option_rev._rev,
+      person_option_rev._revisions,
+      person_option_rev._parent_rev,
+      person_option_rev._depth,
+      person_option_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> person_option_rev._rev
+          and _rev <> ANY(person_option_rev._revisions)
+      )) as _conflicts
+    from
+      person_option_rev
+      join winning_revisions on person_option_rev._rev = winning_revisions._rev
+    on conflict on constraint person_option_person_id_key do update set
+      -- do not update the id = pkey
+      ar_name_deutsch = excluded.ar_name_deutsch,
+      ga_strasse = excluded.ga_strasse,
+      ga_plz = excluded.ga_plz,
+      ga_ort = excluded.ga_ort,
+      ga_geom_point = excluded.ga_geom_point,
+      ga_lat_lng = excluded.ga_lat_lng,
+      ga_aktiv = excluded.ga_aktiv,
+      ga_bemerkungen = excluded.ga_bemerkungen,
+      hk_kanton = excluded.hk_kanton,
+      hk_land = excluded.hk_land,
+      hk_bemerkungen = excluded.hk_bemerkungen,
+      hk_geom_point = excluded.hk_geom_point,
+      li_show_sl_felder = excluded.li_show_sl_felder,
+      li_show_sl = excluded.li_show_sl,
+      sl_show_empty_when_next_to_li = excluded.sl_show_empty_when_next_to_li,
+      sl_auto_copy_edits = excluded.sl_auto_copy_edits,
+      tree_kultur = excluded.tree_kultur,
+      tree_teilkultur = excluded.tree_teilkultur,
+      tree_zaehlung = excluded.tree_zaehlung,
+      tree_lieferung = excluded.tree_lieferung,
+      tree_event = excluded.tree_event,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into person_option (
         id,
         ar_name_deutsch,
@@ -168,7 +216,7 @@ begin
           where
             t.person_id = new.person_id
             and t._parent_rev = person_option_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and person_id = new.person_id
       ),
       max_depths as (
