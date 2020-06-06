448aa2c8985b48bb7bf9674307c0631928bc1311
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-07 00:44:48 +0200

    use mst

--- src/components/TreeContainer/index.js
+++ src/components/TreeContainer/index.js
@@ -5,7 +5,7 @@
  * what data is needed while the user navigates the tree
  * Every time the query is re-run with different variables,
  * it returns empty data while loading.
- * This is bad for the tree. Very hard to build a good user experiences.
+ * This is bad for the tree. Very hard to build a good user experienc.
  * So the data is passed as props to the Tree component.
  * Which can decide not to update nodes if the query is loading
  * but rather use the previous value
@@ -13,7 +13,6 @@
 import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
-import { getSnapshot } from 'mobx-state-tree'
 
 import { StoreContext, useQuery } from '../../models/reactUtils'
 import query from './query'
@@ -33,96 +32,13 @@ const Container = styled.div`
 
 const TreeContainer = () => {
   const store = useContext(StoreContext)
-  const {
-    user,
-    userPerson,
-    artFilter,
-    eventFilter,
-    gartenFilter,
-    kulturFilter,
-    herkunftFilter,
-    personFilter,
-    sammelLieferungFilter,
-    sammlungFilter,
-    lieferungFilter,
-    teilkulturFilter,
-    zaehlungFilter,
-  } = store
-  const { setRefetch, openNodes: openNodesRaw, setLoading } = store.tree
-  // need to extract pure openNodes
-  // to force node building useEffect to run
-  // when openNodes changes
-  const openNodes = getSnapshot(openNodesRaw)
-
-  const { user_role: role } = userPerson
-  const isGardener = role === 'gaertner'
+  const { user } = store
+  const { setRefetch, setLoading, queryVariables } = store.tree
 
   // 1. build list depending on path using react-window
   // 2. every node uses navigate to set url on click
-  const variables = {
-    artFilter,
-    eventFilter,
-    gartenFilter,
-    kulturFilter,
-    herkunftFilter,
-    personFilter,
-    sammlungFilter,
-    lieferungFilter,
-    sammelLieferungFilter,
-    teilkulturFilter,
-    zaehlungFilter,
-    isArt: openNodes.some((n) => n[0] === 'Arten'),
-    //isEvent: openNodes.some((n) => n[0] === 'Events'),
-    isArtKultur: openNodes.some((n) => n[0] === 'Arten' && n[2] === 'Kulturen'),
-    isArtSammlung: openNodes.some(
-      (n) => n[0] === 'Arten' && n[2] === 'Sammlungen',
-    ),
-    isGarten: openNodes.some((n) => n[0] === 'Gaerten'),
-    isGartenKultur: openNodes.some(
-      (n) => n[0] === 'Gaerten' && n[2] === 'Kulturen',
-    ),
-    isHerkunft: openNodes.some((n) => n[0] === 'Herkuenfte'),
-    isHerkunftSammlung: openNodes.some(
-      (n) => n[0] === 'Herkuenfte' && n[2] === 'Sammlungen',
-    ),
-    isLieferung: openNodes.some((n) => n[0] === 'Lieferungen'),
-    isSammelLieferung: openNodes.some((n) => n[0] === 'Sammel-Lieferungen'),
-    isPerson: openNodes.some((n) => n[0] === 'Personen'),
-    isPersonGarten: openNodes.some(
-      (n) => n[0] === 'Personen' && n[2] === 'Gaerten',
-    ),
-    isPersonGartenKultur: openNodes.some(
-      (n) => n[0] === 'Personen' && n[2] === 'Gaerten' && n[4] === 'Kulturen',
-    ),
-    isPersonSammlung: openNodes.some(
-      (n) => n[0] === 'Personen' && n[2] === 'Sammlungen',
-    ),
-    isPersonLieferung: openNodes.some(
-      (n) => n[0] === 'Personen' && n[2] === 'Lieferungen',
-    ),
-    isSammlung: openNodes.some((n) => n[0] === 'Sammlungen'),
-    isSammlungLieferung: openNodes.some(
-      (n) => n[0] === 'Sammlungen' && n[2] === 'Aus-Lieferungen',
-    ),
-    isSammlungLieferungKultur: openNodes.some(
-      (n) =>
-        n[0] === 'Sammlungen' &&
-        n[2] === 'Aus-Lieferungen' &&
-        n[4] === 'Kulturen',
-    ),
-    //isTeilkultur: openNodes.some((n) => n[0] === 'Teilkulturen'),
-    isKultur: openNodes.some((n) => n[0] === 'Kulturen'),
-    //isKulturAnLieferung: openNodes.some(
-    //  (n) => n[0] === 'Kulturen' && n[2] === 'An-Lieferungen',
-    //),
-    //isKulturAusLieferung: openNodes.some(
-    //  (n) => n[0] === 'Kulturen' && n[2] === 'Aus-Lieferungen',
-    //),
-    //isWerteListe: openNodes.some((n) => n[0] === 'Werte-Listen'),
-    isGardener,
-  }
   const { error, loading, query: treeQuery } = useQuery(query, {
-    variables,
+    variables: queryVariables,
   })
 
   useEffect(() => {
