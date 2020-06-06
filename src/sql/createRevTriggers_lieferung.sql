1342fe3ae7063eae17ff7f7ad87b8fdfcc37d51f
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-06 13:14:45 +0200

    work on conflicts

--- src/sql/createRevTriggers_lieferung.sql
+++ src/sql/createRevTriggers_lieferung.sql
@@ -2,230 +2,278 @@ create or replace function lieferung_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into lieferung (
-        id,
-        sammel_lieferung_id,
-        art_id,
-        person_id,
-        von_sammlung_id,
-        von_kultur_id,
-        datum,
-        nach_kultur_id,
-        nach_ausgepflanzt,
-        von_anzahl_individuen,
-        anzahl_pflanzen,
-        anzahl_auspflanzbereit,
-        gramm_samen,
-        andere_menge,
-        geplant,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
+    select
+      lieferung_id,
+      _rev,
+      _depth
+    from
+      lieferung_rev
+    where
+      not exists (
+        select
+          lieferung_id
+        from
+          lieferung_rev as t
+        where
+          t.lieferung_id = new.lieferung_id
+          and t._parent_rev = lieferung_rev._rev)
+        and _deleted = false
+        and lieferung_id = new.lieferung_id
+    ),
+    max_depths as (
       select
-        lieferung_id,
-        _rev,
-        _depth
+        max(_depth) as max_depth
       from
-        lieferung_rev
-      where
-        not exists (
-          select
-            lieferung_id
-          from
-            lieferung_rev as t
-          where
-            t.lieferung_id = new.lieferung_id
-            and t._parent_rev = lieferung_rev._rev)
-          and _deleted = false
-          and lieferung_id = new.lieferung_id
-      )
+        leaves
+    ),
+    winning_revisions as (
       select
-        lieferung_rev.lieferung_id,
-        lieferung_rev.sammel_lieferung_id,
-        lieferung_rev.art_id,
-        lieferung_rev.person_id,
-        lieferung_rev.von_sammlung_id,
-        lieferung_rev.von_kultur_id,
-        lieferung_rev.datum,
-        lieferung_rev.nach_kultur_id,
-        lieferung_rev.nach_ausgepflanzt,
-        lieferung_rev.von_anzahl_individuen,
-        lieferung_rev.anzahl_pflanzen,
-        lieferung_rev.anzahl_auspflanzbereit,
-        lieferung_rev.gramm_samen,
-        lieferung_rev.andere_menge,
-        lieferung_rev.geplant,
-        lieferung_rev.bemerkungen,
-        lieferung_rev.changed,
-        lieferung_rev.changed_by,
-        lieferung_rev._rev,
-        lieferung_rev._revisions,
-        lieferung_rev._parent_rev,
-        lieferung_rev._depth,
-        lieferung_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> lieferung_rev._rev
-            and _rev <> ANY(lieferung_rev._revisions)
-        )) as _conflicts
+        max(leaves._rev) as _rev
       from
-        lieferung_rev
-        where id = new.id
-      on conflict on constraint lieferung_pkey do update set
-        -- do not update the id = pkey
-        sammel_lieferung_id = excluded.sammel_lieferung_id,
-        art_id = excluded.art_id,
-        person_id = excluded.person_id,
-        von_sammlung_id = excluded.von_sammlung_id,
-        von_kultur_id = excluded.von_kultur_id,
-        datum = excluded.datum,
-        nach_kultur_id = excluded.nach_kultur_id,
-        nach_ausgepflanzt = excluded.nach_ausgepflanzt,
-        von_anzahl_individuen = excluded.von_anzahl_individuen,
-        anzahl_pflanzen = excluded.anzahl_pflanzen,
-        anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
-        gramm_samen = excluded.gramm_samen,
-        andere_menge = excluded.andere_menge,
-        geplant = excluded.geplant,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from lieferung_rev
+    join winning_revisions on lieferung_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into lieferung (
+      id,
+      sammel_lieferung_id,
+      art_id,
+      person_id,
+      von_sammlung_id,
+      von_kultur_id,
+      datum,
+      nach_kultur_id,
+      nach_ausgepflanzt,
+      von_anzahl_individuen,
+      anzahl_pflanzen,
+      anzahl_auspflanzbereit,
+      gramm_samen,
+      andere_menge,
+      geplant,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      lieferung_id,
+      _rev,
+      _depth
+    from
+      lieferung_rev
+    where
+      not exists (
+        select
+          lieferung_id
+        from
+          lieferung_rev as t
+        where
+          t.lieferung_id = new.lieferung_id
+          and t._parent_rev = lieferung_rev._rev)
+        and _deleted = false
+        and lieferung_id = new.lieferung_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      lieferung_rev.lieferung_id,
+      lieferung_rev.sammel_lieferung_id,
+      lieferung_rev.art_id,
+      lieferung_rev.person_id,
+      lieferung_rev.von_sammlung_id,
+      lieferung_rev.von_kultur_id,
+      lieferung_rev.datum,
+      lieferung_rev.nach_kultur_id,
+      lieferung_rev.nach_ausgepflanzt,
+      lieferung_rev.von_anzahl_individuen,
+      lieferung_rev.anzahl_pflanzen,
+      lieferung_rev.anzahl_auspflanzbereit,
+      lieferung_rev.gramm_samen,
+      lieferung_rev.andere_menge,
+      lieferung_rev.geplant,
+      lieferung_rev.bemerkungen,
+      lieferung_rev.changed,
+      lieferung_rev.changed_by,
+      lieferung_rev._rev,
+      lieferung_rev._revisions,
+      lieferung_rev._parent_rev,
+      lieferung_rev._depth,
+      lieferung_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> lieferung_rev._rev
+          and _rev <> ANY(lieferung_rev._revisions)
+      )) as _conflicts
+    from
+      lieferung_rev
+      join winning_revisions on lieferung_rev._rev = winning_revisions._rev
+    on conflict on constraint lieferung_pkey do update set
+      -- do not update the id = pkey
+      sammel_lieferung_id = excluded.sammel_lieferung_id,
+      art_id = excluded.art_id,
+      person_id = excluded.person_id,
+      von_sammlung_id = excluded.von_sammlung_id,
+      von_kultur_id = excluded.von_kultur_id,
+      datum = excluded.datum,
+      nach_kultur_id = excluded.nach_kultur_id,
+      nach_ausgepflanzt = excluded.nach_ausgepflanzt,
+      von_anzahl_individuen = excluded.von_anzahl_individuen,
+      anzahl_pflanzen = excluded.anzahl_pflanzen,
+      anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
+      gramm_samen = excluded.gramm_samen,
+      andere_menge = excluded.andere_menge,
+      geplant = excluded.geplant,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into lieferung (
-        id,
-        sammel_lieferung_id,
-        art_id,
-        person_id,
-        von_sammlung_id,
-        von_kultur_id,
-        datum,
-        nach_kultur_id,
-        nach_ausgepflanzt,
-        von_anzahl_individuen,
-        anzahl_pflanzen,
-        anzahl_auspflanzbereit,
-        gramm_samen,
-        andere_menge,
-        geplant,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
+      id,
+      sammel_lieferung_id,
+      art_id,
+      person_id,
+      von_sammlung_id,
+      von_kultur_id,
+      datum,
+      nach_kultur_id,
+      nach_ausgepflanzt,
+      von_anzahl_individuen,
+      anzahl_pflanzen,
+      anzahl_auspflanzbereit,
+      gramm_samen,
+      andere_menge,
+      geplant,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      lieferung_id,
+      _rev,
+      _depth
+    from
+      lieferung_rev
+    where
+      not exists (
+        select
+          lieferung_id
+        from
+          lieferung_rev as t
+        where
+          t.lieferung_id = new.lieferung_id
+          and t._parent_rev = lieferung_rev._rev)
+        --and _deleted = false
+        and lieferung_id = new.lieferung_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
     )
-    with leaves as (
-      select
-        lieferung_id,
-        _rev,
-        _depth
-      from
-        lieferung_rev
-      where
-        not exists (
-          select
-            lieferung_id
-          from
-            lieferung_rev as t
-          where
-            t.lieferung_id = new.lieferung_id
-            and t._parent_rev = lieferung_rev._rev)
-          and _deleted = false
-          and lieferung_id = new.lieferung_id
-      ),
-      max_depths as (
-        select
-          max(_depth) as max_depth
-        from
-          leaves
-      ),
-      winning_revisions as (
-        select
-          max(leaves._rev) as _rev
-        from
-          leaves
-          join max_depths on leaves._depth = max_depths.max_depth
-      )
-      select
-        lieferung_rev.lieferung_id,
-        lieferung_rev.sammel_lieferung_id,
-        lieferung_rev.art_id,
-        lieferung_rev.person_id,
-        lieferung_rev.von_sammlung_id,
-        lieferung_rev.von_kultur_id,
-        lieferung_rev.datum,
-        lieferung_rev.nach_kultur_id,
-        lieferung_rev.nach_ausgepflanzt,
-        lieferung_rev.von_anzahl_individuen,
-        lieferung_rev.anzahl_pflanzen,
-        lieferung_rev.anzahl_auspflanzbereit,
-        lieferung_rev.gramm_samen,
-        lieferung_rev.andere_menge,
-        lieferung_rev.geplant,
-        lieferung_rev.bemerkungen,
-        lieferung_rev.changed,
-        lieferung_rev.changed_by,
-        lieferung_rev._rev,
-        lieferung_rev._revisions,
-        lieferung_rev._parent_rev,
-        lieferung_rev._depth,
-        lieferung_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> lieferung_rev._rev
-            and _rev <> ANY(lieferung_rev._revisions)
-        )) as _conflicts
-      from
-        lieferung_rev
-        join winning_revisions on lieferung_rev._rev = winning_revisions._rev
-      on conflict on constraint lieferung_pkey do update set
-        -- do not update the id = pkey
-        sammel_lieferung_id = excluded.sammel_lieferung_id,
-        art_id = excluded.art_id,
-        person_id = excluded.person_id,
-        von_sammlung_id = excluded.von_sammlung_id,
-        von_kultur_id = excluded.von_kultur_id,
-        datum = excluded.datum,
-        nach_kultur_id = excluded.nach_kultur_id,
-        nach_ausgepflanzt = excluded.nach_ausgepflanzt,
-        von_anzahl_individuen = excluded.von_anzahl_individuen,
-        anzahl_pflanzen = excluded.anzahl_pflanzen,
-        anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
-        gramm_samen = excluded.gramm_samen,
-        andere_menge = excluded.andere_menge,
-        geplant = excluded.geplant,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+    select
+      lieferung_rev.lieferung_id,
+      lieferung_rev.sammel_lieferung_id,
+      lieferung_rev.art_id,
+      lieferung_rev.person_id,
+      lieferung_rev.von_sammlung_id,
+      lieferung_rev.von_kultur_id,
+      lieferung_rev.datum,
+      lieferung_rev.nach_kultur_id,
+      lieferung_rev.nach_ausgepflanzt,
+      lieferung_rev.von_anzahl_individuen,
+      lieferung_rev.anzahl_pflanzen,
+      lieferung_rev.anzahl_auspflanzbereit,
+      lieferung_rev.gramm_samen,
+      lieferung_rev.andere_menge,
+      lieferung_rev.geplant,
+      lieferung_rev.bemerkungen,
+      lieferung_rev.changed,
+      lieferung_rev.changed_by,
+      lieferung_rev._rev,
+      lieferung_rev._revisions,
+      lieferung_rev._parent_rev,
+      lieferung_rev._depth,
+      lieferung_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> lieferung_rev._rev
+          and _rev <> ANY(lieferung_rev._revisions)
+      )) as _conflicts
+    from
+      lieferung_rev
+      join winning_revisions on lieferung_rev._rev = winning_revisions._rev
+    on conflict on constraint lieferung_pkey do update set
+      -- do not update the id = pkey
+      sammel_lieferung_id = excluded.sammel_lieferung_id,
+      art_id = excluded.art_id,
+      person_id = excluded.person_id,
+      von_sammlung_id = excluded.von_sammlung_id,
+      von_kultur_id = excluded.von_kultur_id,
+      datum = excluded.datum,
+      nach_kultur_id = excluded.nach_kultur_id,
+      nach_ausgepflanzt = excluded.nach_ausgepflanzt,
+      von_anzahl_individuen = excluded.von_anzahl_individuen,
+      anzahl_pflanzen = excluded.anzahl_pflanzen,
+      anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
+      gramm_samen = excluded.gramm_samen,
+      andere_menge = excluded.andere_menge,
+      geplant = excluded.geplant,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   end if;
   return new;
 end;
--- src/sql/createRevTriggers_sammel_lieferung.sql
+++ src/sql/createRevTriggers_sammel_lieferung.sql
@@ -2,35 +2,8 @@ create or replace function sammel_lieferung_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into sammel_lieferung (
-        id,
-        art_id,
-        person_id,
-        von_sammlung_id,
-        von_kultur_id,
-        datum,
-        nach_kultur_id,
-        nach_ausgepflanzt,
-        von_anzahl_individuen,
-        anzahl_pflanzen,
-        anzahl_auspflanzbereit,
-        gramm_samen,
-        andere_menge,
-        geplant,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
       select
         sammel_lieferung_id,
@@ -49,64 +22,139 @@ begin
             and t._parent_rev = sammel_lieferung_rev._rev)
           and _deleted = false
           and sammel_lieferung_id = new.sammel_lieferung_id
+      ),
+      max_depths as (
+        select
+          max(_depth) as max_depth
+        from
+          leaves
+      ),
+      winning_revisions as (
+        select
+          max(leaves._rev) as _rev
+        from
+          leaves
+          join max_depths on leaves._depth = max_depths.max_depth
       )
+      select * from sammel_lieferung_rev
+      join winning_revisions on sammel_lieferung_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into sammel_lieferung (
+      id,
+      art_id,
+      person_id,
+      von_sammlung_id,
+      von_kultur_id,
+      datum,
+      nach_kultur_id,
+      nach_ausgepflanzt,
+      von_anzahl_individuen,
+      anzahl_pflanzen,
+      anzahl_auspflanzbereit,
+      gramm_samen,
+      andere_menge,
+      geplant,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      sammel_lieferung_id,
+      _rev,
+      _depth
+    from
+      sammel_lieferung_rev
+    where
+      not exists (
+        select
+          sammel_lieferung_id
+        from
+          sammel_lieferung_rev as t
+        where
+          t.sammel_lieferung_id = new.sammel_lieferung_id
+          and t._parent_rev = sammel_lieferung_rev._rev)
+        and _deleted = false
+        and sammel_lieferung_id = new.sammel_lieferung_id
+    ),
+    max_depths as (
       select
-        sammel_lieferung_rev.sammel_lieferung_id,
-        sammel_lieferung_rev.art_id,
-        sammel_lieferung_rev.person_id,
-        sammel_lieferung_rev.von_sammlung_id,
-        sammel_lieferung_rev.von_kultur_id,
-        sammel_lieferung_rev.datum,
-        sammel_lieferung_rev.nach_kultur_id,
-        sammel_lieferung_rev.nach_ausgepflanzt,
-        sammel_lieferung_rev.von_anzahl_individuen,
-        sammel_lieferung_rev.anzahl_pflanzen,
-        sammel_lieferung_rev.anzahl_auspflanzbereit,
-        sammel_lieferung_rev.gramm_samen,
-        sammel_lieferung_rev.andere_menge,
-        sammel_lieferung_rev.geplant,
-        sammel_lieferung_rev.bemerkungen,
-        sammel_lieferung_rev.changed,
-        sammel_lieferung_rev.changed_by,
-        sammel_lieferung_rev._rev,
-        sammel_lieferung_rev._revisions,
-        sammel_lieferung_rev._parent_rev,
-        sammel_lieferung_rev._depth,
-        sammel_lieferung_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> sammel_lieferung_rev._rev
-            and _rev <> ANY(sammel_lieferung_rev._revisions)
-        )) as _conflicts
+        max(_depth) as max_depth
       from
-        sammel_lieferung_rev
-        where id = new.id
-      on conflict on constraint sammel_lieferung_pkey do update set
-        -- do not update the id = pkey
-        art_id = excluded.art_id,
-        person_id = excluded.person_id,
-        von_sammlung_id = excluded.von_sammlung_id,
-        von_kultur_id = excluded.von_kultur_id,
-        datum = excluded.datum,
-        nach_kultur_id = excluded.nach_kultur_id,
-        nach_ausgepflanzt = excluded.nach_ausgepflanzt,
-        von_anzahl_individuen = excluded.von_anzahl_individuen,
-        anzahl_pflanzen = excluded.anzahl_pflanzen,
-        anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
-        gramm_samen = excluded.gramm_samen,
-        andere_menge = excluded.andere_menge,
-        geplant = excluded.geplant,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      sammel_lieferung_rev.sammel_lieferung_id,
+      sammel_lieferung_rev.art_id,
+      sammel_lieferung_rev.person_id,
+      sammel_lieferung_rev.von_sammlung_id,
+      sammel_lieferung_rev.von_kultur_id,
+      sammel_lieferung_rev.datum,
+      sammel_lieferung_rev.nach_kultur_id,
+      sammel_lieferung_rev.nach_ausgepflanzt,
+      sammel_lieferung_rev.von_anzahl_individuen,
+      sammel_lieferung_rev.anzahl_pflanzen,
+      sammel_lieferung_rev.anzahl_auspflanzbereit,
+      sammel_lieferung_rev.gramm_samen,
+      sammel_lieferung_rev.andere_menge,
+      sammel_lieferung_rev.geplant,
+      sammel_lieferung_rev.bemerkungen,
+      sammel_lieferung_rev.changed,
+      sammel_lieferung_rev.changed_by,
+      sammel_lieferung_rev._rev,
+      sammel_lieferung_rev._revisions,
+      sammel_lieferung_rev._parent_rev,
+      sammel_lieferung_rev._depth,
+      sammel_lieferung_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> sammel_lieferung_rev._rev
+          and _rev <> ANY(sammel_lieferung_rev._revisions)
+      )) as _conflicts
+    from
+      sammel_lieferung_rev
+      join winning_revisions on sammel_lieferung_rev._rev = winning_revisions._rev
+    on conflict on constraint sammel_lieferung_pkey do update set
+      -- do not update the id = pkey
+      art_id = excluded.art_id,
+      person_id = excluded.person_id,
+      von_sammlung_id = excluded.von_sammlung_id,
+      von_kultur_id = excluded.von_kultur_id,
+      datum = excluded.datum,
+      nach_kultur_id = excluded.nach_kultur_id,
+      nach_ausgepflanzt = excluded.nach_ausgepflanzt,
+      von_anzahl_individuen = excluded.von_anzahl_individuen,
+      anzahl_pflanzen = excluded.anzahl_pflanzen,
+      anzahl_auspflanzbereit = excluded.anzahl_auspflanzbereit,
+      gramm_samen = excluded.gramm_samen,
+      andere_menge = excluded.andere_menge,
+      geplant = excluded.geplant,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into sammel_lieferung (
         id,
         art_id,
@@ -148,7 +196,7 @@ begin
           where
             t.sammel_lieferung_id = new.sammel_lieferung_id
             and t._parent_rev = sammel_lieferung_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and sammel_lieferung_id = new.sammel_lieferung_id
       ),
       max_depths as (
