ce41e3c3dba32e79c40154de1db348dcca6da43a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-06 13:14:52 +0200

    work on conflicts

--- src/sql/createRevTriggers_herkunft.sql
+++ src/sql/createRevTriggers_herkunft.sql
@@ -2,91 +2,140 @@ create or replace function herkunft_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into herkunft (
-        id,
-        nr,
-        lokalname,
-        gemeinde,
-        kanton,
-        land,
-        geom_point,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
-      select
-        herkunft_id,
-        _rev,
-        _depth
-      from
-        herkunft_rev
-      where
-        not exists (
-          select
-            herkunft_id
-          from
-            herkunft_rev as t
-          where
-            t.herkunft_id = new.herkunft_id
-            and t._parent_rev = herkunft_rev._rev
-        )
-        and _deleted = false
-        and herkunft_id = new.herkunft_id
+    select
+      herkunft_id,
+      _rev,
+      _depth
+    from
+      herkunft_rev
+    where
+      not exists (
+        select
+          herkunft_id
+        from
+          herkunft_rev as t
+        where
+          t.herkunft_id = new.herkunft_id
+          and t._parent_rev = herkunft_rev._rev
       )
+      and _deleted = false
+      and herkunft_id = new.herkunft_id
+    ),
+    max_depths as (
       select
-        herkunft_rev.herkunft_id,
-        herkunft_rev.nr,
-        herkunft_rev.lokalname,
-        herkunft_rev.gemeinde,
-        herkunft_rev.kanton,
-        herkunft_rev.land,
-        herkunft_rev.geom_point,
-        herkunft_rev.bemerkungen,
-        herkunft_rev.changed,
-        herkunft_rev.changed_by,
-        herkunft_rev._rev,
-        herkunft_rev._revisions,
-        herkunft_rev._parent_rev,
-        herkunft_rev._depth,
-        herkunft_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> herkunft_rev._rev
-            and _rev <> ANY(herkunft_rev._revisions)
-        )) as _conflicts
+        max(_depth) as max_depth
       from
-        herkunft_rev
-        where id = new.id
-      on conflict on constraint herkunft_pkey do update set
-        -- do not update the id = pkey
-        nr = excluded.nr,
-        lokalname = excluded.lokalname,
-        gemeinde = excluded.gemeinde,
-        kanton = excluded.kanton,
-        land = excluded.land,
-        geom_point = excluded.geom_point,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from herkunft_rev
+    join winning_revisions on herkunft_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into herkunft (
+      id,
+      nr,
+      lokalname,
+      gemeinde,
+      kanton,
+      land,
+      geom_point,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      herkunft_id,
+      _rev,
+      _depth
+    from
+      herkunft_rev
+    where
+      not exists (
+        select
+          herkunft_id
+        from
+          herkunft_rev as t
+        where
+          t.herkunft_id = new.herkunft_id
+          and t._parent_rev = herkunft_rev._rev
+      )
+      and _deleted = false
+      and herkunft_id = new.herkunft_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      herkunft_rev.herkunft_id,
+      herkunft_rev.nr,
+      herkunft_rev.lokalname,
+      herkunft_rev.gemeinde,
+      herkunft_rev.kanton,
+      herkunft_rev.land,
+      herkunft_rev.geom_point,
+      herkunft_rev.bemerkungen,
+      herkunft_rev.changed,
+      herkunft_rev.changed_by,
+      herkunft_rev._rev,
+      herkunft_rev._revisions,
+      herkunft_rev._parent_rev,
+      herkunft_rev._depth,
+      herkunft_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> herkunft_rev._rev
+          and _rev <> ANY(herkunft_rev._revisions)
+      )) as _conflicts
+    from
+      herkunft_rev
+      join winning_revisions on herkunft_rev._rev = winning_revisions._rev
+    on conflict on constraint herkunft_pkey do update set
+      -- do not update the id = pkey
+      nr = excluded.nr,
+      lokalname = excluded.lokalname,
+      gemeinde = excluded.gemeinde,
+      kanton = excluded.kanton,
+      land = excluded.land,
+      geom_point = excluded.geom_point,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into herkunft (
         id,
         nr,
@@ -122,7 +171,7 @@ begin
             t.herkunft_id = new.herkunft_id
             and t._parent_rev = herkunft_rev._rev
         )
-        and _deleted = false
+        --and _deleted = false
         and herkunft_id = new.herkunft_id
       ),
       max_depths as (
--- src/sql/createRevTriggers_kultur_option.sql
+++ src/sql/createRevTriggers_kultur_option.sql
@@ -2,99 +2,147 @@ create or replace function kultur_option_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into kultur_option (
-        id,
-        z_bemerkungen,
-        tz_teilkultur_id,
-        tz_anzahl_mutterpflanzen,
-        tz_andere_menge,
-        tz_auspflanzbereit_beschreibung,
-        tz_bemerkungen,
-        tk,
-        tk_bemerkungen,
-        ev_teilkultur_id,
-        ev_geplant,
-        ev_person_id,
-        ev_datum,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
+    select
+      kultur_id,
+      _rev,
+      _depth
+    from
+      kultur_option_rev
+    where
+      not exists (
+        select
+          kultur_id
+        from
+          kultur_option_rev as t
+        where
+          t.kultur_id = new.kultur_id
+          and t._parent_rev = kultur_option_rev._rev)
+        and _deleted = false
+        and kultur_id = new.kultur_id
+    ),
+    max_depths as (
       select
-        kultur_id,
-        _rev,
-        _depth
+        max(_depth) as max_depth
       from
-        kultur_option_rev
-      where
-        not exists (
-          select
-            kultur_id
-          from
-            kultur_option_rev as t
-          where
-            t.kultur_id = new.kultur_id
-            and t._parent_rev = kultur_option_rev._rev)
-          and _deleted = false
-          and kultur_id = new.kultur_id
-      )
+        leaves
+    ),
+    winning_revisions as (
       select
-        kultur_option_rev.kultur_id,
-        kultur_option_rev.z_bemerkungen,
-        kultur_option_rev.tz_teilkultur_id,
-        kultur_option_rev.tz_anzahl_mutterpflanzen,
-        kultur_option_rev.tz_andere_menge,
-        kultur_option_rev.tz_auspflanzbereit_beschreibung,
-        kultur_option_rev.tz_bemerkungen,
-        kultur_option_rev.tk,
-        kultur_option_rev.tk_bemerkungen,
-        kultur_option_rev.ev_teilkultur_id,
-        kultur_option_rev.ev_geplant,
-        kultur_option_rev.ev_person_id,
-        kultur_option_rev.ev_datum,
-        kultur_option_rev._rev,
-        kultur_option_rev._revisions,
-        kultur_option_rev._parent_rev,
-        kultur_option_rev._depth,
-        kultur_option_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> kultur_option_rev._rev
-            and _rev <> ANY(kultur_option_rev._revisions)
-        )) as _conflicts
+        max(leaves._rev) as _rev
       from
-        kultur_option_rev
-        where id = new.id
-      on conflict on constraint kultur_option_kultur_id_key do update set
-        -- do not update the id = pkey
-        z_bemerkungen = excluded.z_bemerkungen,
-        tz_teilkultur_id = excluded.tz_teilkultur_id,
-        tz_anzahl_mutterpflanzen = excluded.tz_anzahl_mutterpflanzen,
-        tz_andere_menge = excluded.tz_andere_menge,
-        tz_auspflanzbereit_beschreibung = excluded.tz_auspflanzbereit_beschreibung,
-        tz_bemerkungen = excluded.tz_bemerkungen,
-        tk = excluded.tk,
-        tk_bemerkungen = excluded.tk_bemerkungen,
-        ev_teilkultur_id = excluded.ev_teilkultur_id,
-        ev_geplant = excluded.ev_geplant,
-        ev_person_id = excluded.ev_person_id,
-        ev_datum = excluded.ev_datum,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from kultur_option_rev
+    join winning_revisions on kultur_option_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into kultur_option (
+      id,
+      z_bemerkungen,
+      tz_teilkultur_id,
+      tz_anzahl_mutterpflanzen,
+      tz_andere_menge,
+      tz_auspflanzbereit_beschreibung,
+      tz_bemerkungen,
+      tk,
+      tk_bemerkungen,
+      ev_teilkultur_id,
+      ev_geplant,
+      ev_person_id,
+      ev_datum,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      kultur_id,
+      _rev,
+      _depth
+    from
+      kultur_option_rev
+    where
+      not exists (
+        select
+          kultur_id
+        from
+          kultur_option_rev as t
+        where
+          t.kultur_id = new.kultur_id
+          and t._parent_rev = kultur_option_rev._rev)
+        and _deleted = false
+        and kultur_id = new.kultur_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      kultur_option_rev.kultur_id,
+      kultur_option_rev.z_bemerkungen,
+      kultur_option_rev.tz_teilkultur_id,
+      kultur_option_rev.tz_anzahl_mutterpflanzen,
+      kultur_option_rev.tz_andere_menge,
+      kultur_option_rev.tz_auspflanzbereit_beschreibung,
+      kultur_option_rev.tz_bemerkungen,
+      kultur_option_rev.tk,
+      kultur_option_rev.tk_bemerkungen,
+      kultur_option_rev.ev_teilkultur_id,
+      kultur_option_rev.ev_geplant,
+      kultur_option_rev.ev_person_id,
+      kultur_option_rev.ev_datum,
+      kultur_option_rev._rev,
+      kultur_option_rev._revisions,
+      kultur_option_rev._parent_rev,
+      kultur_option_rev._depth,
+      kultur_option_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> kultur_option_rev._rev
+          and _rev <> ANY(kultur_option_rev._revisions)
+      )) as _conflicts
+    from
+      kultur_option_rev
+      join winning_revisions on kultur_option_rev._rev = winning_revisions._rev
+    on conflict on constraint kultur_option_kultur_id_key do update set
+      -- do not update the id = pkey
+      z_bemerkungen = excluded.z_bemerkungen,
+      tz_teilkultur_id = excluded.tz_teilkultur_id,
+      tz_anzahl_mutterpflanzen = excluded.tz_anzahl_mutterpflanzen,
+      tz_andere_menge = excluded.tz_andere_menge,
+      tz_auspflanzbereit_beschreibung = excluded.tz_auspflanzbereit_beschreibung,
+      tz_bemerkungen = excluded.tz_bemerkungen,
+      tk = excluded.tk,
+      tk_bemerkungen = excluded.tk_bemerkungen,
+      ev_teilkultur_id = excluded.ev_teilkultur_id,
+      ev_geplant = excluded.ev_geplant,
+      ev_person_id = excluded.ev_person_id,
+      ev_datum = excluded.ev_datum,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into kultur_option (
         id,
         z_bemerkungen,
@@ -132,7 +180,7 @@ begin
           where
             t.kultur_id = new.kultur_id
             and t._parent_rev = kultur_option_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and kultur_id = new.kultur_id
       ),
       max_depths as (
