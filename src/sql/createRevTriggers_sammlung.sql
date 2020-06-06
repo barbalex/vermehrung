67f1002e6a940056f83cfe8d1d1076e8e4fb22f7
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-06 13:14:36 +0200

    work on conflicts

--- src/sql/createRevTriggers_sammlung.sql
+++ src/sql/createRevTriggers_sammlung.sql
@@ -2,82 +2,129 @@ create or replace function sammlung_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into sammlung (
-        id,
-        art_id,
-        person_id,
-        herkunft_id,
-        nr,
-        datum,
-        von_anzahl_individuen,
-        anzahl_pflanzen,
-        gramm_samen,
-        andere_menge,
-        geom_point,
-        geplant,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-        _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
+    select
+      sammlung_id,
+      _rev,
+      _depth
+    from
+      sammlung_rev
+    where
+      not exists (
+        select
+          sammlung_id
+        from
+          sammlung_rev as t
+        where
+          t.sammlung_id = new.sammlung_id
+          and t._parent_rev = sammlung_rev._rev)
+        and _deleted = false
+        and sammlung_id = new.sammlung_id
+    ),
+    max_depths as (
       select
-        sammlung_id,
-        _rev,
-        _depth
+        max(_depth) as max_depth
       from
-        sammlung_rev
-      where
-        not exists (
-          select
-            sammlung_id
-          from
-            sammlung_rev as t
-          where
-            t.sammlung_id = new.sammlung_id
-            and t._parent_rev = sammlung_rev._rev)
-          and _deleted = false
-          and sammlung_id = new.sammlung_id
-      )
+        leaves
+    ),
+    winning_revisions as (
       select
-        sammlung_rev.sammlung_id,
-        sammlung_rev.art_id,
-        sammlung_rev.person_id,
-        sammlung_rev.herkunft_id,
-        sammlung_rev.nr,
-        sammlung_rev.datum,
-        sammlung_rev.von_anzahl_individuen,
-        sammlung_rev.anzahl_pflanzen,
-        sammlung_rev.gramm_samen,
-        sammlung_rev.andere_menge,
-        sammlung_rev.geom_point,
-        sammlung_rev.geplant,
-        sammlung_rev.bemerkungen,
-        sammlung_rev.changed,
-        sammlung_rev.changed_by,
-        sammlung_rev._rev,
-        sammlung_rev._revisions,
-        sammlung_rev._parent_rev,
-        sammlung_rev._depth,
-        sammlung_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> sammlung_rev._rev
-            and _rev <> ANY(sammlung_rev._revisions)
-        )) as _conflicts
+        max(leaves._rev) as _rev
       from
-        sammlung_rev
-        where id = new.id
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select * from sammlung_rev
+    join winning_revisions on sammlung_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into sammlung (
+      id,
+      art_id,
+      person_id,
+      herkunft_id,
+      nr,
+      datum,
+      von_anzahl_individuen,
+      anzahl_pflanzen,
+      gramm_samen,
+      andere_menge,
+      geom_point,
+      geplant,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+      _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      sammlung_id,
+      _rev,
+      _depth
+    from
+      sammlung_rev
+    where
+      not exists (
+        select
+          sammlung_id
+        from
+          sammlung_rev as t
+        where
+          t.sammlung_id = new.sammlung_id
+          and t._parent_rev = sammlung_rev._rev)
+        and _deleted = false
+        and sammlung_id = new.sammlung_id
+    ),
+    max_depths as (
+      select
+        max(_depth) as max_depth
+      from
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      sammlung_rev.sammlung_id,
+      sammlung_rev.art_id,
+      sammlung_rev.person_id,
+      sammlung_rev.herkunft_id,
+      sammlung_rev.nr,
+      sammlung_rev.datum,
+      sammlung_rev.von_anzahl_individuen,
+      sammlung_rev.anzahl_pflanzen,
+      sammlung_rev.gramm_samen,
+      sammlung_rev.andere_menge,
+      sammlung_rev.geom_point,
+      sammlung_rev.geplant,
+      sammlung_rev.bemerkungen,
+      sammlung_rev.changed,
+      sammlung_rev.changed_by,
+      sammlung_rev._rev,
+      sammlung_rev._revisions,
+      sammlung_rev._parent_rev,
+      sammlung_rev._depth,
+      sammlung_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> sammlung_rev._rev
+          and _rev <> ANY(sammlung_rev._revisions)
+      )) as _conflicts
+    from
+      sammlung_rev
+      join winning_revisions on sammlung_rev._rev = winning_revisions._rev
       on conflict on constraint sammlung_pkey do update set
         -- do not update the id = pkey
         art_id = excluded.art_id,
@@ -101,6 +148,7 @@ begin
         _deleted = excluded._deleted,
         _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into sammlung (
         id,
         art_id,
@@ -140,7 +188,7 @@ begin
           where
             t.sammlung_id = new.sammlung_id
             and t._parent_rev = sammlung_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and sammlung_id = new.sammlung_id
       ),
       max_depths as (
--- src/sql/createRevTriggers_zaehlung.sql
+++ src/sql/createRevTriggers_zaehlung.sql
@@ -2,25 +2,8 @@ create or replace function zaehlung_rev_set_winning_revision ()
   returns trigger
   as $body$
 begin
-  -- if is deletion:
-  -- do not search for winner
-  -- insert deletion as winner but list conflicts
-  if new._deleted is true then
-    insert into zaehlung (
-        id,
-        kultur_id,
-        datum,
-        prognose,
-        bemerkungen,
-        changed,
-        changed_by,
-        _rev,
-        _revisions,
-        _parent_rev,
-        _depth,
-          _deleted,
-        _conflicts
-    )
+  -- 1. check if non deleted winner exists
+  if exists(
     with leaves as (
       select
         zaehlung_id,
@@ -39,44 +22,109 @@ begin
             and t._parent_rev = zaehlung_rev._rev)
           and _deleted = false
           and zaehlung_id = new.zaehlung_id
+      ),
+      max_depths as (
+        select
+          max(_depth) as max_depth
+        from
+          leaves
+      ),
+      winning_revisions as (
+        select
+          max(leaves._rev) as _rev
+        from
+          leaves
+          join max_depths on leaves._depth = max_depths.max_depth
       )
+      select * from zaehlung_rev
+      join winning_revisions on zaehlung_rev._rev = winning_revisions._rev
+  ) then
+    -- 2. insert winner of non deleted datasets
+    insert into zaehlung (
+      id,
+      kultur_id,
+      datum,
+      prognose,
+      bemerkungen,
+      changed,
+      changed_by,
+      _rev,
+      _revisions,
+      _parent_rev,
+      _depth,
+        _deleted,
+      _conflicts
+  )
+  with leaves as (
+    select
+      zaehlung_id,
+      _rev,
+      _depth
+    from
+      zaehlung_rev
+    where
+      not exists (
+        select
+          zaehlung_id
+        from
+          zaehlung_rev as t
+        where
+          t.zaehlung_id = new.zaehlung_id
+          and t._parent_rev = zaehlung_rev._rev)
+        and _deleted = false
+        and zaehlung_id = new.zaehlung_id
+    ),
+    max_depths as (
       select
-        zaehlung_rev.zaehlung_id,
-        zaehlung_rev.kultur_id,
-        zaehlung_rev.datum,
-        zaehlung_rev.prognose,
-        zaehlung_rev.bemerkungen,
-        zaehlung_rev.changed,
-        zaehlung_rev.changed_by,
-        zaehlung_rev._rev,
-        zaehlung_rev._revisions,
-        zaehlung_rev._parent_rev,
-        zaehlung_rev._depth,
-        zaehlung_rev._deleted,
-        (select array(
-          select _rev from leaves
-          where 
-            _rev <> zaehlung_rev._rev
-            and _rev <> ANY(zaehlung_rev._revisions)
-        )) as _conflicts
+        max(_depth) as max_depth
       from
-        zaehlung_rev
-        where id = new.id
-      on conflict on constraint zaehlung_pkey do update set
-        -- do not update the id = pkey
-        kultur_id = excluded.kultur_id,
-        datum = excluded.datum,
-        prognose = excluded.prognose,
-        bemerkungen = excluded.bemerkungen,
-        changed = excluded.changed,
-        changed_by = excluded.changed_by,
-        _rev = excluded._rev,
-        _revisions = excluded._revisions,
-        _parent_rev = excluded._parent_rev,
-        _depth = excluded._depth,
-        _deleted = excluded._deleted,
-        _conflicts = excluded._conflicts;
+        leaves
+    ),
+    winning_revisions as (
+      select
+        max(leaves._rev) as _rev
+      from
+        leaves
+        join max_depths on leaves._depth = max_depths.max_depth
+    )
+    select
+      zaehlung_rev.zaehlung_id,
+      zaehlung_rev.kultur_id,
+      zaehlung_rev.datum,
+      zaehlung_rev.prognose,
+      zaehlung_rev.bemerkungen,
+      zaehlung_rev.changed,
+      zaehlung_rev.changed_by,
+      zaehlung_rev._rev,
+      zaehlung_rev._revisions,
+      zaehlung_rev._parent_rev,
+      zaehlung_rev._depth,
+      zaehlung_rev._deleted,
+      (select array(
+        select _rev from leaves
+        where 
+          _rev <> zaehlung_rev._rev
+          and _rev <> ANY(zaehlung_rev._revisions)
+      )) as _conflicts
+    from
+      zaehlung_rev
+      join winning_revisions on zaehlung_rev._rev = winning_revisions._rev
+    on conflict on constraint zaehlung_pkey do update set
+      -- do not update the id = pkey
+      kultur_id = excluded.kultur_id,
+      datum = excluded.datum,
+      prognose = excluded.prognose,
+      bemerkungen = excluded.bemerkungen,
+      changed = excluded.changed,
+      changed_by = excluded.changed_by,
+      _rev = excluded._rev,
+      _revisions = excluded._revisions,
+      _parent_rev = excluded._parent_rev,
+      _depth = excluded._depth,
+      _deleted = excluded._deleted,
+      _conflicts = excluded._conflicts;
   else
+    -- 3. insert winner of deleted datasets
     insert into zaehlung (
         id,
         kultur_id,
@@ -108,7 +156,7 @@ begin
           where
             t.zaehlung_id = new.zaehlung_id
             and t._parent_rev = zaehlung_rev._rev)
-          and _deleted = false
+          --and _deleted = false
           and zaehlung_id = new.zaehlung_id
       ),
       max_depths as (
