c945dad7dc7be963f7d9856c1b9568413592dfc1
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-07 17:32:21 +0200

    optimize

--- src/components/Data/Sammlung/DeleteButton.js
+++ src/components/Data/Sammlung/DeleteButton.js
@@ -23,6 +23,7 @@ const Title = styled.div`
 
 const SammlungDeleteButton = ({ row }) => {
   const store = useContext(StoreContext)
+  const { showDeleted } = store
   const { activeNodeArray, setActiveNodeArray } = store.tree
 
   const [anchorEl, setAnchorEl] = useState(null)
@@ -30,14 +31,15 @@ const SammlungDeleteButton = ({ row }) => {
     setAnchorEl(null)
   }, [])
 
-  const onClickDelete = useCallback(
+  const conClickButton = useCallback(
     (event) => setAnchorEl(event.currentTarget),
     [],
   )
   const remove = useCallback(() => {
     row.delete()
-    setActiveNodeArray(activeNodeArray.slice(0, -1))
-  }, [activeNodeArray, row, setActiveNodeArray])
+    !showDeleted && setActiveNodeArray(activeNodeArray.slice(0, -1))
+    setAnchorEl(null)
+  }, [activeNodeArray, row, setActiveNodeArray, showDeleted])
 
   return (
     <ErrorBoundary>
@@ -46,7 +48,8 @@ const SammlungDeleteButton = ({ row }) => {
         aria-haspopup="true"
         aria-label="Sammlung löschen"
         title="Sammlung löschen"
-        onClick={onClickDelete}
+        onClick={conClickButton}
+        disabled={row._deleted}
       >
         <FaMinus />
       </IconButton>
