59429bac1aaf3c6f8c44d290b94b3c24ce0d2351
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-08 19:43:27 +0200

    enhance

--- src/components/TreeContainer/Tree/delete.js
+++ src/components/TreeContainer/Tree/delete.js
@@ -1,7 +1,7 @@
 import tableFromTitleHash from '../../../utils/tableFromTitleHash'
 
 export default async ({ node, store }) => {
-  const { activeNodeArray, setActiveNodeArray } = store.tree
+  const { activeNodeArray, setActiveNodeArray, removeOpenNode } = store.tree
   // get table and id from url
   const title = node.url.slice(-2)[0]
   const id = node.url.slice(-1)[0]
@@ -11,4 +11,6 @@ export default async ({ node, store }) => {
   if (!me?.delete) throw new Error(`Kein Modell für Tabelle ${table} gefunden`)
   me.delete()
   setActiveNodeArray(activeNodeArray.slice(0, -1))
+  // TODO: need to remove openNode from openNodes
+  removeOpenNode(node.url)
 }
