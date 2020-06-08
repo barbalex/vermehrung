d313eff841b48484e87c1ca18c41a8cfe94563f8
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-08 14:28:47 +0200

    use mst

---
+++ src/utils/qkSort.js
@@ -0,0 +1,18 @@
+export default (a, b) => {
+  const sortA = a?.sort
+  const sortB = b?.sort
+  if (sortA < sortB) return -1
+  if (sortA > sortB) return 1
+
+  const titelA = a?.titel?.toString()?.toLowerCase()
+  const titelB = b?.titel?.toString()?.toLowerCase()
+  if (titelA < titelB) return -1
+  if (titelA > titelB) return 1
+
+  const nameA = a?.name?.toString()?.toLowerCase()
+  const nameB = b?.name?.toString()?.toLowerCase()
+  if (nameA < nameB) return -1
+  if (nameA > nameB) return 1
+
+  return 0
+}
