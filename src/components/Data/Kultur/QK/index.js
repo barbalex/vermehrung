da186c7be78975b1baafc73ef8e3f78e54919e97
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-11 18:22:24 +0200

    use subs

--- src/components/Data/Kultur/QK/index.js
+++ src/components/Data/Kultur/QK/index.js
@@ -6,17 +6,12 @@ import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 import IconButton from '@material-ui/core/IconButton'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
-import gql from 'graphql-tag'
 
-import { useQuery, StoreContext } from '../../../../models/reactUtils'
+import { StoreContext } from '../../../../models/reactUtils'
 import Qk from './Qk'
 import Choose from './Choose'
 import appBaseUrl from '../../../../utils/appBaseUrl'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
-import {
-  kulturQk as kulturQkFragment,
-  kulturQkChoosen as kulturQkChoosenFragment,
-} from '../../../../utils/fragments'
 
 const TitleRow = styled.div`
   background-color: rgba(237, 230, 244, 1);
@@ -49,19 +44,6 @@ const Body = styled.div`
   padding: 10px 0;
 `
 
-const allDataQuery = gql`
-  query AllDataQueryForKulturQk($kulturId: uuid!) {
-    kultur_qk {
-      ...KulturQkFields
-    }
-    kultur_qk_choosen(where: { kultur_id: { _eq: $kulturId } }) {
-      ...KulturQkChoosenFields
-    }
-  }
-  ${kulturQkFragment}
-  ${kulturQkChoosenFragment}
-`
-
 const KulturQk = ({ kultur }) => {
   const store = useContext(StoreContext)
   const { kulturQksSorted } = store
@@ -70,11 +52,6 @@ const KulturQk = ({ kultur }) => {
   const [tab, setTab] = useState('qk')
   const onChangeTab = useCallback((event, value) => setTab(value), [])
 
-  const { loading, error } = useQuery(
-    allDataQuery,
-    { variables: { kulturId: kultur.id } },
-    { fetchPolicy: 'no-cache' },
-  )
   const kulturQkChoosen = [...store.kultur_qk_choosens.values()].filter(
     (q) => q.kultur_id === kultur.id,
   )
@@ -89,8 +66,8 @@ const KulturQk = ({ kultur }) => {
     ]),
   )
 
-  const qkCount = loading ? '...' : kulturQksSorted.length
-  const kulturQkCount = loading ? '...' : kulturQkChoosen.length
+  const qkCount = kulturQksSorted.length
+  const kulturQkCount = kulturQkChoosen.length
 
   const openDocs = useCallback((e) => {
     e.stopPropagation()
@@ -110,7 +87,6 @@ const KulturQk = ({ kultur }) => {
     [open],
   )
 
-  if (error) return `Fehler: ${error.message}`
   return (
     <ErrorBoundary>
       <TitleRow onClick={onClickToggle} title={open ? 'schliessen' : 'öffnen'}>
