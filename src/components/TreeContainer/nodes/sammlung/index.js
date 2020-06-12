e32ed99ac6bfdfa378117386e737bd7afd1ae060
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-13 00:00:42 +0200

    build live relations

--- src/components/TreeContainer/nodes/sammlung/index.js
+++ src/components/TreeContainer/nodes/sammlung/index.js
@@ -2,7 +2,7 @@ import isEqual from 'lodash/isEqual'
 import moment from 'moment'
 
 export default ({ store }) => {
-  const { personsSorted } = store
+  const { personsSorted, herkunftsSorted, artsSorted, aeArtsSorted } = store
   const { showSammlung, visibleOpenNodes } = store.tree
 
   if (!showSammlung) return []
@@ -15,19 +15,24 @@ export default ({ store }) => {
       )
       .map((el) => {
         const { datum } = el
-        const art = el?.art?.art_ae_art?.name ?? '(keine Art)'
+        const art = artsSorted.find((a) => a.id === el.art_id)
+        const aeArt = aeArtsSorted.find((ae) => ae.id === art?.ae_id)
+        const artName = aeArt ? aeArt?.name ?? '(Art ohne Name)' : '(keine Art)'
         const person = personsSorted.find((p) => p.id === el.person_id)
         const personName = person
           ? person?.name ?? '(Person ohne Name)'
           : '(keine Person)'
-        const herkunft = el?.herkunft?.nr ?? '(keine Herkunft-Nr)'
+        const herkunft = herkunftsSorted.find((h) => h.id === el.herkunft_id)
+        const herkunftNr = herkunft
+          ? herkunft?.nr ?? '(Herkunft ohne Nr)'
+          : '(keine Herkunft)'
         const date = datum
           ? moment(datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
           : 'kein Datum'
         const geplant = el.geplant ? ' (geplant)' : ''
         const label = `${
           el.nr ?? '(keine Nr)'
-        }, ${date}: Herkunft ${herkunft}, ${personName}; ${art}${geplant}`
+        }, ${date}: Herkunft ${herkunftNr}, ${personName}; ${artName}${geplant}`
 
         return {
           nodeType: 'table',
