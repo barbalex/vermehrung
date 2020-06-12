4aea1de900b3d3c2b2c4bc7bd1a984b8d4a87fda
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-12 14:24:50 +0200

    rename

--- src/utils/checkAuthOnError.js
+++ src/utils/checkAuthOnError.js
@@ -2,7 +2,7 @@ import getAuthToken from './getAuthToken'
 
 export default ({ error, store }) => {
   if (error.message.includes('JWTExpired')) {
-    console.log('TreeContainer, JWT expired, will re-set hasura claims')
+    console.log('TreeContainer, JWT expired, will re-set')
     getAuthToken({ store })
   }
 }
