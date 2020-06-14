32a6b5cfda0972ee8084060daa50287e65cdc976
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-14 12:39:03 +0200

    work on open nodes check

--- src/components/Tree/isNodeOpen.js
+++ src/components/Tree/isNodeOpen.js
@@ -1,25 +1,9 @@
 import isEqual from 'lodash/isEqual'
-//import { getSnapshot } from 'mobx-state-tree'
-
-import artIdInUrl from '../../utils/artIdInUrl'
-import herkunftIdInUrl from '../../utils/herkunftIdInUrl'
-import gartenIdInUrl from '../../utils/gartenIdInUrl'
-import kulturIdInUrl from '../../utils/kulturIdInUrl'
-import anLieferungIdInUrl from '../../utils/anLieferungIdInUrl'
-import ausLieferungIdInUrl from '../../utils/ausLieferungIdInUrl'
-import teilkulturIdInUrl from '../../utils/teilkulturIdInUrl'
-import personIdInUrl from '../../utils/personIdInUrl'
-import sammelLieferungIdInUrl from '../../utils/sammelLieferungIdInUrl'
-import sammlungIdInUrl from '../../utils/sammlungIdInUrl'
-import kulturIdOfAnLieferungInUrl from '../../utils/kulturIdOfAnLieferungInUrl'
-import kulturIdOfAusLieferungInUrl from '../../utils/kulturIdOfAusLieferungInUrl'
-import zaehlungIdInUrl from '../../utils/zaehlungIdInUrl'
 
 //export default (openNodes, url) => {
 export default ({ store, url }) => {
-  const { openNodes } = store.tree
+  const { openNodesNotFiltered } = store.tree
   if (!url) return false
-  //console.log('isNodeOpen', { openNodes: getSnapshot(openNodes), url })
 
-  return openNodes.some((n) => isEqual(n, url))
+  return openNodesNotFiltered.some((n) => isEqual(n, url))
 }
