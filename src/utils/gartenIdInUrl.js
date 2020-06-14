5936955ea23a1c7941869ab6c5fd37fce9e83a99
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-14 11:57:36 +0200

    prepare open nodes check

---
+++ src/utils/gartenIdInUrl.js
@@ -0,0 +1,12 @@
+import isUuid from 'is-uuid'
+
+export default (url) => {
+  if (url.includes('Gaerten')) {
+    const indexOfId = url.indexOf('Gaerten') + 1
+    if (url.length > indexOfId) {
+      const id = url?.[indexOfId]
+      if (isUuid.v1(id)) return id
+    }
+  }
+  return undefined
+}
