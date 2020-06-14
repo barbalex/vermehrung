aacdff3478c40e510e78b2aa69167b0f226bbd68
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-14 12:04:59 +0200

    prepare open nodes check

---
+++ src/utils/sammlungIdInUrl.js
@@ -0,0 +1,12 @@
+import isUuid from 'is-uuid'
+
+export default (url) => {
+  if (url.includes('Sammlungen')) {
+    const indexOfId = url.indexOf('Sammlungen') + 1
+    if (url.length > indexOfId) {
+      const id = url?.[indexOfId]
+      if (isUuid.v1(id)) return id
+    }
+  }
+  return undefined
+}
