20122fbb925edff18035c1fc09393c5b0f06e72c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-15 10:25:03 +0200

    use mst

--- src/components/Data/Zaehlung/Teilzaehlungen/index.js
+++ src/components/Data/Zaehlung/Teilzaehlungen/index.js
@@ -3,13 +3,11 @@ import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 import IconButton from '@material-ui/core/IconButton'
 import { FaPlus } from 'react-icons/fa'
-import gql from 'graphql-tag'
 
-import { useQuery, StoreContext } from '../../../../models/reactUtils'
+import { StoreContext } from '../../../../models/reactUtils'
 import TeilzaehlungenRows from './TeilzaehlungenRows'
 import Settings from './Settings'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
-import { teilzaehlung as teilzaehlungFragment } from '../../../../utils/fragments'
 
 const Container = styled.div`
   display: flex;
@@ -36,67 +34,16 @@ const Title = styled.div`
   margin-bottom: auto;
 `
 
-const allDataQuery = gql`
-  query AllDataQueryForTeilzaehlungs(
-    $teilzaehlungFilter: teilzaehlung_bool_exp!
-    $totalCountFilter: teilzaehlung_bool_exp!
-  ) {
-    teilzaehlung(where: $teilzaehlungFilter) {
-      ...TeilzaehlungFields
-      zaehlung {
-        id
-        __typename
-        kultur {
-          id
-          __typename
-          kultur_option {
-            id
-            __typename
-            tk
-          }
-        }
-      }
-    }
-    teilzaehlung_total_count: teilzaehlung_aggregate(where: $totalCountFilter) {
-      aggregate {
-        count
-      }
-    }
-    teilzaehlung_filtered_count: teilzaehlung_aggregate(
-      where: $teilzaehlungFilter
-    ) {
-      aggregate {
-        count
-      }
-    }
-  }
-  ${teilzaehlungFragment}
-`
-
 const Teilzaehlungen = ({ zaehlungId }) => {
   const store = useContext(StoreContext)
-  const {
-    insertTeilzaehlungRev,
-    teilzaehlungsSorted,
-    deletedTableFilter,
-  } = store
+  const { insertTeilzaehlungRev, teilzaehlungsSorted } = store
 
   const zaehlung = store.zaehlungs.get(zaehlungId) ?? {}
   const kulturId = zaehlung.kultur_id
 
-  const hierarchyFilter = { zaehlung_id: { _eq: zaehlungId } }
-  const totalCountFilter = {
-    ...hierarchyFilter,
-    ...deletedTableFilter,
-  }
-  const { error, loading } = useQuery(allDataQuery, {
-    variables: {
-      teilzaehlungFilter: totalCountFilter,
-      totalCountFilter,
-    },
-  })
+  const hierarchyFilter = (r) => r.zaehlung_id === zaehlungId
 
-  const storeRows = teilzaehlungsSorted
+  const storeRowsFiltered = teilzaehlungsSorted.filter(hierarchyFilter)
 
   const kulturOption = store.kultur_options.get(kulturId) ?? {}
   const { tk } = kulturOption
@@ -105,18 +52,8 @@ const Teilzaehlungen = ({ zaehlungId }) => {
     insertTeilzaehlungRev()
   }, [insertTeilzaehlungRev])
 
-  const showNew = storeRows.length === 0 || tk
+  const showNew = storeRowsFiltered.length === 0 || tk
   const title = tk ? 'Teil-Zählungen' : 'Mengen'
-  const rows = teilzaehlungsSorted.filter((v) => v.zaehlung_id === zaehlungId)
-
-  if (loading && !rows.length) {
-    return <Container>Lade...</Container>
-  }
-  if (error && !error.message.includes('Failed to fetch')) {
-    return (
-      <Container>{`Fehler beim Laden der Daten: ${error.message}`}</Container>
-    )
-  }
 
   return (
     <ErrorBoundary>
