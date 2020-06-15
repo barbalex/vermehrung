142b254902ccdefc46ffc43936dadb34b89d4ed6
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-15 18:12:32 +0200

    use mst

--- src/components/Data/Kultur/Settings.js
+++ src/components/Data/Kultur/Settings.js
@@ -5,11 +5,11 @@ import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Radio from '@material-ui/core/Radio'
-import { FaCog, FaFrown } from 'react-icons/fa'
+import { FaCog } from 'react-icons/fa'
 import { IoMdInformationCircleOutline } from 'react-icons/io'
 import styled from 'styled-components'
 
-import { StoreContext, useQuery } from '../../../models/reactUtils'
+import { StoreContext } from '../../../models/reactUtils'
 import appBaseUrl from '../../../utils/appBaseUrl'
 import ErrorBoundary from '../../shared/ErrorBoundary'
 
@@ -27,11 +27,7 @@ const Info = styled.div`
 
 const SettingsKultur = ({ kulturId }) => {
   const store = useContext(StoreContext)
-  const { addNotification } = store
 
-  const { error, loading } = useQuery((store) =>
-    store.queryKultur_option({ where: { id: { _eq: kulturId } } }),
-  )
   const kulturOption = store.kultur_options.get(kulturId) ?? {}
   const { tk } = kulturOption
 
@@ -43,11 +39,6 @@ const SettingsKultur = ({ kulturId }) => {
     },
     [kulturOption],
   )
-  const onClickFrown = useCallback(() => {
-    addNotification({
-      message: error.message,
-    })
-  }, [addNotification, error])
 
   const [anchorEl, setAnchorEl] = useState(null)
 
@@ -67,19 +58,6 @@ const SettingsKultur = ({ kulturId }) => {
     }
   }, [])
 
-  if (error) {
-    return (
-      <IconButton
-        aria-label="Felder wählen"
-        aria-owns={anchorEl ? 'long-menu' : null}
-        aria-haspopup="true"
-        title={error.message}
-        onClick={onClickFrown}
-      >
-        <FaFrown />
-      </IconButton>
-    )
-  }
   return (
     <ErrorBoundary>
       <IconButton
@@ -91,7 +69,7 @@ const SettingsKultur = ({ kulturId }) => {
       >
         <FaCog />
       </IconButton>
-      {!loading && (
+      {
         <Menu
           id="long-menu"
           anchorEl={anchorEl}
@@ -125,7 +103,7 @@ const SettingsKultur = ({ kulturId }) => {
           </MenuItem>
           <Info>Die Wahl gilt (nur) für diese Kultur.</Info>
         </Menu>
-      )}
+      }
     </ErrorBoundary>
   )
 }
