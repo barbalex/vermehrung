784f48dc52f698f2c4119b8058ed795829731b79
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-18 11:38:50 +0200

    add models

--- src/models/art_qk_choosenModel.js
+++ src/models/art_qk_choosenModel.js
@@ -1,18 +1,82 @@
-import { art_qk_choosenModelBase } from "./art_qk_choosenModel.base"
+import { getParent } from 'mobx-state-tree'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
 
+import { art_qk_choosenModelBase } from './art_qk_choosenModel.base'
+import toPgArray from '../utils/toPgArray'
+import toStringIfPossible from '../utils/toStringIfPossible'
 
 /* A graphql query fragment builders for art_qk_choosenModel */
-export { selectFromart_qk_choosen, art_qk_choosenModelPrimitives, art_qk_choosenModelSelector } from "./art_qk_choosenModel.base"
+export {
+  selectFromart_qk_choosen,
+  art_qk_choosenModelPrimitives,
+  art_qk_choosenModelSelector,
+} from './art_qk_choosenModel.base'
 
 /**
  * art_qk_choosenModel
  *
  * columns and relationships of "art_qk_choosen"
  */
-export const art_qk_choosenModel = art_qk_choosenModelBase
-  .actions(self => ({
-    // This is an auto-generated example action.
-    log() {
-      console.log(JSON.stringify(self))
+export const art_qk_choosenModel = art_qk_choosenModelBase.actions((self) => ({
+  edit({ field, value }) {
+    const store = getParent(self, 2)
+    const { addQueuedQuery, user, upsertArtQkChoosenModel } = store
+
+    // first build the part that will be revisioned
+    const newDepth = self._depth + 1
+    const newObject = {
+      art_qk_choosen_id: self.id,
+      art_id: field === 'art_id' ? value : self.art_id,
+      qk_name: field === 'qk_name' ? toStringIfPossible(value) : self.qk_name,
+      choosen: field === 'choosen' ? value : self.choosen,
+      changed: new window.Date().toISOString(),
+      changed_by: user.email,
+      _parent_rev: self._rev,
+      _depth: newDepth,
+      _deleted: field === '_deleted' ? value : self._deleted,
     }
-  }))
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    // DO NOT include id in rev - or revs with same data will conflict
+    newObject.id = uuidv1()
+    newObject._rev = rev
+    const newObjectForStore = { ...newObject }
+    // convert to string as hasura does not support arrays yet
+    // https://github.com/hasura/graphql-engine/pull/2243
+    newObject._revisions = self._revisions
+      ? toPgArray([rev, ...self._revisions])
+      : toPgArray([rev])
+    addQueuedQuery({
+      name: 'mutateInsert_art_qk_choosen_rev_one',
+      variables: JSON.stringify({
+        object: newObject,
+        on_conflict: {
+          constraint: 'art_qk_choosen_rev_pkey',
+          update_columns: ['id'],
+        },
+      }),
+      callbackQuery: 'queryArt_qk_choosen',
+      callbackQueryVariables: JSON.stringify({
+        where: { id: { _eq: self.id } },
+      }),
+      revertTable: 'art_qk_choosen',
+      revertId: self.id,
+      revertField: field,
+      revertValue: self[field],
+    })
+    // do not stringify revisions for store
+    // as _that_ is a real array
+    newObjectForStore._revisions = self._revisions
+      ? [rev, ...self._revisions]
+      : [rev]
+    newObjectForStore._conflicts = self._conflicts
+    // for store: convert rev to winner
+    newObjectForStore.id = self.id
+    delete newObjectForStore.art_qk_choosen_id
+    // optimistically update store
+    upsertArtQkChoosenModel(newObjectForStore)
+  },
+  delete() {
+    self.edit({ field: '_deleted', value: true })
+  },
+}))
--- src/models/art_qk_choosen_revModel.js
+++ src/models/art_qk_choosen_revModel.js
@@ -1,16 +1,65 @@
-import { art_qk_choosen_revModelBase } from "./art_qk_choosen_revModel.base"
+import { getParent } from 'mobx-state-tree'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
 
+import { art_qk_choosen_revModelBase } from './art_qk_choosen_revModel.base'
+import toPgArray from '../utils/toPgArray'
 
 /* A graphql query fragment builders for art_qk_choosen_revModel */
-export { selectFromart_qk_choosen_rev, art_qk_choosen_revModelPrimitives, art_qk_choosen_revModelSelector } from "./art_qk_choosen_revModel.base"
+export {
+  selectFromart_qk_choosen_rev,
+  art_qk_choosen_revModelPrimitives,
+  art_qk_choosen_revModelSelector,
+} from './art_qk_choosen_revModel.base'
 
 /**
  * art_qk_choosen_revModel
  */
-export const art_qk_choosen_revModel = art_qk_choosen_revModelBase
-  .actions(self => ({
-    // This is an auto-generated example action.
-    log() {
-      console.log(JSON.stringify(self))
-    }
-  }))
+export const art_qk_choosen_revModel = art_qk_choosen_revModelBase.actions(
+  (self) => ({
+    setDeleted() {
+      const store = getParent(self, 2)
+      const { addQueuedQuery, user, deleteArtQkChoosenRevModel } = store
+
+      // build new object
+      const newDepth = self._depth + 1
+      const newObject = {
+        art_qk_choosen_id: self.art_qk_choosen_id,
+        art_id: self.art_id,
+        qk_name: self.qk_name,
+        choosen: self.choosen,
+        changed: new window.Date().toISOString(),
+        changed_by: user.email,
+        _parent_rev: self._rev,
+        _depth: newDepth,
+        _deleted: true,
+      }
+      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+      newObject._rev = rev
+      newObject.id = uuidv1()
+      newObject._revisions = self._revisions
+        ? toPgArray([rev, ...self._revisions])
+        : toPgArray([rev])
+
+      addQueuedQuery({
+        name: 'mutateInsert_art_qk_choosen_rev_one',
+        variables: JSON.stringify({
+          object: newObject,
+          on_conflict: {
+            constraint: 'art_qk_choosen_rev_pkey',
+            update_columns: ['id'],
+          },
+        }),
+        callbackQuery: 'queryArt_qk_choosen',
+        callbackQueryVariables: JSON.stringify({
+          where: { id: { _eq: self.id } },
+        }),
+        revertTable: 'art_qk_choosen',
+        revertId: self.art_qk_choosen_id,
+        revertField: '_deleted',
+        revertValue: false,
+      })
+      deleteArtQkChoosenRevModel(self)
+    },
+  }),
+)
