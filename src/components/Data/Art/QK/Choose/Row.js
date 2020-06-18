8eebf8160244ea25f861ba0134a55128e023ae94
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-18 11:38:30 +0200

    use new structure

--- src/components/Data/Art/QK/Choose/Row.js
+++ src/components/Data/Art/QK/Choose/Row.js
@@ -2,7 +2,6 @@ import React, { useCallback, useContext } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import Checkbox from '@material-ui/core/Checkbox'
-import { v1 as uuidv1 } from 'uuid'
 
 import { StoreContext } from '../../../../../models/reactUtils'
 
@@ -33,32 +32,11 @@ const ChooseArtQkRow = ({ artId, qk }) => {
     (v) => v.art_id === artId && v.qk_name === qk.name,
   )
 
-  const artQkChoosenId = artQkChoosen?.id
-  const checked = !!artQkChoosenId
+  const checked = artQkChoosen.choosen
 
-  const onChange = useCallback(async () => {
-    // 1. if checked, delete artQkChoosen
-    // 2. else create artQkChoosen
-    if (checked) {
-      return store.mutateDelete_art_qk_choosen(
-        {
-          where: {
-            art_id: { _eq: artId },
-            qk_name: { _eq: qk.name },
-          },
-        },
-        undefined,
-        () => store.deleteArtQkChoosenModel({ id: artQkChoosenId }),
-      )
-    }
-    store.mutateInsert_art_qk_choosen_one({
-      object: { art_id: artId, qk_name: qk.name, id: uuidv1() },
-      on_conflict: {
-        constraint: 'art_qk_choosen_pkey',
-        update_columns: ['id'],
-      },
-    })
-  }, [artId, artQkChoosenId, checked, qk.name, store])
+  const onChange = useCallback(() => {
+    qk.edit({ field: 'choosen', value: !checked })
+  }, [checked, qk])
 
   return (
     <Row>
