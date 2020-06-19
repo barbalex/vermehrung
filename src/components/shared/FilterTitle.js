d99f4e8f44bde7d065b9b54a8c7528ab68554806
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-19 18:35:46 +0200

    style

--- src/components/shared/FilterTitle.js
+++ src/components/shared/FilterTitle.js
@@ -9,7 +9,6 @@ import { StoreContext } from '../../models/reactUtils'
 
 const Container = styled.div`
   background-color: #ffe0b2;
-  padding-bottom: 10px;
   flex-shrink: 0;
   display: flex;
   flex-direction: column;
@@ -20,28 +19,21 @@ const Container = styled.div`
 const TitleRow = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   user-select: none;
 `
 const FilterNumbers = styled.div`
-  padding-top: 11px;
   padding-right: 8px;
   cursor: default;
   user-select: none;
 `
-const StyledIconButton = styled(IconButton)`
-  height: 34px !important;
-  width: 34px !important;
-  margin-top: -2px !important;
-`
 const StyledDeleteFilterIcon = styled(DeleteFilterIcon)`
   cursor: pointer;
   pointer-events: auto;
-  /*font-size: 28px !important;*/
 `
 const StyledDeleteFilterIcon2 = styled(DeleteFilterIcon2)`
   cursor: pointer;
   pointer-events: auto;
-  /*font-size: 28px !important;*/
 `
 
 const FilterTitle = ({ title, table, totalNr, filteredNr }) => {
@@ -64,22 +56,24 @@ const FilterTitle = ({ title, table, totalNr, filteredNr }) => {
           <span title="ungefilterte Anzahl">{totalNr}</span>
         </FilterNumbers>
         {existsTableFilter && (
-          <StyledIconButton
+          <IconButton
             aria-label={`${title}-Filter entfernen`}
             title={`${title}-Filter entfernen`}
             onClick={onEmptyTable}
+            size="medium"
           >
             <StyledDeleteFilterIcon2 />
-          </StyledIconButton>
+          </IconButton>
         )}
         {isFiltered && (
-          <StyledIconButton
+          <IconButton
             aria-label="Alle Filter entfernen"
             title="Alle Filter entfernen"
             onClick={empty}
+            size="medium"
           >
             <StyledDeleteFilterIcon />
-          </StyledIconButton>
+          </IconButton>
         )}
       </TitleRow>
     </Container>
