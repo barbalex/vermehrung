fea544440df717b7020c4441292ef07e0564007a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-19 15:16:54 +0200

    reset queryingAllData

--- src/utils/recreatePersistedStore.js
+++ src/utils/recreatePersistedStore.js
@@ -24,6 +24,7 @@ export default async ({ store }) => {
     'gqlHttpClient',
     'gqlWsClient',
     'initialDataQueried',
+    'queryingAllData',
     'gettingAuthUser',
   ]
   await persist('store', store, {
