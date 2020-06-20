c77d71413df2edd83837036c26635ffa182079f7
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-20 11:08:05 +0200

    optimize

---
+++ src/utils/treeLabelFromKultur.js
@@ -0,0 +1,7 @@
+export default (el) => {
+  const garten =
+    el?.garten?.name ?? `(${el?.garten?.person?.name ?? 'kein Name'})`
+  const art = el?.art?.art_ae_art?.name ?? '(keine Art)'
+  const herkunft = el?.herkunft?.nr ?? '(Herkunft ohne Nr)'
+  return `${art}, von: ${herkunft}, in: ${garten}`
+}
