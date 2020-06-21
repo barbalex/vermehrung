2ebb19492bb7de7120e12c9613381b34cbc58459
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-21 17:14:57 +0200

    use fullname

--- src/components/Data/Lieferung/Lieferung/kulturLabelFromKultur.js
+++ src/components/Data/Lieferung/Lieferung/kulturLabelFromKultur.js
@@ -1,5 +1,5 @@
 export default (k) => {
-  const personName = k?.garten?.person?.name ?? '(kein Name)'
+  const personName = k?.garten?.person?.fullname ?? '(kein Name)'
   const personOrt = k?.garten?.person?.ort ?? null
   const personLabel = `${personName}${personOrt ? ` (${personOrt})` : ''}`
   const label = k?.garten?.name ?? personLabel
