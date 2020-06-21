dce051bc89db13d280b7c98b9f2ea31d2909f13a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-21 17:13:30 +0200

    use fullname

--- src/utils/treeLabelKultur.js
+++ src/utils/treeLabelKultur.js
@@ -1,7 +1,8 @@
 export default (el) => {
   const garten =
-    el?.garten?.name ?? `(${el?.garten?.person?.name ?? 'kein Name'})`
+    el?.garten?.name ?? `${el?.garten?.person?.fullname ?? 'kein Name'}`
   const art = el?.art?.art_ae_art?.name ?? '(keine Art)'
   const herkunft = el?.herkunft?.nr ?? '(Herkunft ohne Nr)'
+
   return `${art}, von: ${herkunft}, in: ${garten}`
 }
