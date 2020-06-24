d80a89393ac161da5f3692190759030ffcc492a6
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-24 12:11:26 +0200

    add events list to tk

---
+++ src/components/Data/Teilkultur/Events.js
@@ -0,0 +1,99 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import format from 'date-fns/format'
+
+import { StoreContext } from '../../../models/reactUtils'
+import exists from '../../../utils/exists'
+import ErrorBoundary from '../../shared/ErrorBoundary'
+
+const TitleRow = styled.div`
+  background-color: rgba(237, 230, 244, 1);
+  flex-shrink: 0;
+  display: flex;
+  height: 48px;
+  justify-content: space-between;
+  margin-left: -10px;
+  margin-right: -10px;
+  padding: 0 10px;
+  position: sticky;
+  top: -10px;
+  z-index: 1;
+  user-select: none;
+`
+const Title = styled.div`
+  font-weight: bold;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+const Rows = styled.div`
+  overflow: auto !important;
+`
+const Row = styled.div`
+  border-top: thin solid rgba(74, 20, 140, 0.1);
+  border-bottom: ${(props) => (props['data-last'] ? '1px' : 'thin')} solid
+    rgba(74, 20, 140, 0.1);
+  border-collapse: collapse;
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  &:hover {
+    background-color: rgba(74, 20, 140, 0.03);
+  }
+`
+const Datum = styled.div`
+  flex-basis: 85px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  margin-right: 10px;
+`
+const Name = styled.div`
+  flex-basis: 150px;
+  flex-shrink: 1;
+  flex-grow: 1;
+  margin-right: 10px;
+`
+const Beschreibung = styled.div`
+  flex-basis: 170px;
+  flex-shrink: 5;
+  flex-grow: 5;
+  margin-right: 10px;
+`
+const Geplant = styled.div`
+  flex-basis: 60px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  margin-right: 10px;
+`
+
+const TkEvents = ({ kulturId, teilkulturId }) => {
+  const store = useContext(StoreContext)
+  const { eventsSorted } = store
+
+  // there should only be one ev per teilkultur_id
+  const events = eventsSorted.filter(
+    (ev) => ev?.kultur_id === kulturId && ev.teilkultur_id === teilkulturId,
+  )
+
+  return (
+    <ErrorBoundary>
+      <TitleRow>
+        <Title>Events</Title>
+      </TitleRow>
+      <Rows>
+        {events.map((ev, i) => (
+          <Row key={ev.id} data-last={i === events.length - 1}>
+            <Datum>
+              {ev.datum ? `${format(new Date(ev.datum), 'yyyy.MM.dd')}:` : ''}
+            </Datum>
+            <Geplant>{ev?.geplant ? 'geplant' : ' '}</Geplant>
+            <Name>{ev?.person?.fullname ?? ''}</Name>
+            <Beschreibung>{ev?.beschreibung ?? ''}</Beschreibung>
+          </Row>
+        ))}
+      </Rows>
+    </ErrorBoundary>
+  )
+}
+
+export default TkEvents
