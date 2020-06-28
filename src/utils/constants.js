b40741d7f19fe133eb785b2f0360c1ab7e2344b0
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-28 13:55:17 +0200

    rename

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ export default () => {
   return {
     graphQlUri: 'https://api.vermehrung.ch/v1/graphql',
     graphQlWsUri: 'wss://api.vermehrung.ch/v1/graphql',
-    heaslthUri: 'https://api.vermehrung.ch/healthz',
+    healthUri: 'https://api.vermehrung.ch/healthz',
     authUri: 'https://auth.vermehrung.ch',
     appUri: isLocalhost ? `http://${hostname}:8000` : 'https://vermehrung.ch',
     testArten: ['1ab6bbb1-979a-4232-a5d8-62efb5cb984a'],
