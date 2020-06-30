1b736ec1ae54adfbd39c10cfe658c98325feb50b
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-30 21:53:33 +0200

    work on qq

--- src/components/QueuedQueries/index.js
+++ src/components/QueuedQueries/index.js
@@ -1,26 +1,61 @@
-import React, { useContext } from 'react'
-import Button from '@material-ui/core/Button'
+import React, { useContext, useCallback } from 'react'
 import styled from 'styled-components'
+import { FaTimes } from 'react-icons/fa'
+import IconButton from '@material-ui/core/IconButton'
+import { observer } from 'mobx-react-lite'
 
-import ErrorBoundary from './shared/ErrorBoundary'
-import { StoreContext } from '../models/reactUtils'
+import ErrorBoundary from '../shared/ErrorBoundary'
+import { StoreContext } from '../../models/reactUtils'
 import QueuedQuery from './QueuedQuery'
+
+const Container = styled.div`
+  padding: 15px;
+`
+const CloseIcon = styled(IconButton)`
+  position: absolute !important;
+  right: 3px;
+  top: 70px;
+`
 
 const QueuedQueries = () => {
   const store = useContext(StoreContext)
-  const { queuedQueries } = store
+  const { queuedQueries, setShowQueuedQueries } = store
 
-  if (!queuedQueries.length) {
-    return 'keine Pendenten Operationen!'
+  const onClickCloseIcon = useCallback(() => setShowQueuedQueries(false), [
+    setShowQueuedQueries,
+  ])
+
+  if (!queuedQueries.size) {
+    return (
+      <Container>
+        <CloseIcon
+          title="schliessen"
+          aria-label="schliessen"
+          onClick={onClickCloseIcon}
+        >
+          <FaTimes />
+        </CloseIcon>
+        Es gibt momentan keine pendenten Operationen
+      </Container>
+    )
   }
 
   return (
     <ErrorBoundary>
-      {queuedQueries.map((qq) => (
-        <QueuedQuery key={qq.id} qq={qq} />
-      ))}
+      <Container>
+        <CloseIcon
+          title="schliessen"
+          aria-label="schliessen"
+          onClick={onClickCloseIcon}
+        >
+          <FaTimes />
+        </CloseIcon>
+        {[...queuedQueries.values()].map((qq) => (
+          <QueuedQuery key={qq.id} qq={qq} />
+        ))}
+      </Container>
     </ErrorBoundary>
   )
 }
 
-export default QueuedQueries
+export default observer(QueuedQueries)
