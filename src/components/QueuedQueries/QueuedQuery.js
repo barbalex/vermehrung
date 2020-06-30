23da8d635597196d9074832498bbf498a77dd2ea
Alexander Gabriel <alex.barbalex@gmail.com>
2020-06-30 21:53:17 +0200

    work on qq

--- src/components/QueuedQueries/QueuedQuery.js
+++ src/components/QueuedQueries/QueuedQuery.js
@@ -1,27 +1,37 @@
 import React, { useContext } from 'react'
-import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
+import moment from 'moment'
+import { observer } from 'mobx-react-lite'
 
-import ErrorBoundary from './shared/ErrorBoundary'
-import { StoreContext } from '../models/reactUtils'
+import { StoreContext } from '../../models/reactUtils'
+
+const Container = styled.div`
+  display: flex;
+`
+const Value = styled.div`
+  padding: 5px;
+`
 
 const QueuedQuery = ({ qq }) => {
   const store = useContext(StoreContext)
-  const {
-    id,
-    time,
-    name,
-    variables,
-    callbackQuery,
-    callbackQueryVariables,
-    revertTable,
-    revertId,
-    revertField,
-    revertValue,
-    revertValues,
-  } = qq
+  const { time, revertTable, revertField, revertValue, newValue } = qq
 
-  return <ErrorBoundary></ErrorBoundary>
+  /**
+   * TODO:
+   * - revert
+   * - revert all
+   * - delete
+   * - delete all
+   * - nice table
+   */
+
+  return (
+    <Container>
+      <Value>{`${moment(time).format('YYYY.MM.DD hh.mm.ss')}:`}</Value>
+      <Value>{`${revertTable}.${revertField}`}</Value>
+      <Value>{`${revertValue} > ${newValue}`}</Value>
+    </Container>
+  )
 }
 
-export default QueuedQuery
+export default observer(QueuedQuery)
