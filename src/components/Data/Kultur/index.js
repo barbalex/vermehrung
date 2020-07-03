bfa5b9c4efbf8b0dd3a94700dacd74feda56ecb6
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-03 13:37:42 +0200

    add nav icons

--- src/components/Data/Kultur/index.js
+++ src/components/Data/Kultur/index.js
@@ -40,6 +40,7 @@ import ZaDownSvg from '../../../svg/to_za_down.inline.svg'
 import AnLiDownSvg from '../../../svg/to_anli_down.inline.svg'
 import AusLiDownSvg from '../../../svg/to_ausli_down.inline.svg'
 import EvDownSvg from '../../../svg/to_ev_down.inline.svg'
+import TkDownSvg from '../../../svg/to_tk_down.inline.svg'
 
 const constants = getConstants()
 
@@ -217,6 +218,26 @@ const Kultur = ({
     () => setActiveNodeArray(activeNodeArray.slice(0, -1)),
     [activeNodeArray, setActiveNodeArray],
   )
+  const onClickToZaehlungen = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Zaehlungen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToAnLieferungen = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'An-Lieferungen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToAusLieferungen = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Aus-Lieferungen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToEvents = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Events']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToTks = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Teilkulturen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
 
   useEffect(() => {
     unsetError('kultur')
@@ -318,6 +339,32 @@ const Kultur = ({
             <TitleSymbols>
               <IconButton title="Zu allen Kulturen" onClick={onClickToKulturen}>
                 <KuUpSvg />
+              </IconButton>
+              {row?.kultur_option?.tk && (
+                <IconButton title="Zu den Teilkulturen" onClick={onClickToTks}>
+                  <TkDownSvg />
+                </IconButton>
+              )}
+              <IconButton
+                title="Zu den Zählungen"
+                onClick={onClickToZaehlungen}
+              >
+                <ZaDownSvg />
+              </IconButton>
+              <IconButton
+                title="Zu den An-Lieferungen"
+                onClick={onClickToAnLieferungen}
+              >
+                <AnLiDownSvg />
+              </IconButton>
+              <IconButton
+                title="Zu den Aus-Lieferungen"
+                onClick={onClickToAusLieferungen}
+              >
+                <AusLiDownSvg />
+              </IconButton>
+              <IconButton title="Zu den Events" onClick={onClickToEvents}>
+                <EvDownSvg />
               </IconButton>
               <AddButton />
               <DeleteButton row={row} />
