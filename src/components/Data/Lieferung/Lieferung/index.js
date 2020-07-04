4a0d8f6b047261e43e8cf113eb1fb24e9768ca92
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-04 17:42:07 +0200

    style filter numbers

--- src/components/Data/Lieferung/Lieferung/index.js
+++ src/components/Data/Lieferung/Lieferung/index.js
@@ -30,6 +30,7 @@ import kulturSort from '../../../../utils/kulturSort'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
 import Conflict from './Conflict'
 import ConflictList from '../../../shared/ConflictList'
+import FilterNumbers from '../../../shared/FilterNumbers'
 import sammlungLabelFromSammlung from './sammlungLabelFromSammlung'
 import kulturLabelFromKultur from './kulturLabelFromKultur'
 import UpSvg from '../../../../svg/to_up.inline.svg'
@@ -63,15 +64,6 @@ const TitleSymbols = styled.div`
   display: flex;
   margin-top: auto;
   margin-bottom: auto;
-`
-const TitleFilterNumbers = styled.div`
-  cursor: default;
-  user-select: none;
-  padding: 0 5px;
-  margin-top: auto;
-  margin-bottom: auto;
-  min-width: 48px;
-  text-align: center;
 `
 const FieldsContainer = styled.div`
   padding: 10px;
@@ -449,7 +441,7 @@ const Lieferung = ({ showFilter, sammelLieferung = {} }) => {
                 <IoMdInformationCircleOutline />
               </IconButton>
               {(store.filter.show || isFiltered) && (
-                <TitleFilterNumbers>{`${filteredNr}/${totalNr}`}</TitleFilterNumbers>
+                <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} />
               )}
             </TitleSymbols>
           </TitleContainer>
--- src/components/Data/Lieferungen/index.js
+++ src/components/Data/Lieferungen/index.js
@@ -10,6 +10,7 @@ import { StoreContext } from '../../../models/reactUtils'
 import FilterTitle from '../../shared/FilterTitle'
 import Row from './Row'
 import ErrorBoundary from '../../shared/ErrorBoundary'
+import FilterNumbers from '../../shared/FilterNumbers'
 import exists from '../../../utils/exists'
 import UpSvg from '../../../svg/to_up.inline.svg'
 
@@ -40,15 +41,6 @@ const TitleSymbols = styled.div`
   display: flex;
   margin-top: auto;
   margin-bottom: auto;
-`
-const TitleFilterNumbers = styled.div`
-  cursor: default;
-  user-select: none;
-  padding: 0 5px;
-  margin-top: auto;
-  margin-bottom: auto;
-  min-width: 48px;
-  text-align: center;
 `
 const FieldsContainer = styled.div`
   overflow: auto !important;
@@ -178,7 +170,7 @@ const Lieferungen = ({ filter: showFilter, width, height }) => {
                 <FaPlus />
               </IconButton>
               {(store.filter.show || isFiltered) && (
-                <TitleFilterNumbers>{`${filteredNr}/${totalNr}`}</TitleFilterNumbers>
+                <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} />
               )}
             </TitleSymbols>
           </TitleContainer>
