4d8ce5579d9f4a779f23524a1c705f398c86cd0a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-04 17:41:55 +0200

    style filter numbers

--- src/components/Data/Teilkultur/index.js
+++ src/components/Data/Teilkultur/index.js
@@ -26,6 +26,7 @@ import getConstants from '../../../utils/constants'
 import ErrorBoundary from '../../shared/ErrorBoundary'
 import Conflict from './Conflict'
 import ConflictList from '../../shared/ConflictList'
+import FilterNumbers from '../../shared/FilterNumbers'
 import kulturLabelFromKultur from './kulturLabelFromKultur'
 import UpSvg from '../../../svg/to_up.inline.svg'
 
@@ -57,15 +58,6 @@ const TitleSymbols = styled.div`
   display: flex;
   margin-top: auto;
   margin-bottom: auto;
-`
-const TitleFilterNumbers = styled.div`
-  cursor: default;
-  user-select: none;
-  padding: 0 5px;
-  margin-top: auto;
-  margin-bottom: auto;
-  min-width: 48px;
-  text-align: center;
 `
 const FieldsContainer = styled.div`
   padding: 10px;
@@ -230,7 +222,7 @@ const Teilkultur = ({
                 <IoMdInformationCircleOutline />
               </IconButton>
               {(store.filter.show || isFiltered) && (
-                <TitleFilterNumbers>{`${filteredNr}/${totalNr}`}</TitleFilterNumbers>
+                <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} />
               )}
             </TitleSymbols>
           </TitleContainer>
