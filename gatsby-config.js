199efd812e487843c0689ec48ab437ab05a40b7e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-04 22:28:51 +0200

    correct

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,7 @@ module.exports = {
         precachePages: [
           '/Dokumentation/',
           '/Dokumentation/*',
-          'Dokumentation/Ziele/*',
+          '/Dokumentation/Ziele/*',
         ],
       },
     },
