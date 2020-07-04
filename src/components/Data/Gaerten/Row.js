e70f9c6593ca7f63d5108a2db9ff1c9314e79459
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-04 22:01:41 +0200

    names not in parentheses

--- src/components/Data/Gaerten/Row.js
+++ src/components/Data/Gaerten/Row.js
@@ -35,8 +35,7 @@ const Arten = ({ row, style, last }) => {
     () => setActiveNodeArray([...activeNodeArray, row.id]),
     [activeNodeArray, row.id, setActiveNodeArray],
   )
-  const label =
-    row.name || `(${row?.person?.fullname ?? 'keine Person gewählt'})`
+  const label = row.name || `${row?.person?.fullname ?? 'keine Person gewählt'}`
 
   return (
     <Row key={row.id} onClick={onClickRow} style={style} data-last={last}>
