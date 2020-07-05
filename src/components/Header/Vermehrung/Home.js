e71e1322231babfeaa879fc81f1aab0909b5ca9d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-05 17:45:17 +0200

    refactor

---
+++ src/components/Header/Vermehrung/Home.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import { FaHome } from 'react-icons/fa'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { observer } from 'mobx-react-lite'
+
+import { StoreContext } from '../../../models/reactUtils'
+import exists from '../../../utils/exists'
+
+const SiteTitle = styled(Button)`
+  display: none;
+  color: white !important;
+  font-size: 20px !important;
+  border-color: rgba(255, 255, 255, 0.5) !important;
+  border-width: 0 !important;
+  text-transform: unset !important;
+  @media (min-width: 700px) {
+    display: block;
+  }
+  &:hover {
+    border-width: 1px !important;
+  }
+`
+
+const Home = () => {
+  const store = useContext(StoreContext)
+  const { widthEnforced } = store.tree
+
+  if (exists(widthEnforced)) {
+    return (
+      <IconButton
+        color="inherit"
+        aria-label="Home"
+        component={Link}
+        to="/"
+        title="Home"
+      >
+        <FaHome />
+      </IconButton>
+    )
+  }
+
+  return (
+    <SiteTitle variant="outlined" component={Link} to="/" title="Home">
+      Vermehrung
+    </SiteTitle>
+  )
+}
+
+export default observer(Home)
