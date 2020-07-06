f79c7992857acc49d9b43309ccabf86dcb8fdfaf
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-06 14:43:15 +0200

    add menu

--- src/components/Data/Kultur/FormTitle/AddButton.js
+++ src/components/Data/Kultur/FormTitle/AddButton.js
@@ -2,17 +2,22 @@ import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import { FaPlus } from 'react-icons/fa'
 import IconButton from '@material-ui/core/IconButton'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { StoreContext } from '../../../../models/reactUtils'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
 
-const KulturAddButton = () => {
+const KulturAddButton = ({ asMenu }) => {
   const store = useContext(StoreContext)
   const { insertKulturRev } = store.tree
 
   const add = useCallback(() => {
     insertKulturRev()
   }, [insertKulturRev])
+
+  if (asMenu) {
+    return <MenuItem onClick={add}>neue Kultur</MenuItem>
+  }
 
   return (
     <ErrorBoundary>
