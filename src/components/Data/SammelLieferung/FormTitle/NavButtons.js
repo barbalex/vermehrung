33ea252b29a53432c56d720ffe970cee8dfae6bc
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-06 22:02:01 +0200

    refactor

---
+++ src/components/Data/SammelLieferung/FormTitle/NavButtons.js
@@ -0,0 +1,35 @@
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react-lite'
+import IconButton from '@material-ui/core/IconButton'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import UpSvg from '../../../../svg/to_up.inline.svg'
+import LiDownSvg from '../../../../svg/to_li_down.inline.svg'
+
+const SlNavButtons = () => {
+  const store = useContext(StoreContext)
+
+  const { activeNodeArray, setActiveNodeArray } = store.tree
+
+  const onClickUp = useCallback(
+    () => setActiveNodeArray(activeNodeArray.slice(0, -1)),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToLieferungen = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Lieferungen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+
+  return (
+    <>
+      <IconButton title="Zur Liste" onClick={onClickUp}>
+        <UpSvg />
+      </IconButton>
+      <IconButton title="Zu den Lieferungen" onClick={onClickToLieferungen}>
+        <LiDownSvg />
+      </IconButton>
+    </>
+  )
+}
+
+export default observer(SlNavButtons)
