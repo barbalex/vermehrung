6ffd1f94257ad7245884c7a066ec346807acf6bb
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-06 14:21:12 +0200

    add menu

--- src/components/shared/FilterNumbers.js
+++ src/components/shared/FilterNumbers.js
@@ -1,8 +1,14 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { StoreContext } from '../../models/reactUtils'
 
+const OuterContainer = styled.div`
+  min-width: 48px;
+  display: flex;
+  justify-content: center;
+`
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -10,7 +16,6 @@ const Container = styled.div`
   font-weight: 500;
   min-height: 48px;
   justify-content: center;
-  padding: 0 5px;
   cursor: default;
   user-select: none;
 `
@@ -19,20 +24,27 @@ const Filtered = styled.div`
 `
 const Total = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.54);
+  text-align: center;
 `
 
-const FilterNumbers = ({ filteredNr, totalNr }) => {
+const FilterNumbers = ({ filteredNr, totalNr, asMenu }) => {
   const store = useContext(StoreContext)
   const { isFiltered: runIsFiltered, show } = store.filter
   const isFiltered = runIsFiltered()
 
   if (!(show || isFiltered)) return null
 
+  if (asMenu) {
+    return <MenuItem>{`${filteredNr}/${totalNr}`}</MenuItem>
+  }
+
   return (
-    <Container>
-      <Filtered title="gefilterte Anzahl">{filteredNr}</Filtered>
-      <Total title="totale Anzahl">{totalNr}</Total>
-    </Container>
+    <OuterContainer>
+      <Container>
+        <Filtered title="gefilterte Anzahl">{filteredNr}</Filtered>
+        <Total title="totale Anzahl">{totalNr}</Total>
+      </Container>
+    </OuterContainer>
   )
 }
 
