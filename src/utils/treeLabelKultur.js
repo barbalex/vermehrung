3496efe2707f62a0bb0e404e4bb65041b42b4a83
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-23 14:34:29 +0200

    label with zl

--- src/utils/treeLabelKultur.js
+++ src/utils/treeLabelKultur.js
@@ -3,6 +3,7 @@ export default (el) => {
     el?.garten?.name ?? `${el?.garten?.person?.fullname ?? 'kein Name'}`
   const art = el?.art?.art_ae_art?.name ?? '(keine Art)'
   const herkunft = el?.herkunft?.nr ?? '(Herkunft ohne Nr)'
+  const zwischenlager = el?.zwischenlager ? `, Zwischenlager` : ''
 
-  return `${art}, von: ${herkunft}, in: ${garten}`
+  return `${art}, von: ${herkunft}, in: ${garten}${zwischenlager}`
 }
