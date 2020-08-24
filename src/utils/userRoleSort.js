c43c7c75f718690649072aafe990bf1bf31cbc1e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-24 16:06:52 +0200

    sort better

--- src/utils/userRoleSort.js
+++ src/utils/userRoleSort.js
@@ -1,11 +1,11 @@
 export default (a, b) => {
-  const sortA = a.sort
-  const sortB = b.sort
+  const sortA = a.sort ?? ''
+  const sortB = b.sort ?? ''
   if (sortA < sortB) return -1
   if (sortA > sortB) return 1
 
-  const nameA = a?.name?.toString()?.toLowerCase()
-  const nameB = b?.name?.toString()?.toLowerCase()
+  const nameA = a?.name?.toString()?.toLowerCase() ?? ''
+  const nameB = b?.name?.toString()?.toLowerCase() ?? ''
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
 
