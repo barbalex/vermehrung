fa1f8f928a487d28e816a2ab3a6a4df5b4dd6d4e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-24 16:07:09 +0200

    sort better

--- src/utils/aeArtSort.js
+++ src/utils/aeArtSort.js
@@ -1,6 +1,6 @@
 export default (a, b) => {
-  const nameA = a?.name?.toLowerCase()
-  const nameB = b?.name?.toLowerCase()
+  const nameA = a?.name?.toLowerCase() ?? ''
+  const nameB = b?.name?.toLowerCase() ?? ''
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
 
--- src/utils/eventSort.js
+++ src/utils/eventSort.js
@@ -1,11 +1,11 @@
 export default (a, b) => {
-  const datumA = new Date(a?.datum ?? null)
-  const datumB = new Date(b?.datum ?? null)
+  const datumA = new Date(a?.datum ?? '')
+  const datumB = new Date(b?.datum ?? '')
   if (datumA < datumB) return -1
   if (datumA > datumB) return 1
 
-  const beschreibungA = a?.beschreibung?.toString()?.toLowerCase()
-  const beschreibungB = b?.beschreibung?.toString()?.toLowerCase()
+  const beschreibungA = a?.beschreibung?.toString()?.toLowerCase() ?? ''
+  const beschreibungB = b?.beschreibung?.toString()?.toLowerCase() ?? ''
   if (beschreibungA < beschreibungB) return -1
   if (beschreibungA > beschreibungB) return 1
 
