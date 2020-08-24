424af119e1fb057656686bed052e0015548b45e0
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-24 16:06:59 +0200

    sort better

--- src/utils/lieferungSort.js
+++ src/utils/lieferungSort.js
@@ -1,6 +1,6 @@
 export default (a, b) => {
-  const datumA = new Date(a?.datum ?? null)
-  const datumB = new Date(b?.datum ?? null)
+  const datumA = new Date(a?.datum ?? '')
+  const datumB = new Date(b?.datum ?? '')
   if (datumA < datumB) return -1
   if (datumA > datumB) return 1
 
