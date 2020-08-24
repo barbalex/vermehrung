4658bb4ca426812da0d46b33793b4a5a253c6359
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-24 16:07:09 +0200

    sort better

--- src/utils/avSort.js
+++ src/utils/avSort.js
@@ -1,6 +1,6 @@
 export default (a, b) => {
-  const nameA = a?.art?.art_ae_art?.name?.toLowerCase()
-  const nameB = b?.art?.art_ae_art?.name?.toLowerCase()
+  const nameA = a?.art?.art_ae_art?.name?.toLowerCase() ?? ''
+  const nameB = b?.art?.art_ae_art?.name?.toLowerCase() ?? ''
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
 
--- src/utils/fileSort.js
+++ src/utils/fileSort.js
@@ -1,6 +1,6 @@
 export default (a, b) => {
-  const nameA = a?.name?.toString()?.toLowerCase()
-  const nameB = b?.name?.toString()?.toLowerCase()
+  const nameA = a?.name?.toString()?.toLowerCase() ?? ''
+  const nameB = b?.name?.toString()?.toLowerCase() ?? ''
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
 
