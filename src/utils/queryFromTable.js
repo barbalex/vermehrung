07c7edeaa7d97371eb0252d9c193fb0e33006269
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-02 16:48:18 +0200

    refactor showDeleted

--- src/utils/queryFromTable.js
+++ src/utils/queryFromTable.js
@@ -1,21 +1,30 @@
 import types from '../models/Filter/simpleTypes'
 
-export default ({ store, table, filter: filterPassed = {} }) => {
-  const { filter: storeFilter, deletedTableFilter } = store
-  const baseFilter = { id: { _is_null: false } }
-  const filter = {
-    ...baseFilter,
-    ...filterPassed,
-    ...deletedTableFilter,
+export default ({ store, table }) => {
+  const { filter: storeFilter } = store
+  const deletedTableFilter = {
+    _or: [
+      { _deleted: { _eq: false } },
+      // filtering for empty array, see: https://stackoverflow.com/a/737678/712005
+      {
+        _and: [{ _deleted: { _eq: false } }, { _conflicts: { _neq: '{}' } }],
+      },
+    ],
+  }
+  let filter = { id: { _is_null: false } }
+  if (storeFilter[table]._deleted === false) {
+    filter = {
+      ...filter,
+      ...deletedTableFilter,
+    }
   }
 
   if (!storeFilter[table]) return filter
   if (!Object.entries(storeFilter[table]).length) return filter
 
-  const filterValues = [
-    ...Object.entries(storeFilter[table]),
-    ...Object.entries(filterPassed),
-  ].filter((e) => e[1] || e[1] === 0)
+  const filterValues = [...Object.entries(storeFilter[table])].filter(
+    (e) => e[1] || e[1] === 0,
+  )
 
   filterValues.forEach(([key, value]) => {
     const type = types[table][key] || 'string'
