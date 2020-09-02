3825ffc4a8e82300ca058340f43f1c237af5e9f9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-02 16:49:30 +0200

    refactor showDeleted

--- src/components/Data/Garten/DeleteButton.js
+++ src/components/Data/Garten/DeleteButton.js
@@ -24,7 +24,7 @@ const Title = styled.div`
 
 const GartenDeleteButton = ({ row }) => {
   const store = useContext(StoreContext)
-  const { showDeleted } = store
+  const { filter } = store
   const {
     activeNodeArray,
     setActiveNodeArray,
@@ -43,17 +43,17 @@ const GartenDeleteButton = ({ row }) => {
   const remove = useCallback(() => {
     row.delete()
     setAnchorEl(null)
-    if (!showDeleted) {
+    if (filter.garten._deleted === false) {
       // need to remove openNode from openNodes
       removeOpenNodeWithChildren(activeNodeArray)
       setActiveNodeArray(activeNodeArray.slice(0, -1))
     }
   }, [
-    activeNodeArray,
     row,
+    filter.garten._deleted,
     removeOpenNodeWithChildren,
+    activeNodeArray,
     setActiveNodeArray,
-    showDeleted,
   ])
 
   return (
