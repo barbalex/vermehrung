7fcc90df82cc82f531612c3585cded7413ac93f7
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-08 17:10:44 +0200

    refactor

---
+++ src/components/Data/Garten/FormTitle/AddButton.js
@@ -0,0 +1,26 @@
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react-lite'
+import { FaPlus } from 'react-icons/fa'
+import IconButton from '@material-ui/core/IconButton'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
+const GartenAddButton = () => {
+  const store = useContext(StoreContext)
+  const { insertGartenRev } = store
+
+  const add = useCallback(() => {
+    insertGartenRev()
+  }, [insertGartenRev])
+
+  return (
+    <ErrorBoundary>
+      <IconButton aria-label="neuer Garten" title="neuer Garten" onClick={add}>
+        <FaPlus />
+      </IconButton>
+    </ErrorBoundary>
+  )
+}
+
+export default observer(GartenAddButton)
