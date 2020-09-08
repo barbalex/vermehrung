43582723c3df4f87bffb999fd50cbb9e7d5c0e17
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-08 16:22:11 +0200

    rename

---
+++ src/components/Data/Garten/Form/index.js
@@ -0,0 +1,322 @@
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from 'react'
+import { observer } from 'mobx-react-lite'
+import styled from 'styled-components'
+import IconButton from '@material-ui/core/IconButton'
+import SplitPane from 'react-split-pane'
+
+import { StoreContext } from '../../../models/reactUtils'
+import Select from '../../shared/Select'
+import TextField from '../../shared/TextField'
+import Checkbox2States from '../../shared/Checkbox2States'
+import Checkbox3States from '../../shared/Checkbox3States'
+import FilterTitle from '../../shared/FilterTitle'
+import ifIsNumericAsNumber from '../../../utils/ifIsNumericAsNumber'
+import Files from '../Files'
+import Coordinates from '../../shared/Coordinates'
+import Settings from './Settings'
+import Personen from './Personen'
+import DeleteButton from './DeleteButton'
+import AddButton from './AddButton'
+import Download from './Download'
+import ErrorBoundary from '../../shared/ErrorBoundary'
+import Conflict from './Conflict'
+import ConflictList from '../../shared/ConflictList'
+import FilterNumbers from '../../shared/FilterNumbers'
+import UpSvg from '../../../svg/to_up.inline.svg'
+import KuDownSvg from '../../../svg/to_ku_down.inline.svg'
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
+`
+const TitleContainer = styled.div`
+  background-color:rgba(74, 20, 140, 0.1);
+  flex-shrink: 0;
+  display: flex;
+  @media print {
+    display: none !important;
+  }
+  height: 48px;
+  justify-content: space-between;
+  padding 0 10px;
+`
+const Title = styled.div`
+  font-weight: bold;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+const TitleSymbols = styled.div`
+  display: flex;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+const FieldsContainer = styled.div`
+  padding: 10px;
+  overflow: auto !important;
+  height: 100%;
+`
+const StyledSplitPane = styled(SplitPane)`
+  height: calc(100vh - 64px - 48px) !important;
+  .Resizer {
+    background: rgba(74, 20, 140, 0.1);
+    opacity: 1;
+    z-index: 1;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    width: 7px;
+    cursor: col-resize;
+  }
+  .Resizer:hover {
+    -webkit-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+    background-color: #fff59d !important;
+  }
+  .Resizer.disabled {
+    cursor: not-allowed;
+  }
+  .Resizer.disabled:hover {
+    border-color: transparent;
+  }
+  .Pane {
+    overflow: hidden;
+  }
+`
+const CaseConflictTitle = styled.h4`
+  margin-bottom: 10px;
+`
+const Rev = styled.span`
+  font-weight: normal;
+  padding-left: 7px;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 0.8em;
+`
+
+const Garten = ({
+  filter: showFilter,
+  id = '99999999-9999-9999-9999-999999999999',
+}) => {
+  const store = useContext(StoreContext)
+
+  const {
+    filter,
+    online,
+    gartensSorted,
+    gartensFiltered,
+    userPersonOption,
+    personIdInActiveNodeArray,
+    personsSorted,
+    errors,
+    unsetError,
+    insertGvRev,
+  } = store
+  const { activeNodeArray, setActiveNodeArray } = store.tree
+
+  const hierarchyFilter = (e) => {
+    if (personIdInActiveNodeArray)
+      return e.person_id === personIdInActiveNodeArray
+    return true
+  }
+
+  const totalNr = gartensSorted.filter(hierarchyFilter).length
+  const filteredNr = gartensFiltered.filter(hierarchyFilter).length
+
+  const row = useMemo(
+    () => (showFilter ? filter.garten : store.gartens.get(id) || {}),
+    [filter.garten, id, showFilter, store.gartens],
+  )
+
+  const [activeConflict, setActiveConflict] = useState(null)
+  const conflictDisposalCallback = useCallback(
+    () => setActiveConflict(null),
+    [],
+  )
+  const conflictSelectionCallback = useCallback(
+    () => setActiveConflict(null),
+    [],
+  )
+
+  const {
+    ga_strasse,
+    ga_plz,
+    ga_ort,
+    ga_geom_point,
+    ga_aktiv,
+    ga_bemerkungen,
+  } = userPersonOption
+
+  useEffect(() => {
+    unsetError('garten')
+  }, [id, unsetError])
+
+  const personWerte = useMemo(
+    () =>
+      personsSorted.map((el) => ({
+        value: el.id,
+        label: `${el.fullname || '(kein Name)'} (${el.ort || 'kein Ort'})`,
+      })),
+    [personsSorted],
+  )
+
+  const saveToDb = useCallback(
+    async (event) => {
+      const field = event.target.name
+      let value = ifIsNumericAsNumber(event.target.value)
+      if (event.target.value === undefined) value = null
+      if (event.target.value === '') value = null
+
+      if (showFilter) {
+        return filter.setValue({ table: 'garten', key: field, value })
+      }
+
+      const previousValue = row[field]
+      // only update if value has changed
+      if (value === previousValue) return
+      row.edit({ field, value })
+      if (field === 'person_id') {
+        insertGvRev({ values: { garten_id: row.id, person_id: value } })
+      }
+    },
+    [filter, insertGvRev, row, showFilter],
+  )
+
+  const showDeleted = showFilter || row._deleted
+
+  return (
+    <FieldsContainer>
+      {activeConflict && (
+        <CaseConflictTitle>
+          Aktuelle Version<Rev>{row._rev}</Rev>
+        </CaseConflictTitle>
+      )}
+      {showDeleted && (
+        <>
+          {showFilter ? (
+            <Checkbox3States
+              key={`${row.id}_deleted`}
+              label="gelöscht"
+              name="_deleted"
+              value={row._deleted}
+              saveToDb={saveToDb}
+              error={errors?.garten?._deleted}
+            />
+          ) : (
+            <Checkbox2States
+              key={`${row.id}_deleted`}
+              label="gelöscht"
+              name="_deleted"
+              value={row._deleted}
+              saveToDb={saveToDb}
+              error={errors?.garten?._deleted}
+            />
+          )}
+        </>
+      )}
+      <TextField
+        key={`${row.id}name`}
+        name="name"
+        label="Name"
+        value={row.name}
+        saveToDb={saveToDb}
+        error={errors?.garten?.name}
+      />
+      <Select
+        key={`${row.id}${row.person_id}person_id`}
+        name="person_id"
+        value={row.person_id}
+        field="person_id"
+        label="Person"
+        options={personWerte}
+        saveToDb={saveToDb}
+        error={errors?.garten?.person_id}
+      />
+      {ga_strasse && (
+        <TextField
+          key={`${row.id}strasse`}
+          name="strasse"
+          label="Strasse"
+          value={row.strasse}
+          saveToDb={saveToDb}
+          error={errors?.garten?.strasse}
+        />
+      )}
+      {ga_plz && (
+        <TextField
+          key={`${row.id}plz`}
+          name="plz"
+          label="PLZ"
+          value={row.plz}
+          saveToDb={saveToDb}
+          error={errors?.garten?.plz}
+          type="number"
+        />
+      )}
+      {ga_ort && (
+        <TextField
+          key={`${row.id}ort`}
+          name="ort"
+          label="Ort"
+          value={row.ort}
+          saveToDb={saveToDb}
+          error={errors?.garten?.ort}
+        />
+      )}
+      {!showFilter && ga_geom_point && (
+        <Coordinates row={row} saveToDb={saveToDb} />
+      )}
+      {ga_aktiv && (
+        <>
+          {showFilter ? (
+            <Checkbox3States
+              key={`${row.id}aktiv`}
+              label="aktiv"
+              name="aktiv"
+              value={row.aktiv}
+              saveToDb={saveToDb}
+              error={errors?.garten?.aktiv}
+            />
+          ) : (
+            <Checkbox2States
+              key={`${row.id}aktiv`}
+              label="aktiv"
+              name="aktiv"
+              value={row.aktiv}
+              saveToDb={saveToDb}
+              error={errors?.garten?.aktiv}
+            />
+          )}
+        </>
+      )}
+      {ga_bemerkungen && (
+        <TextField
+          key={`${row.id}bemerkungen`}
+          name="bemerkungen"
+          label="Bemerkungen"
+          value={row.bemerkungen}
+          saveToDb={saveToDb}
+          error={errors?.garten?.bemerkungen}
+          multiLine
+        />
+      )}
+      {online && !showFilter && row._conflicts && row._conflicts.map && (
+        <ConflictList
+          conflicts={row._conflicts}
+          activeConflict={activeConflict}
+          setActiveConflict={setActiveConflict}
+        />
+      )}
+      <Personen gartenId={row.id} />
+      {!showFilter && <Files parentId={row.id} parent="garten" />}
+    </FieldsContainer>
+  )
+}
+
+export default observer(Garten)
--- src/components/Data/Herkunft/Conflict.js
+++ src/components/Data/Herkunft/Conflict.js
@@ -11,8 +11,8 @@ import Conflict from '../../shared/Conflict'
 const HerkunftConflict = ({
   rev,
   row,
-  callbackAfterVerwerfen,
-  callbackAfterUebernehmen,
+  conflictDisposalCallback,
+  conflictSelectionCallback,
   setActiveConflict,
 }) => {
   const store = useContext(StoreContext)
@@ -77,8 +77,8 @@ const HerkunftConflict = ({
 
   const onClickVerwerfen = useCallback(() => {
     revRow.setDeleted()
-    setTimeout(() => callbackAfterVerwerfen())
-  }, [callbackAfterVerwerfen, revRow])
+    setTimeout(() => conflictDisposalCallback())
+  }, [conflictDisposalCallback, revRow])
   const onClickUebernehmen = useCallback(async () => {
     // need to attach to the winner, that is row
     // otherwise risk to still have lower depth and thus loosing
@@ -118,10 +118,10 @@ const HerkunftConflict = ({
         message: error.message,
       })
     }
-    callbackAfterUebernehmen()
+    conflictSelectionCallback()
   }, [
     addNotification,
-    callbackAfterUebernehmen,
+    conflictSelectionCallback,
     revRow._deleted,
     revRow.bemerkungen,
     revRow.gemeinde,
