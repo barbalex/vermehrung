3194fdad33460b3db8189f55d3013518816ba9f8
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 15:00:30 +0200

    refactor

--- src/components/Data/Event/Conflict.js
+++ src/components/Data/Event/Conflict.js
@@ -6,7 +6,7 @@ import gql from 'graphql-tag'
 
 import { useQuery, StoreContext } from '../../../models/reactUtils'
 import Conflict from '../../shared/Conflict'
-import kulturLabelFromKultur from './kulturLabelFromKultur'
+import createDataArrayForRevComparison from './createDataArrayForRevComparison'
 import checkForOnlineError from '../../../utils/checkForOnlineError'
 import toPgArray from '../../../utils/toPgArray'
 
@@ -95,53 +95,10 @@ const EventConflict = ({
     [id, rev, store.event_revs],
   )
 
-  const dataArray = [
-    {
-      valueInRow: kulturLabelFromKultur(row?.kultur),
-      valueInRev: kulturLabelFromKultur(revRow?.kultur),
-      label: 'Kultur',
-    },
-    {
-      valueInRow: row?.teilkultur?.name,
-      valueInRev: revRow?.teilkultur?.name,
-      label: 'Teilkultur',
-    },
-    {
-      valueInRow: row?.person?.fullname,
-      valueInRev: revRow?.person?.fullname,
-      label: 'Person',
-    },
-    {
-      valueInRow: row?.beschreibung,
-      valueInRev: revRow?.beschreibung,
-      label: 'Beschreibung',
-    },
-    {
-      valueInRow: row?.geplant == true,
-      valueInRev: revRow?.geplant == true,
-      label: 'geplant',
-    },
-    {
-      valueInRow: row.datum,
-      valueInRev: revRow.datum,
-      label: 'Datum',
-    },
-    {
-      valueInRow: row.changed,
-      valueInRev: revRow.changed,
-      label: 'geändert',
-    },
-    {
-      valueInRow: row.changed_by,
-      valueInRev: revRow.changed_by,
-      label: 'geändert von',
-    },
-    {
-      valueInRow: row._deleted,
-      valueInRev: revRow._deleted,
-      label: 'gelöscht',
-    },
-  ]
+  const dataArray = useMemo(
+    () => createDataArrayForRevComparison({ row, revRow }),
+    [revRow, row],
+  )
 
   const onClickVerwerfen = useCallback(() => {
     revRow.setDeleted()
