d890508aa5d790ccdf361f1f326290f9ecbed6fe
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 10:43:55 +0200

    refactor

---
+++ src/components/Data/Herkunft/FormTitle/FormTitle.js
@@ -0,0 +1,120 @@
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react-lite'
+import styled from 'styled-components'
+import IconButton from '@material-ui/core/IconButton'
+import { withResizeDetector } from 'react-resize-detector'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import DeleteButton from './DeleteButton'
+import AddButton from './AddButton'
+import Settings from './Settings'
+import Anleitung from './Anleitung'
+import FilterNumbers from '../../../shared/FilterNumbers'
+import HistoryButton from '../../../shared/HistoryButton'
+import Menu from '../../../shared/Menu'
+import UpSvg from '../../../../svg/to_up.inline.svg'
+import SaDownSvg from '../../../../svg/to_sa_down.inline.svg'
+
+const TitleContainer = styled.div`
+  background-color: rgba(74, 20, 140, 0.1);
+  flex-shrink: 0;
+  display: flex;
+  @media print {
+    display: none !important;
+  }
+  height: 48px;
+  justify-content: space-between;
+  padding 0 10px;
+`
+const Title = styled.div`
+  font-weight: bold;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+const TitleSymbols = styled.div`
+  display: flex;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+
+const Herkunft = ({
+  row,
+  totalNr,
+  filteredNr,
+  width,
+  showHistory,
+  setShowHistory,
+  activeConflict,
+}) => {
+  const store = useContext(StoreContext)
+  const { activeNodeArray, setActiveNodeArray } = store.tree
+
+  const onClickUp = useCallback(
+    () => setActiveNodeArray(activeNodeArray.slice(0, -1)),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const onClickToSammlungen = useCallback(
+    () => setActiveNodeArray([...activeNodeArray, 'Sammlungen']),
+    [activeNodeArray, setActiveNodeArray],
+  )
+  const showToSa = activeNodeArray[0] === 'Herkuenfte'
+
+  if (width < 520) {
+    return (
+      <TitleContainer>
+        <Title>{`Herkunft${activeConflict ? ': Konflikt lösen' : ''}`}</Title>
+        <TitleSymbols>
+          <IconButton title="Zur Liste" onClick={onClickUp}>
+            <UpSvg />
+          </IconButton>
+          {showToSa && (
+            <IconButton title="Zu den Sammlungen" onClick={onClickToSammlungen}>
+              <SaDownSvg />
+            </IconButton>
+          )}
+          <AddButton />
+          <DeleteButton row={row} />
+          <Menu white={false}>
+            <HistoryButton
+              row={row}
+              showHistory={showHistory}
+              setShowHistory={setShowHistory}
+              asMenu
+            />
+            <Anleitung asMenu />
+            <Settings />
+            <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} asMenu />
+          </Menu>
+        </TitleSymbols>
+      </TitleContainer>
+    )
+  }
+
+  return (
+    <TitleContainer>
+      <Title>{`Herkunft${activeConflict ? ': Konflikt lösen' : ''}`}</Title>
+      <TitleSymbols>
+        <IconButton title="Zur Liste" onClick={onClickUp}>
+          <UpSvg />
+        </IconButton>
+        {showToSa && (
+          <IconButton title="Zu den Sammlungen" onClick={onClickToSammlungen}>
+            <SaDownSvg />
+          </IconButton>
+        )}
+        <AddButton />
+        <DeleteButton row={row} />
+        <HistoryButton
+          row={row}
+          showHistory={showHistory}
+          setShowHistory={setShowHistory}
+        />
+        <Anleitung />
+        <Settings />
+        <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} />
+      </TitleSymbols>
+    </TitleContainer>
+  )
+}
+
+export default withResizeDetector(observer(Herkunft))
