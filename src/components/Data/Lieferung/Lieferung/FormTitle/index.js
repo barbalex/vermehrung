fee82d86c494cd2729b54c535fd99d16b84fa218
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 17:15:01 +0200

    mobile first

--- src/components/Data/Lieferung/Lieferung/FormTitle/index.js
+++ src/components/Data/Lieferung/Lieferung/FormTitle/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { StoreContext } from '../../../../../models/reactUtils'
