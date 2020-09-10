433bb2912eeac355c6d5189a7b5fa7826e8fdc68
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 16:43:22 +0200

    refactor

--- src/components/Data/Lieferung/Lieferung/index.js
+++ src/components/Data/Lieferung/Lieferung/index.js
@@ -7,59 +7,27 @@ import React, {
 } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
-import IconButton from '@material-ui/core/IconButton'
-import { IoMdInformationCircleOutline } from 'react-icons/io'
 import SplitPane from 'react-split-pane'
 
 import { StoreContext } from '../../../../models/reactUtils'
 import Checkbox2States from '../../../shared/Checkbox2States'
 import Checkbox3States from '../../../shared/Checkbox3States'
-import FilterTitle from '../../../shared/FilterTitle'
 import exists from '../../../../utils/exists'
 import ifIsNumericAsNumber from '../../../../utils/ifIsNumericAsNumber'
-import Settings from './Settings'
-import AddButton from './AddButton'
-import DeleteButton from './DeleteButton'
-import getConstants from '../../../../utils/constants'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
 import Conflict from './Conflict'
-import FilterNumbers from '../../../shared/FilterNumbers'
-import UpSvg from '../../../../svg/to_up.inline.svg'
-import KuDownSvg from '../../../../svg/to_ku_down.inline.svg'
 import Was from './Was'
 import Von from './Von'
 import Nach from './Nach'
 import Wann from './Wann'
 import Wer from './Wer'
-
-const constants = getConstants()
+import FormTitle from './FormTitle'
 
 const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
   background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
-`
-const TitleContainer = styled.div`
-  background-color: rgba(74, 20, 140, 0.1);
-  flex-shrink: 0;
-  display: flex;
-  @media print {
-    display: none !important;
-  }
-  height: 48px;
-  justify-content: space-between;
-  padding 0 10px;
-`
-const Title = styled.div`
-  font-weight: bold;
-  margin-top: auto;
-  margin-bottom: auto;
-`
-const TitleSymbols = styled.div`
-  display: flex;
-  margin-top: auto;
-  margin-bottom: auto;
 `
 const FieldsContainer = styled.div`
   padding: 10px;
@@ -107,49 +75,12 @@ const Lieferung = ({ id, showFilter, sammelLieferung = {} }) => {
   const existsSammelLieferung = !!sammelLieferung?.id
   const store = useContext(StoreContext)
 
-  const {
-    errors,
-    filter,
-    kulturIdInActiveNodeArray,
-    lieferungsFiltered,
-    lieferungsSorted,
-    online,
-    personIdInActiveNodeArray,
-    sammelLieferungIdInActiveNodeArray,
-    sammlungIdInActiveNodeArray,
-    unsetError,
-    userPersonOption,
-  } = store
-  const { activeNodeArray, setActiveNodeArray } = store.tree
-
-  const hierarchyFilter = (e) => {
-    if (kulturIdInActiveNodeArray) {
-      if (activeNodeArray.includes('Aus-Lieferungen')) {
-        return e.von_kultur_id === kulturIdInActiveNodeArray
-      }
-      if (activeNodeArray.includes('An-Lieferungen')) {
-        return e.nach_kultur_id === kulturIdInActiveNodeArray
-      }
-    }
-    if (sammelLieferungIdInActiveNodeArray && !kulturIdInActiveNodeArray) {
-      return e.sammel_lieferung_id === sammelLieferungIdInActiveNodeArray
-    }
-    if (personIdInActiveNodeArray && !kulturIdInActiveNodeArray) {
-      return e.person_id === personIdInActiveNodeArray
-    }
-    if (sammlungIdInActiveNodeArray && !kulturIdInActiveNodeArray) {
-      return e.von_sammlung_id === sammlungIdInActiveNodeArray
-    }
-    return true
-  }
+  const { errors, filter, online, unsetError, userPersonOption } = store
 
   const row = useMemo(
     () => (showFilter ? filter.lieferung : store.lieferungs.get(id) ?? {}),
     [filter.lieferung, id, showFilter, store.lieferungs],
   )
-
-  const totalNr = lieferungsSorted.filter(hierarchyFilter).length
-  const filteredNr = lieferungsFiltered.filter(hierarchyFilter).length
 
   const [activeConflict, setActiveConflict] = useState(null)
   const conflictDisposalCallback = useCallback(
@@ -207,33 +138,11 @@ const Lieferung = ({ id, showFilter, sammelLieferung = {} }) => {
     },
     [filter, row, showFilter],
   )
-  const openLieferungDocs = useCallback(() => {
-    const url = `${constants?.appUri}/Dokumentation/Lieferungen`
-    if (typeof window !== 'undefined') {
-      if (window.matchMedia('(display-mode: standalone)').matches) {
-        return window.open(url, '_blank', 'toolbar=no')
-      }
-      window.open(url)
-    }
-  }, [])
-
-  const onClickUp = useCallback(
-    () => setActiveNodeArray(activeNodeArray.slice(0, -1)),
-    [activeNodeArray, setActiveNodeArray],
-  )
-  const nachKulturId = row?.nach_kultur_id
-  const onClickToKultur = useCallback(
-    () =>
-      setActiveNodeArray([
-        ...activeNodeArray.filter((n) => n !== 'Kulturen'),
-        'Kulturen',
-        nachKulturId,
-      ]),
-    [activeNodeArray, nachKulturId, setActiveNodeArray],
-  )
-  const showToKu = activeNodeArray[0] === 'Sammlungen'
 
   const showDeleted = showFilter || row._deleted
+
+  const [showHistory, setShowHistory] = useState(null)
+  const historyTakeoverCallback = useCallback(() => setShowHistory(null), [])
 
   if (!row || (!showFilter && filter.show)) return null
 
@@ -244,39 +153,12 @@ const Lieferung = ({ id, showFilter, sammelLieferung = {} }) => {
   return (
     <ErrorBoundary>
       <Container showfilter={showFilter}>
-        {showFilter ? (
-          <FilterTitle
-            title="Lieferung"
-            table="lieferung"
-            totalNr={totalNr}
-            filteredNr={filteredNr}
-          />
-        ) : (
-          <TitleContainer>
-            <Title>Lieferung</Title>
-            <TitleSymbols>
-              <IconButton title="Zur Liste" onClick={onClickUp}>
-                <UpSvg />
-              </IconButton>
-              {showToKu && (
-                <IconButton title="Zur Kultur" onClick={onClickToKultur}>
-                  <KuDownSvg />
-                </IconButton>
-              )}
-              <AddButton />
-              <DeleteButton row={row} />
-              <Settings />
-              <IconButton
-                aria-label="Anleitung öffnen"
-                title="Anleitung öffnen"
-                onClick={openLieferungDocs}
-              >
-                <IoMdInformationCircleOutline />
-              </IconButton>
-              <FilterNumbers filteredNr={filteredNr} totalNr={totalNr} />
-            </TitleSymbols>
-          </TitleContainer>
-        )}
+        <FormTitle
+          row={row}
+          showFilter={showFilter}
+          showHistory={showHistory}
+          setShowHistory={setShowHistory}
+        />
         <Container>
           <StyledSplitPane
             split="vertical"
