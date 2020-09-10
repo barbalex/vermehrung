f97f228807908752b1a8c5498631f4ae7d8dc762
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 13:05:54 +0200

    refactor

---
+++ src/components/Data/Sammlung/FormTitle/index.js
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import FilterTitle from '../../../shared/FilterTitle'
+import FormTitle from './FormTitle'
+
+const SammlungFormTitleChooser = ({
+  row,
+  showFilter,
+  showHistory,
+  setShowHistory,
+}) => {
+  const store = useContext(StoreContext)
+  const {
+    herkunftIdInActiveNodeArray,
+    personIdInActiveNodeArray,
+    artIdInActiveNodeArray,
+    sammlungsSorted,
+    sammlungsFiltered,
+  } = store
+
+  const hierarchyFilter = (s) => {
+    if (artIdInActiveNodeArray) {
+      return s.art_id === artIdInActiveNodeArray
+    }
+    if (herkunftIdInActiveNodeArray) {
+      return s.herkunft_id === herkunftIdInActiveNodeArray
+    }
+    if (personIdInActiveNodeArray) {
+      return s.person_id === personIdInActiveNodeArray
+    }
+    return true
+  }
+
+  const totalNr = sammlungsSorted.filter(hierarchyFilter).length
+  const filteredNr = sammlungsFiltered.filter(hierarchyFilter).length
+
+  if (showFilter) {
+    return (
+      <FilterTitle
+        title="Sammlung"
+        table="sammlung"
+        totalNr={totalNr}
+        filteredNr={filteredNr}
+      />
+    )
+  }
+
+  return (
+    <FormTitle
+      row={row}
+      totalNr={totalNr}
+      filteredNr={filteredNr}
+      showHistory={showHistory}
+      setShowHistory={setShowHistory}
+    />
+  )
+}
+
+export default observer(SammlungFormTitleChooser)
