7c6177853c0b599c75bbe80fc50af7dfde1586f0
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 10:43:55 +0200

    refactor

---
+++ src/components/Data/Herkunft/FormTitle/index.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import FilterTitle from '../../../shared/FilterTitle'
+import FormTitle from './FormTitle'
+
+const HerkunftFormTitleChooser = ({
+  row,
+  showFilter,
+  showHistory,
+  setShowHistory,
+  activeConflict,
+}) => {
+  const store = useContext(StoreContext)
+  const {
+    herkunftsSorted,
+    herkunftsFiltered,
+    sammlungIdInActiveNodeArray,
+  } = store
+
+  const hierarchyFilter = (e) => {
+    if (sammlungIdInActiveNodeArray) {
+      return (e?.sammlungs ?? [])
+        .map((s) => s.id)
+        .includes(sammlungIdInActiveNodeArray)
+    }
+    return true
+  }
+
+  const totalNr = herkunftsSorted.filter(hierarchyFilter).length
+  const filteredNr = herkunftsFiltered.filter(hierarchyFilter).length
+
+  if (showFilter) {
+    return (
+      <FilterTitle
+        title="Herkunft"
+        table="herkunft"
+        totalNr={totalNr}
+        filteredNr={filteredNr}
+      />
+    )
+  }
+
+  return (
+    <FormTitle
+      row={row}
+      totalNr={totalNr}
+      filteredNr={filteredNr}
+      showHistory={showHistory}
+      setShowHistory={setShowHistory}
+      activeConflict={activeConflict}
+    />
+  )
+}
+
+export default observer(HerkunftFormTitleChooser)
