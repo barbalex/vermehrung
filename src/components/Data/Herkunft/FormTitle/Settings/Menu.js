a1deee5d01b7e44b6da55f84350d4510eb6f430e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 11:41:07 +0200

    add history

--- src/components/Data/Herkunft/FormTitle/Settings/Menu.js
+++ src/components/Data/Herkunft/FormTitle/Settings/Menu.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback, useMemo } from 'react'
+import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
