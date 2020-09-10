cfb363f9e8376220ced42c5c96b40932c85a214c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-10 17:15:01 +0200

    mobile first

---
+++ src/components/Data/Lieferung/Lieferung/FormTitle/Settings/Menu.js
@@ -0,0 +1,114 @@
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react-lite'
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import { IoMdInformationCircleOutline } from 'react-icons/io'
+import styled from 'styled-components'
+
+import { StoreContext } from '../../../../../../models/reactUtils'
+import getConstants from '../../../../../../utils/constants'
+
+const constants = getConstants()
+
+const TitleRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-right: 16px;
+  user-select: none;
+`
+const Title = styled.div`
+  padding: 12px 16px;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 800;
+  user-select: none;
+`
+const Info = styled.div`
+  padding: 12px 16px;
+  color: rgba(0, 0, 0, 0.4);
+  user-select: none;
+`
+
+const SettingsLieferungMenu = ({ anchorEl, setAnchorEl }) => {
+  const store = useContext(StoreContext)
+  const { userPersonOption } = store
+  const { li_show_sl_felder, li_show_sl } = userPersonOption
+
+  const saveToDb = useCallback(
+    async (event) => {
+      const field = event.target.name
+      const value = event.target.value === 'false'
+      userPersonOption.edit({ field, value })
+    },
+    [userPersonOption],
+  )
+  const openSettingsDocs = useCallback(() => {
+    setAnchorEl(null)
+    const url = `${constants?.appUri}/Dokumentation/Felder-blenden`
+    if (typeof window !== 'undefined') {
+      if (window.matchMedia('(display-mode: standalone)').matches) {
+        return window.open(url, '_blank', 'toolbar=no')
+      }
+      window.open(url)
+    }
+  }, [setAnchorEl])
+
+  const onClose = useCallback(() => setAnchorEl(null), [setAnchorEl])
+
+  return (
+    <Menu
+      id="long-menu"
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={onClose}
+    >
+      <TitleRow>
+        <Title>Optionen für Lieferungen wählen:</Title>
+        <div>
+          <IconButton
+            aria-label="Anleitung öffnen"
+            title="Anleitung öffnen"
+            onClick={openSettingsDocs}
+          >
+            <IoMdInformationCircleOutline />
+          </IconButton>
+        </div>
+      </TitleRow>
+      <MenuItem>
+        <FormControlLabel
+          value={li_show_sl === true ? 'true' : 'false'}
+          control={
+            <Checkbox
+              color="primary"
+              checked={li_show_sl}
+              onClick={saveToDb}
+              name="li_show_sl"
+            />
+          }
+          label="Sammel-Lieferung rechts neben der Lieferung anzeigen"
+          labelPlacement="end"
+        />
+      </MenuItem>
+      <MenuItem>
+        <FormControlLabel
+          value={li_show_sl_felder === true ? 'true' : 'false'}
+          control={
+            <Checkbox
+              color="primary"
+              checked={li_show_sl_felder}
+              onClick={saveToDb}
+              name="li_show_sl_felder"
+            />
+          }
+          label="Felder anzeigen, deren Werte in der Sammel-Lieferung gesetzt wurden"
+          labelPlacement="end"
+        />
+      </MenuItem>
+      <Info>Die Wahl gilt für alle Lieferungen.</Info>
+    </Menu>
+  )
+}
+
+export default observer(SettingsLieferungMenu)
