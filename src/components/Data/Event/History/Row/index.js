71ccc38c1e67dcba11294369dd7af331e19eaa69
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 11:33:26 +0200

    add history to event

---
+++ src/components/Data/Event/History/Row/index.js
@@ -0,0 +1,101 @@
+import React, { useCallback, useContext, useMemo } from 'react'
+import { observer } from 'mobx-react-lite'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
+
+import History from '../../../../shared/History'
+import { StoreContext } from '../../../../../models/reactUtils'
+import checkForOnlineError from '../../../../../utils/checkForOnlineError'
+import toPgArray from '../../../../../utils/toPgArray'
+import createDataArrayForRevComparison from '../../createDataArrayForRevComparison'
+
+const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
+  const store = useContext(StoreContext)
+  const { user, addNotification, upsertKulturModel } = store
+
+  const dataArray = useMemo(
+    () => createDataArrayForRevComparison({ row, revRow }),
+    [revRow, row],
+  )
+  const onClickUebernehmen = useCallback(async () => {
+    // need to attach to the winner, that is row
+    // otherwise risk to still have lower depth and thus loosing
+    const newDepth = row._depth + 1
+    const newObject = {
+      event_id: revRow.event_id,
+      kultur_id: revRow.kultur_id,
+      teilkultur_id: revRow.teilkultur_id,
+      person_id: revRow.person_id,
+      beschreibung: revRow.beschreibung,
+      geplant: revRow.geplant,
+      datum: revRow.datum,
+      _parent_rev: row._rev,
+      _depth: newDepth,
+      _deleted: revRow._deleted,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject.changed = new window.Date().toISOString()
+    newObject.changed_by = user.email
+    newObject._revisions = toPgArray([rev, ...row._revisions])
+    const newObjectForStore = { ...newObject }
+    //console.log('Event History', { row, revRow, newObject })
+    try {
+      await store.mutateInsert_event_rev_one({
+        object: newObject,
+        on_conflict: {
+          constraint: 'event_rev_pkey',
+          update_columns: ['id'],
+        },
+      })
+    } catch (error) {
+      checkForOnlineError(error)
+      addNotification({
+        message: error.message,
+      })
+    }
+    historyTakeoverCallback()
+    // do not stringify revisions for store
+    // as _that_ is a real array
+    newObjectForStore._revisions = row._revisions
+      ? [rev, ...row._revisions]
+      : [rev]
+    // TODO: is this a good idea?
+    newObjectForStore._conflicts = row._conflicts
+    // for store: convert rev to winner
+    newObjectForStore.id = row.id
+    delete newObjectForStore.event_id
+    // optimistically update store
+    upsertKulturModel(newObjectForStore)
+  }, [
+    addNotification,
+    historyTakeoverCallback,
+    revRow._deleted,
+    revRow.beschreibung,
+    revRow.datum,
+    revRow.event_id,
+    revRow.geplant,
+    revRow.kultur_id,
+    revRow.person_id,
+    revRow.teilkultur_id,
+    row._conflicts,
+    row._depth,
+    row._rev,
+    row._revisions,
+    row.id,
+    store,
+    upsertKulturModel,
+    user.email,
+  ])
+
+  return (
+    <History
+      rev={revRow._rev}
+      dataArray={dataArray}
+      onClickUebernehmen={onClickUebernehmen}
+    />
+  )
+}
+
+export default observer(HistoryRow)
---
+++ src/components/Data/Event/History/index.js
@@ -0,0 +1,141 @@
+import React, { useContext, useMemo } from 'react'
+import { observer } from 'mobx-react-lite'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+import Slider from 'react-slick'
+
+import { useQuery, StoreContext } from '../../../../models/reactUtils'
+import checkForOnlineError from '../../../../utils/checkForOnlineError'
+import Spinner from '../../../shared/Spinner'
+import Row from './Row'
+
+const eventRevQuery = gql`
+  query eventRevForHistoryQuery($rev: [String!]) {
+    event_rev(where: { _rev: { _in: $rev } }) {
+      id
+      __typename
+      event_id
+      kultur_id
+      kultur {
+        id
+        __typename
+        art {
+          id
+          __typename
+          art_ae_art {
+            id
+            __typename
+            name
+          }
+        }
+        garten {
+          id
+          __typename
+          name
+          person {
+            id
+            __typename
+            name
+            ort
+          }
+        }
+      }
+      teilkultur_id
+      teilkultur {
+        id
+        __typename
+        name
+      }
+      person_id
+      person {
+        id
+        __typename
+        name
+      }
+      beschreibung
+      geplant
+      datum
+      changed
+      changed_by
+      _rev
+      _parent_rev
+      _depth
+      _deleted
+    }
+  }
+`
+
+const Container = styled.div`
+  padding: 0 25px;
+  overflow: auto !important;
+  height: 100%;
+  .slick-prev:before,
+  .slick-next:before,
+  .slick-dots li button:before {
+    color: #4a148c;
+  }
+  .slick-prev {
+    left: -20px;
+  }
+  .slick-next {
+    right: -20px;
+  }
+  .slick-dots {
+    bottom: -10px;
+  }
+`
+const ErrorContainer = styled.div`
+  padding: 25px;
+`
+
+const sliderSettings = {
+  dots: true,
+  infinite: false,
+}
+
+const KulturHistory = ({ row, historyTakeoverCallback }) => {
+  const store = useContext(StoreContext)
+
+  // need to use this query to ensure that the person's name is queried
+  const { error, loading } = useQuery(eventRevQuery, {
+    variables: {
+      rev: row._revisions,
+    },
+  })
+  error && checkForOnlineError(error)
+
+  // need to grab store object to ensure this remains up to date
+  const revRows = useMemo(
+    () =>
+      [...store.event_revs.values()]
+        .filter((v) => row?._revisions?.includes(v._rev) ?? true)
+        .filter((r) => r._rev !== row._rev)
+        .sort((a, b) => b._depth - a._depth) || {},
+    [row._rev, row._revisions, store.event_revs],
+  )
+
+  if (loading) {
+    return <Spinner message="lade Versionen" />
+  }
+
+  if (error) {
+    return <ErrorContainer>{error.message}</ErrorContainer>
+  }
+
+  return (
+    <Container>
+      <Slider {...sliderSettings}>
+        {revRows.map((r) => (
+          <Row
+            key={row._rev}
+            revRow={r}
+            row={row}
+            historyTakeoverCallback={historyTakeoverCallback}
+          />
+        ))}
+      </Slider>
+    </Container>
+  )
+}
+
+export default observer(KulturHistory)
