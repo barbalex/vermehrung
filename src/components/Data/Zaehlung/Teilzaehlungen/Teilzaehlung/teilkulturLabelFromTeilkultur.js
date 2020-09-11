c80c71be2749b319660bbbb30efda663926559db
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 13:19:22 +0200

    add history to zaehlung

--- src/components/Data/Zaehlung/Teilzaehlungen/Teilzaehlung/teilkulturLabelFromTeilkultur.js
+++
@@ -1,9 +0,0 @@
-export default (tk) => {
-  if (tk?.name) return tk?.name
-  if (tk?.ort1) {
-    return `${tk?.ort1}${
-      tk?.ort2 ? ` / ${tk?.ort2}${tk?.ort3 ? ` / ${tk?.ort3}` : ''}` : ''
-    }`
-  }
-  return '(kein Name, kein Ort)'
-}
