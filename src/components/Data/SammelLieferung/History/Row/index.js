49e2fc736d341343c7b120512f6dc232793cf1ed
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 15:55:00 +0200

    implement history for sammel_lieferung

---
+++ src/components/Data/SammelLieferung/History/Row/index.js
@@ -0,0 +1,117 @@
+import React, { useCallback, useContext, useMemo } from 'react'
+import { observer } from 'mobx-react-lite'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
+
+import History from '../../../../shared/History'
+import { StoreContext } from '../../../../../models/reactUtils'
+import checkForOnlineError from '../../../../../utils/checkForOnlineError'
+import toPgArray from '../../../../../utils/toPgArray'
+import createDataArrayForRevComparison from '../../createDataArrayForRevComparison'
+
+const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
+  const store = useContext(StoreContext)
+  const { user, addNotification, upsertSammelLieferungModel } = store
+
+  const dataArray = useMemo(
+    () => createDataArrayForRevComparison({ row, revRow }),
+    [revRow, row],
+  )
+  const onClickUebernehmen = useCallback(async () => {
+    // need to attach to the winner, that is row
+    // otherwise risk to still have lower depth and thus loosing
+    const newDepth = row._depth + 1
+    const newObject = {
+      sammel_lieferung_id: revRow.sammel_lieferung_id,
+      art_id: revRow.art_id,
+      person_id: revRow.person_id,
+      von_sammlung_id: revRow.von_sammlung_id,
+      von_kultur_id: revRow.von_kultur_id,
+      datum: revRow.datum,
+      nach_kultur_id: revRow.nach_kultur_id,
+      nach_ausgepflanzt: revRow.nach_ausgepflanzt,
+      von_anzahl_individuen: revRow.von_anzahl_individuen,
+      anzahl_pflanzen: revRow.anzahl_pflanzen,
+      anzahl_auspflanzbereit: revRow.anzahl_auspflanzbereit,
+      gramm_samen: revRow.gramm_samen,
+      andere_menge: revRow.andere_menge,
+      geplant: revRow.geplant,
+      bemerkungen: revRow.bemerkungen,
+      _parent_rev: row._rev,
+      _depth: newDepth,
+      _deleted: revRow._deleted,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject.changed = new window.Date().toISOString()
+    newObject.changed_by = user.email
+    newObject._revisions = toPgArray([rev, ...row._revisions])
+    const newObjectForStore = { ...newObject }
+    //console.log('SammelLieferung History', { row, revRow, newObject })
+    try {
+      await store.mutateInsert_sammel_lieferung_rev_one({
+        object: newObject,
+        on_conflict: {
+          constraint: 'sammel_lieferung_rev_pkey',
+          update_columns: ['id'],
+        },
+      })
+    } catch (error) {
+      checkForOnlineError(error)
+      addNotification({
+        message: error.message,
+      })
+    }
+    historyTakeoverCallback()
+    // do not stringify revisions for store
+    // as _that_ is a real array
+    newObjectForStore._revisions = row._revisions
+      ? [rev, ...row._revisions]
+      : [rev]
+    // TODO: is this a good idea?
+    newObjectForStore._conflicts = row._conflicts
+    // for store: convert rev to winner
+    newObjectForStore.id = row.id
+    delete newObjectForStore.kultur_id
+    // optimistically update store
+    upsertSammelLieferungModel(newObjectForStore)
+  }, [
+    addNotification,
+    historyTakeoverCallback,
+    revRow._deleted,
+    revRow.andere_menge,
+    revRow.anzahl_auspflanzbereit,
+    revRow.anzahl_pflanzen,
+    revRow.art_id,
+    revRow.bemerkungen,
+    revRow.datum,
+    revRow.geplant,
+    revRow.gramm_samen,
+    revRow.nach_ausgepflanzt,
+    revRow.nach_kultur_id,
+    revRow.person_id,
+    revRow.sammel_lieferung_id,
+    revRow.von_anzahl_individuen,
+    revRow.von_kultur_id,
+    revRow.von_sammlung_id,
+    row._conflicts,
+    row._depth,
+    row._rev,
+    row._revisions,
+    row.id,
+    store,
+    upsertSammelLieferungModel,
+    user.email,
+  ])
+
+  return (
+    <History
+      rev={revRow._rev}
+      dataArray={dataArray}
+      onClickUebernehmen={onClickUebernehmen}
+    />
+  )
+}
+
+export default observer(HistoryRow)
