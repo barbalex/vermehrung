573140e2674ac87931fdca9c99787f2e22fc1d80
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 16:31:34 +0200

    add history to art

---
+++ src/components/Data/Art/Form/Timeline/Pflanzen/LabelZaehlung.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text } from 'recharts'
+
+const LabelZaehlung = ({ x, y, stroke, value }) => {
+  if (value === undefined) return null
+  return (
+    <Text
+      x={x}
+      y={y}
+      dy={-20}
+      dx={3}
+      angle={-90}
+      fill={stroke}
+      fontSize={10}
+      fontWeight={700}
+      textAnchor="middle"
+    >
+      {value}
+    </Text>
+  )
+}
+
+export default LabelZaehlung
