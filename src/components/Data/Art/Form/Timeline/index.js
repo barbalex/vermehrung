ce2541559191b6f619ab56852279aa1b512ef338
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 16:31:42 +0200

    add history to art

---
+++ src/components/Data/Art/Form/Timeline/index.js
@@ -0,0 +1,108 @@
+import React, { useCallback, useState, useRef, useEffect } from 'react'
+import styled from 'styled-components'
+import { observer } from 'mobx-react-lite'
+import { IoMdInformationCircleOutline } from 'react-icons/io'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
+import IconButton from '@material-ui/core/IconButton'
+
+import Pflanzen from './Pflanzen'
+import ErrorBoundary from '../../../../shared/ErrorBoundary'
+import getConstants from '../../../../../utils/constants'
+
+const constants = getConstants()
+
+const TitleRow = styled.div`
+  background-color: rgba(237, 230, 244, 1);
+  flex-shrink: 0;
+  display: flex;
+  height: 48px;
+  justify-content: space-between;
+  margin-left: -10px;
+  margin-right: -10px;
+  margin-bottom: 10px;
+  padding: 0 10px;
+  cursor: pointer;
+  user-select: none;
+  ${(props) => props['data-open'] && 'position: sticky;'}
+  ${(props) =>
+    props['data-sticky'] &&
+    'border-top: 1px solid rgba(0, 0, 0, 0.3);'}
+  top: -10px;
+  z-index: 1;
+  &:first-of-type {
+    margin-top: -10px;
+  }
+`
+const Title = styled.div`
+  font-weight: bold;
+  margin-top: auto;
+  margin-bottom: auto;
+`
+
+const TimelineArea = ({ artId = '99999999-9999-9999-9999-999999999999' }) => {
+  const [open, setOpen] = useState(false)
+
+  const openDocs = useCallback(() => {
+    const url = `${constants?.appUri}/Dokumentation/Zeitachse-Art`
+    if (typeof window !== 'undefined') {
+      if (window.matchMedia('(display-mode: standalone)').matches) {
+        return window.open(url, '_blank', 'toolbar=no')
+      }
+      window.open(url)
+    }
+  }, [])
+  const onClickToggle = useCallback(
+    (e) => {
+      e.stopPropagation()
+      setOpen(!open)
+    },
+    [open],
+  )
+
+  const titleRowRef = useRef(null)
+  const [isSticky, setIsSticky] = useState(false)
+  const scrollHandler = useCallback(() => {
+    const { top } = titleRowRef?.current?.getBoundingClientRect()
+    if (top < 112 && !isSticky) return setIsSticky(true)
+    if (top > 112 && isSticky) setIsSticky(false)
+  }, [isSticky])
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler, true)
+    return () => {
+      window.removeEventListener('scroll', scrollHandler, true)
+    }
+  }, [scrollHandler])
+
+  return (
+    <ErrorBoundary>
+      <TitleRow
+        onClick={onClickToggle}
+        title={open ? 'schliessen' : 'öffnen'}
+        data-open={open}
+        ref={titleRowRef}
+        data-sticky={isSticky}
+      >
+        <Title>Zeit-Achse</Title>
+        <div>
+          <IconButton
+            aria-label="Anleitung öffnen"
+            title="Anleitung öffnen"
+            onClick={openDocs}
+          >
+            <IoMdInformationCircleOutline />
+          </IconButton>
+          <IconButton
+            aria-label={open ? 'schliessen' : 'öffnen'}
+            title={open ? 'schliessen' : 'öffnen'}
+            onClick={onClickToggle}
+          >
+            {open ? <FaChevronUp /> : <FaChevronDown />}
+          </IconButton>
+        </div>
+      </TitleRow>
+      {open && <Pflanzen key={artId} artId={artId} />}
+    </ErrorBoundary>
+  )
+}
+
+export default observer(TimelineArea)
