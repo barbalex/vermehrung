469fdef5c4883b764a3442280877b1a7fb57d058
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 11:11:33 +0200

    refactor, mobile first

---
+++ src/components/Data/Event/FormTitle/Settings/index.js
@@ -0,0 +1,52 @@
+import React, { useCallback, useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import IconButton from '@material-ui/core/IconButton'
+import MenuItem from '@material-ui/core/MenuItem'
+import { FaCog } from 'react-icons/fa'
+
+import ErrorBoundary from '../../../../shared/ErrorBoundary'
+import Menu from './Menu'
+
+const SettingsEvents = ({ asMenu, kulturId }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const onClickConfig = useCallback(
+    (event) => setAnchorEl(event.currentTarget),
+    [],
+  )
+
+  if (asMenu) {
+    return (
+      <>
+        <MenuItem
+          aria-owns={anchorEl ? 'menu' : null}
+          aria-haspopup="true"
+          onClick={onClickConfig}
+        >
+          Optionen wählen
+        </MenuItem>
+        <Menu
+          anchorEl={anchorEl}
+          setAnchorEl={setAnchorEl}
+          kulturId={kulturId}
+        />
+      </>
+    )
+  }
+
+  return (
+    <ErrorBoundary>
+      <IconButton
+        aria-label="Felder wählen"
+        aria-owns={anchorEl ? 'long-menu' : null}
+        aria-haspopup="true"
+        title="Felder wählen"
+        onClick={onClickConfig}
+      >
+        <FaCog />
+      </IconButton>
+      <Menu anchorEl={anchorEl} setAnchorEl={setAnchorEl} kulturId={kulturId} />
+    </ErrorBoundary>
+  )
+}
+
+export default observer(SettingsEvents)
