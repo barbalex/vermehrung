f1212cac25a9909d7a20ffc9dd9024f26de1d5d9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 11:02:00 +0200

    refactor

---
+++ src/components/Data/Event/FormTitle/index.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { StoreContext } from '../../../../models/reactUtils'
+import FilterTitle from '../../../shared/FilterTitle'
+import FormTitle from './FormTitle'
+
+const EventFormTitle = ({ row, showFilter, showHistory, setShowHistory }) => {
+  const store = useContext(StoreContext)
+  const { kulturIdInActiveNodeArray, eventsSorted, eventsFiltered } = store
+
+  const hierarchyFilter = (e) => {
+    if (kulturIdInActiveNodeArray)
+      return e.kultur_id === kulturIdInActiveNodeArray
+    return true
+  }
+  const totalNr = eventsSorted.filter(hierarchyFilter).length
+  const filteredNr = eventsFiltered.filter(hierarchyFilter).length
+
+  if (showFilter) {
+    return (
+      <FilterTitle
+        title="Event"
+        table="event"
+        totalNr={totalNr}
+        filteredNr={filteredNr}
+      />
+    )
+  }
+
+  return (
+    <FormTitle
+      row={row}
+      totalNr={totalNr}
+      filteredNr={filteredNr}
+      showHistory={showHistory}
+      setShowHistory={setShowHistory}
+    />
+  )
+}
+
+export default observer(EventFormTitle)
