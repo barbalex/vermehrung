fbc81231b12d1eb25eaeb5b386683bddd63bca11
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-11 16:41:57 +0200

    refactor

---
+++ src/components/Data/Kultur/History/Row.js
@@ -0,0 +1,105 @@
+import React, { useCallback, useContext, useMemo } from 'react'
+import { observer } from 'mobx-react-lite'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
+
+import History from '../../../shared/History'
+import { StoreContext } from '../../../../models/reactUtils'
+import checkForOnlineError from '../../../../utils/checkForOnlineError'
+import toPgArray from '../../../../utils/toPgArray'
+import createDataArrayForRevComparison from '../createDataArrayForRevComparison'
+
+const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
+  const store = useContext(StoreContext)
+  const { user, addNotification, upsertKulturModel } = store
+
+  const dataArray = useMemo(
+    () => createDataArrayForRevComparison({ row, revRow }),
+    [revRow, row],
+  )
+  const onClickUebernehmen = useCallback(async () => {
+    // need to attach to the winner, that is row
+    // otherwise risk to still have lower depth and thus loosing
+    const newDepth = row._depth + 1
+    const newObject = {
+      kultur_id: revRow.kultur_id,
+      art_id: revRow.art_id,
+      herkunft_id: revRow.herkunft_id,
+      garten_id: revRow.garten_id,
+      zwischenlager: revRow.zwischenlager,
+      erhaltungskultur: revRow.erhaltungskultur,
+      von_anzahl_individuen: revRow.von_anzahl_individuen,
+      bemerkungen: revRow.bemerkungen,
+      aktiv: revRow.aktiv,
+      _parent_rev: row._rev,
+      _depth: newDepth,
+      _deleted: revRow._deleted,
+    }
+    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+    newObject._rev = rev
+    newObject.id = uuidv1()
+    newObject.changed = new window.Date().toISOString()
+    newObject.changed_by = user.email
+    newObject._revisions = toPgArray([rev, ...row._revisions])
+    const newObjectForStore = { ...newObject }
+    //console.log('Kultur History', { row, revRow, newObject })
+    try {
+      await store.mutateInsert_kultur_rev_one({
+        object: newObject,
+        on_conflict: {
+          constraint: 'kultur_rev_pkey',
+          update_columns: ['id'],
+        },
+      })
+    } catch (error) {
+      checkForOnlineError(error)
+      addNotification({
+        message: error.message,
+      })
+    }
+    historyTakeoverCallback()
+    // do not stringify revisions for store
+    // as _that_ is a real array
+    newObjectForStore._revisions = row._revisions
+      ? [rev, ...row._revisions]
+      : [rev]
+    // TODO: is this a good idea?
+    newObjectForStore._conflicts = row._conflicts
+    // for store: convert rev to winner
+    newObjectForStore.id = row.id
+    delete newObjectForStore.kultur_id
+    // optimistically update store
+    upsertKulturModel(newObjectForStore)
+  }, [
+    addNotification,
+    historyTakeoverCallback,
+    revRow._deleted,
+    revRow.aktiv,
+    revRow.art_id,
+    revRow.bemerkungen,
+    revRow.erhaltungskultur,
+    revRow.garten_id,
+    revRow.herkunft_id,
+    revRow.kultur_id,
+    revRow.von_anzahl_individuen,
+    revRow.zwischenlager,
+    row._conflicts,
+    row._depth,
+    row._rev,
+    row._revisions,
+    row.id,
+    store,
+    upsertKulturModel,
+    user.email,
+  ])
+
+  return (
+    <History
+      rev={revRow._rev}
+      dataArray={dataArray}
+      onClickUebernehmen={onClickUebernehmen}
+    />
+  )
+}
+
+export default observer(HistoryRow)
--- src/components/Data/Lieferung/Lieferung/History/Row.js
+++ src/components/Data/Lieferung/Lieferung/History/Row.js
@@ -7,11 +7,11 @@ import History from '../../../../shared/History'
 import { StoreContext } from '../../../../../models/reactUtils'
 import checkForOnlineError from '../../../../../utils/checkForOnlineError'
 import toPgArray from '../../../../../utils/toPgArray'
-import createDataArrayForRevComparison from '../../createDataArrayForRevComparison'
+import createDataArrayForRevComparison from '../createDataArrayForRevComparison'
 
 const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
   const store = useContext(StoreContext)
-  const { user, addNotification, upsertHerkunftModel } = store
+  const { user, addNotification, upsertLieferungModel } = store
 
   const dataArray = useMemo(
     () => createDataArrayForRevComparison({ row, revRow }),
@@ -22,13 +22,21 @@ const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
     // otherwise risk to still have lower depth and thus loosing
     const newDepth = row._depth + 1
     const newObject = {
-      herkunft_id: revRow.herkunft_id,
-      nr: revRow.nr,
-      lokalname: revRow.lokalname,
-      gemeinde: revRow.gemeinde,
-      kanton: revRow.kanton,
-      land: revRow.land,
-      geom_point: revRow.geom_point,
+      lieferung_id: revRow.lieferung_id,
+      sammel_lieferung_id: revRow.sammel_lieferung_id,
+      art_id: revRow.art_id,
+      person_id: revRow.person_id,
+      von_sammlung_id: revRow.von_sammlung_id,
+      von_kultur_id: revRow.von_kultur_id,
+      datum: revRow.datum,
+      nach_kultur_id: revRow.nach_kultur_id,
+      nach_ausgepflanzt: revRow.nach_ausgepflanzt,
+      von_anzahl_individuen: revRow.von_anzahl_individuen,
+      anzahl_pflanzen: revRow.anzahl_pflanzen,
+      anzahl_auspflanzbereit: revRow.anzahl_auspflanzbereit,
+      gramm_samen: revRow.gramm_samen,
+      andere_menge: revRow.andere_menge,
+      geplant: revRow.geplant,
       bemerkungen: revRow.bemerkungen,
       _parent_rev: row._rev,
       _depth: newDepth,
@@ -41,12 +49,12 @@ const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
     newObject.changed_by = user.email
     newObject._revisions = toPgArray([rev, ...row._revisions])
     const newObjectForStore = { ...newObject }
-    //console.log('Herkunft History', { row, revRow, newObject })
+    //console.log('Lieferung History', { row, revRow, newObject })
     try {
-      await store.mutateInsert_herkunft_rev_one({
+      await store.mutateInsert_lieferung_rev_one({
         object: newObject,
         on_conflict: {
-          constraint: 'herkunft_rev_pkey',
+          constraint: 'lieferung_rev_pkey',
           update_columns: ['id'],
         },
       })
@@ -66,28 +74,36 @@ const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
     newObjectForStore._conflicts = row._conflicts
     // for store: convert rev to winner
     newObjectForStore.id = row.id
-    delete newObjectForStore.herkunft_id
+    delete newObjectForStore.lieferung_id
     // optimistically update store
-    upsertHerkunftModel(newObjectForStore)
+    upsertLieferungModel(newObjectForStore)
   }, [
     addNotification,
     historyTakeoverCallback,
     revRow._deleted,
+    revRow.andere_menge,
+    revRow.anzahl_auspflanzbereit,
+    revRow.anzahl_pflanzen,
+    revRow.art_id,
     revRow.bemerkungen,
-    revRow.gemeinde,
-    revRow.geom_point,
-    revRow.herkunft_id,
-    revRow.kanton,
-    revRow.land,
-    revRow.lokalname,
-    revRow.nr,
+    revRow.datum,
+    revRow.geplant,
+    revRow.gramm_samen,
+    revRow.lieferung_id,
+    revRow.nach_ausgepflanzt,
+    revRow.nach_kultur_id,
+    revRow.person_id,
+    revRow.sammel_lieferung_id,
+    revRow.von_anzahl_individuen,
+    revRow.von_kultur_id,
+    revRow.von_sammlung_id,
     row._conflicts,
     row._depth,
     row._rev,
     row._revisions,
     row.id,
     store,
-    upsertHerkunftModel,
+    upsertLieferungModel,
     user.email,
   ])
 
