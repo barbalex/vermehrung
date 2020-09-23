ab2536e011d5eb2945d0f70837b6e2f85c0b1aa6
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-23 10:51:01 +0200

    use luxon

--- src/components/Data/Lieferung/Lieferung/sammlungLabelFromSammlung.js
+++ src/components/Data/Lieferung/Lieferung/sammlungLabelFromSammlung.js
@@ -1,7 +1,9 @@
-import moment from 'moment'
+import { DateTime } from 'luxon'
 
 export default (s) => {
-  const datum = s?.datum ? moment(s.datum).format('DD.MM.YYYY') : '(kein Datum)'
+  const datum = s?.datum
+    ? DateTime.fromSQL(s.datum).toFormat('dd.LL.yyyy')
+    : '(kein Datum)'
   const nr = s?.herkunft?.nr ?? '(keine Nr)'
   const person = s?.person?.fullname ?? '(kein Name)'
   const label = `${datum}: Herkunft ${nr}; ${person}`
