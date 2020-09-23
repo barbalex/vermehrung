49b7ace84950e3105e7b1095ca14062475d6d634
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-23 23:28:43 +0200

    use luxon

--- src/components/Tree/nodes/person/lieferung/index.js
+++ src/components/Tree/nodes/person/lieferung/index.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import { DateTime } from 'luxon'
 
 import exists from '../../../../../utils/exists'
 
@@ -24,7 +24,7 @@ export default ({ store }) => {
     return lieferungen
       .map((el) => {
         const datum = el.datum
-          ? moment(el.datum, 'YYYY-MM-DD').format('YYYY.MM.DD')
+          ? DateTime.fromSQL(el.datum).toFormat('yyyy.LL.dd')
           : 'kein Datum'
         const anz = exists(el.anzahl_pflanzen) ? el.anzahl_pflanzen : '_'
         const anzAb = exists(el.anzahl_auspflanzbereit)
