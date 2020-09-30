cb2fb64ee3dc876f73cfdbfbcce56bfe02409026
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-30 18:09:33 +0200

    fetch directly

--- src/utils/treeLabelKultur.js
+++ src/utils/treeLabelKultur.js
@@ -1,9 +1,42 @@
-export default (el) => {
-  const garten =
-    el?.garten?.name ?? `${el?.garten?.person?.fullname ?? 'kein Name'}`
-  const art = el?.art?.art_ae_art?.name ?? '(keine Art)'
-  const herkunft = el?.herkunft?.nr ?? '(Herkunft ohne Nr)'
-  const zwischenlager = el?.zwischenlager ? `, Zwischenlager` : ''
+export default ({ kultur, store }) => {
+  let garten
+  if (kultur.garten_id) {
+    garten = store.gartens.get(kultur.garten_id)
+  }
+  let gartenName = ''
+  if (garten && garten.name) {
+    gartenName = garten.name
+  }
+  if (!gartenName && garten?.person_id) {
+    const person = store.persons.get(garten.person_id)
+    if (person && person.fullname) {
+      gartenName = person.fullname
+    } else {
+      gartenName = 'kein Name'
+    }
+  }
 
-  return `${art}, von: ${herkunft}, in: ${garten}${zwischenlager}`
+  let art
+  if (kultur?.art_id) {
+    art = store.arts.get(kultur.art_id)
+  }
+  let aeArt
+  if (art?.ae_id) {
+    aeArt = store.ae_arts.get(art.ae_id)
+  }
+  let artName = '(keine Art)'
+  if (art && aeArt && aeArt.name) {
+    artName = aeArt.name
+  }
+
+  let herkunftName = '(Herkunft ohne Nr)'
+  if (kultur.herkunft_id) {
+    const herkunft = store.herkunfts.get(kultur.herkunft_id)
+    if (herkunft.nr) {
+      herkunftName = herkunft.nr
+    }
+  }
+  const zwischenlager = kultur?.zwischenlager ? `, Zwischenlager` : ''
+
+  return `${artName}, von: ${herkunftName}, in: ${gartenName}${zwischenlager}`
 }
