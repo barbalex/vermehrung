cf3a0400dd7b84d2007fa359a138aec1226d3185
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-01 17:16:48 +0200

    fetch directly

--- src/utils/artSort.js
+++ src/utils/artSort.js
@@ -1,8 +1,8 @@
 import artLabelFromArt from './artLabelFromArt'
 
 export default ({ a, b, store }) => {
-  const nameA = artLabelFromArt({ art: a, store }).toLowerCase() ?? ''
-  const nameB = b?.art_ae_art?.name?.toLowerCase() ?? ''
+  const nameA = artLabelFromArt({ art: a, store }).toLowerCase()
+  const nameB = artLabelFromArt({ art: b, store }).toLowerCase()
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
 
--- src/utils/sammlungSort.js
+++ src/utils/sammlungSort.js
@@ -1,40 +1,46 @@
+import artLabelFromSammlung from './artLabelFromSammlung'
+
 export default ({ a, b, store }) => {
   const nrA = a?.nr?.toString()?.toLowerCase() ?? ''
   const nrB = b?.nr?.toString()?.toLowerCase() ?? ''
   if (nrA < nrB) return -1
   if (nrA > nrB) return 1
 
-  const datumA = new Date(a?.datum ?? '')
-  const datumB = new Date(b?.datum ?? '')
+  const datumA = a?.datum ? new Date(a?.datum) : ''
+  const datumB = b?.datum ? new Date(b?.datum) : ''
   if (datumA < datumB) return -1
   if (datumA > datumB) return 1
 
-  const herkunftNrA = a?.herkunft?.nr?.toString()?.toLowerCase() ?? ''
-  const herkunftNrB = b?.herkunft?.nr?.toString()?.toLowerCase() ?? ''
+  const herkunftA = a.herkunft_id ? store.herkunfts.get(a.herkunft_id) : {}
+  const herkunftB = b.herkunft_id ? store.herkunfts.get(b.herkunft_id) : {}
+
+  const herkunftNrA = herkunftA?.nr?.toString()?.toLowerCase() ?? ''
+  const herkunftNrB = herkunftB?.nr?.toString()?.toLowerCase() ?? ''
   if (herkunftNrA < herkunftNrB) return -1
   if (herkunftNrA > herkunftNrB) return 1
 
-  const herkunftGemeindeA =
-    a?.herkunft?.gemeinde?.toString()?.toLowerCase() ?? ''
-  const herkunftGemeindeB =
-    b?.herkunft?.gemeinde?.toString()?.toLowerCase() ?? ''
+  const herkunftGemeindeA = herkunftA?.gemeinde?.toString()?.toLowerCase() ?? ''
+  const herkunftGemeindeB = herkunftB?.gemeinde?.toString()?.toLowerCase() ?? ''
   if (herkunftGemeindeA < herkunftGemeindeB) return -1
   if (herkunftGemeindeA > herkunftGemeindeB) return 1
 
   const herkunftLokalnameA =
-    a?.herkunft?.lokalname?.toString()?.toLowerCase() ?? ''
+    herkunftA?.lokalname?.toString()?.toLowerCase() ?? ''
   const herkunftLokalnameB =
-    b?.herkunft?.lokalname?.toString()?.toLowerCase() ?? ''
+    herkunftB?.lokalname?.toString()?.toLowerCase() ?? ''
   if (herkunftLokalnameA < herkunftLokalnameB) return -1
   if (herkunftLokalnameA > herkunftLokalnameB) return 1
 
-  const personNameA = a?.person?.fullname?.toString()?.toLowerCase() ?? ''
-  const personNameB = b?.person?.fullname?.toString()?.toLowerCase() ?? ''
+  const personA = a.person_id ? store.persons.get(a.person_id) : {}
+  const personB = b.person_id ? store.persons.get(b.person_id) : {}
+
+  const personNameA = personA?.fullname?.toString()?.toLowerCase() ?? ''
+  const personNameB = personB?.fullname?.toString()?.toLowerCase() ?? ''
   if (personNameA < personNameB) return -1
   if (personNameA > personNameB) return 1
 
-  const artNameA = a?.art?.art_ae_art?.name?.toLowerCase() ?? ''
-  const artNameB = b?.art?.art_ae_art?.name?.toLowerCase() ?? ''
+  const artNameA = artLabelFromSammlung({ sammlung: a, store })
+  const artNameB = artLabelFromSammlung({ sammlung: b, store })
   if (artNameA < artNameB) return -1
   if (artNameA > artNameB) return 1
 
