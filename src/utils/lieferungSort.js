694991742b849fb44a4291540f3542675fc8ece3
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-01 18:44:04 +0200

    fetch directly

--- src/utils/lieferungSort.js
+++ src/utils/lieferungSort.js
@@ -1,6 +1,6 @@
 export default ({ a, b }) => {
-  const datumA = new Date(a?.datum ?? '')
-  const datumB = new Date(b?.datum ?? '')
+  const datumA = a?.datum ? new Date(a.datum) : ''
+  const datumB = b?.datum ? new Date(b.datum) : ''
   if (datumA < datumB) return -1
   if (datumA > datumB) return 1
 
