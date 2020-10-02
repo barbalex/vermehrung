606b5f3edb6724a04150a4d4f033e46c7dad8277
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-02 18:12:37 +0200

    fetch directly

--- src/components/Tree/nodes/person/garten/index.js
+++ src/components/Tree/nodes/person/garten/index.js
@@ -1,3 +1,5 @@
+import gartenLabelFromGarten from '../../../../../utils/gartenLabelFromGarten'
+
 export default ({ store }) => {
   const { showPerson, visibleOpenNodes, person } = store.tree
   if (!showPerson) return []
@@ -18,24 +20,15 @@ export default ({ store }) => {
     )
 
     return gaerten
-      .map((el) => {
-        let label = el.name
-        if (!label && el.person_id) {
-          const person = store.persons.get(el.person_id)
-          if (person) label = person?.fullname
-        }
-        if (!label) label = 'kein Name'
-
-        return {
-          nodeType: 'table',
-          menuTitle: 'Garten',
-          table: 'garten',
-          id: `${personId}${el.id}`,
-          label,
-          url: ['Personen', personId, 'Gaerten', el.id],
-          hasChildren: true,
-        }
-      })
+      .map((el) => ({
+        nodeType: 'table',
+        menuTitle: 'Garten',
+        table: 'garten',
+        id: `${personId}${el.id}`,
+        label: gartenLabelFromGarten({ garten: el, store }),
+        url: ['Personen', personId, 'Gaerten', el.id],
+        hasChildren: true,
+      }))
       .map((el, index) => {
         el.sort = [11, personIndex, 2, index]
         return el
--- src/utils/lieferungSort.js
+++ src/utils/lieferungSort.js
@@ -4,5 +4,10 @@ export default ({ a, b }) => {
   if (datumA < datumB) return -1
   if (datumA > datumB) return 1
 
+  const anzahlPflanzenA = a?.anzahl_pflanzen ?? ''
+  const anzahlPflanzenB = b?.anzahl_pflanzen ?? ''
+  if (anzahlPflanzenA < anzahlPflanzenB) return -1
+  if (anzahlPflanzenA > anzahlPflanzenB) return 1
+
   return 0
 }
