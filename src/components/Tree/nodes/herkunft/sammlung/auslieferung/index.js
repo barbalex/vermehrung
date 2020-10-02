4654b8c89bd22fea92f0fe0097d3e548338c6266
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-02 18:12:43 +0200

    fetch directly

--- src/components/Tree/nodes/herkunft/sammlung/auslieferung/index.js
+++ src/components/Tree/nodes/herkunft/sammlung/auslieferung/index.js
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon'
+import lieferungLabelFromLieferung from '../../../../../../utils/lieferungLabelFromLieferung'
 
 export default ({ store }) => {
   const {
@@ -32,35 +32,22 @@ export default ({ store }) => {
     )
 
     return lieferungen
-      .map((el) => {
-        const datum = el.datum
-          ? DateTime.fromSQL(el.datum).toFormat('yyyy.LL.dd')
-          : 'kein Datum'
-        const anz = el.anzahl_pflanzen ?? '_'
-        const anzAb = el.anzahl_auspflanzbereit ?? '_'
-        const numbers = `${anz
-          .toString()
-          .padStart(3, '_')}/${anzAb.toString().padStart(3, '_')}`
-        const geplant = el.geplant ? ' (geplant)' : ''
-        const label = `${datum}: ${numbers}${geplant}`
-
-        return {
-          nodeType: 'table',
-          menuTitle: 'Aus-Lieferung',
-          table: 'lieferung',
-          id: `${herkunftId}${sammlungId}${el.id}`,
-          label,
-          url: [
-            'Herkuenfte',
-            herkunftId,
-            'Sammlungen',
-            sammlungId,
-            'Aus-Lieferungen',
-            el.id,
-          ],
-          hasChildren: false,
-        }
-      })
+      .map((el) => ({
+        nodeType: 'table',
+        menuTitle: 'Aus-Lieferung',
+        table: 'lieferung',
+        id: `${herkunftId}${sammlungId}${el.id}`,
+        label: lieferungLabelFromLieferung({ lieferung: el }),
+        url: [
+          'Herkuenfte',
+          herkunftId,
+          'Sammlungen',
+          sammlungId,
+          'Aus-Lieferungen',
+          el.id,
+        ],
+        hasChildren: false,
+      }))
       .map((el, index) => {
         el.sort = [2, herkunftIndex, 2, sammlungIndex, 1, index]
         return el
