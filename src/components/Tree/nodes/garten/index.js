3e4c7b12fc0894fad5abb76035f0b18b646318a3
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-02 18:12:52 +0200

    fetch directly

--- src/components/Tree/nodes/garten/index.js
+++ src/components/Tree/nodes/garten/index.js
@@ -1,5 +1,7 @@
 import isEqual from 'lodash/isEqual'
 
+import gartenLabelFromGarten from '../../../../utils/gartenLabelFromGarten'
+
 export default ({ store }) => {
   const { showGarten, visibleOpenNodes } = store.tree
   if (!showGarten) return []
@@ -8,19 +10,15 @@ export default ({ store }) => {
     store.gartensFiltered
       // only show if parent node exists
       .filter(() => visibleOpenNodes.some((node) => isEqual(['Gaerten'], node)))
-      .map((el) => {
-        const person = el.person_id ? store.persons.get(el.person_id) : {}
-
-        return {
-          nodeType: 'table',
-          menuTitle: 'Garten',
-          table: 'garten',
-          id: el.id,
-          label: el.name ?? `${person?.fullname ?? 'kein Name'}`,
-          url: ['Gaerten', el.id],
-          hasChildren: true,
-        }
-      })
+      .map((el) => ({
+        nodeType: 'table',
+        menuTitle: 'Garten',
+        table: 'garten',
+        id: el.id,
+        label: gartenLabelFromGarten({ garten: el, store }),
+        url: ['Gaerten', el.id],
+        hasChildren: true,
+      }))
       .map((el, index) => {
         el.sort = [4, index]
         return el
