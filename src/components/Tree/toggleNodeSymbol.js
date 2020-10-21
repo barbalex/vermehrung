c6a65f4c88fc827ca1d653657b6ae6e63bacbb2a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-21 15:56:45 +0200

    enhance tree behaviour

--- src/components/Tree/toggleNodeSymbol.js
+++ src/components/Tree/toggleNodeSymbol.js
@@ -2,9 +2,11 @@ import isEqual from 'lodash/isEqual'
 import { getSnapshot } from 'mobx-state-tree'
 
 import isNodeOpen from './isNodeOpen'
+import isNodeInActiveNodePath from './isNodeInActiveNodePath'
 
 export default ({ node, store }) => {
   const { addNotification } = store
+  const { setActiveNodeArray, activeNodeArray } = store.tree
   if (!node.url) {
     console.log('passsed node has no url:', node)
     return addNotification({
@@ -20,6 +22,13 @@ export default ({ node, store }) => {
   if (isNodeOpen({ store, url: node.url })) {
     newOpenNodes = newOpenNodes.filter((n) => !isEqual(n, node.url))
     setOpenNodes(newOpenNodes)
+    if (isNodeInActiveNodePath({ node, activeNodeArray })) {
+      // when a user closes a folder in the active node path
+      // the active node should swith to the node's parent
+      const newActiveNodeArray = [...node.url]
+      newActiveNodeArray.pop()
+      setActiveNodeArray(newActiveNodeArray)
+    }
   } else {
     addOpenNodes([node.url])
   }
