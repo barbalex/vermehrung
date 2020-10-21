4b331f8210cc646ce1196f537b1579d1633bd198
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-21 15:10:30 +0200

    label better

--- src/utils/zaehlungLabelFromZaehlung.js
+++ src/utils/zaehlungLabelFromZaehlung.js
@@ -1,23 +1,31 @@
 import { DateTime } from 'luxon'
 
 export default ({ zaehlung }) => {
-  const datum = zaehlung.datum
+  const datumLabel = zaehlung.datum
     ? DateTime.fromSQL(zaehlung.datum).toFormat('yyyy.LL.dd')
-    : 'kein Datum'
-  const anz =
-    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_pflanzen ?? '-'
-  const anzAb =
-    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_auspflanzbereit ??
-    '-'
-  const anzMu =
-    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_mutterpflanzen ??
-    '-'
-  const numbers = `${anz
-    .toString()
-    .padStart(3, '\u00A0')}/${anzAb
-    .toString()
-    .padStart(3, '\u00A0')}/${anzMu.toString().padStart(3, '\u00A0')}`
-  const prognose = zaehlung.prognose ? ' (Prognose)' : ''
+    : 'Kein Datum'
 
-  return `${datum}: ${numbers}${prognose}`
+  const anz = (
+    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_pflanzen ?? ''
+  )
+    .toString()
+    .padStart(3, '_')
+  const anzAb = (
+    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_auspflanzbereit ??
+    ''
+  )
+    .toString()
+    .padStart(3, '_')
+  const anzMu = (
+    zaehlung?.teilzaehlungs_aggregate?.aggregate?.sum?.anzahl_mutterpflanzen ??
+    ''
+  )
+    .toString()
+    .padStart(3, '_')
+  const numbers = `${anz}/${anzAb}/${anzMu}`
+
+  const prognose = zaehlung.prognose ? 'Prognose' : ''
+  const label = [datumLabel, numbers, prognose].filter((e) => !!e).join('; ')
+
+  return label
 }
