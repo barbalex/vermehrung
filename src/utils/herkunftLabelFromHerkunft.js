5c59428d787090b173de44a77ed265bfb97f67b5
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-21 15:10:42 +0200

    label better

--- src/utils/herkunftLabelFromHerkunft.js
+++ src/utils/herkunftLabelFromHerkunft.js
@@ -1,12 +1,12 @@
 export default ({ herkunft }) => {
-  if (!herkunft) return '(keine Herkunft)'
-  if (!herkunft?.id) return '(keine Herkunft)'
+  if (!herkunft) return 'keine Herkunft'
+  if (!herkunft?.id) return 'keine Herkunft'
   // only show lokalname if exist
   // does not exist if user does not have right to see it
-  const gemeinde = herkunft?.gemeinde ?? ''
-  const lokalname = herkunft?.lokalname ?? ''
-  const nr = herkunft?.nr ?? '(keine Nr.)'
-  const label = [nr, gemeinde, lokalname].filter((e) => !!e).join(', ')
+  const gemeinde = herkunft?.gemeinde ?? 'keine Gemeinde'
+  const lokalname = herkunft?.lokalname
+  const nr = herkunft?.nr ?? 'keine Nr.'
+  const label = [gemeinde, lokalname, nr].filter((e) => !!e).join('; ')
 
   return label
 }
--- src/utils/herkunftSort.js
+++ src/utils/herkunftSort.js
@@ -1,9 +1,4 @@
 export default ({ a, b }) => {
-  const nrA = a?.nr?.toString()?.toLowerCase() ?? ''
-  const nrB = b?.nr?.toString()?.toLowerCase() ?? ''
-  if (nrA < nrB) return -1
-  if (nrA > nrB) return 1
-
   const gemeindeA = a?.gemeinde?.toString()?.toLowerCase() ?? ''
   const gemeindeB = b?.gemeinde?.toString()?.toLowerCase() ?? ''
   if (gemeindeA < gemeindeB) return -1
@@ -13,6 +8,11 @@ export default ({ a, b }) => {
   const lokalnameB = b?.lokalname?.toString()?.toLowerCase() ?? ''
   if (lokalnameA < lokalnameB) return -1
   if (lokalnameA > lokalnameB) return 1
+
+  const nrA = a?.nr?.toString()?.toLowerCase() ?? ''
+  const nrB = b?.nr?.toString()?.toLowerCase() ?? ''
+  if (nrA < nrB) return -1
+  if (nrA > nrB) return 1
 
   return 0
 }
