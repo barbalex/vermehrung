b37eeaafb76f32cb2ccc3b1a7dc3231c491bc465
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-28 11:59:01 +0100

    optimize

--- src/components/Data/Event/Form/index.js
+++ src/components/Data/Event/Form/index.js
@@ -264,7 +264,7 @@ const EventForm = ({
               </div>
             </FieldRow>
           )}
-          {online && !showFilter && row._conflicts && row._conflicts.map && (
+          {online && !showFilter && row?._conflicts?.map && (
             <ConflictList
               conflicts={row._conflicts}
               activeConflict={activeConflict}
