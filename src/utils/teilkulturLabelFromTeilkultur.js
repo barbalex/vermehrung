f5d357d1b3593c08b82279de4b53326d9a9f4860
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-09 22:06:05 +0100

    lint

--- src/utils/teilkulturLabelFromTeilkultur.js
+++ src/utils/teilkulturLabelFromTeilkultur.js
@@ -1,5 +1,7 @@
-export default ({ teilkultur }) => {
+const teilkulturLabelFromTeilkultur = ({ teilkultur }) => {
   if (!teilkultur) return 'keine Teilkultur'
 
   return teilkultur?.name || 'kein Name'
 }
+
+export default teilkulturLabelFromTeilkultur
