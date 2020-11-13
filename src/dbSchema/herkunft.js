4c85f8b7a44e1ca951baa666f19c0ae54e9a054a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-13 22:04:55 +0100

    work on wm

--- src/dbSchema/herkunft.js
+++ src/dbSchema/herkunft.js
@@ -10,9 +10,7 @@ const herkunft = {
     { name: 'gemeinde', type: 'string', isOptional: true },
     { name: 'kanton', type: 'string', isOptional: true },
     { name: 'land', type: 'string', isOptional: true },
-    // TODO:
-    // https://nozbe.github.io/WatermelonDB/Advanced/AdvancedFields.html
-    //{ name: 'geom_point', type: 'string', isOptional: true },
+    { name: 'geom_point', type: 'string', isOptional: true },
     { name: 'wgs84_lat', type: 'string', isOptional: true },
     { name: 'wgs84_long', type: 'string', isOptional: true },
     { name: 'lv95_x', type: 'string', isOptional: true },
@@ -22,12 +20,10 @@ const herkunft = {
     { name: 'changed_by', type: 'string', isOptional: true },
     { name: '_rev', type: 'string', isOptional: true },
     { name: '_parent_rev', type: 'string', isOptional: true },
-    // TODO:
-    //{ name: '_revisions', type: 'string', isOptional: true },
+    { name: '_revisions', type: 'string', isOptional: true },
     { name: '_depth', type: 'string', isOptional: true },
     { name: '_deleted', type: 'boolean', isOptional: true },
-    // TODO:
-    //{ name: '_conflicts', type: 'string', isOptional: true },
+    { name: '_conflicts', type: 'string', isOptional: true },
   ],
 }
 
