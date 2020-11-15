2800dcee894a2aeb8bfccad4e6234076746610cb
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-15 23:10:42 +0100

    comment

--- src/dbSchema/ae_art.js
+++ src/dbSchema/ae_art.js
@@ -6,6 +6,7 @@ const ae_art = {
     { name: 'name', type: 'string', isOptional: true },
     { name: 'name_deutsch', type: 'string', isOptional: true },
     { name: 'name_latein', type: 'string', isOptional: true },
+    // changed exists but is not yet in the mst-model
     //{ name: 'changed', type: 'string', isOptional: true },
   ],
 }
