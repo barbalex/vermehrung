56b892d7408ca2a8e55c8c019aa628481888bc49
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-19 10:34:09 +0100

    initiate lieferung export

--- src/components/Header/Vermehrung/Menu/Menu.js
+++ src/components/Header/Vermehrung/Menu/Menu.js
@@ -1,26 +1,17 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useContext, useCallback, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
-import styled from 'styled-components'
 
 import { StoreContext } from '../../../../models/reactUtils'
+import ExportMenu from './Export'
 
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  user-select: none;
-`
-const Title = styled.div`
-  padding: 12px 16px;
-  color: rgba(0, 0, 0, 0.6);
-  font-weight: 800;
-  user-select: none;
-`
-
-const SettingsOverallMenu = ({ anchorEl, setAnchorEl }) => {
+const SettingsOverallMenu = ({
+  anchorEl: parentAnchorEl,
+  setAnchorEl: setParentAnchorEl,
+}) => {
   const store = useContext(StoreContext)
   const { filter } = store
 
@@ -69,18 +60,35 @@ const SettingsOverallMenu = ({ anchorEl, setAnchorEl }) => {
     filter.teilzaehlung._deleted === false &&
     filter.zaehlung._deleted === false
 
-  const onClose = useCallback(() => setAnchorEl(null), [setAnchorEl])
+  const onClose = useCallback(() => setParentAnchorEl(null), [
+    setParentAnchorEl,
+  ])
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const onClickExporte = useCallback(
+    (event) => setAnchorEl(event.currentTarget),
+    [],
+  )
 
   return (
     <Menu
       id="menu"
-      anchorEl={anchorEl}
-      open={Boolean(anchorEl)}
+      anchorEl={parentAnchorEl}
+      open={Boolean(parentAnchorEl)}
       onClose={onClose}
     >
-      <TitleRow>
-        <Title>Optionen wählen:</Title>
-      </TitleRow>
+      <MenuItem
+        aria-owns={anchorEl ? 'menu' : null}
+        aria-haspopup="true"
+        onClick={onClickExporte}
+      >
+        Exporte
+      </MenuItem>
+      <ExportMenu
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        setParentAnchorEl={setParentAnchorEl}
+      />
       <MenuItem>
         <FormControlLabel
           control={
