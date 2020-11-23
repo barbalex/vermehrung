7f1f2db223548e54f479adac64d7261f11557f5c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-23 15:44:18 +0100

    work on wm

--- src/components/Data/Herkunft/DataProvider.js
+++ src/components/Data/Herkunft/DataProvider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDatabase } from '@nozbe/watermelondb/hooks'
-import { useObservableEagerState, useObservable } from 'observable-hooks'
+import { useObservableState } from 'observable-hooks'
 
 import Herkunft from './Herkunft'
 
@@ -12,10 +12,10 @@ const HerkunftDataProvider = ({ id, filter }) => {
   // maybe due to observables not being "hot"
   // see: https://github.com/Nozbe/withObservables/issues/16#issuecomment-661790668
   const db = useDatabase()
-  const herkunft$ = useObservable(() =>
+  const herkunft = useObservableState(
     db.collections.get('herkunft').findAndObserve(id),
+    null,
   )
-  const herkunft = useObservableEagerState(herkunft$, null)
 
   return <Herkunft id={id} filter={filter} row={herkunft} />
 }
