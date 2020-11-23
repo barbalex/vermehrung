474c56739dce206666dd30a37b1a1a1f1ea4d723
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-23 15:44:18 +0100

    work on wm

--- src/utils/initializeSubscriptions.js
+++ src/utils/initializeSubscriptions.js
@@ -20,17 +20,13 @@ const initializeSubscriptions = ({ store }) => {
   unsubscribe.ae_art = store.subscribeAe_art(
     undefined,
     ae_artModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'ae_art', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'ae_art', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.art = store.subscribeArt(
     undefined,
     artModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'art', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'art', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.art_file = store.subscribeArt_file()
@@ -41,9 +37,7 @@ const initializeSubscriptions = ({ store }) => {
   unsubscribe.garten = store.subscribeGarten(
     undefined,
     gartenModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'garten', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'garten', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.garten_file = store.subscribeGarten_file()
@@ -51,18 +45,14 @@ const initializeSubscriptions = ({ store }) => {
   unsubscribe.herkunft = store.subscribeHerkunft(
     undefined,
     herkunftModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'herkunft', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'herkunft', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.herkunft_file = store.subscribeHerkunft_file()
   unsubscribe.kultur = store.subscribeKultur(
     undefined,
     kulturModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'kultur', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'kultur', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.kultur_file = store.subscribeKultur_file()
@@ -72,9 +62,7 @@ const initializeSubscriptions = ({ store }) => {
   unsubscribe.lieferung = store.subscribeLieferung(
     undefined,
     lieferungModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'lieferung', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'lieferung', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.lieferung_file = store.subscribeLieferung_file()
@@ -85,35 +73,27 @@ const initializeSubscriptions = ({ store }) => {
   unsubscribe.sammlung = store.subscribeSammlung(
     undefined,
     sammlungModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'sammlung', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'sammlung', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.sammlung_file = store.subscribeSammlung_file()
   unsubscribe.teilkultur = store.subscribeTeilkultur(
     undefined,
     teilkulturModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'teilkultur', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'teilkultur', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.teilzaehlung = store.subscribeTeilzaehlung(
     undefined,
     teilzaehlungModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'teilzaehlung', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'teilzaehlung', db: store.db }),
     (error) => onError({ error }),
   )
   unsubscribe.user_role = store.subscribeUser_role()
   unsubscribe.zaehlung = store.subscribeZaehlung(
     undefined,
     zaehlungModelPrimitives.toString(),
-    (data) => {
-      updateWmFromData({ data, table: 'zaehlung', db: store.db })
-    },
+    (data) => updateWmFromData({ data, table: 'zaehlung', db: store.db }),
     (error) => onError({ error }),
   )
   return unsubscribe
