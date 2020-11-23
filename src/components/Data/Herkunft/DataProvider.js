13b3adf2c20b22f887833ecf514694503a91b375
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-23 15:32:53 +0100

    work on wm

--- src/components/Data/Herkunft/DataProvider.js
+++ src/components/Data/Herkunft/DataProvider.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useDatabase } from '@nozbe/watermelondb/hooks'
-import { useObservableState } from 'observable-hooks'
+import { useObservableEagerState, useObservable } from 'observable-hooks'
 
 import Herkunft from './Herkunft'
 
 const HerkunftDataProvider = ({ id, filter }) => {
   // see: https://github.com/Nozbe/withObservables/issues/16#issuecomment-661444478
+  // TODO:
+  // when being updated from live queries
+  // the value is always one step behind!
+  // maybe due to observables not being "hot"
+  // see: https://github.com/Nozbe/withObservables/issues/16#issuecomment-661790668
   const db = useDatabase()
-  const herkunft = useObservableState(
+  const herkunft$ = useObservable(() =>
     db.collections.get('herkunft').findAndObserve(id),
-    null,
   )
+  const herkunft = useObservableEagerState(herkunft$, null)
 
   return <Herkunft id={id} filter={filter} row={herkunft} />
 }
