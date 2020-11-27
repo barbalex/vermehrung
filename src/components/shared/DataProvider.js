6def52013c402891ea0db05ba033a3f428d524f9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-27 22:15:38 +0100

    use wm

--- src/components/shared/DataProvider.js
+++ src/components/shared/DataProvider.js
@@ -5,12 +5,6 @@ import { useObservableState, useSubscription } from 'observable-hooks'
 import ErrorBoundary from './ErrorBoundaryForDataProvider'
 
 const HerkunftDataProvider = ({ id, table, children }) => {
-  // see: https://github.com/Nozbe/withObservables/issues/16#issuecomment-661444478
-  // TODO:
-  // when being updated from live queries
-  // the value is always one step behind!
-  // maybe due to observables not being "hot"
-  // see: https://github.com/Nozbe/withObservables/issues/16#issuecomment-661790668
   const db = useDatabase()
   const row = useObservableState(
     db.collections.get(table).findAndObserve(id),
@@ -23,10 +17,6 @@ const HerkunftDataProvider = ({ id, table, children }) => {
     // this is a trick to get react components to rerender
     // when the observable changes
     // NEED TO GET RID OF THIS HACK
-    // used to be: JSON.stringify(val._raw)
-    // maybe using a running counter would be more efficient?
-    // no need to stringify...
-    // BUT: starts an endless cycle
     setRenderEnforcer(JSON.stringify(val._raw))
   })
 
