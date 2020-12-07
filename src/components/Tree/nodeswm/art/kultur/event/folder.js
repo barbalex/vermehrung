9102efb8f4799ac3facb4dec3c46e1661a25e300
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-07 20:56:23 +0100

    work on tree

--- src/components/Tree/nodeswm/art/kultur/event/folder.js
+++ src/components/Tree/nodeswm/art/kultur/event/folder.js
@@ -1,34 +1,18 @@
-const artKulturEventFolder = ({ store }) => {
-  const { initialDataQueried } = store
-  const { showArt, visibleOpenNodes, art, artKultur } = store.tree
-  if (!showArt) return []
-
-  const parentNodes = visibleOpenNodes.filter(
-    (node) =>
-      node.length === 4 && node[0] === 'Arten' && node[2] === 'Kulturen',
-  )
-
-  return parentNodes.map((node) => {
-    const artId = node[1]
-    const artIndex = art.findIndex((a) => a.id === artId)
-    const kulturId = node[3]
-    const kulturIndex = artKultur.findIndex(
-      (a) => a.id === `${artId}${kulturId}`,
-    )
-    const events = store.eventsFiltered.filter((e) => e.kultur_id === kulturId)
-    const nr = !initialDataQueried && !events.length ? '...' : events.length
-
-    return {
-      nodeType: 'folder',
-      menuTitle: 'Events',
-      id: `${artId}${kulturId}EventFolder`,
-      label: `Events (${nr})`,
-      url: ['Arten', artId, 'Kulturen', kulturId, 'Events'],
-      sort: [1, artIndex, 2, kulturIndex, 5],
-      hasChildren: true,
-      childrenCount: nr,
-    }
-  })
-}
+const artKulturEventFolder = ({
+  kulturId,
+  kulturIndex,
+  artId,
+  artIndex,
+  children,
+}) => ({
+  nodeType: 'folder',
+  menuTitle: 'Events',
+  id: `${artId}${kulturId}EventFolder`,
+  label: `Events (${children.length})`,
+  url: ['Arten', artId, 'Kulturen', kulturId, 'Events'],
+  sort: [1, artIndex, 2, kulturIndex, 5],
+  hasChildren: true,
+  childrenCount: children.length,
+})
 
 export default artKulturEventFolder
