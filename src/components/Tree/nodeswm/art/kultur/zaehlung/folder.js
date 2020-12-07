f2f225cd7ed88cb6c679e796c0fcaf7035428b51
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-07 16:04:22 +0100

    work on tree

--- src/components/Tree/nodeswm/art/kultur/zaehlung/folder.js
+++ src/components/Tree/nodeswm/art/kultur/zaehlung/folder.js
@@ -1,38 +1,18 @@
-const artKulturZaehlungFolder = ({ store }) => {
-  const { initialDataQueried } = store
-  const { showArt, visibleOpenNodes, art, artKultur } = store.tree
-  if (!showArt) return []
-
-  const parentNodes = visibleOpenNodes.filter(
-    (node) =>
-      node.length === 4 && node[0] === 'Arten' && node[2] === 'Kulturen',
-  )
-
-  return parentNodes.map((node) => {
-    const artId = node[1]
-    const artIndex = art.findIndex((a) => a.id === artId)
-    const kulturId = node[3]
-    const kulturIndex = artKultur.findIndex(
-      (a) => a.id === `${artId}${kulturId}`,
-    )
-
-    const zaehlungen = store.zaehlungsFiltered.filter(
-      (z) => z.kultur_id === kulturId,
-    )
-    const nr =
-      !initialDataQueried && !zaehlungen.length ? '...' : zaehlungen.length
-
-    return {
-      nodeType: 'folder',
-      menuTitle: 'Zählungen',
-      id: `${artId}${kulturId}ZaehlungFolder`,
-      label: `Zählungen (${nr})`,
-      url: ['Arten', artId, 'Kulturen', kulturId, 'Zaehlungen'],
-      sort: [1, artIndex, 2, kulturIndex, 2],
-      hasChildren: true,
-      childrenCount: nr,
-    }
-  })
-}
+const artKulturZaehlungFolder = ({
+  kulturId,
+  kulturIndex,
+  artId,
+  artIndex,
+  children,
+}) => ({
+  nodeType: 'folder',
+  menuTitle: 'Zählungen',
+  id: `${artId}${kulturId}ZaehlungFolder`,
+  label: `Zählungen (${children.length})`,
+  url: ['Arten', artId, 'Kulturen', kulturId, 'Zaehlungen'],
+  sort: [1, artIndex, 2, kulturIndex, 2],
+  hasChildren: true,
+  childrenCount: children.length,
+})
 
 export default artKulturZaehlungFolder
