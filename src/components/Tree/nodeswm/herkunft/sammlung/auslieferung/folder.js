2eaa73a164bfcc2e14d975a9642942d7324d45d4
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-07 22:20:49 +0100

    add nodes

--- src/components/Tree/nodeswm/herkunft/sammlung/auslieferung/folder.js
+++ src/components/Tree/nodeswm/herkunft/sammlung/auslieferung/folder.js
@@ -1,49 +1,18 @@
-const herkunftSammlungAuslieferungFolder = ({ store }) => {
-  const { initialDataQueried } = store
-  const {
-    showHerkunft,
-    visibleOpenNodes,
-    herkunft,
-    herkunftSammlung,
-  } = store.tree
-  if (!showHerkunft) return []
-
-  const parentNodes = visibleOpenNodes.filter(
-    (node) =>
-      node.length === 4 && node[0] === 'Herkuenfte' && node[2] === 'Sammlungen',
-  )
-
-  return parentNodes.map((node) => {
-    const herkunftId = node[1]
-    const herkunftIndex = herkunft.findIndex((a) => a.id === herkunftId)
-    const sammlungId = node[3]
-    const sammlungIndex = herkunftSammlung.findIndex(
-      (a) => a.id === `${herkunftId}${sammlungId}`,
-    )
-
-    const lieferungen = store.lieferungsFiltered.filter(
-      (l) => l.von_sammlung_id === sammlungId,
-    )
-    const nr =
-      !initialDataQueried && !lieferungen.length ? '...' : lieferungen.length
-
-    return {
-      nodeType: 'folder',
-      menuTitle: 'Aus-Lieferungen',
-      id: `${herkunftId}${sammlungId}SammlungLieferungFolder`,
-      label: `Aus-Lieferungen (${nr})`,
-      url: [
-        'Herkuenfte',
-        herkunftId,
-        'Sammlungen',
-        sammlungId,
-        'Aus-Lieferungen',
-      ],
-      sort: [2, herkunftIndex, 2, sammlungIndex, 1],
-      hasChildren: true,
-      childrenCount: nr,
-    }
-  })
-}
+const herkunftSammlungAuslieferungFolder = ({
+  sammlungId,
+  sammlungIndex,
+  herkunftId,
+  herkunftIndex,
+  children,
+}) => ({
+  nodeType: 'folder',
+  menuTitle: 'Aus-Lieferungen',
+  id: `${herkunftId}${sammlungId}SammlungLieferungFolder`,
+  label: `Aus-Lieferungen (${children.length})`,
+  url: ['Herkuenfte', herkunftId, 'Sammlungen', sammlungId, 'Aus-Lieferungen'],
+  sort: [2, herkunftIndex, 2, sammlungIndex, 1],
+  hasChildren: true,
+  childrenCount: children.length,
+})
 
 export default herkunftSammlungAuslieferungFolder
--- src/components/Tree/nodeswm/herkunft/sammlung/auslieferung/index.js
+++ src/components/Tree/nodeswm/herkunft/sammlung/auslieferung/index.js
@@ -1,58 +1,28 @@
 import lieferungLabelFromLieferung from '../../../../../../utils/lieferungLabelFromLieferung'
 
-const herkunftSammlungAuslieferungNodes = ({ store }) => {
-  const {
-    showHerkunft,
-    visibleOpenNodes,
-    herkunft,
-    herkunftSammlung,
-  } = store.tree
-  if (!showHerkunft) return []
-
-  const parentNodes = visibleOpenNodes.filter(
-    (node) =>
-      node.length === 5 &&
-      node[0] === 'Herkuenfte' &&
-      node[2] === 'Sammlungen' &&
-      node[4] === 'Aus-Lieferungen',
-  )
-
-  if (!parentNodes.length) return []
-
-  return parentNodes.flatMap((node) => {
-    const herkunftId = node[1]
-    const herkunftIndex = herkunft.findIndex((a) => a.id === herkunftId)
-    const sammlungId = node[3]
-    const sammlungIndex = herkunftSammlung.findIndex(
-      (a) => a.id === `${herkunftId}${sammlungId}`,
-    )
-
-    const lieferungen = store.lieferungsFiltered.filter(
-      (l) => l.von_sammlung_id === sammlungId,
-    )
-
-    return lieferungen
-      .map((el) => ({
-        nodeType: 'table',
-        menuTitle: 'Aus-Lieferung',
-        table: 'lieferung',
-        id: `${herkunftId}${sammlungId}${el.id}`,
-        label: lieferungLabelFromLieferung({ lieferung: el }),
-        url: [
-          'Herkuenfte',
-          herkunftId,
-          'Sammlungen',
-          sammlungId,
-          'Aus-Lieferungen',
-          el.id,
-        ],
-        hasChildren: false,
-      }))
-      .map((el, index) => {
-        el.sort = [2, herkunftIndex, 2, sammlungIndex, 1, index]
-        return el
-      })
-  })
-}
+const herkunftSammlungAuslieferungNodes = ({
+  lieferung,
+  lieferungIndex,
+  sammlungId,
+  sammlungIndex,
+  herkunftId,
+  herkunftIndex,
+}) => ({
+  nodeType: 'table',
+  menuTitle: 'Aus-Lieferung',
+  table: 'lieferung',
+  id: `${herkunftId}${sammlungId}${lieferung.id}`,
+  label: lieferungLabelFromLieferung({ lieferung }),
+  url: [
+    'Herkuenfte',
+    herkunftId,
+    'Sammlungen',
+    sammlungId,
+    'Aus-Lieferungen',
+    lieferung.id,
+  ],
+  sort: [2, herkunftIndex, 2, sammlungIndex, 1, lieferungIndex],
+  hasChildren: false,
+})
 
 export default herkunftSammlungAuslieferungNodes
