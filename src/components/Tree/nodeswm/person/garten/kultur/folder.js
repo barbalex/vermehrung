67cd8c5c29e228e36af8a1ab14d206bd92592c3e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-10 00:00:14 +0100

    add nodes

--- src/components/Tree/nodeswm/person/garten/kultur/folder.js
+++ src/components/Tree/nodeswm/person/garten/kultur/folder.js
@@ -1,37 +1,20 @@
-const personGartenKulturFolder = ({ store }) => {
-  const { initialDataQueried } = store
-  const { showPerson, visibleOpenNodes, person, personGarten } = store.tree
-  if (!showPerson) return []
-
-  const parentNodes = visibleOpenNodes.filter(
-    (node) =>
-      node.length === 4 && node[0] === 'Personen' && node[2] === 'Gaerten',
-  )
-
-  return parentNodes.map((node) => {
-    const personId = node[1]
-    const personIndex = person.findIndex((a) => a.id === personId)
-    const gartenId = node[3]
-    const gartenIndex = personGarten.findIndex(
-      (a) => a.id === `${personId}${gartenId}`,
-    )
-
-    const kulturen = store.kultursFiltered.filter(
-      (s) => s.garten_id === gartenId,
-    )
-    const nr = !initialDataQueried && !kulturen.length ? '...' : kulturen.length
-
-    return {
-      nodeType: 'folder',
-      menuTitle: 'Kulturen',
-      id: `${personId}${gartenId}KulturFolder`,
-      label: `Kulturen (${nr})`,
-      url: ['Personen', personId, 'Gaerten', gartenId, 'Kulturen'],
-      sort: [11, personIndex, 2, gartenIndex, 1],
-      hasChildren: true,
-      childrenCount: nr,
-    }
-  })
-}
+const personGartenKulturFolder = ({
+  gartenId,
+  gartenIndex,
+  personId,
+  personIndex,
+  children,
+}) => [
+  {
+    nodeType: 'folder',
+    menuTitle: 'Kulturen',
+    id: `${personId}${gartenId}KulturFolder`,
+    label: `Kulturen (${children.length})`,
+    url: ['Personen', personId, 'Gaerten', gartenId, 'Kulturen'],
+    sort: [11, personIndex, 2, gartenIndex, 1],
+    hasChildren: true,
+    childrenCount: children.length,
+  },
+]
 
 export default personGartenKulturFolder
