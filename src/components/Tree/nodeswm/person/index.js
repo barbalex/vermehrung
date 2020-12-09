c8c538941d8fba016a95d016069c960ca3e97af4
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-09 16:49:22 +0100

    work on tree

--- src/components/Tree/nodeswm/person/index.js
+++ src/components/Tree/nodeswm/person/index.js
@@ -1,30 +1,14 @@
-import isEqual from 'lodash/isEqual'
-
 import personLabelFromPerson from '../../../../utils/personLabelFromPerson'
 
-const personNodes = ({ store, persons }) => {
-  const { showPerson, visibleOpenNodes } = store.tree
-
-  if (!showPerson) return []
-  // only show if parent node exists
-  if (!visibleOpenNodes.some((node) => isEqual(['Personen'], node))) {
-    return []
-  }
-
-  return persons
-    .map((el) => ({
-      nodeType: 'table',
-      menuTitle: 'Person',
-      table: 'person',
-      id: el.id,
-      label: personLabelFromPerson({ person: el }),
-      url: ['Personen', el.id],
-      hasChildren: true,
-    }))
-    .map((el, index) => {
-      el.sort = [11, index]
-      return el
-    })
-}
+const personNodes = ({ person, index }) => ({
+  nodeType: 'table',
+  menuTitle: 'Person',
+  table: 'person',
+  id: person.id,
+  label: personLabelFromPerson({ person }),
+  url: ['Personen', person.id],
+  sort: [11, index],
+  hasChildren: true,
+})
 
 export default personNodes
