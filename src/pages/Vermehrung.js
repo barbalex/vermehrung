ab4b2c3000ebfc06f05e4a8cda0175ebbd11043a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-18 00:08:49 +0100

    usw wm

--- src/pages/Vermehrung.js
+++ src/pages/Vermehrung.js
@@ -137,8 +137,8 @@ const Vermehrung = ({ location }) => {
       unsubscribe = initializeSubscriptions({ store })
     }
     return function cleanup() {
-      if (unsubscribe && Object.keys(unsubscribe)) {
-        Object.keys(unsubscribe).forEach((table) => unsubscribe[table]())
+      if (unsubscribe && Object.values(unsubscribe)) {
+        Object.values(unsubscribe).forEach((value) => value.unsubscribe())
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
