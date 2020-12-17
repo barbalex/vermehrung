a6ed2bf09b83df51a19b5be03ef16be8d446060e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-17 19:52:34 +0100

    use wm

--- src/components/Data/SammelLieferung/Lieferschein/Lieferung.js
+++ src/components/Data/SammelLieferung/Lieferschein/Lieferung.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import styled from 'styled-components'
+import { combineLatest, of as $of } from 'rxjs'
+import { first as first$ } from 'rxjs/operators'
+import { Q } from '@nozbe/watermelondb'
 
 import { StoreContext } from '../../../../models/reactUtils'
-import artLabelFromLieferung from '../../../../utils/artLabelFromLieferung'
 import herkunftLabelFromHerkunft from '../../../../utils/herkunftLabelFromHerkunft'
 
 const StyledTableCell = styled(TableCell)`
@@ -15,25 +17,72 @@ const Zeile = ({ value }) => <div>{value}</div>
 
 const LieferungForLieferschein = ({ lieferung: l }) => {
   const store = useContext(StoreContext)
+  const { db } = store
 
-  const art = artLabelFromLieferung({ lieferung: l, store })
-  const vonKultur = l.von_kultur_id ? store.kulturs.get(l.von_kultur_id) : {}
-  const vonKulturHerkunft = vonKultur?.herkunft_id
-    ? store.herkunfts.get(vonKultur.herkunft_id)
-    : undefined
-  const vonSammlung = l.von_sammlung_id
-    ? store.sammlungs.get(l.von_sammlung_id)
-    : {}
-  const vonSammlungHerkunft = vonSammlung?.herkunft_id
-    ? store.herkunfts.get(vonSammlung.herkunft_id)
-    : undefined
-  const herkunft = vonKulturHerkunft
-    ? herkunftLabelFromHerkunft({
-        herkunft: vonKulturHerkunft,
-      })
-    : vonSammlungHerkunft
-    ? herkunftLabelFromHerkunft({ herkunft: vonSammlungHerkunft })
-    : ''
+  console.log('LieferungForLieferschein', { l })
+  const [dataState, setDataState] = useState({
+    artLabel: '',
+    herkunftLabel: '',
+  })
+  useEffect(() => {
+    console.log('LieferungForLieferschein useEffect running')
+    const artObservable = l.art.observe()
+    const vonKulturHerkunftObservable = l.von_kultur_id
+      ? db.collections
+          .get('herkunft')
+          .query(Q.on('kultur', Q.where('id', l.von_kultur_id)))
+          .observe()
+      : $of()
+    const vonSammlungHerkunftObservable = l.von_sammlung_id
+      ? db.collections
+          .get('herkunft')
+          .query(Q.on('sammlung', Q.where('id', l.von_sammlung_id)))
+          .observe()
+      : $of()
+    console.log('LieferungForLieferschein useEffect', {
+      artObservable,
+      vonKulturHerkunftObservable,
+      vonSammlungHerkunftObservable,
+    })
+    const combinedObservables = combineLatest([
+      artObservable,
+      vonKulturHerkunftObservable,
+      vonSammlungHerkunftObservable,
+    ])
+    console.log(
+      'LieferungForLieferschein useEffect, combinedObservables:',
+      combinedObservables,
+    )
+    const subscription = combinedObservables.subscribe(
+      async ([art, vonKulturHerkunft, vonSammlungHerkunft]) => {
+        console.log('LieferungForLieferschein subscription result', {
+          art,
+          vonKulturHerkunft,
+          vonSammlungHerkunft,
+        })
+        const artLabel = await art.label.pipe(first$()).toPromise()
+        const herkunftLabel = vonKulturHerkunft
+          ? herkunftLabelFromHerkunft({
+              herkunft: vonKulturHerkunft,
+            })
+          : vonSammlungHerkunft
+          ? herkunftLabelFromHerkunft({ herkunft: vonSammlungHerkunft })
+          : ''
+        console.log('LieferungForLieferschein subscription result', {
+          artLabel,
+          herkunftLabel,
+        })
+
+        setDataState({ artLabel, herkunftLabel })
+      },
+    )
+
+    console.log('LieferungForLieferschein subscription', subscription)
+
+    return () => subscription.unsubscribe()
+  }, [db.collections, l.art, l.von_kultur_id, l.von_sammlung_id])
+  const { artLabel, herkunftLabel } = dataState
+
   const wasArray = []
   l.anzahl_pflanzen && wasArray.push(`${l.anzahl_pflanzen} Pflanzen`)
   l.anzahl_auspflanzbereit &&
@@ -42,12 +91,12 @@ const LieferungForLieferschein = ({ lieferung: l }) => {
   l.von_anzahl_individuen &&
     wasArray.push(`von ${l.von_anzahl_individuen} Individuen`)
   l.andere_menge && wasArray.push(l.andere_menge)
-  const bemerkungen = l.bemerkungen
+  const bemerkungen = l.bemerkungen ?? ''
 
   return (
     <TableRow>
-      <StyledTableCell>{art}</StyledTableCell>
-      <StyledTableCell>{herkunft}</StyledTableCell>
+      <StyledTableCell>{artLabel}</StyledTableCell>
+      <StyledTableCell>{herkunftLabel}</StyledTableCell>
       <StyledTableCell>
         {wasArray.map((w, i) => (
           <Zeile key={i} value={w} />
