001a25906f61e8e3901b65802ebaa20a2790b95d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-19 17:50:29 +0100

    use wm

--- src/models/gv_revModel.js
+++ src/models/gv_revModel.js
@@ -1,9 +1,4 @@
-import { getParent } from 'mobx-state-tree'
-import md5 from 'blueimp-md5'
-import { v1 as uuidv1 } from 'uuid'
-
 import { gv_revModelBase } from './gv_revModel.base'
-import toPgArray from '../utils/toPgArray'
 
 /* A graphql query fragment builders for gv_revModel */
 export {
@@ -15,44 +10,4 @@ export {
 /**
  * gv_revModel
  */
-export const gv_revModel = gv_revModelBase.actions((self) => ({
-  setDeleted() {
-    const store = getParent(self, 2)
-    const { addQueuedQuery, user, deleteGvRevModel } = store
-
-    // build new object
-    const newDepth = self._depth + 1
-    const newObject = {
-      gv_id: self.gv_id,
-      garten_id: self.garten_id,
-      person_id: self.person_id,
-      _parent_rev: self._rev,
-      _depth: newDepth,
-      _deleted: true,
-    }
-    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-    newObject._rev = rev
-    newObject.id = uuidv1()
-    newObject.changed = new window.Date().toISOString()
-    newObject.changed_by = user.email
-    newObject._revisions = self._revisions
-      ? toPgArray([rev, ...self._revisions])
-      : toPgArray([rev])
-
-    addQueuedQuery({
-      name: 'mutateInsert_gv_rev_one',
-      variables: JSON.stringify({
-        object: newObject,
-        on_conflict: {
-          constraint: 'gv_rev_pkey',
-          update_columns: ['id'],
-        },
-      }),
-      revertTable: 'gv',
-      revertId: self.gv_id,
-      revertField: '_deleted',
-      revertValue: false,
-    })
-    deleteGvRevModel(self)
-  },
-}))
+export const gv_revModel = gv_revModelBase
--- src/models/kultur_qk_choosen_revModel.js
+++ src/models/kultur_qk_choosen_revModel.js
@@ -1,9 +1,4 @@
-import { getParent } from 'mobx-state-tree'
-import md5 from 'blueimp-md5'
-import { v1 as uuidv1 } from 'uuid'
-
 import { kultur_qk_choosen_revModelBase } from './kultur_qk_choosen_revModel.base'
-import toPgArray from '../utils/toPgArray'
 
 /* A graphql query fragment builders for kultur_qk_choosen_revModel */
 export {
@@ -15,47 +10,4 @@ export {
 /**
  * kultur_qk_choosen_revModel
  */
-export const kultur_qk_choosen_revModel = kultur_qk_choosen_revModelBase.actions(
-  (self) => ({
-    setDeleted() {
-      const store = getParent(self, 2)
-      const { addQueuedQuery, user, deleteKulturQkChoosenRevModel } = store
-
-      // build new object
-      const newDepth = self._depth + 1
-      const newObject = {
-        kultur_qk_choosen_id: self.kultur_qk_choosen_id,
-        kultur_id: self.kultur_id,
-        qk_id: self.qk_id,
-        choosen: self.choosen,
-        _parent_rev: self._rev,
-        _depth: newDepth,
-        _deleted: true,
-      }
-      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-      newObject._rev = rev
-      newObject.id = uuidv1()
-      newObject.changed = new window.Date().toISOString()
-      newObject.changed_by = user.email
-      newObject._revisions = self._revisions
-        ? toPgArray([rev, ...self._revisions])
-        : toPgArray([rev])
-
-      addQueuedQuery({
-        name: 'mutateInsert_kultur_qk_choosen_rev_one',
-        variables: JSON.stringify({
-          object: newObject,
-          on_conflict: {
-            constraint: 'kultur_qk_choosen_rev_pkey',
-            update_columns: ['id'],
-          },
-        }),
-        revertTable: 'kultur_qk_choosen',
-        revertId: self.kultur_qk_choosen_id,
-        revertField: '_deleted',
-        revertValue: false,
-      })
-      deleteKulturQkChoosenRevModel(self)
-    },
-  }),
-)
+export const kultur_qk_choosen_revModel = kultur_qk_choosen_revModelBase
