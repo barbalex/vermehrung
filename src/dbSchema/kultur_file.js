cf7f8ecff887eec5c98f497280f8123e0549eb6b
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-20 01:55:07 +0100

    get files to work

--- src/dbSchema/kultur_file.js
+++ src/dbSchema/kultur_file.js
@@ -9,6 +9,7 @@ const kultur_file = {
     { name: 'name', type: 'string', isOptional: true, isIndexed: true },
     { name: 'beschreibung', type: 'string', isOptional: true },
     { name: 'changed', type: 'string', isOptional: true },
+    { name: '_rev_at', type: 'string', isOptional: true },
   ],
 }
 
