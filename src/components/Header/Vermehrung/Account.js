328d8c4fd71dc931dba3698e369f379d5930b307
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2020-12-28 17:15:46 +0100

    warn when logging out

--- src/components/Header/Vermehrung/Account.js
+++ src/components/Header/Vermehrung/Account.js
@@ -2,7 +2,13 @@ import React, { useState, useCallback, useContext, useEffect } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import MenuItem from '@material-ui/core/MenuItem'
 import Menu from '@material-ui/core/Menu'
-import { FaUserCircle as UserIcon } from 'react-icons/fa'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import { FaUserCircle as UserIcon, FaExclamationCircle } from 'react-icons/fa'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import { of as $of } from 'rxjs'
@@ -19,10 +25,18 @@ const constants = getConstants()
 const StyledUserIcon = styled(UserIcon)`
   color: white;
 `
+const StyledButton = styled(Button)`
+  text-transform: none !important;
+`
+const RiskyButton = styled(Button)`
+  text-transform: none !important;
+  color: #d84315 !important;
+  border-color: #d84315 !important;
+`
 
 const Account = () => {
   const store = useContext(StoreContext)
-  const { user, firebase, online, db } = store
+  const { user, firebase, online, db, queuedQueries } = store
 
   const [userPerson, setUserPerson] = useState(undefined)
   useEffect(() => {
@@ -47,10 +61,20 @@ const Account = () => {
   )
   const onCloseMenu = useCallback(() => setAnchorEl(null), [])
 
+  const [
+    pendingOperationsDialogOpen,
+    setPendingOperationsDialogOpen,
+  ] = useState(false)
   const onClickLogout = useCallback(async () => {
     setAnchorEl(null)
+    // TODO:
+    // if exist pending operations
+    // ask user if willing to loose them
+    if (queuedQueries.size) {
+      return setPendingOperationsDialogOpen(true)
+    }
     logout({ store })
-  }, [store])
+  }, [queuedQueries.size, store])
 
   const { email } = user || {}
 
@@ -108,6 +132,48 @@ const Account = () => {
           } abmelden`}</MenuItem>
           <MenuItem onClick={onClickResetPassword}>{resetTitle}</MenuItem>
         </Menu>
+        <Dialog
+          open={pendingOperationsDialogOpen}
+          onClose={() => setPendingOperationsDialogOpen(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          maxWidth="md"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {'Wirklich abmelden?'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {`Beim Abmelden werden aus Datenschutzgründen alle lokalen Daten
+              entfernt. Es gibt noch ${queuedQueries.size} ausstehende
+              Operationen. Wenn Sie jetzt abmelden, gehen diese verloren.
+              Vermutlich warten Sie besser, bis diese Operationen an den Server
+              übermittelt wurden.`}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <StyledButton
+              onClick={() => setPendingOperationsDialogOpen(false)}
+              color="primary"
+              autoFocus
+              variant="outlined"
+            >
+              Ich bleibe angemeldet, um die ausstehenden Operationen nicht zu
+              verlieren
+            </StyledButton>
+            <RiskyButton
+              onClick={() => {
+                setPendingOperationsDialogOpen(false)
+                logout({ store })
+              }}
+              variant="outlined"
+              startIcon={<FaExclamationCircle />}
+            >
+              Ich will abmelden, obwohl ich die ausstehenden Operationen
+              verliere
+            </RiskyButton>
+          </DialogActions>
+        </Dialog>
       </>
     </ErrorBoundary>
   )
