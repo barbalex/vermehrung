94fb8077a7f977c0cdb479cb708b84487e07fd12
Alexander Gabriel <alex.barbalex@gmail.com>
2021-01-27 13:35:38 +0100

    optimize

--- src/components/Tree/toggleNodeSymbol.js
+++ src/components/Tree/toggleNodeSymbol.js
@@ -1,27 +1,25 @@
-import isEqual from 'lodash/isEqual'
-import { getSnapshot } from 'mobx-state-tree'
-
 import isNodeOpen from './isNodeOpen'
 import isNodeInActiveNodePath from './isNodeInActiveNodePath'
 
 const toggleNodeSymbol = ({ node, store }) => {
-  const { addNotification } = store
-  const { setActiveNodeArray, activeNodeArray } = store.tree
   if (!node.url) {
     console.log('passsed node has no url:', node)
     return addNotification({
       message: 'Fehler: Dem Knoten im Navigationsbaum fehlt die url',
     })
   }
-  const { setOpenNodes, addOpenNodes, openNodes: openNodesRaw } = store.tree
-  const openNodes = getSnapshot(openNodesRaw)
+  const { addNotification } = store
+  const {
+    addOpenNodes,
+    setActiveNodeArray,
+    activeNodeArray,
+    removeOpenNodeWithChildren,
+  } = store.tree
 
   store.filter.setShow(false)
-  let newOpenNodes = [...openNodes]
   // TODO: tell user if childrenCount is 0 he can create
   if (isNodeOpen({ store, url: node.url })) {
-    newOpenNodes = newOpenNodes.filter((n) => !isEqual(n, node.url))
-    setOpenNodes(newOpenNodes)
+    removeOpenNodeWithChildren(node.url)
     if (isNodeInActiveNodePath({ node, activeNodeArray })) {
       // when a user closes a folder in the active node path
       // the active node should swith to the node's parent
