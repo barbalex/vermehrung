55e2a09fde56c8de659e68fa22762cca46c53bc3
Alexander Gabriel <alex.barbalex@gmail.com>
2021-01-27 15:17:13 +0100

    repair tree

--- src/components/Tree/nodes/sammlung/herkunft/index.js
+++ src/components/Tree/nodes/sammlung/herkunft/index.js
@@ -1,18 +1,13 @@
 import herkunftLabelFromHerkunft from '../../../../../utils/herkunftLabelFromHerkunft'
 
-const sammlungHerkunftNodes = ({
-  herkunft,
-  herkunftIndex,
-  sammlungId,
-  sammlungIndex,
-}) => ({
+const sammlungHerkunftNodes = ({ herkunft, sammlungId, sammlungIndex }) => ({
   nodeType: 'table_no_menu',
   menuTitle: 'Herkunft',
   table: 'herkunft',
   id: `${sammlungId}${herkunft.id}`,
   label: herkunftLabelFromHerkunft({ herkunft }),
   url: ['Sammlungen', sammlungId, 'Herkuenfte', herkunft.id],
-  sort: [3, sammlungIndex, 1, herkunftIndex],
+  sort: [3, sammlungIndex, 1, 1],
   hasChildren: false,
 })
 
