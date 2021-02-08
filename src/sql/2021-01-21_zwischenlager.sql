62c881fe67c895a5fee61f86ff20cdadf4466401
Alexander Gabriel <alex.barbalex@gmail.com>
2021-02-08 15:44:27 +0100

    add fields

--- src/sql/2021-01-21_zwischenlager.sql
+++ src/sql/2021-01-21_zwischenlager.sql
@@ -16,8 +16,7 @@ with list as (
     on ae_art.id = art.ae_id
   on art.id = kultur.art_id
   where
-    kultur.garten_id in ('cc033efa-b555-11ea-b3de-0242ac130004', '1c3ba9f0-ed20-11ea-be2b-93662cfc26b3')
-    and kultur.zwischenlager = true
+    kultur.zwischenlager = true
     and kultur.art_id is not null
     and kultur.herkunft_id is not null
   order by
@@ -35,12 +34,57 @@ with list as (
     garten_id,
     herkunft_id,
     art_id
+), anlieferung as (
+  select
+    nach_kultur_id as id,
+    count(id) as anzahl
+  from lieferung
+  group by nach_kultur_id
+), auslieferung as (
+  select
+    von_kultur_id as id,
+    count(id) as anzahl
+  from lieferung
+  group by von_kultur_id
+), zaehlung as (
+  select
+    kultur_id as id,
+    count(id) as anzahl
+  from zaehlung
+  group by kultur_id
+), event as (
+  select
+    kultur_id as id,
+    count(id) as anzahl
+  from event
+  group by kultur_id
+), teilkultur as (
+  select
+    kultur_id as id,
+    count(id) as anzahl
+  from teilkultur
+  group by kultur_id
 )
 select
   list.*,
-  id_count.anzahl as anzahl_art_herkunft_in_garten
+  id_count.anzahl as anzahl_art_herkunft_in_garten,
+  coalesce(anlieferung.anzahl, 0) as anzahl_anlieferungen,
+  coalesce(auslieferung.anzahl, 0) as anzahl_auslieferungen,
+  coalesce(zaehlung.anzahl, 0) as anzahl_zaehlungen,
+  coalesce(event.anzahl, 0) as anzahl_events,
+  coalesce(teilkultur.anzahl, 0) as anzahl_teilkulturen
 from list
-left join id_count
-on id_count.garten_id = list.garten_id
-  and id_count.herkunft_id = list.herkunft_id
-  and id_count.art_id = list.art_id
+  left join id_count
+  on id_count.garten_id = list.garten_id
+    and id_count.herkunft_id = list.herkunft_id
+    and id_count.art_id = list.art_id
+  left join anlieferung
+  on anlieferung.id = list.id
+  left join auslieferung
+  on auslieferung.id = list.id
+  left join zaehlung
+  on zaehlung.id = list.id
+  left join event
+  on event.id = list.id
+  left join teilkultur
+  on teilkultur.id = list.id
