d1d554baea78ab816bcc39e8ac2304d854c64ed9
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-23 14:02:45 +0200

    optimize

--- src/components/Data/Lieferung/Lieferung/Form/Nach/index.js
+++ src/components/Data/Lieferung/Lieferung/Form/Nach/index.js
@@ -1,10 +1,4 @@
-import React, {
-  useContext,
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-} from 'react'
+import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
@@ -32,10 +26,8 @@ const TitleRow = styled.div`
   margin-bottom: 10px;
   padding: 0 10px;
   position: sticky;
-  ${(props) =>
-    props['data-sticky'] && 'border-top: 1px solid rgba(0, 0, 0, 0.3);'}
+  top: 0;
   user-select: none;
-  top: -10px;
   z-index: 1;
   &:first-of-type {
     margin-top: -10px;
@@ -48,27 +40,9 @@ const LieferungNach = ({ showFilter, row, saveToDb, ifNeeded, herkunft }) => {
 
   const { nachKulturWerte } = useData({ showFilter, row, herkunft, db, filter })
 
-  const titleRowRef = useRef(null)
-  const [isSticky, setIsSticky] = useState(false)
-  const scrollHandler = useCallback(() => {
-    const top = titleRowRef?.current?.getBoundingClientRect()?.top
-    if (top < 112 && !isSticky) return setIsSticky(true)
-    if (top > 112 && isSticky) setIsSticky(false)
-  }, [isSticky])
-  useEffect(() => {
-    window.addEventListener('scroll', scrollHandler, true)
-    return () => {
-      window.removeEventListener('scroll', scrollHandler, true)
-    }
-  }, [scrollHandler])
-
   return (
     <>
-      <TitleRow
-        data-filter={showFilter}
-        ref={titleRowRef}
-        data-sticky={isSticky}
-      >
+      <TitleRow data-filter={showFilter}>
         <Title>nach</Title>
       </TitleRow>
       {ifNeeded('nach_kultur_id') && (
