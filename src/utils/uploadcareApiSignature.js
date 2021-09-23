c89f61f8fbbed757a95ef82deaac1522106457c0
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-23 14:50:27 +0200

    remove md5-hex

--- src/utils/uploadcareApiSignature.js
+++ src/utils/uploadcareApiSignature.js
@@ -2,17 +2,17 @@
  * Not in use due to cors issue
  */
 
-import md5Hex from 'md5-hex'
+import md5 from 'blueimp-md5'
 
 const uploadcareApiSignature = ({ verb, uri }) => {
   if (!verb || !uri) return
   const secret = process.env.UPLOADCARE_SECRET_KEY
   // seems that we are sending an empty string?
-  const contentMd5 = md5Hex('')
+  const contentMd5 = md5('')
   const contentType = 'application/json'
   const date = new window.Date().toISOString()
   const signString = [verb, contentMd5, contentType, date, uri].join('\n')
-  return md5Hex(secret + signString)
+  return md5(secret + signString)
 }
 
 export default uploadcareApiSignature
