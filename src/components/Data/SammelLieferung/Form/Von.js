ce4dc1e93c523f6f0fb8b1f289d09ca851e36118
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-23 14:38:19 +0200

    optimize

--- src/components/Data/SammelLieferung/Form/Von.js
+++ src/components/Data/SammelLieferung/Form/Von.js
@@ -1,10 +1,4 @@
-import React, {
-  useContext,
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-} from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 import { Q } from '@nozbe/watermelondb'
@@ -36,10 +30,8 @@ const TitleRow = styled.div`
   margin-bottom: 10px;
   padding: 0 10px;
   position: sticky;
-  ${(props) =>
-    props['data-sticky'] && 'border-top: 1px solid rgba(0, 0, 0, 0.3);'}
+  top: 0;
   user-select: none;
-  top: -10px;
   z-index: 1;
   &:first-of-type {
     margin-top: -10px;
@@ -205,27 +197,9 @@ const SammelLieferungVon = ({
   ])
   const { herkunftLabel, vonKulturWerte, sammlungWerte } = dataState
 
-  const titleRowRef = useRef(null)
-  const [isSticky, setIsSticky] = useState(false)
-  const scrollHandler = useCallback(() => {
-    const top = titleRowRef?.current?.getBoundingClientRect()?.top
-    if (top < 112 && !isSticky) return setIsSticky(true)
-    if (top > 112 && isSticky) setIsSticky(false)
-  }, [isSticky])
-  useEffect(() => {
-    window.addEventListener('scroll', scrollHandler, true)
-    return () => {
-      window.removeEventListener('scroll', scrollHandler, true)
-    }
-  }, [scrollHandler])
-
   return (
     <>
-      <TitleRow
-        data-filter={showFilter}
-        ref={titleRowRef}
-        data-sticky={isSticky}
-      >
+      <TitleRow data-filter={showFilter}>
         <Title>von</Title>
       </TitleRow>
       {ifNeeded('von_sammlung_id') && (
