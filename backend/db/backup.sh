3af7e3d785004bb84a9e255a39ef7fed79e1c1e9
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-23 18:52:26 +0200

    backup with rclone

--- backend/db/backup.sh
+++ backend/db/backup.sh
@@ -6,6 +6,6 @@ echo "creating dump file..."
 FILENAME=$(date +"%Y-%m-%d_%H-%M-%S_vermehrung.backup")
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U postgres -h localhost --file=/sik_data/$FILENAME -Fc -Z9 vermehrung
 echo "uploading dump file..."
-/dropbox_uploader.sh -f dropbox_uploader.conf upload /sik_data/$FILENAME $FILENAME
-rm /sik_data/$FILENAME
+#/dropbox_uploader.sh -f dropbox_uploader.conf upload /sik_data/$FILENAME $FILENAME
+rclone move /sik_data/$FILENAME dropbox:Apps/vermehrung
 echo "backed up $FILENAME" >> /var/log/cron.log 2>&1
