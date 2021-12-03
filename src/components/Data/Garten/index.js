450bc0a69be29324a15ae2b875d6f64668b74522
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-03 10:38:07 +0100

    optimize

--- src/components/Data/Garten/index.js
+++ src/components/Data/Garten/index.js
@@ -11,6 +11,9 @@ import Conflict from './Conflict'
 import Form from './Form'
 import FormTitle from './FormTitle'
 import History from './History'
+import getConstants from '../../../utils/constants'
+
+const constants = getConstants()
 
 const Container = styled.div`
   height: 100%;
@@ -19,7 +22,7 @@ const Container = styled.div`
   background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
 `
 const StyledSplitPane = styled(SplitPane)`
-  height: calc(100vh - 64px - 48px) !important;
+  height: calc(100vh - ${constants.appBarHeight}px - 48px) !important;
   .Resizer {
     background: rgba(74, 20, 140, 0.1);
     opacity: 1;
