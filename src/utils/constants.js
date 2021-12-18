9b2e99f6708fc14969ff461f833620f211dc08c4
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-18 23:19:16 +0100

    solve window issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,23 +2,31 @@ const constants = {
   appBarHeight: 64,
   titleRowHeight: 48,
   singleRowHeight: 48,
-  getGraphQlUri: () =>
-    window?.location?.hostname === 'localhost'
+  getGraphQlUri: () => {
+    if (typeof window === 'undefined') return
+    return window?.location?.hostname === 'localhost'
       ? `http://${window.location.hostname}:8080/v1/graphql`
-      : 'https://api.vermehrung.ch/v1/graphql',
-  getGraphQlWsUri: () =>
-    window?.location?.hostname === 'localhost'
+      : 'https://api.vermehrung.ch/v1/graphql'
+  },
+  getGraphQlWsUri: () => {
+    if (typeof window === 'undefined') return
+    return window?.location?.hostname === 'localhost'
       ? `ws://${window.location.hostname}:8080/v1/graphql`
-      : 'wss://api.vermehrung.ch/v1/graphql',
-  getHealthUri: () =>
-    window?.location?.hostname === 'localhost'
+      : 'wss://api.vermehrung.ch/v1/graphql'
+  },
+  getHealthUri: () => {
+    if (typeof window === 'undefined') return
+    return window?.location?.hostname === 'localhost'
       ? `http://${window.location.hostname}:8080/healthz`
-      : 'https://api.vermehrung.ch/healthz',
+      : 'https://api.vermehrung.ch/healthz'
+  },
   authUri: 'https://auth.vermehrung.ch',
-  getAppUri: () =>
-    window?.location?.hostname === 'localhost'
+  getAppUri: () => {
+    if (typeof window === 'undefined') return
+    return window?.location?.hostname === 'localhost'
       ? `http://${window.location.hostname}:8000`
-      : 'https://vermehrung.ch',
+      : 'https://vermehrung.ch'
+  },
   testArten: ['1ab6bbb1-979a-4232-a5d8-62efb5cb984a'],
   tree: {
     minimalWidth: 331,
