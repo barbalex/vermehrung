e2a5e6c19ad9e3a706040ab5dbe897b391434408
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-12 19:36:21 +0200

    remove simplebar

--- src/components/Data/Garten/History/index.js
+++ src/components/Data/Garten/History/index.js
@@ -3,7 +3,6 @@ import { observer } from 'mobx-react-lite'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 import Slider from 'react-slick'
-import SimpleBar from 'simplebar-react'
 import { useQuery } from 'urql'
 
 import checkForOnlineError from '../../../../utils/checkForOnlineError'
@@ -36,6 +35,7 @@ const gartenRevQuery = gql`
 `
 
 const Container = styled.div`
+  overflow-y: auto;
   padding: 0 25px;
   height: 100%;
   .slick-prev:before,
@@ -74,9 +74,10 @@ const GartenHistory = ({ row, rawRow, historyTakeoverCallback }) => {
   })
   error && checkForOnlineError({ error, store })
 
-  const revRowsUnsorted = useMemo(() => data?.garten_rev ?? [], [
-    data?.garten_rev,
-  ])
+  const revRowsUnsorted = useMemo(
+    () => data?.garten_rev ?? [],
+    [data?.garten_rev],
+  )
   const revRows = revRowsUnsorted.sort((a, b) => b._depth - a._depth)
 
   if (fetching) {
@@ -88,21 +89,19 @@ const GartenHistory = ({ row, rawRow, historyTakeoverCallback }) => {
   }
 
   return (
-    <SimpleBar style={{ maxHeight: '100%', height: '100%' }}>
-      <Container>
-        <Slider {...sliderSettings}>
-          {revRows.map((r) => (
-            <Row
-              key={row._rev}
-              revRow={r}
-              row={row}
-              rawRow={rawRow}
-              historyTakeoverCallback={historyTakeoverCallback}
-            />
-          ))}
-        </Slider>
-      </Container>
-    </SimpleBar>
+    <Container>
+      <Slider {...sliderSettings}>
+        {revRows.map((r) => (
+          <Row
+            key={row._rev}
+            revRow={r}
+            row={row}
+            rawRow={rawRow}
+            historyTakeoverCallback={historyTakeoverCallback}
+          />
+        ))}
+      </Slider>
+    </Container>
   )
 }
 
