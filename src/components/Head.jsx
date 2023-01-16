b2c0b89322c38aa761d8a28490a320c6914055bb
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-16 13:39:39 +0100

    up version

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Head = () => (
   <>
-    <title>Vermehrung v1.3.59</title>
+    <title>Vermehrung v1.3.60</title>
     <meta name="description" content="Bedrohte Pflanzenarten vermehren" />
     <meta
       name="keywords"
