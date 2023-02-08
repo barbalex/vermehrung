865192c5c2aa3851e424189eb266368197c55ab5
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-08 22:39:57 +0100

    work on docs

---
+++ src/components/Documentation/docs/GenetischeVielfalt.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import { DokuDate } from '..'
+
+const GenetischeVielfalt = () => (
+  <>
+    <h1>Herkünfte</h1>
+    <DokuDate>17.01.2020</DokuDate>
+    <p>
+      <Link to="/Dokumentation/ziele">Projekt-Ziel</Link> 2 lautet: &quot;Die
+      genetische Vielfalt von Kulturen ist bekannt.&quot;
+      <br />
+    </p>
+    <p>
+      Das ist nicht ganz einfach zu realisieren. Schon der Begriff
+      &quot;genetische Vielfalt&quot; lässt viel Spielraum. Trotzdem ist klar,
+      dass er für bedrohte Arten grosse Bedeutung hat. Jede Methode wird daher
+      nur eine Annäherung sein. In vermehrung geschieht es so:
+      <br />
+    </p>
+    <p>
+      <strong>
+        In Sammlungen, Lieferungen und Kulturen wird im Feld &quot;von Anzahl
+        Individuen&quot; erfasst bzw. geschätzt, von wie vielen Individuen die
+        Pflanzen stammen. Unabhängig davon, welche Einheiten gezählt wurden.
+      </strong>
+    </p>
+  </>
+)
+
+export default GenetischeVielfalt
