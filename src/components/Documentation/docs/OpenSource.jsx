7d6508d24916e8d8ccff63cbfd53185f08ac3d33
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-08 22:53:47 +0100

    work on docs

---
+++ src/components/Documentation/docs/OpenSource.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom'
+import { DokuDate } from '..'
+
+const OpenSource = () => (
+  <>
+    <h1>Open Source</h1>
+    <DokuDate>15.06.2019</DokuDate>
+    <p>
+      vermehrung.ch wird von{' '}
+      <a href="//gabriel-software.ch" target="_blank" rel="noreferrer">
+        Gabriel-Software
+      </a>{' '}
+      für die{' '}
+      <a href="//naturschutz.zh.ch" target="_blank" rel="noreferrer">
+        Fachstelle Naturschutz des Kantons Zürich
+      </a>{' '}
+      entwickelt und soll im Rahmen des{' '}
+      <a
+        href="//www.zh.ch/de/umwelt-tiere/naturschutz/artenschutz.html"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Aktionsplans Flora
+      </a>{' '}
+      eingesetzt werden.
+      <br />
+    </p>
+    <p>
+      Der Code ist{' '}
+      <a
+        href="//github.com/barbalex/vermehrung/blob/master/LICENSE"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open source
+      </a>
+      . Es steht allen frei, ihn zu kopieren und die App selber zu betreiben.
+      <br />
+    </p>
+    <p>
+      Es ist vorgesehen, dass auf vermehrung.ch weitere Projekte aufgeschaltet
+      werden können. Interessiert? Melden Sie sich bei{' '}
+      <a
+        href="mailto:alex@gabriel-software.ch"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Alex
+      </a>
+      .
+    </p>
+  </>
+)
+
+export default OpenSource
