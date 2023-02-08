7658112ff15f6cfff634f0d52a2e8ee0e63a9d52
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-08 22:43:35 +0100

    work on docs

---
+++ src/components/Documentation/docs/QualitatesKontrollen.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import { DokuDate } from '..'
+
+const QualitaetsKontrollen = () => (
+  <>
+    <h1>Qualitäts-Kontrollen</h1>
+    <DokuDate>28.06.2020</DokuDate>
+    <h3>Ziel</h3>
+    <ul>
+      <li>
+        <Link to="/Dokumentation/ziele">Projekt-Ziel</Link> 11:
+        &quot;Qualitätskontrollen erhöhen die Qualität der Arbeit&quot;
+        <br />
+      </li>
+    </ul>
+    <h3>Umsetzung</h3>
+    <ul>
+      <li>
+        Manager und Art-Verantwortliche können auf Ebene Art und Kultur
+        Qualitäts-Kontrollen ausführen
+      </li>
+      <li>Gärtner können auf Ebene Kultur Qualitäts-Kontrollen ausführen</li>
+      <li>
+        Ein paar Dutzend Abfragen listen Verdachtsfälle. Die Liste besteht aus
+        Links, welche direkt den entsprechenden Datensatz öffnen
+      </li>
+      <li>
+        Die Liste kann gefiltert werden. Sie aktualisiert sich nach Korrekturen
+        automatisch selbst
+      </li>
+    </ul>
+  </>
+)
+
+export default QualitaetsKontrollen
