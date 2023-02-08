85bc0f71038588696a9bcca8689900cb4e765877
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-08 20:24:20 +0100

    work on docs

--- src/DocRoutes.jsx
+++ src/DocRoutes.jsx
@@ -1,17 +1,21 @@
 import { Route } from 'react-router-dom'
 
 import Ziele from './components/Documentation/docs/Ziele'
+import ZieleHerkuenfte from './components/Documentation/docs/ZieleHerkuenfte'
 import Herkuenfte from './components/Documentation/docs/Herkuenfte'
 import TechnischeVoraussetzungen from './components/Documentation/docs/TechnischeVoraussetzungen'
+import ZeitachseFuerArten from './components/Documentation/docs/ZeitachseFuerArten'
 
 const DocRoutes = () => (
   <>
     <Route path="ziele" element={<Ziele />} />
-    <Route path="ziele/herkuenfte" element={<Herkuenfte />} />
+    <Route path="ziele/herkuenfte" element={<ZieleHerkuenfte />} />
     <Route
       path="technische-voraussetzungen"
       element={<TechnischeVoraussetzungen />}
     />
+    <Route path="zeitachse-art" element={<ZeitachseFuerArten />} />
+    <Route path="herkuenfte" element={<Herkuenfte />} />
   </>
 )
 
--- src/docs/ziele_herkuenfte.md
+++
@@ -1,4 +0,0 @@
-Ziel 1 lautet: "Herkünfte sind bekannt und werden innerhalb von Kulturen nicht vermischt."<br/><br/>
-
-TODO: Wie wird das unterstützt?<br/><br/>
-
