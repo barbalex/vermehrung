d5c270dbb603e98efff2f40e2ec25e0153ac157d
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-08 19:24:33 +0100

    work on docs

--- src/DocRoutes.jsx
+++ src/DocRoutes.jsx
@@ -1,10 +1,17 @@
 import { Route } from 'react-router-dom'
 
 import Ziele from './components/Documentation/docs/Ziele'
+import Herkuenfte from './components/Documentation/docs/Herkuenfte'
+import TechnischeVoraussetzungen from './components/Documentation/docs/TechnischeVoraussetzungen'
 
 const DocRoutes = () => (
   <>
     <Route path="ziele" element={<Ziele />} />
+    <Route path="ziele/herkuenfte" element={<Herkuenfte />} />
+    <Route
+      path="technische-voraussetzungen"
+      element={<TechnischeVoraussetzungen />}
+    />
   </>
 )
 
