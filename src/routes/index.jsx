48888f9f49ba0942a02b0625f9b2fe5ca40e31ac
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-09 11:37:16 +0100

    optimize images

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,9 +5,8 @@ import styled from '@emotion/styled'
 
 import ErrorBoundary from '../components/shared/ErrorBoundary'
 import constants from '../utils/constants'
-// TODO: 2'660 KB is too much
-import image from '../images/puls_vulg.jpg'
-import placeholderSrc from '../images/puls_vulg.jpg' // TODO: build small placeholder
+import image from '../images/puls_vulg_2500.webp'
+import placeholderSrc from '../images/puls_vulg_500.webp' // TODO: build small placeholder
 import ProgressiveImg from '../components/shared/ProgressiveImg'
 
 const OuterContainer = styled.div`
