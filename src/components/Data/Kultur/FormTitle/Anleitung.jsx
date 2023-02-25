42e4eb26ccc48a70c49d763cd249acc19ac8ca35
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-02-25 14:48:36 +0100

    correct link

--- src/components/Data/Kultur/FormTitle/Anleitung.jsx
+++ src/components/Data/Kultur/FormTitle/Anleitung.jsx
@@ -7,7 +7,7 @@ import constants from '../../../../utils/constants'
 
 const KulturAnleitung = ({ asMenu }) => {
   const openKulturDocs = useCallback(() => {
-    const url = `${constants?.getAppUri()}/kulturen`
+    const url = `${constants?.getAppUri()}/Dokumentation/kulturen`
     if (window.matchMedia('(display-mode: standalone)').matches) {
       return window.open(url, '_blank', 'toolbar=no')
     }
