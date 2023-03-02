108c965f691c85e720e63ec84f040a9f5baac47b
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-03-02 14:43:43 +0100

    correct

--- src/components/Data/Art/Form/HerkunftTimeline/Pflanzen/index.jsx
+++ src/components/Data/Art/Form/HerkunftTimeline/Pflanzen/index.jsx
@@ -14,7 +14,7 @@ import {
   Scatter,
 } from 'recharts'
 import { observer } from 'mobx-react-lite'
-import { withResizeDetector } from 'react-resize-detector'
+import { useResizeDetector } from 'react-resize-detector'
 import styled from '@emotion/styled'
 
 import CustomTooltip from './Tooltip'
@@ -32,11 +32,16 @@ const H4 = styled.h4`
 const NoData = styled.div`
   padding: 0 10px 15px 0;
 `
+const Container = styled.div`
+  width: 100%;
+`
 
-const ArtTimeline = ({ artId, herkunft, width }) => {
+const ArtTimeline = ({ artId, herkunft }) => {
   const store = useContext(StoreContext)
   const { db } = store
   const herkunftId = herkunft.id
+
+  const { width, ref } = useResizeDetector()
 
   const [data, setData] = useState(null)
   useEffect(() => {
@@ -52,15 +57,7 @@ const ArtTimeline = ({ artId, herkunft, width }) => {
     }
   }, [artId, herkunftId, db])
 
-  const [narrow, setNarrow] = useState(false)
-  useEffect(() => {
-    if (width < 1100 && !narrow) {
-      setNarrow(true)
-    }
-    if (width > 1100 && narrow) {
-      setNarrow(false)
-    }
-  }, [narrow, width])
+  const isNarrow = width < 1100
 
   const herkunftLabel = herkunftLabelFromHerkunft({ herkunft })
 
@@ -79,95 +76,97 @@ const ArtTimeline = ({ artId, herkunft, width }) => {
   // https://github.com/recharts/recharts/issues/806
 
   return (
-    <ErrorBoundary>
-      <h4>{herkunftLabel}</h4>
-      <ResponsiveContainer width="99%" height={450}>
-        <ComposedChart
-          data={data}
-          margin={{ top: 25, right: 0, left: 0, bottom: 45 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" horizontal={false} />
-          <XAxis dataKey="datum" tick={CustomAxisTick} interval={0} />
-          <YAxis
-            label={{
-              value: 'Anzahl Pflanzen',
-              angle: -90,
-              position: 'insideLeft',
-              offset: 15,
-              fontSize: 14,
-              fontWeight: 700,
-            }}
-            fontSize={12}
-          />
-          <Tooltip content={<CustomTooltip />} />
-          {narrow ? (
-            <Legend
-              layout="horizontal"
-              align="center"
-              wrapperStyle={{ bottom: 0, fontSize: 12 }}
+    <Container ref={ref}>
+      <ErrorBoundary>
+        <h4>{herkunftLabel}</h4>
+        <ResponsiveContainer width="99%" height={450}>
+          <ComposedChart
+            data={data}
+            margin={{ top: 25, right: 0, left: 0, bottom: 45 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+            <XAxis dataKey="datum" tick={CustomAxisTick} interval={0} />
+            <YAxis
+              label={{
+                value: 'Anzahl Pflanzen',
+                angle: -90,
+                position: 'insideLeft',
+                offset: 15,
+                fontSize: 14,
+                fontWeight: 700,
+              }}
+              fontSize={12}
             />
-          ) : (
-            <Legend
-              layout="vertical"
-              align="right"
-              wrapperStyle={{ right: -10, bottom: 150, fontSize: 12 }}
+            <Tooltip content={<CustomTooltip />} />
+            {isNarrow ? (
+              <Legend
+                layout="horizontal"
+                align="center"
+                wrapperStyle={{ bottom: 0, fontSize: 12 }}
+              />
+            ) : (
+              <Legend
+                layout="vertical"
+                align="right"
+                wrapperStyle={{ right: -10, bottom: 150, fontSize: 12 }}
+              />
+            )}
+            <ReferenceLine y={0} stroke="#000" />
+            <Bar
+              dataKey="Sammlung"
+              fill="orange"
+              label={<LabelLieferung />}
+              isAnimationActive={false}
             />
-          )}
-          <ReferenceLine y={0} stroke="#000" />
-          <Bar
-            dataKey="Sammlung"
-            fill="orange"
-            label={<LabelLieferung />}
-            isAnimationActive={false}
-          />
-          <Bar
-            dataKey="Sammlung geplant"
-            fill="yellow"
-            label={<LabelLieferung />}
-            isAnimationActive={false}
-          />
-          <Bar
-            dataKey="Auspflanzung"
-            fill="#016526"
-            label={<LabelLieferung />}
-            isAnimationActive={false}
-          />
-          <Bar
-            dataKey="Auspflanzung geplant"
-            fill="#9cffc0"
-            label={<LabelLieferung />}
-            isAnimationActive={false}
-          />
-          <Line
-            type="monotone"
-            connectNulls={true}
-            dataKey="Anzahl berechnet"
-            stroke="#4a148c"
-            strokeWidth={3}
-            label={<LabelZaehlung />}
-            isAnimationActive={false}
-            dot={false}
-          />
-          <Scatter
-            dataKey="Zählung"
-            stroke="#4a148c"
-            fill="#4a148c"
-            // fill white makes legend go completely white...
-            //fill="white"
-            strokeWidth={3}
-            isAnimationActive={false}
-          ></Scatter>
-          <Scatter
-            dataKey="Prognose"
-            stroke="#e0e0ff"
-            fill="#e0e0ff"
-            strokeWidth={3}
-            isAnimationActive={false}
-          ></Scatter>
-        </ComposedChart>
-      </ResponsiveContainer>
-    </ErrorBoundary>
+            <Bar
+              dataKey="Sammlung geplant"
+              fill="yellow"
+              label={<LabelLieferung />}
+              isAnimationActive={false}
+            />
+            <Bar
+              dataKey="Auspflanzung"
+              fill="#016526"
+              label={<LabelLieferung />}
+              isAnimationActive={false}
+            />
+            <Bar
+              dataKey="Auspflanzung geplant"
+              fill="#9cffc0"
+              label={<LabelLieferung />}
+              isAnimationActive={false}
+            />
+            <Line
+              type="monotone"
+              connectNulls={true}
+              dataKey="Anzahl berechnet"
+              stroke="#4a148c"
+              strokeWidth={3}
+              label={<LabelZaehlung />}
+              isAnimationActive={false}
+              dot={false}
+            />
+            <Scatter
+              dataKey="Zählung"
+              stroke="#4a148c"
+              fill="#4a148c"
+              // fill white makes legend go completely white...
+              //fill="white"
+              strokeWidth={3}
+              isAnimationActive={false}
+            ></Scatter>
+            <Scatter
+              dataKey="Prognose"
+              stroke="#e0e0ff"
+              fill="#e0e0ff"
+              strokeWidth={3}
+              isAnimationActive={false}
+            ></Scatter>
+          </ComposedChart>
+        </ResponsiveContainer>
+      </ErrorBoundary>
+    </Container>
   )
 }
 
-export default withResizeDetector(observer(ArtTimeline))
+export default observer(ArtTimeline)
