29d0535af03432f59dff13ecbf8767700cf12467
Alexander Gabriel <alex.barbalex@gmail.com>
2023-04-20 12:09:10 +0200

    refactor

--- src/components/Data/Lieferung/Lieferung/Form/Nach/Add/index.jsx
+++ src/components/Data/Lieferung/Lieferung/Form/Nach/Add/index.jsx
@@ -1,43 +1,15 @@
 import { useCallback, useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import IconButton from '@mui/material/IconButton'
-import Dialog from '@mui/material/Dialog'
-import DialogTitle from '@mui/material/DialogTitle'
-import DialogContent from '@mui/material/DialogContent'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import FormControl from '@mui/material/FormControl'
 
 import styled from '@emotion/styled'
 
-import ErrorBoundary from '../../../../../shared/ErrorBoundary'
+import ErrorBoundary from '../../../../../../shared/ErrorBoundary'
+import TypeDialog from './TypeDialog'
 
 const StyledButton = styled(IconButton)`
   margin-bottom: 19px;
 `
-
-const TypeDialog = ({ open, onClose, onChangeType }) => (
-  <Dialog open={open} onClose={onClose}>
-    <DialogTitle>Typ der Kultur wählen:</DialogTitle>
-    <DialogContent>
-      <FormControl>
-        <RadioGroup name="type_waehlen" onChange={onChangeType}>
-          <FormControlLabel
-            value="zwischenlager"
-            control={<Radio />}
-            label="Zwischenlager"
-          />
-          <FormControlLabel
-            value="anders"
-            control={<Radio />}
-            label="Andere Kultur"
-          />
-        </RadioGroup>
-      </FormControl>
-    </DialogContent>
-  </Dialog>
-)
 
 const Add = ({ saveToDb, disabled }) => {
   const [kulturType, setKulturType] = useState()
