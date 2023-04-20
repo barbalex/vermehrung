5f000c524cded4f34b0bc854f65608e24a569d3b
Alexander Gabriel <alex.barbalex@gmail.com>
2023-04-20 12:06:12 +0200

    choose kulture type

--- src/components/Data/Lieferung/Lieferung/Form/Nach/Add.jsx
+++ src/components/Data/Lieferung/Lieferung/Form/Nach/Add.jsx
@@ -1,6 +1,14 @@
 import { useCallback, useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import IconButton from '@mui/material/IconButton'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormControl from '@mui/material/FormControl'
+
 import styled from '@emotion/styled'
 
 import ErrorBoundary from '../../../../../shared/ErrorBoundary'
@@ -9,13 +17,45 @@ const StyledButton = styled(IconButton)`
   margin-bottom: 19px;
 `
 
+const TypeDialog = ({ open, onClose, onChangeType }) => (
+  <Dialog open={open} onClose={onClose}>
+    <DialogTitle>Typ der Kultur wählen:</DialogTitle>
+    <DialogContent>
+      <FormControl>
+        <RadioGroup name="type_waehlen" onChange={onChangeType}>
+          <FormControlLabel
+            value="zwischenlager"
+            control={<Radio />}
+            label="Zwischenlager"
+          />
+          <FormControlLabel
+            value="anders"
+            control={<Radio />}
+            label="Andere Kultur"
+          />
+        </RadioGroup>
+      </FormControl>
+    </DialogContent>
+  </Dialog>
+)
+
 const Add = ({ saveToDb, disabled }) => {
-  const [typeDialogOpen, setTypeDialogOpen] = useState()
-  const [gardenDialogOpen, setGardenDialogOpen] = useState()
+  const [kulturType, setKulturType] = useState()
+  const [typeDialogOpen, setTypeDialogOpen] = useState(false)
+  const [gardenDialogOpen, setGardenDialogOpen] = useState(false)
+
+  const onCloseTypeDialog = useCallback(() => setTypeDialogOpen(false), [])
+  const onChangeType = useCallback((event) => {
+    setKulturType(event.target.value)
+    setTypeDialogOpen(false)
+    setGardenDialogOpen(true)
+  }, [])
 
   const add = useCallback(() => {
-    console.log('TODO:')
+    setTypeDialogOpen(true)
   }, [])
+
+  console.log('Add', { kulturType, gardenDialogOpen })
 
   return (
     <ErrorBoundary>
@@ -28,6 +68,11 @@ const Add = ({ saveToDb, disabled }) => {
       >
         <FaPlus />
       </StyledButton>
+      <TypeDialog
+        open={typeDialogOpen}
+        onClose={onCloseTypeDialog}
+        onChangeType={onChangeType}
+      />
     </ErrorBoundary>
   )
 }
