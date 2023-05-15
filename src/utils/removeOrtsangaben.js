970a653e4b659a16e2d4a880787c8b6e972ca864
Alexander Gabriel <alex.barbalex@gmail.com>
2023-05-15 11:30:26 +0200

    97: remove ortsangaben

---
+++ src/utils/removeOrtsangaben.js
@@ -0,0 +1,6 @@
+const removeOrtsangaben = () => {
+  // need to remove some data for gaertner in case they had synced them earlier
+  // TODO: remove: lokalname, geom_point, lv95_x, lv95_y, wgs84_lat, wgs84_long
+}
+
+export default removeOrtsangaben
