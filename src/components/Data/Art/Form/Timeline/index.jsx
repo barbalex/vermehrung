17266543c83b243e8e59797297c8b8e3ef27667c
Alexander Gabriel <alex.barbalex@gmail.com>
2023-05-22 17:51:16 +0200

    correct style

--- src/components/Data/Art/Form/Timeline/index.jsx
+++ src/components/Data/Art/Form/Timeline/index.jsx
@@ -13,17 +13,17 @@ import constants from '../../../../../utils/constants'
 const TitleRow = styled.div`
   background-color: rgba(248, 243, 254, 1);
   flex-shrink: 0;
-  display: flex;
+  display: flex !important;
   height: ${constants.titleRowHeight}px;
-  justify-content: space-between;
+  justify-content: space-between !important;
   margin-left: -10px;
   margin-right: -10px;
   margin-bottom: 10px;
   padding: 0 10px;
   cursor: pointer;
   user-select: none;
-  position: sticky;
-  top: 0;
+  position: sticky !important;
+  top: -10px;
   z-index: 1;
   &:first-of-type {
     margin-top: -10px;
