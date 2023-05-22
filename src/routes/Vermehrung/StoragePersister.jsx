69bdbe98d332a629a7a6d2a0d92d4148400f2d0e
Alexander Gabriel <alex.barbalex@gmail.com>
2023-05-22 14:14:31 +0200

    modularize

---
+++ src/routes/Vermehrung/StoragePersister.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+
+// trying to persist indexedDB
+// https://dexie.org/docs/StorageManager#controlling-persistence
+// TODO: consider calling this only if user choose it in settings
+// or pop own window to explain as shown in above link
+// because it pops a request window
+async function persist() {
+  return (
+    (await navigator.storage) &&
+    navigator.storage.persist &&
+    navigator.storage.persist()
+  )
+}
+
+const StoragePersister = () => {
+  useEffect(() => {
+    persist().then((val) => console.log('storage is persisted safely:', val))
+  }, [])
+
+  return null
+}
+
+export default StoragePersister
