4db2eefd90541d07aca529e198fdcda6bbd014e7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-16 22:42:21 +0200

    import using full path

--- src/components/Documentation/docs/Datenschutz.jsx
+++ src/components/Documentation/docs/Datenschutz.jsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom'
-import { DokuDate } from '..'
+import { DokuDate } from '../index.jsx'
 
 const Datenschutz = () => (
   <>
--- src/components/Documentation/docs/Pwa/index.jsx
+++ src/components/Documentation/docs/Pwa/index.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom'
-
 import { DokuDate } from '../../index.jsx'
 import installieren from './installieren.webp'
 
