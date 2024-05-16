94ae71e61c3bd44e35bc179204aadaebf8327574
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-16 22:34:21 +0200

    import using full path

--- src/components/Data/Teilkulturen/Row.jsx
+++ src/components/Data/Teilkulturen/Row.jsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react-lite'
 import styled from '@emotion/styled'
 
 import StoreContext from '../../../storeContext.js'
-import teilkulturLabelFromTeilkultur from '../../../utils/teilkulturLabelFromTeilkultur'
+import teilkulturLabelFromTeilkultur from '../../../utils/teilkulturLabelFromTeilkultur.js'
 import constants from '../../../utils/constants.js'
 
 const Row = styled.div`
