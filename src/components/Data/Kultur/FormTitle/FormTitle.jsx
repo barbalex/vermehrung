745d15e11e78821e3c789c277b79266ccb1d9862
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-08 22:29:09 +0100

    upgrade using resize detector

--- src/components/Data/Kultur/FormTitle/FormTitle.jsx
+++ src/components/Data/Kultur/FormTitle/FormTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from '@emotion/styled'
-import { withResizeDetector } from 'react-resize-detector'
+import { useResizeDetector } from 'react-resize-detector'
 
 import Settings from './Settings/index.jsx'
 import Delete from './Delete/index.jsx'
@@ -40,178 +40,164 @@ const KulturFormTitle = ({
   row,
   totalCount,
   filteredCount,
-  width,
   showHistory,
   setShowHistory,
 }) => {
-  if (width < 520) {
-    return (
-      <TitleContainer>
-        <Title>Kultur</Title>
-        <TitleSymbols>
-          <NavButtons row={row} />
-          <Menu white={false}>
-            <Add asMenu />
-            <Delete asMenu row={row} />
-            <Download row={row} asMenu />
-            <HistoryButton
-              table="kultur"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Anleitung asMenu />
-            <Settings kulturId={row.id} asMenu />
-            <FilterNumbers
-              filteredCount={filteredCount}
-              totalCount={totalCount}
-              asMenu
-            />
-          </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
-
-  if (width < 567) {
-    return (
-      <TitleContainer>
-        <Title>Kultur</Title>
-        <TitleSymbols>
-          <NavButtons row={row} />
-          <Add />
-          <Delete row={row} />
-          <Menu white={false}>
-            <Download row={row} asMenu />
-            <HistoryButton
-              table="kultur"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Anleitung asMenu />
-            <Settings kulturId={row.id} asMenu />
-            <FilterNumbers
-              filteredCount={filteredCount}
-              totalCount={totalCount}
-              asMenu
-            />
-          </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
-
-  if (width < 613) {
-    return (
-      <TitleContainer>
-        <Title>Kultur</Title>
-        <TitleSymbols>
-          <NavButtons row={row} />
-          <Add />
-          <Delete row={row} />
-          <Settings kulturId={row.id} />
-          <Menu white={false}>
-            <Download row={row} asMenu />
-            <HistoryButton
-              table="kultur"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Anleitung asMenu />
-            <FilterNumbers
-              filteredCount={filteredCount}
-              totalCount={totalCount}
-              asMenu
-            />
-          </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
-
-  if (width < 660) {
-    return (
-      <TitleContainer>
-        <Title>Kultur</Title>
-        <TitleSymbols>
-          <NavButtons row={row} />
-          <Add />
-          <Delete row={row} />
-          <Settings kulturId={row.id} />
-          <FilterNumbers
-            filteredCount={filteredCount}
-            totalCount={totalCount}
-          />
-          <Menu white={false}>
-            <Download row={row} asMenu />
-            <HistoryButton
-              table="kultur"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Anleitung asMenu />
-          </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
-
-  if (width < 703) {
-    return (
-      <TitleContainer>
-        <Title>Kultur</Title>
-        <TitleSymbols>
-          <NavButtons row={row} />
-          <Add />
-          <Delete row={row} />
-          <Download row={row} />
-          <Settings kulturId={row.id} />
-          <FilterNumbers
-            filteredCount={filteredCount}
-            totalCount={totalCount}
-          />
-          <Menu white={false}>
-            <HistoryButton
-              table="kultur"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Anleitung asMenu />
-          </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
+  const { width, ref } = useResizeDetector()
 
   return (
-    <TitleContainer>
+    <TitleContainer ref={ref}>
       <Title>Kultur</Title>
       <TitleSymbols>
         <NavButtons row={row} />
-        <Add />
-        <Delete row={row} />
-        <Download row={row} />
-        <HistoryButton
-          table="kultur"
-          id={row.id}
-          showHistory={showHistory}
-          setShowHistory={setShowHistory}
-        />
-        <Anleitung />
-        <Settings kulturId={row.id} />
-        <FilterNumbers filteredCount={filteredCount} totalCount={totalCount} />
+        {width < 520 ?
+          <Menu white={false}>
+            <Add asMenu />
+            <Delete
+              asMenu
+              row={row}
+            />
+            <Download
+              row={row}
+              asMenu
+            />
+            <HistoryButton
+              table="kultur"
+              id={row.id}
+              showHistory={showHistory}
+              setShowHistory={setShowHistory}
+              asMenu
+            />
+            <Anleitung asMenu />
+            <Settings
+              kulturId={row.id}
+              asMenu
+            />
+            <FilterNumbers
+              filteredCount={filteredCount}
+              totalCount={totalCount}
+              asMenu
+            />
+          </Menu>
+        : width < 567 ?
+          <>
+            <Add />
+            <Delete row={row} />
+            <Menu white={false}>
+              <Download
+                row={row}
+                asMenu
+              />
+              <HistoryButton
+                table="kultur"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+                asMenu
+              />
+              <Anleitung asMenu />
+              <Settings
+                kulturId={row.id}
+                asMenu
+              />
+              <FilterNumbers
+                filteredCount={filteredCount}
+                totalCount={totalCount}
+                asMenu
+              />
+            </Menu>
+          </>
+        : width < 613 ?
+          <>
+            <Add />
+            <Delete row={row} />
+            <Settings kulturId={row.id} />
+            <Menu white={false}>
+              <Download
+                row={row}
+                asMenu
+              />
+              <HistoryButton
+                table="kultur"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+                asMenu
+              />
+              <Anleitung asMenu />
+              <FilterNumbers
+                filteredCount={filteredCount}
+                totalCount={totalCount}
+                asMenu
+              />
+            </Menu>
+          </>
+        : width < 660 ?
+          <>
+            <Add />
+            <Delete row={row} />
+            <Settings kulturId={row.id} />
+            <FilterNumbers
+              filteredCount={filteredCount}
+              totalCount={totalCount}
+            />
+            <Menu white={false}>
+              <Download
+                row={row}
+                asMenu
+              />
+              <HistoryButton
+                table="kultur"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+                asMenu
+              />
+              <Anleitung asMenu />
+            </Menu>
+          </>
+        : width < 703 ?
+          <>
+            <Add />
+            <Delete row={row} />
+            <Download row={row} />
+            <Settings kulturId={row.id} />
+            <FilterNumbers
+              filteredCount={filteredCount}
+              totalCount={totalCount}
+            />
+            <Menu white={false}>
+              <HistoryButton
+                table="kultur"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+                asMenu
+              />
+              <Anleitung asMenu />
+            </Menu>
+          </>
+        : <>
+            <Add />
+            <Delete row={row} />
+            <Download row={row} />
+            <HistoryButton
+              table="kultur"
+              id={row.id}
+              showHistory={showHistory}
+              setShowHistory={setShowHistory}
+            />
+            <Anleitung />
+            <Settings kulturId={row.id} />
+            <FilterNumbers
+              filteredCount={filteredCount}
+              totalCount={totalCount}
+            />
+          </>
+        }
       </TitleSymbols>
     </TitleContainer>
   )
 }
 
-export default withResizeDetector(observer(KulturFormTitle))
+export default observer(KulturFormTitle)
