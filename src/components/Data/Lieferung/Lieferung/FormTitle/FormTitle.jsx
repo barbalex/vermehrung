0dd06366d3c01d43a5e0a5f18e31cf00f2d4969f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-08 22:33:23 +0100

    upgrade using resize detector

--- src/components/Data/Lieferung/Lieferung/FormTitle/FormTitle.jsx
+++ src/components/Data/Lieferung/Lieferung/FormTitle/FormTitle.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from '@emotion/styled'
 import IconButton from '@mui/material/IconButton'
-import { withResizeDetector } from 'react-resize-detector'
+import { useResizeDetector } from 'react-resize-detector'
 
 import StoreContext from '../../../../../storeContext.js'
 import Settings from './Settings/index.jsx'
@@ -43,12 +43,13 @@ const LieferungTitleFormTitle = ({
   row,
   totalCount,
   filteredCount,
-  width,
   showHistory,
   setShowHistory,
 }) => {
   const store = useContext(StoreContext)
   const { activeNodeArray, setActiveNodeArray, removeOpenNode } = store.tree
+
+  const { width, ref } = useResizeDetector()
 
   const onClickUp = useCallback(() => {
     removeOpenNode(activeNodeArray)
@@ -67,25 +68,29 @@ const LieferungTitleFormTitle = ({
   // to kulturen is not implemented in nodes, so turned off
   const showToKu = false && activeNodeArray[0] === 'Sammlungen'
 
-  if (width < 520) {
-    return (
-      <TitleContainer>
-        <Title>Lieferung</Title>
-        <TitleSymbols>
-          <IconButton title="Zur Liste" onClick={onClickUp} size="large">
-            <UpSvg />
+  return (
+    <TitleContainer ref={ref}>
+      <Title>Lieferung</Title>
+      <TitleSymbols>
+        <IconButton
+          title="Zur Liste"
+          onClick={onClickUp}
+          size="large"
+        >
+          <UpSvg />
+        </IconButton>
+        {showToKu && (
+          <IconButton
+            title="Zur Kultur"
+            onClick={onClickToKultur}
+            size="large"
+          >
+            <KuDownSvg />
           </IconButton>
-          {showToKu && (
-            <IconButton
-              title="Zur Kultur"
-              onClick={onClickToKultur}
-              size="large"
-            >
-              <KuDownSvg />
-            </IconButton>
-          )}
-          <AddButton />
-          <DeleteButton row={row} />
+        )}
+        <AddButton />
+        <DeleteButton row={row} />
+        {width < 520 ?
           <Menu white={false}>
             <HistoryButton
               table="lieferung"
@@ -102,37 +107,24 @@ const LieferungTitleFormTitle = ({
               asMenu
             />
           </Menu>
-        </TitleSymbols>
-      </TitleContainer>
-    )
-  }
-
-  return (
-    <TitleContainer>
-      <Title>Lieferung</Title>
-      <TitleSymbols>
-        <IconButton title="Zur Liste" onClick={onClickUp} size="large">
-          <UpSvg />
-        </IconButton>
-        {showToKu && (
-          <IconButton title="Zur Kultur" onClick={onClickToKultur} size="large">
-            <KuDownSvg />
-          </IconButton>
-        )}
-        <AddButton />
-        <DeleteButton row={row} />
-        <HistoryButton
-          table="lieferung"
-          id={row.id}
-          showHistory={showHistory}
-          setShowHistory={setShowHistory}
-        />
-        <Settings />
-        <Anleitung />
-        <FilterNumbers filteredCount={filteredCount} totalCount={totalCount} />
+        : <>
+            <HistoryButton
+              table="lieferung"
+              id={row.id}
+              showHistory={showHistory}
+              setShowHistory={setShowHistory}
+            />
+            <Settings />
+            <Anleitung />
+            <FilterNumbers
+              filteredCount={filteredCount}
+              totalCount={totalCount}
+            />
+          </>
+        }
       </TitleSymbols>
     </TitleContainer>
   )
 }
 
-export default withResizeDetector(observer(LieferungTitleFormTitle))
+export default observer(LieferungTitleFormTitle)
