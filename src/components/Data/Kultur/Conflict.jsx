c7fe657242f9d2e722c99975ea580d6f7621f8c8
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 17:01:00 +0100

    refactor: export components

--- src/components/Data/Kultur/Conflict.jsx
+++ src/components/Data/Kultur/Conflict.jsx
@@ -37,183 +37,186 @@ const kulturRevQuery = gql`
   }
 `
 
-const KulturConflict = ({
-  id,
-  rev,
-  row,
-  conflictDisposalCallback,
-  conflictSelectionCallback,
-  setActiveConflict,
-}) => {
-  const store = useContext(MobxStoreContext)
-  const { user, addNotification, addQueuedQuery, db, gqlClient } = store
-
-  // need to use this query to ensure that the person's name is queried
-  const [{ error, data, fetching }] = useQuery({
-    query: kulturRevQuery,
-    variables: {
-      rev,
-      id,
-    },
-  })
-  error && checkForOnlineError({ error, store })
-
-  const revRow = useMemo(() => data?.kultur_rev?.[0] ?? {}, [data?.kultur_rev])
-
-  const dataArray = useMemo(
-    () => createDataArrayForKulturRevComparison({ row, revRow }),
-    [revRow, row],
-  )
-
-  const onClickAktuellUebernehmen = useCallback(async () => {
-    // build new object
-    const newDepth = revRow._depth + 1
-    const newObject = {
-      kultur_id: revRow.kultur_id,
-      art_id: revRow.art_id,
-      herkunft_id: revRow.herkunft_id,
-      garten_id: revRow.garten_id,
-      zwischenlager: revRow.zwischenlager,
-      erhaltungskultur: revRow.erhaltungskultur,
-      von_anzahl_individuen: revRow.von_anzahl_individuen,
-      bemerkungen: revRow.bemerkungen,
-      aktiv: revRow.aktiv,
-      _parent_rev: revRow._rev,
-      _depth: newDepth,
-      _deleted: true,
-    }
-    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-    newObject._rev = rev
-    newObject.id = uuidv1()
-    // do not revision the following fields as this leads to unwanted conflicts
-    newObject.changed = new window.Date().toISOString()
-    newObject.changed_by = user.email
-    newObject._revisions =
-      revRow._revisions ?
-        toPgArray([rev, ...revRow._revisions])
-      : toPgArray([rev])
-
-    addQueuedQuery({
-      name: 'mutateInsert_kultur_rev_one',
-      variables: JSON.stringify({
-        object: newObject,
-        on_conflict: {
-          constraint: 'kultur_rev_pkey',
-          update_columns: ['id'],
-        },
-      }),
-      revertTable: 'kultur',
-      revertId: revRow.kultur_id,
-      revertField: '_deleted',
-      revertValue: false,
-    })
-    // update model: remove this conflict
-    try {
-      const model = await db.get('kultur').find(revRow.kultur_id)
-      await model.removeConflict(revRow._rev)
-    } catch {}
-    conflictDisposalCallback()
-  }, [
-    addQueuedQuery,
+export const KulturConflict = observer(
+  ({
+    id,
+    rev,
+    row,
     conflictDisposalCallback,
-    db,
-    revRow._depth,
-    revRow._rev,
-    revRow._revisions,
-    revRow.aktiv,
-    revRow.art_id,
-    revRow.bemerkungen,
-    revRow.erhaltungskultur,
-    revRow.garten_id,
-    revRow.herkunft_id,
-    revRow.kultur_id,
-    revRow.von_anzahl_individuen,
-    revRow.zwischenlager,
-    user.email,
-  ])
-  const onClickWiderspruchUebernehmen = useCallback(async () => {
-    // need to attach to the winner, that is row
-    // otherwise risk to still have lower depth and thus loosing
-    const newDepth = row._depth + 1
-    const newObject = {
-      kultur_id: revRow.kultur_id,
-      art_id: revRow.art_id,
-      herkunft_id: revRow.herkunft_id,
-      garten_id: revRow.garten_id,
-      zwischenlager: revRow.zwischenlager,
-      erhaltungskultur: revRow.erhaltungskultur,
-      von_anzahl_individuen: revRow.von_anzahl_individuen,
-      bemerkungen: revRow.bemerkungen,
-      aktiv: revRow.aktiv,
-      _parent_rev: row._rev,
-      _depth: newDepth,
-      _deleted: revRow._deleted,
-    }
-    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-    newObject._rev = rev
-    newObject.id = uuidv1()
-    newObject.changed = new window.Date().toISOString()
-    newObject.changed_by = user.email
-    newObject._revisions =
-      row._revisions ? toPgArray([rev, ...row._revisions]) : toPgArray([rev])
-    const response = await gqlClient
-      .query(mutations.mutateInsert_kultur_rev_one, {
-        object: newObject,
-        on_conflict: {
-          constraint: 'kultur_rev_pkey',
-          update_columns: ['id'],
-        },
-      })
-      .toPromise()
-    if (response.error) {
-      checkForOnlineError({ error: response.error, store })
-      return addNotification({
-        message: response.error.message,
-      })
-    }
-    // now we need to delete the previous conflict
-    onClickAktuellUebernehmen()
-    conflictSelectionCallback()
-  }, [
-    addNotification,
     conflictSelectionCallback,
-    gqlClient,
-    onClickAktuellUebernehmen,
-    revRow._deleted,
-    revRow.aktiv,
-    revRow.art_id,
-    revRow.bemerkungen,
-    revRow.erhaltungskultur,
-    revRow.garten_id,
-    revRow.herkunft_id,
-    revRow.kultur_id,
-    revRow.von_anzahl_individuen,
-    revRow.zwischenlager,
-    row._depth,
-    row._rev,
-    row._revisions,
-    store,
-    user.email,
-  ])
-  const onClickSchliessen = useCallback(
-    () => setActiveConflict(null),
-    [setActiveConflict],
-  )
+    setActiveConflict,
+  }) => {
+    const store = useContext(MobxStoreContext)
+    const { user, addNotification, addQueuedQuery, db, gqlClient } = store
 
-  console.log('Kultur Conflict', { dataArray, row, revRow })
+    // need to use this query to ensure that the person's name is queried
+    const [{ error, data, fetching }] = useQuery({
+      query: kulturRevQuery,
+      variables: {
+        rev,
+        id,
+      },
+    })
+    error && checkForOnlineError({ error, store })
 
-  return (
-    <Conflict
-      name="Kultur"
-      rev={rev}
-      dataArray={dataArray}
-      fetching={fetching}
-      error={error}
-      onClickAktuellUebernehmen={onClickAktuellUebernehmen}
-      onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
-      onClickSchliessen={onClickSchliessen}
-    />
-  )
-}
+    const revRow = useMemo(
+      () => data?.kultur_rev?.[0] ?? {},
+      [data?.kultur_rev],
+    )
 
-export default observer(KulturConflict)
+    const dataArray = useMemo(
+      () => createDataArrayForKulturRevComparison({ row, revRow }),
+      [revRow, row],
+    )
+
+    const onClickAktuellUebernehmen = useCallback(async () => {
+      // build new object
+      const newDepth = revRow._depth + 1
+      const newObject = {
+        kultur_id: revRow.kultur_id,
+        art_id: revRow.art_id,
+        herkunft_id: revRow.herkunft_id,
+        garten_id: revRow.garten_id,
+        zwischenlager: revRow.zwischenlager,
+        erhaltungskultur: revRow.erhaltungskultur,
+        von_anzahl_individuen: revRow.von_anzahl_individuen,
+        bemerkungen: revRow.bemerkungen,
+        aktiv: revRow.aktiv,
+        _parent_rev: revRow._rev,
+        _depth: newDepth,
+        _deleted: true,
+      }
+      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+      newObject._rev = rev
+      newObject.id = uuidv1()
+      // do not revision the following fields as this leads to unwanted conflicts
+      newObject.changed = new window.Date().toISOString()
+      newObject.changed_by = user.email
+      newObject._revisions =
+        revRow._revisions ?
+          toPgArray([rev, ...revRow._revisions])
+        : toPgArray([rev])
+
+      addQueuedQuery({
+        name: 'mutateInsert_kultur_rev_one',
+        variables: JSON.stringify({
+          object: newObject,
+          on_conflict: {
+            constraint: 'kultur_rev_pkey',
+            update_columns: ['id'],
+          },
+        }),
+        revertTable: 'kultur',
+        revertId: revRow.kultur_id,
+        revertField: '_deleted',
+        revertValue: false,
+      })
+      // update model: remove this conflict
+      try {
+        const model = await db.get('kultur').find(revRow.kultur_id)
+        await model.removeConflict(revRow._rev)
+      } catch {}
+      conflictDisposalCallback()
+    }, [
+      addQueuedQuery,
+      conflictDisposalCallback,
+      db,
+      revRow._depth,
+      revRow._rev,
+      revRow._revisions,
+      revRow.aktiv,
+      revRow.art_id,
+      revRow.bemerkungen,
+      revRow.erhaltungskultur,
+      revRow.garten_id,
+      revRow.herkunft_id,
+      revRow.kultur_id,
+      revRow.von_anzahl_individuen,
+      revRow.zwischenlager,
+      user.email,
+    ])
+    const onClickWiderspruchUebernehmen = useCallback(async () => {
+      // need to attach to the winner, that is row
+      // otherwise risk to still have lower depth and thus loosing
+      const newDepth = row._depth + 1
+      const newObject = {
+        kultur_id: revRow.kultur_id,
+        art_id: revRow.art_id,
+        herkunft_id: revRow.herkunft_id,
+        garten_id: revRow.garten_id,
+        zwischenlager: revRow.zwischenlager,
+        erhaltungskultur: revRow.erhaltungskultur,
+        von_anzahl_individuen: revRow.von_anzahl_individuen,
+        bemerkungen: revRow.bemerkungen,
+        aktiv: revRow.aktiv,
+        _parent_rev: row._rev,
+        _depth: newDepth,
+        _deleted: revRow._deleted,
+      }
+      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+      newObject._rev = rev
+      newObject.id = uuidv1()
+      newObject.changed = new window.Date().toISOString()
+      newObject.changed_by = user.email
+      newObject._revisions =
+        row._revisions ? toPgArray([rev, ...row._revisions]) : toPgArray([rev])
+      const response = await gqlClient
+        .query(mutations.mutateInsert_kultur_rev_one, {
+          object: newObject,
+          on_conflict: {
+            constraint: 'kultur_rev_pkey',
+            update_columns: ['id'],
+          },
+        })
+        .toPromise()
+      if (response.error) {
+        checkForOnlineError({ error: response.error, store })
+        return addNotification({
+          message: response.error.message,
+        })
+      }
+      // now we need to delete the previous conflict
+      onClickAktuellUebernehmen()
+      conflictSelectionCallback()
+    }, [
+      addNotification,
+      conflictSelectionCallback,
+      gqlClient,
+      onClickAktuellUebernehmen,
+      revRow._deleted,
+      revRow.aktiv,
+      revRow.art_id,
+      revRow.bemerkungen,
+      revRow.erhaltungskultur,
+      revRow.garten_id,
+      revRow.herkunft_id,
+      revRow.kultur_id,
+      revRow.von_anzahl_individuen,
+      revRow.zwischenlager,
+      row._depth,
+      row._rev,
+      row._revisions,
+      store,
+      user.email,
+    ])
+    const onClickSchliessen = useCallback(
+      () => setActiveConflict(null),
+      [setActiveConflict],
+    )
+
+    console.log('Kultur Conflict', { dataArray, row, revRow })
+
+    return (
+      <Conflict
+        name="Kultur"
+        rev={rev}
+        dataArray={dataArray}
+        fetching={fetching}
+        error={error}
+        onClickAktuellUebernehmen={onClickAktuellUebernehmen}
+        onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
+        onClickSchliessen={onClickSchliessen}
+      />
+    )
+  },
+)
