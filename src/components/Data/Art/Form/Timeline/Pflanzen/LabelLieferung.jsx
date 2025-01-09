ddd3b5a4882be9b3397a153bcdcb714f17a34b3a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 12:18:38 +0100

    refactor: export components

--- src/components/Data/Art/Form/Timeline/Pflanzen/LabelLieferung.jsx
+++ src/components/Data/Art/Form/Timeline/Pflanzen/LabelLieferung.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text } from 'recharts'
 
-const LabelLieferung = ({ x, y, stroke, value }) => {
+export const LabelLieferung = ({ x, y, stroke, value }) => {
   if (value === undefined) return null
   return (
     <Text
@@ -18,5 +18,3 @@ const LabelLieferung = ({ x, y, stroke, value }) => {
     </Text>
   )
 }
-
-export default LabelLieferung
