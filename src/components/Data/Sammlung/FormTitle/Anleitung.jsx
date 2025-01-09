99ff136283f86d3be34db4071ff58fe159bd6757
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 23:40:37 +0100

    refactor: export components

--- src/components/Data/Sammlung/FormTitle/Anleitung.jsx
+++ src/components/Data/Sammlung/FormTitle/Anleitung.jsx
@@ -5,7 +5,7 @@ import MenuItem from '@mui/material/MenuItem'
 
 import { constants } from '../../../../utils/constants.js'
 
-const KulturAnleitung = ({ asMenu }) => {
+export const SammlungAnleitung = ({ asMenu }) => {
   const openSammlungDocs = useCallback(() => {
     const url = `${constants?.getAppUri()}/Dokumentation/sammlungen`
     if (window.matchMedia('(display-mode: standalone)').matches) {
@@ -29,5 +29,3 @@ const KulturAnleitung = ({ asMenu }) => {
     </IconButton>
   )
 }
-
-export default KulturAnleitung
