8c55b60c56987d45ef7f91fdc79e07d367dc1bed
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 15:35:11 +0100

    refactor: export components

--- src/components/Data/Garten/FormTitle/Download/index.jsx
+++ src/components/Data/Garten/FormTitle/Download/index.jsx
@@ -7,7 +7,7 @@ import { FaDownload } from 'react-icons/fa'
 import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../../../mobxStoreContext.js'
-import buildExceljsWorksheetsForDaten from './buildExceljsWorksheetsForDaten.js'
+import { buildExceljsWorksheetsForDaten } from './buildExceljsWorksheetsForDaten.js'
 import { buildExceljsWorksheetsForTzSums } from './buildExceljsWorksheetsForTzSums.js'
 import { downloadExceljsWorkbook } from '../../../../../utils/downloadExceljsWorkbook.js'
 import { ErrorBoundary } from '../../../../shared/ErrorBoundary.jsx'
