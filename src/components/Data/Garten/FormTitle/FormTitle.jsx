b13e2dda1b99a35e6452dfc5f3355b9ccb6f871c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 15:30:09 +0100

    refactor: export components

--- src/components/Data/Garten/FormTitle/FormTitle.jsx
+++ src/components/Data/Garten/FormTitle/FormTitle.jsx
@@ -8,7 +8,7 @@ import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import Settings from './Settings/index.jsx'
 import DeleteButton from './DeleteButton.jsx'
 import AddButton from './AddButton.jsx'
-import Download from './Download/index.jsx'
+import { GartenDownload as Download } from './Download/index.jsx'
 import { FilterNumbers } from '../../../shared/FilterNumbers.jsx'
 import UpSvg from '../../../../svg/to_up.svg?react'
 import KuDownSvg from '../../../../svg/to_ku_down.svg?react'
@@ -39,85 +39,78 @@ const TitleSymbols = styled.div`
   margin-bottom: auto;
 `
 
-const GartenFormTitle = ({
-  row,
-  rawRow,
-  showHistory,
-  setShowHistory,
-  totalCount,
-  filteredCount,
-}) => {
-  const store = useContext(MobxStoreContext)
-  const { activeNodeArray, setActiveNodeArray, removeOpenNode } = store.tree
+export const GartenFormTitle = observer(
+  ({ row, rawRow, showHistory, setShowHistory, totalCount, filteredCount }) => {
+    const store = useContext(MobxStoreContext)
+    const { activeNodeArray, setActiveNodeArray, removeOpenNode } = store.tree
 
-  const { width, ref } = useResizeDetector()
+    const { width, ref } = useResizeDetector()
 
-  const onClickUp = useCallback(() => {
-    removeOpenNode(activeNodeArray)
-    setActiveNodeArray(activeNodeArray.slice(0, -1))
-  }, [activeNodeArray, removeOpenNode, setActiveNodeArray])
-  const onClickToKulturen = useCallback(
-    () => setActiveNodeArray([...activeNodeArray, 'Kulturen']),
-    [activeNodeArray, setActiveNodeArray],
-  )
+    const onClickUp = useCallback(() => {
+      removeOpenNode(activeNodeArray)
+      setActiveNodeArray(activeNodeArray.slice(0, -1))
+    }, [activeNodeArray, removeOpenNode, setActiveNodeArray])
+    const onClickToKulturen = useCallback(
+      () => setActiveNodeArray([...activeNodeArray, 'Kulturen']),
+      [activeNodeArray, setActiveNodeArray],
+    )
 
-  return (
-    <Container ref={ref}>
-      <Title>Garten</Title>
-      <TitleSymbols>
-        <IconButton
-          title="Zur Liste"
-          onClick={onClickUp}
-          size="large"
-        >
-          <UpSvg />
-        </IconButton>
-        <IconButton
-          title="Zu den Kulturen"
-          onClick={onClickToKulturen}
-          size="large"
-        >
-          <KuDownSvg />
-        </IconButton>
-        <AddButton />
-        <DeleteButton
-          row={row}
-          rawRow={rawRow}
-        />
-        <Download gartenId={row.id} />
-        {width < 520 ?
-          <Menu white={false}>
-            <HistoryButton
-              table="garten"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-              asMenu
-            />
-            <Settings asMenu />
-            <FilterNumbers
-              filteredCount={filteredCount}
-              totalCount={totalCount}
-              asMenu
-            />
-          </Menu>
-        : <>
-            <HistoryButton
-              table="garten"
-              id={row.id}
-              showHistory={showHistory}
-              setShowHistory={setShowHistory}
-            />
-            <Settings />
-            <FilterNumbers
-              filteredCount={filteredCount}
-              totalCount={totalCount}
-            />
-          </>
-        }
-      </TitleSymbols>
-    </Container>
-  )
-}
-
-export default observer(GartenFormTitle)
+    return (
+      <Container ref={ref}>
+        <Title>Garten</Title>
+        <TitleSymbols>
+          <IconButton
+            title="Zur Liste"
+            onClick={onClickUp}
+            size="large"
+          >
+            <UpSvg />
+          </IconButton>
+          <IconButton
+            title="Zu den Kulturen"
+            onClick={onClickToKulturen}
+            size="large"
+          >
+            <KuDownSvg />
+          </IconButton>
+          <AddButton />
+          <DeleteButton
+            row={row}
+            rawRow={rawRow}
+          />
+          <Download gartenId={row.id} />
+          {width < 520 ?
+            <Menu white={false}>
+              <HistoryButton
+                table="garten"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+                asMenu
+              />
+              <Settings asMenu />
+              <FilterNumbers
+                filteredCount={filteredCount}
+                totalCount={totalCount}
+                asMenu
+              />
+            </Menu>
+          : <>
+              <HistoryButton
+                table="garten"
+                id={row.id}
+                showHistory={showHistory}
+                setShowHistory={setShowHistory}
+              />
+              <Settings />
+              <FilterNumbers
+                filteredCount={filteredCount}
+                totalCount={totalCount}
+              />
+            </>
+          }
+        </TitleSymbols>
+      </Container>
+    )
+  },
+)
--- src/components/Data/Garten/FormTitle/index.jsx
+++ src/components/Data/Garten/FormTitle/index.jsx
@@ -5,101 +5,92 @@ import { combineLatest } from 'rxjs'
 
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { FilterTitle } from '../../../shared/FilterTitle.jsx'
-import FormTitle from './FormTitle.jsx'
+import { GartenFormTitle as FormTitle } from './FormTitle.jsx'
 import { tableFilter } from '../../../../utils/tableFilter.js'
 
-const GartenFormTitle = ({
-  showFilter,
-  row,
-  rawRow,
-  showHistory,
-  setShowHistory,
-}) => {
-  const store = useContext(MobxStoreContext)
-  const { personIdInActiveNodeArray, db, filter } = store
+export const GartenFormTitle = observer(
+  ({ showFilter, row, rawRow, showHistory, setShowHistory }) => {
+    const store = useContext(MobxStoreContext)
+    const { personIdInActiveNodeArray, db, filter } = store
 
-  const [countState, setCountState] = useState({
-    totalCount: 0,
-    filteredCount: 0,
-  })
-  useEffect(() => {
-    const hierarchyQuery = personIdInActiveNodeArray
-      ? [
-          Q.experimentalJoinTables(['person']),
-          Q.on('person', 'id', personIdInActiveNodeArray),
-        ]
-      : []
-    const collection = db.get('garten')
-    const delQuery =
-      filter.garten._deleted === false
-        ? Q.where('_deleted', false)
-        : filter.garten._deleted === true
-          ? Q.where('_deleted', true)
-          : Q.or(
-              Q.where('_deleted', false),
-              Q.where('_deleted', true),
-              Q.where('_deleted', null),
-            )
-    const aktivQuery =
-      filter.garten.aktiv === false
-        ? Q.where('aktiv', false)
-        : filter.garten.aktiv === true
-          ? Q.where('aktiv', true)
-          : Q.or(
-              Q.where('aktiv', false),
-              Q.where('aktiv', true),
-              Q.where('aktiv', null),
-            )
-    const totalCountObservable = collection
-      .query(delQuery, aktivQuery, ...hierarchyQuery)
-      .observeCount()
-    const filteredCountObservable = collection
-      .query(...tableFilter({ store, table: 'garten' }), ...hierarchyQuery)
-      .observeCount()
-    const combinedObservables = combineLatest([
-      totalCountObservable,
-      filteredCountObservable,
+    const [countState, setCountState] = useState({
+      totalCount: 0,
+      filteredCount: 0,
+    })
+    useEffect(() => {
+      const hierarchyQuery =
+        personIdInActiveNodeArray ?
+          [
+            Q.experimentalJoinTables(['person']),
+            Q.on('person', 'id', personIdInActiveNodeArray),
+          ]
+        : []
+      const collection = db.get('garten')
+      const delQuery =
+        filter.garten._deleted === false ? Q.where('_deleted', false)
+        : filter.garten._deleted === true ? Q.where('_deleted', true)
+        : Q.or(
+            Q.where('_deleted', false),
+            Q.where('_deleted', true),
+            Q.where('_deleted', null),
+          )
+      const aktivQuery =
+        filter.garten.aktiv === false ? Q.where('aktiv', false)
+        : filter.garten.aktiv === true ? Q.where('aktiv', true)
+        : Q.or(
+            Q.where('aktiv', false),
+            Q.where('aktiv', true),
+            Q.where('aktiv', null),
+          )
+      const totalCountObservable = collection
+        .query(delQuery, aktivQuery, ...hierarchyQuery)
+        .observeCount()
+      const filteredCountObservable = collection
+        .query(...tableFilter({ store, table: 'garten' }), ...hierarchyQuery)
+        .observeCount()
+      const combinedObservables = combineLatest([
+        totalCountObservable,
+        filteredCountObservable,
+      ])
+      const subscription = combinedObservables.subscribe(
+        ([totalCount, filteredCount]) =>
+          setCountState({ totalCount, filteredCount }),
+      )
+
+      return () => subscription?.unsubscribe?.()
+    }, [
+      db,
+      personIdInActiveNodeArray,
+      // need to rerender if any of the values of gartenFilter changes
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ...Object.values(store.filter.garten),
+      store,
+      filter.garten._deleted,
+      filter.garten.aktiv,
     ])
-    const subscription = combinedObservables.subscribe(
-      ([totalCount, filteredCount]) =>
-        setCountState({ totalCount, filteredCount }),
-    )
 
-    return () => subscription?.unsubscribe?.()
-  }, [
-    db,
-    personIdInActiveNodeArray,
-    // need to rerender if any of the values of gartenFilter changes
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    ...Object.values(store.filter.garten),
-    store,
-    filter.garten._deleted,
-    filter.garten.aktiv,
-  ])
+    const { totalCount, filteredCount } = countState
 
-  const { totalCount, filteredCount } = countState
+    if (showFilter) {
+      return (
+        <FilterTitle
+          title="Garten"
+          table="garten"
+          totalCount={totalCount}
+          filteredCount={filteredCount}
+        />
+      )
+    }
 
-  if (showFilter) {
     return (
-      <FilterTitle
-        title="Garten"
-        table="garten"
+      <FormTitle
+        row={row}
+        rawRow={rawRow}
+        showHistory={showHistory}
+        setShowHistory={setShowHistory}
         totalCount={totalCount}
         filteredCount={filteredCount}
       />
     )
-  }
-
-  return (
-    <FormTitle
-      row={row}
-      rawRow={rawRow}
-      showHistory={showHistory}
-      setShowHistory={setShowHistory}
-      totalCount={totalCount}
-      filteredCount={filteredCount}
-    />
-  )
-}
-
-export default observer(GartenFormTitle)
+  },
+)
