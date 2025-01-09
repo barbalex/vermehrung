62fb2d72585b1dbdd16d369f676ea110981b5cd5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 18:08:54 +0100

    refactor: export components

--- src/components/Data/SammelLieferung/Form/Was.jsx
+++ src/components/Data/SammelLieferung/Form/Was.jsx
@@ -49,153 +49,151 @@ const FieldRow = styled.div`
   }
 `
 
-const SammelLieferungWas = ({ showFilter, row, ifNeeded, saveToDb }) => {
-  const store = useContext(MobxStoreContext)
-  const { db, errors, filter } = store
+export const SammelLieferungWas = observer(
+  ({ showFilter, row, ifNeeded, saveToDb }) => {
+    const store = useContext(MobxStoreContext)
+    const { db, errors, filter } = store
 
-  const [artWerte, setArtWerte] = useState([])
-  useEffect(() => {
-    const artDelQuery =
-      filter.art._deleted === false
-        ? Q.where('_deleted', false)
-        : filter.art._deleted === true
-        ? Q.where('_deleted', true)
+    const [artWerte, setArtWerte] = useState([])
+    useEffect(() => {
+      const artDelQuery =
+        filter.art._deleted === false ? Q.where('_deleted', false)
+        : filter.art._deleted === true ? Q.where('_deleted', true)
         : Q.or(
             Q.where('_deleted', false),
             Q.where('_deleted', true),
             Q.where('_deleted', null),
           )
-    const artsObservable = db.get('art').query(artDelQuery).observe()
-    const combinedObservables = combineLatest([artsObservable])
-    const subscription = combinedObservables.subscribe(async ([arts]) => {
-      let art
-      try {
-        art = await row.art.fetch()
-      } catch {}
-      const artsIncludingChoosen = uniqBy(
-        [...arts, ...(art && !showFilter ? [art] : [])],
-        'id',
-      )
-      const artsSorted = await artsSortedFromArts(artsIncludingChoosen)
-      const artWerte = await Promise.all(
-        artsSorted.map(async (el) => {
-          let label
-          try {
-            label = await el.label.pipe(first$()).toPromise()
-          } catch {}
+      const artsObservable = db.get('art').query(artDelQuery).observe()
+      const combinedObservables = combineLatest([artsObservable])
+      const subscription = combinedObservables.subscribe(async ([arts]) => {
+        let art
+        try {
+          art = await row.art.fetch()
+        } catch {}
+        const artsIncludingChoosen = uniqBy(
+          [...arts, ...(art && !showFilter ? [art] : [])],
+          'id',
+        )
+        const artsSorted = await artsSortedFromArts(artsIncludingChoosen)
+        const artWerte = await Promise.all(
+          artsSorted.map(async (el) => {
+            let label
+            try {
+              label = await el.label.pipe(first$()).toPromise()
+            } catch {}
 
-          return {
-            value: el.id,
-            label,
-            link: ['Arten', el.id],
-          }
-        }),
-      )
+            return {
+              value: el.id,
+              label,
+              link: ['Arten', el.id],
+            }
+          }),
+        )
 
-      setArtWerte(artWerte)
-    })
+        setArtWerte(artWerte)
+      })
 
-    return () => subscription?.unsubscribe?.()
-  }, [db, filter.art._deleted, row.art, showFilter])
+      return () => subscription?.unsubscribe?.()
+    }, [db, filter.art._deleted, row.art, showFilter])
 
-  const openGenVielfaldDocs = useCallback(() => {
-    const url = `${constants?.getAppUri()}/Dokumentation/genetische-vielfalt`
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      return window.open(url, '_blank', 'toolbar=no')
-    }
-    window.open(url)
-  }, [])
+    const openGenVielfaldDocs = useCallback(() => {
+      const url = `${constants?.getAppUri()}/Dokumentation/genetische-vielfalt`
+      if (window.matchMedia('(display-mode: standalone)').matches) {
+        return window.open(url, '_blank', 'toolbar=no')
+      }
+      window.open(url)
+    }, [])
 
-  return (
-    <>
-      <TitleRow data-filter={showFilter}>
-        <Title>was</Title>
-      </TitleRow>
-      {ifNeeded('art_id') && (
-        <Select
-          key={`${row.id}art_id`}
-          name="art_id"
-          value={row.art_id}
-          field="art_id"
-          label="Art"
-          options={artWerte}
-          saveToDb={saveToDb}
-          error={errors?.sammel_lieferung?.art_id}
-        />
-      )}
-      <FieldRow>
-        {ifNeeded('anzahl_pflanzen') && (
-          <TextField
-            key={`${row.id}anzahl_pflanzen`}
-            name="anzahl_pflanzen"
-            label="Anzahl Pflanzen"
-            value={row.anzahl_pflanzen}
+    return (
+      <>
+        <TitleRow data-filter={showFilter}>
+          <Title>was</Title>
+        </TitleRow>
+        {ifNeeded('art_id') && (
+          <Select
+            key={`${row.id}art_id`}
+            name="art_id"
+            value={row.art_id}
+            field="art_id"
+            label="Art"
+            options={artWerte}
             saveToDb={saveToDb}
-            error={errors?.sammel_lieferung?.anzahl_pflanzen}
-            type="number"
+            error={errors?.sammel_lieferung?.art_id}
           />
         )}
-        {ifNeeded('anzahl_auspflanzbereit') && (
-          <TextField
-            key={`${row.id}anzahl_auspflanzbereit`}
-            name="anzahl_auspflanzbereit"
-            label="Anzahl auspflanzbereit"
-            value={row.anzahl_auspflanzbereit}
-            saveToDb={saveToDb}
-            error={errors?.sammel_lieferung?.anzahl_auspflanzbereit}
-            type="number"
-          />
-        )}
-      </FieldRow>
-      <FieldRow>
-        {ifNeeded('gramm_samen') && (
-          <TextField
-            key={`${row.id}gramm_samen`}
-            name="gramm_samen"
-            label="Gramm Samen"
-            value={row.gramm_samen}
-            saveToDb={saveToDb}
-            error={errors?.sammel_lieferung?.gramm_samen}
-            type="number"
-          />
-        )}
-        {ifNeeded('andere_menge') && (
-          <TextField
-            key={`${row.id}andere_menge`}
-            name="andere_menge"
-            label={`Andere Menge (z.B. "3 Zwiebeln")`}
-            value={row.andere_menge}
-            saveToDb={saveToDb}
-            error={errors?.sammel_lieferung?.andere_menge}
-            type="text"
-          />
-        )}
-      </FieldRow>
-      {ifNeeded('von_anzahl_individuen') && (
         <FieldRow>
-          <TextField
-            key={`${row.id}von_anzahl_individuen`}
-            name="von_anzahl_individuen"
-            label="von Anzahl Individuen"
-            value={row.von_anzahl_individuen}
-            saveToDb={saveToDb}
-            error={errors?.sammel_lieferung?.von_anzahl_individuen}
-            type="number"
-          />
-          <div>
-            <IconButton
-              aria-label="Anleitung öffnen"
-              title="Anleitung öffnen"
-              onClick={openGenVielfaldDocs}
-              size="large"
-            >
-              <IoMdInformationCircleOutline />
-            </IconButton>
-          </div>
+          {ifNeeded('anzahl_pflanzen') && (
+            <TextField
+              key={`${row.id}anzahl_pflanzen`}
+              name="anzahl_pflanzen"
+              label="Anzahl Pflanzen"
+              value={row.anzahl_pflanzen}
+              saveToDb={saveToDb}
+              error={errors?.sammel_lieferung?.anzahl_pflanzen}
+              type="number"
+            />
+          )}
+          {ifNeeded('anzahl_auspflanzbereit') && (
+            <TextField
+              key={`${row.id}anzahl_auspflanzbereit`}
+              name="anzahl_auspflanzbereit"
+              label="Anzahl auspflanzbereit"
+              value={row.anzahl_auspflanzbereit}
+              saveToDb={saveToDb}
+              error={errors?.sammel_lieferung?.anzahl_auspflanzbereit}
+              type="number"
+            />
+          )}
         </FieldRow>
-      )}
-    </>
-  )
-}
-
-export default observer(SammelLieferungWas)
+        <FieldRow>
+          {ifNeeded('gramm_samen') && (
+            <TextField
+              key={`${row.id}gramm_samen`}
+              name="gramm_samen"
+              label="Gramm Samen"
+              value={row.gramm_samen}
+              saveToDb={saveToDb}
+              error={errors?.sammel_lieferung?.gramm_samen}
+              type="number"
+            />
+          )}
+          {ifNeeded('andere_menge') && (
+            <TextField
+              key={`${row.id}andere_menge`}
+              name="andere_menge"
+              label={`Andere Menge (z.B. "3 Zwiebeln")`}
+              value={row.andere_menge}
+              saveToDb={saveToDb}
+              error={errors?.sammel_lieferung?.andere_menge}
+              type="text"
+            />
+          )}
+        </FieldRow>
+        {ifNeeded('von_anzahl_individuen') && (
+          <FieldRow>
+            <TextField
+              key={`${row.id}von_anzahl_individuen`}
+              name="von_anzahl_individuen"
+              label="von Anzahl Individuen"
+              value={row.von_anzahl_individuen}
+              saveToDb={saveToDb}
+              error={errors?.sammel_lieferung?.von_anzahl_individuen}
+              type="number"
+            />
+            <div>
+              <IconButton
+                aria-label="Anleitung öffnen"
+                title="Anleitung öffnen"
+                onClick={openGenVielfaldDocs}
+                size="large"
+              >
+                <IoMdInformationCircleOutline />
+              </IconButton>
+            </div>
+          </FieldRow>
+        )}
+      </>
+    )
+  },
+)
