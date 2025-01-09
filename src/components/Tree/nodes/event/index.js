0de59c864b44a60f45ab9eb64bfc26c9689b3108
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 15:17:30 +0100

    refactor: export components

--- src/components/Tree/nodes/event/index.js
+++ src/components/Tree/nodes/event/index.js
@@ -1,4 +1,4 @@
-import eventLabelFromEvent from '../../../../utils/eventLabelFromEvent.js'
+import { eventLabelFromEvent } from '../../../../utils/eventLabelFromEvent.js'
 
 const eventNodes = ({ event, index }) => ({
   nodeType: 'table',
