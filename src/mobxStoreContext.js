ec17a1abac2e02a23704fab261c6d7d0451bc222
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 11:07:13 +0100

    clean

--- src/mobxStoreContext.js
+++ src/mobxStoreContext.js
@@ -3,4 +3,3 @@ import React from 'react'
 export const MobxStoreContext = React.createContext({})
 export const MobxStoreProvider = MobxStoreContext.Provider
 export const Consumer = MobxStoreContext.Consumer
-// export default MobxStoreContext
