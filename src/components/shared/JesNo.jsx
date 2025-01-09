8368433bc28bdf496b1f9239b47491e3b243dc15
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 12:35:48 +0100

    refactor: export components

--- src/components/shared/JesNo.jsx
+++ src/components/shared/JesNo.jsx
@@ -41,101 +41,94 @@ const dataSource = [
   },
 ]
 
-const RadioButtonGroup = ({
-  value: valuePassed,
-  label,
-  name,
-  error,
-  helperText = '',
-  saveToDb,
-}) => {
-  const [stateValue, setStateValue] = useState(valuePassed)
-  useEffect(() => {
-    setStateValue(valuePassed)
-  }, [valuePassed])
+export const JesNo = observer(
+  ({ value: valuePassed, label, name, error, helperText = '', saveToDb }) => {
+    const [stateValue, setStateValue] = useState(valuePassed)
+    useEffect(() => {
+      setStateValue(valuePassed)
+    }, [valuePassed])
 
-  const onClickButton = useCallback(
-    (event) => {
-      /**
-       * if clicked element is active value: set null
-       * Problem: does not work on change event on RadioGroup
-       * because that only fires on changes
-       * Solution: do this in click event of button
-       */
-      const targetValue = event.target.value === 'true'
-      if (targetValue === stateValue) {
-        // an already active option was clicked
-        // set value null
-        setStateValue(null)
+    const onClickButton = useCallback(
+      (event) => {
+        /**
+         * if clicked element is active value: set null
+         * Problem: does not work on change event on RadioGroup
+         * because that only fires on changes
+         * Solution: do this in click event of button
+         */
+        const targetValue = event.target.value === 'true'
+        if (targetValue === stateValue) {
+          // an already active option was clicked
+          // set value null
+          setStateValue(null)
+          const fakeEvent = {
+            target: {
+              value: null,
+              name,
+            },
+          }
+          return saveToDb(fakeEvent)
+        }
+      },
+      [stateValue, name, saveToDb],
+    )
+    const onChangeGroup = useCallback(
+      (event) => {
+        // group only changes if value changes
+        const targetValue = event.target.value
+        // values are passed as strings > need to convert
+        const newValue = targetValue === 'true'
+        setStateValue(newValue)
         const fakeEvent = {
           target: {
-            value: null,
+            value: newValue,
             name,
           },
         }
-        return saveToDb(fakeEvent)
-      }
-    },
-    [stateValue, name, saveToDb],
-  )
-  const onChangeGroup = useCallback(
-    (event) => {
-      // group only changes if value changes
-      const targetValue = event.target.value
-      // values are passed as strings > need to convert
-      const newValue = targetValue === 'true'
-      setStateValue(newValue)
-      const fakeEvent = {
-        target: {
-          value: newValue,
-          name,
-        },
-      }
-      saveToDb(fakeEvent)
-    },
-    [name, saveToDb],
-  )
+        saveToDb(fakeEvent)
+      },
+      [name, saveToDb],
+    )
 
-  const valueSelected =
-    stateValue !== null && stateValue !== undefined
-      ? toStringIfPossible(stateValue)
+    const valueSelected =
+      stateValue !== null && stateValue !== undefined ?
+        toStringIfPossible(stateValue)
       : ''
 
-  return (
-    <Container>
-      <StyledFormControl
-        component="fieldset"
-        error={!!error}
-        aria-describedby={`${label}ErrorText`}
-        variant="standard"
-      >
-        <StyledFormLabel component="legend">{label}</StyledFormLabel>
-        <RadioGroup
-          aria-label={label}
-          value={valueSelected}
-          onChange={onChangeGroup}
+    return (
+      <Container>
+        <StyledFormControl
+          component="fieldset"
+          error={!!error}
+          aria-describedby={`${label}ErrorText`}
+          variant="standard"
         >
-          {dataSource.map((e, index) => (
-            <FormControlLabel
-              key={index}
-              value={toStringIfPossible(e.value)}
-              control={<StyledRadio color="primary" />}
-              label={e.label}
-              onClick={onClickButton}
-            />
-          ))}
-        </RadioGroup>
-        {!!error && (
-          <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
-        )}
-        {!!helperText && (
-          <FormHelperText id={`${label}HelperText`}>
-            {helperText}
-          </FormHelperText>
-        )}
-      </StyledFormControl>
-    </Container>
-  )
-}
-
-export default observer(RadioButtonGroup)
+          <StyledFormLabel component="legend">{label}</StyledFormLabel>
+          <RadioGroup
+            aria-label={label}
+            value={valueSelected}
+            onChange={onChangeGroup}
+          >
+            {dataSource.map((e, index) => (
+              <FormControlLabel
+                key={index}
+                value={toStringIfPossible(e.value)}
+                control={<StyledRadio color="primary" />}
+                label={e.label}
+                onClick={onClickButton}
+              />
+            ))}
+          </RadioGroup>
+          {!!error && (
+            <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
+          )}
+          {!!helperText && (
+            <FormHelperText id={`${label}HelperText`}>
+              {helperText}
+            </FormHelperText>
+          )}
+        </StyledFormControl>
+      </Container>
+    )
+  },
+)
