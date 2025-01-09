5800656ab855c41183f3bd4c7aa7d00b1ce60d2f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 21:45:18 +0100

    refactor: export components

--- src/components/Data/Person/History/index.jsx
+++ src/components/Data/Person/History/index.jsx
@@ -8,7 +8,7 @@ import { useQuery } from 'urql'
 import { checkForOnlineError } from '../../../../utils/checkForOnlineError.js'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
-import Row from './Row.jsx'
+import { PersonHistoryRow as Row } from './Row.jsx'
 
 const personRevQuery = gql`
   query personRevForHistoryQuery($rev: [String!]) {
@@ -72,47 +72,47 @@ const sliderSettings = {
   infinite: false,
 }
 
-const PersonHistory = ({ row, rawRow, historyTakeoverCallback }) => {
-  const store = useContext(MobxStoreContext)
+export const PersonHistory = observer(
+  ({ row, rawRow, historyTakeoverCallback }) => {
+    const store = useContext(MobxStoreContext)
 
-  const priorRevisions = row?._revisions?.slice(1) ?? []
-  const [{ error, data, fetching }] = useQuery({
-    query: personRevQuery,
-    variables: {
-      rev: priorRevisions,
-    },
-  })
-  error && checkForOnlineError({ error, store })
+    const priorRevisions = row?._revisions?.slice(1) ?? []
+    const [{ error, data, fetching }] = useQuery({
+      query: personRevQuery,
+      variables: {
+        rev: priorRevisions,
+      },
+    })
+    error && checkForOnlineError({ error, store })
 
-  const revRowsUnsorted = useMemo(
-    () => data?.person_rev ?? [],
-    [data?.person_rev],
-  )
-  const revRows = revRowsUnsorted.sort((a, b) => b._depth - a._depth)
+    const revRowsUnsorted = useMemo(
+      () => data?.person_rev ?? [],
+      [data?.person_rev],
+    )
+    const revRows = revRowsUnsorted.sort((a, b) => b._depth - a._depth)
 
-  if (fetching) {
-    return <Spinner message="lade Versionen" />
-  }
+    if (fetching) {
+      return <Spinner message="lade Versionen" />
+    }
 
-  if (error) {
-    return <ErrorContainer>{error.message}</ErrorContainer>
-  }
+    if (error) {
+      return <ErrorContainer>{error.message}</ErrorContainer>
+    }
 
-  return (
-    <Container>
-      <Slider {...sliderSettings}>
-        {revRows.map((r) => (
-          <Row
-            key={row._rev}
-            revRow={r}
-            row={row}
-            rawRow={rawRow}
-            historyTakeoverCallback={historyTakeoverCallback}
-          />
-        ))}
-      </Slider>
-    </Container>
-  )
-}
-
-export default observer(PersonHistory)
+    return (
+      <Container>
+        <Slider {...sliderSettings}>
+          {revRows.map((r) => (
+            <Row
+              key={row._rev}
+              revRow={r}
+              row={row}
+              rawRow={rawRow}
+              historyTakeoverCallback={historyTakeoverCallback}
+            />
+          ))}
+        </Slider>
+      </Container>
+    )
+  },
+)
