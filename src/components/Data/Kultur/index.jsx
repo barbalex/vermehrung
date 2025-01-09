59728d9710705141ddb3943bdef26e4efa48e2a8
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 16:37:50 +0100

    refactor: export components

--- src/components/Data/Kultur/index.jsx
+++ src/components/Data/Kultur/index.jsx
@@ -10,7 +10,7 @@ import { Spinner } from '../../shared/Spinner.jsx'
 import Conflict from './Conflict.jsx'
 import FormTitle from './FormTitle/index.jsx'
 import Form from './Form/index.jsx'
-import History from './History/index.jsx'
+import { KulturHistory as History } from './History/index.jsx'
 
 const Container = styled.div`
   height: 100%;
@@ -35,11 +35,10 @@ const Kultur = ({
   // need raw row because observable does not provoke rerendering of components
   const [rawRow, setRawRow] = useState(null)
   useEffect(() => {
-    const observable = showFilter
-      ? $of(filter.kultur)
-      : initialDataQueried
-        ? db.get('kultur').findAndObserve(id)
-        : $of({})
+    const observable =
+      showFilter ? $of(filter.kultur)
+      : initialDataQueried ? db.get('kultur').findAndObserve(id)
+      : $of({})
     const subscription = observable.subscribe((newRow) => {
       setRow(newRow)
       setRawRow(JSON.stringify(newRow._raw))
@@ -93,7 +92,7 @@ const Kultur = ({
               showHistory={showHistory}
             />
             <Allotment.Pane visible={paneIsSplit}>
-              {activeConflict ? (
+              {activeConflict ?
                 <Conflict
                   rev={activeConflict}
                   id={id}
@@ -103,13 +102,13 @@ const Kultur = ({
                   conflictSelectionCallback={conflictSelectionCallback}
                   setActiveConflict={setActiveConflict}
                 />
-              ) : showHistory ? (
+              : showHistory ?
                 <History
                   row={row}
                   rawRow={rawRow}
                   historyTakeoverCallback={historyTakeoverCallback}
                 />
-              ) : null}
+              : null}
             </Allotment.Pane>
           </Allotment>
         </SplitPaneContainer>
