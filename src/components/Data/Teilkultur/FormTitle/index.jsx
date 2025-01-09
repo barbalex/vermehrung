f2260fbec7fcd5682f8baf47feadf06674b1d6bb
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-09 23:56:37 +0100

    refactor: export components

--- src/components/Data/Teilkultur/FormTitle/index.jsx
+++ src/components/Data/Teilkultur/FormTitle/index.jsx
@@ -5,90 +5,86 @@ import { combineLatest } from 'rxjs'
 
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { FilterTitle } from '../../../shared/FilterTitle.jsx'
-import FormTitle from './FormTitle.jsx'
+import { TeilkulturFormTitle as FormTitle } from './FormTitle.jsx'
 import { tableFilter } from '../../../../utils/tableFilter.js'
 
-const TeilkulturFormTitleChooser = ({
-  row,
-  rawRow,
-  showFilter,
-  showHistory,
-  setShowHistory,
-}) => {
-  const store = useContext(MobxStoreContext)
-  const { kulturIdInActiveNodeArray, db, filter } = store
+export const TeilkulturFormTitleChooser = observer(
+  ({ row, rawRow, showFilter, showHistory, setShowHistory }) => {
+    const store = useContext(MobxStoreContext)
+    const { kulturIdInActiveNodeArray, db, filter } = store
 
-  const [countState, setCountState] = useState({
-    totalCount: 0,
-    filteredCount: 0,
-  })
-  useEffect(() => {
-    const hierarchyQuery = kulturIdInActiveNodeArray
-      ? [
-          Q.experimentalJoinTables(['kultur']),
-          Q.on('kultur', 'id', kulturIdInActiveNodeArray),
-        ]
-      : []
-    const collection = db.get('teilkultur')
-    const teilkulturDelQuery =
-      filter.teilkultur._deleted === false
-        ? Q.where('_deleted', false)
-        : filter.teilkultur._deleted === true
-          ? Q.where('_deleted', true)
-          : Q.or(
-              Q.where('_deleted', false),
-              Q.where('_deleted', true),
-              Q.where('_deleted', null),
-            )
-    const totalCountObservable = collection
-      .query(teilkulturDelQuery, ...hierarchyQuery)
-      .observeCount()
-    const filteredCountObservable = collection
-      .query(...tableFilter({ store, table: 'teilkultur' }), ...hierarchyQuery)
-      .observeCount()
-    const combinedObservables = combineLatest([
-      totalCountObservable,
-      filteredCountObservable,
+    const [countState, setCountState] = useState({
+      totalCount: 0,
+      filteredCount: 0,
+    })
+    useEffect(() => {
+      const hierarchyQuery =
+        kulturIdInActiveNodeArray ?
+          [
+            Q.experimentalJoinTables(['kultur']),
+            Q.on('kultur', 'id', kulturIdInActiveNodeArray),
+          ]
+        : []
+      const collection = db.get('teilkultur')
+      const teilkulturDelQuery =
+        filter.teilkultur._deleted === false ? Q.where('_deleted', false)
+        : filter.teilkultur._deleted === true ? Q.where('_deleted', true)
+        : Q.or(
+            Q.where('_deleted', false),
+            Q.where('_deleted', true),
+            Q.where('_deleted', null),
+          )
+      const totalCountObservable = collection
+        .query(teilkulturDelQuery, ...hierarchyQuery)
+        .observeCount()
+      const filteredCountObservable = collection
+        .query(
+          ...tableFilter({ store, table: 'teilkultur' }),
+          ...hierarchyQuery,
+        )
+        .observeCount()
+      const combinedObservables = combineLatest([
+        totalCountObservable,
+        filteredCountObservable,
+      ])
+      const subscription = combinedObservables.subscribe(
+        ([totalCount, filteredCount]) =>
+          setCountState({ totalCount, filteredCount }),
+      )
+
+      return () => subscription?.unsubscribe?.()
+    }, [
+      db,
+      kulturIdInActiveNodeArray,
+      // need to rerender if any of the values of teilkulturFilter changes
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ...Object.values(store.filter.teilkultur),
+      store,
+      filter.teilkultur._deleted,
     ])
-    const subscription = combinedObservables.subscribe(
-      ([totalCount, filteredCount]) =>
-        setCountState({ totalCount, filteredCount }),
-    )
 
-    return () => subscription?.unsubscribe?.()
-  }, [
-    db,
-    kulturIdInActiveNodeArray,
-    // need to rerender if any of the values of teilkulturFilter changes
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    ...Object.values(store.filter.teilkultur),
-    store,
-    filter.teilkultur._deleted,
-  ])
+    const { totalCount, filteredCount } = countState
 
-  const { totalCount, filteredCount } = countState
+    if (showFilter) {
+      return (
+        <FilterTitle
+          title="Teilkultur"
+          table="teilkultur"
+          totalCount={totalCount}
+          filteredCount={filteredCount}
+        />
+      )
+    }
 
-  if (showFilter) {
     return (
-      <FilterTitle
-        title="Teilkultur"
-        table="teilkultur"
+      <FormTitle
+        row={row}
+        rawRow={rawRow}
         totalCount={totalCount}
         filteredCount={filteredCount}
+        showHistory={showHistory}
+        setShowHistory={setShowHistory}
       />
     )
-  }
-
-  return (
-    <FormTitle
-      row={row}
-      rawRow={rawRow}
-      totalCount={totalCount}
-      filteredCount={filteredCount}
-      showHistory={showHistory}
-      setShowHistory={setShowHistory}
-    />
-  )
-}
-
-export default observer(TeilkulturFormTitleChooser)
+  },
+)
--- src/components/Data/Teilkultur/index.jsx
+++ src/components/Data/Teilkultur/index.jsx
@@ -8,7 +8,7 @@ import { MobxStoreContext } from '../../../mobxStoreContext.js'
 import { ErrorBoundary } from '../../shared/ErrorBoundary.jsx'
 import { Spinner } from '../../shared/Spinner.jsx'
 import Conflict from './Conflict.jsx'
-import FormTitle from './FormTitle/index.jsx'
+import { TeilkulturFormTitleChooser as FormTitle } from './FormTitle/index.jsx'
 import { TeilkulturForm as Form } from './Form/index.jsx'
 import { TeilkulturHistory as History } from './History/index.jsx'
 
