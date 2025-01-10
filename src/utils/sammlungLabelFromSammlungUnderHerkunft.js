41c066b21f639c0e194350d8444d1e7ba656fbc3
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-10 14:45:49 +0100

    refactor: export components

--- src/utils/sammlungLabelFromSammlungUnderHerkunft.js
+++ src/utils/sammlungLabelFromSammlungUnderHerkunft.js
@@ -2,11 +2,17 @@ import { DateTime } from 'luxon'
 
 import { personLabelFromPerson } from './personLabelFromPerson.js'
 
-const sammlungLabelFromSammlung = ({ sammlung, art, ae_art, person }) => {
-  const artLabel = art ? ae_art?.name ?? '(Art ohne Name)' : '(keine Art)'
+export const sammlungLabelFromSammlungUnderHerkunft = ({
+  sammlung,
+  art,
+  ae_art,
+  person,
+}) => {
+  const artLabel = art ? (ae_art?.name ?? '(Art ohne Name)') : '(keine Art)'
   const personLabel = personLabelFromPerson({ person })
-  const datumLabel = sammlung?.datum
-    ? DateTime.fromSQL(sammlung?.datum).toFormat('yyyy.LL.dd')
+  const datumLabel =
+    sammlung?.datum ?
+      DateTime.fromSQL(sammlung?.datum).toFormat('yyyy.LL.dd')
     : 'Kein Datum'
   const geplantLabel = sammlung?.geplant ? 'geplant' : undefined
   const label = [datumLabel, artLabel, personLabel, geplantLabel]
@@ -15,5 +21,3 @@ const sammlungLabelFromSammlung = ({ sammlung, art, ae_art, person }) => {
 
   return label
 }
-
-export default sammlungLabelFromSammlung
