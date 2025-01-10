5807592c1a3b3b0aae37419bbbf1152046789bfb
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-10 12:26:09 +0100

    refactor: export components

--- src/components/Data/Zaehlung/Form/Teilzaehlungen/Teilzaehlung/History/Row.jsx
+++ src/components/Data/Zaehlung/Form/Teilzaehlungen/Teilzaehlung/History/Row.jsx
@@ -11,103 +11,103 @@ import { toPgArray } from '../../../../../../../utils/toPgArray.js'
 import { mutations } from '../../../../../../../utils/mutations.js'
 import { createDataArrayForTeilzaehlungRevComparison as createDataArray } from '../createDataArrayForRevComparison.js'
 
-const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
-  const store = useContext(MobxStoreContext)
-  const { user, addNotification, db, gqlClient } = store
+export const TeilzaehlungHistoryRow = observer(
+  ({ row, revRow, historyTakeoverCallback }) => {
+    const store = useContext(MobxStoreContext)
+    const { user, addNotification, db, gqlClient } = store
 
-  const dataArray = useMemo(
-    () => createDataArray({ row, revRow, store }),
-    [revRow, row, store],
-  )
-  const onClickWiderspruchUebernehmen = useCallback(async () => {
-    // need to attach to the winner, that is row
-    // otherwise risk to still have lower depth and thus loosing
-    const newDepth = row._depth + 1
-    const newObject = {
-      teilzaehlung_id: revRow.teilzaehlung_id,
-      zaehlung_id: revRow.zaehlung_id,
-      teilkultur_id: revRow.teilkultur_id,
-      anzahl_pflanzen: revRow.anzahl_pflanzen,
-      anzahl_auspflanzbereit: revRow.anzahl_auspflanzbereit,
-      anzahl_mutterpflanzen: revRow.anzahl_mutterpflanzen,
-      andere_menge: revRow.andere_menge,
-      auspflanzbereit_beschreibung: revRow.auspflanzbereit_beschreibung,
-      bemerkungen: revRow.bemerkungen,
-      prognose_von_tz: revRow.prognose_von_tz,
-      _parent_rev: row._rev,
-      _depth: newDepth,
-      _deleted: revRow._deleted,
-    }
-    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-    newObject._rev = rev
-    newObject.id = uuidv1()
-    newObject.changed = new window.Date().toISOString()
-    newObject.changed_by = user.email
-    newObject._revisions = toPgArray([rev, ...row._revisions])
-    const newObjectForStore = { ...newObject }
-    const response = await gqlClient
-      .query(mutations.mutateInsert_teilzaehlung_rev_one, {
-        object: newObject,
-        on_conflict: {
-          constraint: 'teilzaehlung_rev_pkey',
-          update_columns: ['id'],
-        },
-      })
-      .toPromise()
-    if (response.error) {
-      checkForOnlineError({ error: response.error, store })
-      return addNotification({
-        message: response.error.message,
-      })
-    }
-    historyTakeoverCallback()
-    // do not stringify revisions for store
-    // as _that_ is a real array
-    newObjectForStore._revisions =
-      row._revisions ? [rev, ...row._revisions] : [rev]
-    // TODO: is this a good idea?
-    newObjectForStore._conflicts = row._conflicts
-    // for store: convert rev to winner
-    newObjectForStore.id = row.id
-    delete newObjectForStore.zaehlung_id
-    // optimistically update store
-    await db.write(async () => {
-      await row.update((row) => {
-        Object.entries(newObjectForStore).forEach(([key, value]) => {
-          if (!isEqual(value, row[key])) {
-            row[key] = value
-          }
+    const dataArray = useMemo(
+      () => createDataArray({ row, revRow, store }),
+      [revRow, row, store],
+    )
+    const onClickWiderspruchUebernehmen = useCallback(async () => {
+      // need to attach to the winner, that is row
+      // otherwise risk to still have lower depth and thus loosing
+      const newDepth = row._depth + 1
+      const newObject = {
+        teilzaehlung_id: revRow.teilzaehlung_id,
+        zaehlung_id: revRow.zaehlung_id,
+        teilkultur_id: revRow.teilkultur_id,
+        anzahl_pflanzen: revRow.anzahl_pflanzen,
+        anzahl_auspflanzbereit: revRow.anzahl_auspflanzbereit,
+        anzahl_mutterpflanzen: revRow.anzahl_mutterpflanzen,
+        andere_menge: revRow.andere_menge,
+        auspflanzbereit_beschreibung: revRow.auspflanzbereit_beschreibung,
+        bemerkungen: revRow.bemerkungen,
+        prognose_von_tz: revRow.prognose_von_tz,
+        _parent_rev: row._rev,
+        _depth: newDepth,
+        _deleted: revRow._deleted,
+      }
+      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+      newObject._rev = rev
+      newObject.id = uuidv1()
+      newObject.changed = new window.Date().toISOString()
+      newObject.changed_by = user.email
+      newObject._revisions = toPgArray([rev, ...row._revisions])
+      const newObjectForStore = { ...newObject }
+      const response = await gqlClient
+        .query(mutations.mutateInsert_teilzaehlung_rev_one, {
+          object: newObject,
+          on_conflict: {
+            constraint: 'teilzaehlung_rev_pkey',
+            update_columns: ['id'],
+          },
+        })
+        .toPromise()
+      if (response.error) {
+        checkForOnlineError({ error: response.error, store })
+        return addNotification({
+          message: response.error.message,
+        })
+      }
+      historyTakeoverCallback()
+      // do not stringify revisions for store
+      // as _that_ is a real array
+      newObjectForStore._revisions =
+        row._revisions ? [rev, ...row._revisions] : [rev]
+      // TODO: is this a good idea?
+      newObjectForStore._conflicts = row._conflicts
+      // for store: convert rev to winner
+      newObjectForStore.id = row.id
+      delete newObjectForStore.zaehlung_id
+      // optimistically update store
+      await db.write(async () => {
+        await row.update((row) => {
+          Object.entries(newObjectForStore).forEach(([key, value]) => {
+            if (!isEqual(value, row[key])) {
+              row[key] = value
+            }
+          })
         })
       })
-    })
-  }, [
-    row,
-    revRow.teilzaehlung_id,
-    revRow.zaehlung_id,
-    revRow.teilkultur_id,
-    revRow.anzahl_pflanzen,
-    revRow.anzahl_auspflanzbereit,
-    revRow.anzahl_mutterpflanzen,
-    revRow.andere_menge,
-    revRow.auspflanzbereit_beschreibung,
-    revRow.bemerkungen,
-    revRow.prognose_von_tz,
-    revRow._deleted,
-    user.email,
-    gqlClient,
-    historyTakeoverCallback,
-    db,
-    store,
-    addNotification,
-  ])
+    }, [
+      row,
+      revRow.teilzaehlung_id,
+      revRow.zaehlung_id,
+      revRow.teilkultur_id,
+      revRow.anzahl_pflanzen,
+      revRow.anzahl_auspflanzbereit,
+      revRow.anzahl_mutterpflanzen,
+      revRow.andere_menge,
+      revRow.auspflanzbereit_beschreibung,
+      revRow.bemerkungen,
+      revRow.prognose_von_tz,
+      revRow._deleted,
+      user.email,
+      gqlClient,
+      historyTakeoverCallback,
+      db,
+      store,
+      addNotification,
+    ])
 
-  return (
-    <History
-      rev={revRow._rev}
-      dataArray={dataArray}
-      onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
-    />
-  )
-}
-
-export default observer(HistoryRow)
+    return (
+      <History
+        rev={revRow._rev}
+        dataArray={dataArray}
+        onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
+      />
+    )
+  },
+)
--- src/components/Data/Zaehlung/Form/Teilzaehlungen/Teilzaehlung/History/index.jsx
+++ src/components/Data/Zaehlung/Form/Teilzaehlungen/Teilzaehlung/History/index.jsx
@@ -8,7 +8,7 @@ import { useQuery } from 'urql'
 import { checkForOnlineError } from '../../../../../../../utils/checkForOnlineError.js'
 import { Spinner } from '../../../../../../shared/Spinner.jsx'
 import { MobxStoreContext } from '../../../../../../../mobxStoreContext.js'
-import Row from './Row.jsx'
+import { TeilzaehlungHistoryRow as Row } from './Row.jsx'
 
 const teilzaehlungRevQuery = gql`
   query teilzaehlungRevForHistoryQuery($rev: [String!]) {
@@ -63,46 +63,47 @@ const sliderSettings = {
   infinite: false,
 }
 
-const TeilzaehlungHistory = ({ row, rawRow, historyTakeoverCallback }) => {
-  const store = useContext(MobxStoreContext)
+export const TeilzaehlungHistory = observer(
+  ({ row, rawRow, historyTakeoverCallback }) => {
+    const store = useContext(MobxStoreContext)
 
-  const priorRevisions = row?._revisions?.slice(1) ?? []
-  const [{ error, data, fetching }] = useQuery({
-    query: teilzaehlungRevQuery,
-    variables: {
-      rev: priorRevisions,
-    },
-  })
-  error && checkForOnlineError({ error, store })
+    const priorRevisions = row?._revisions?.slice(1) ?? []
+    const [{ error, data, fetching }] = useQuery({
+      query: teilzaehlungRevQuery,
+      variables: {
+        rev: priorRevisions,
+      },
+    })
+    error && checkForOnlineError({ error, store })
 
-  const revRowsUnsorted = useMemo(() => data?.teilzaehlung_rev ?? [], [
-    data?.teilzaehlung_rev,
-  ])
-  const revRows = revRowsUnsorted.sort((a, b) => b._depth - a._depth)
+    const revRowsUnsorted = useMemo(
+      () => data?.teilzaehlung_rev ?? [],
+      [data?.teilzaehlung_rev],
+    )
+    const revRows = revRowsUnsorted.sort((a, b) => b._depth - a._depth)
 
-  if (fetching) {
-    return <Spinner message="lade Versionen" />
-  }
+    if (fetching) {
+      return <Spinner message="lade Versionen" />
+    }
 
-  if (error) {
-    return <ErrorContainer>{error.message}</ErrorContainer>
-  }
+    if (error) {
+      return <ErrorContainer>{error.message}</ErrorContainer>
+    }
 
-  return (
-    <Container>
-      <Slider {...sliderSettings}>
-        {revRows.map((r) => (
-          <Row
-            key={row._rev}
-            revRow={r}
-            row={row}
-            rawRow={rawRow}
-            historyTakeoverCallback={historyTakeoverCallback}
-          />
-        ))}
-      </Slider>
-    </Container>
-  )
-}
-
-export default observer(TeilzaehlungHistory)
+    return (
+      <Container>
+        <Slider {...sliderSettings}>
+          {revRows.map((r) => (
+            <Row
+              key={row._rev}
+              revRow={r}
+              row={row}
+              rawRow={rawRow}
+              historyTakeoverCallback={historyTakeoverCallback}
+            />
+          ))}
+        </Slider>
+      </Container>
+    )
+  },
+)
