e98330dd78b572f120630f8a720aa2baf46436bd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-10 13:07:22 +0100

    refactor: export components

--- src/components/Header/Vermehrung/Menu/Export/index.jsx
+++ src/components/Header/Vermehrung/Menu/Export/index.jsx
@@ -14,64 +14,64 @@ const FirstMenuItem = styled(MenuItem)`
   margin-top: -5px !important;
 `
 
-const SettingsOverallMenu = ({
-  anchorEl: parentAnchorEl,
-  setAnchorEl: setParentAnchorEl,
-  setParentAnchorEl: setGrandParentAnchorEl,
-}) => {
-  const store = useContext(MobxStoreContext)
+export const HeaderExportMenu = observer(
+  ({
+    anchorEl: parentAnchorEl,
+    setAnchorEl: setParentAnchorEl,
+    setParentAnchorEl: setGrandParentAnchorEl,
+  }) => {
+    const store = useContext(MobxStoreContext)
 
-  const onClickLieferungenDesJahrs = useCallback(
-    (event) => {
-      const year = event.target.value
-      if (year.length === 4) {
-        buildExceljsWorksheetsForLieferungenOfYear({ year, store })
-        setParentAnchorEl(null)
-        setGrandParentAnchorEl(null)
-      }
-    },
-    [setGrandParentAnchorEl, setParentAnchorEl, store],
-  )
-  const onClickKulturenFuerBedarfsplanung = useCallback(async () => {
-    const { Workbook } = await import('exceljs/dist/exceljs.min.js')
-    const workbook = new Workbook()
-    await buildExceljsWorksheetsForKulturBedarfsplanung({
-      store,
-      workbook,
-    })
-    downloadExceljsWorkbook({
-      store,
-      fileName: `kulturenFuerBedarfsplanung`,
-      workbook,
-    })
-    setParentAnchorEl(null)
-    setGrandParentAnchorEl(null)
-  }, [setGrandParentAnchorEl, setParentAnchorEl, store])
+    const onClickLieferungenDesJahrs = useCallback(
+      (event) => {
+        const year = event.target.value
+        if (year.length === 4) {
+          buildExceljsWorksheetsForLieferungenOfYear({ year, store })
+          setParentAnchorEl(null)
+          setGrandParentAnchorEl(null)
+        }
+      },
+      [setGrandParentAnchorEl, setParentAnchorEl, store],
+    )
+    const onClickKulturenFuerBedarfsplanung = useCallback(async () => {
+      const { Workbook } = await import('exceljs/dist/exceljs.min.js')
+      const workbook = new Workbook()
+      await buildExceljsWorksheetsForKulturBedarfsplanung({
+        store,
+        workbook,
+      })
+      downloadExceljsWorkbook({
+        store,
+        fileName: `kulturenFuerBedarfsplanung`,
+        workbook,
+      })
+      setParentAnchorEl(null)
+      setGrandParentAnchorEl(null)
+    }, [setGrandParentAnchorEl, setParentAnchorEl, store])
 
-  const onClose = useCallback(
-    () => setParentAnchorEl(null),
-    [setParentAnchorEl],
-  )
+    const onClose = useCallback(
+      () => setParentAnchorEl(null),
+      [setParentAnchorEl],
+    )
 
-  return (
-    <Menu
-      id="menuExport"
-      anchorEl={parentAnchorEl}
-      open={Boolean(parentAnchorEl)}
-      onClose={onClose}
-    >
-      <FirstMenuItem>
-        <TextField
-          label="Lieferungen des Jahrs:"
-          onChange={onClickLieferungenDesJahrs}
-          variant="standard"
-        />
-      </FirstMenuItem>
-      <MenuItem onClick={onClickKulturenFuerBedarfsplanung}>
-        aktueller Stand Kulturen für die Bedarfsplanung
-      </MenuItem>
-    </Menu>
-  )
-}
-
-export default observer(SettingsOverallMenu)
+    return (
+      <Menu
+        id="menuExport"
+        anchorEl={parentAnchorEl}
+        open={Boolean(parentAnchorEl)}
+        onClose={onClose}
+      >
+        <FirstMenuItem>
+          <TextField
+            label="Lieferungen des Jahrs:"
+            onChange={onClickLieferungenDesJahrs}
+            variant="standard"
+          />
+        </FirstMenuItem>
+        <MenuItem onClick={onClickKulturenFuerBedarfsplanung}>
+          aktueller Stand Kulturen für die Bedarfsplanung
+        </MenuItem>
+      </Menu>
+    )
+  },
+)
