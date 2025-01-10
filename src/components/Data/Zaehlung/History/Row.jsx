f320b31fb109c2b29eed65c8c3df28f49a82d255
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-10 12:14:22 +0100

    refactor: export components

--- src/components/Data/Zaehlung/History/Row.jsx
+++ src/components/Data/Zaehlung/History/Row.jsx
@@ -9,95 +9,95 @@ import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { checkForOnlineError } from '../../../../utils/checkForOnlineError.js'
 import { mutations } from '../../../../utils/mutations.js'
 import { toPgArray } from '../../../../utils/toPgArray.js'
-import createDataArrayForRevComparison from '../createDataArrayForRevComparison.js'
+import { createDataArrayForZaehlungRevComparison as createDataArray } from '../createDataArrayForRevComparison.js'
 
-const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
-  const store = useContext(MobxStoreContext)
-  const { user, addNotification, db, gqlClient } = store
+export const ZaehlungHistoryRow = observer(
+  ({ row, revRow, historyTakeoverCallback }) => {
+    const store = useContext(MobxStoreContext)
+    const { user, addNotification, db, gqlClient } = store
 
-  const dataArray = useMemo(
-    () => createDataArrayForRevComparison({ row, revRow, store }),
-    [revRow, row, store],
-  )
-  const onClickWiderspruchUebernehmen = useCallback(async () => {
-    // need to attach to the winner, that is row
-    // otherwise risk to still have lower depth and thus loosing
-    const newDepth = row._depth + 1
-    const newObject = {
-      zaehlung_id: revRow.zaehlung_id,
-      kultur_id: revRow.kultur_id,
-      datum: revRow.datum,
-      prognose: revRow.prognose,
-      bemerkungen: revRow.bemerkungen,
-      _parent_rev: row._rev,
-      _depth: newDepth,
-      _deleted: revRow._deleted,
-    }
-    const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
-    newObject._rev = rev
-    newObject.id = uuidv1()
-    newObject.changed = new window.Date().toISOString()
-    newObject.changed_by = user.email
-    newObject._revisions = toPgArray([rev, ...row._revisions])
-    const newObjectForStore = { ...newObject }
-    const response = await gqlClient
-      .query(mutations.mutateInsert_zaehlung_rev_one, {
-        object: newObject,
-        on_conflict: {
-          constraint: 'zaehlung_rev_pkey',
-          update_columns: ['id'],
-        },
-      })
-      .toPromise()
-    if (response.error) {
-      checkForOnlineError({ error: response.error, store })
-      return addNotification({
-        message: response.error.message,
-      })
-    }
-    historyTakeoverCallback()
-    // do not stringify revisions for store
-    // as _that_ is a real array
-    newObjectForStore._revisions =
-      row._revisions ? [rev, ...row._revisions] : [rev]
-    // TODO: is this a good idea?
-    newObjectForStore._conflicts = row._conflicts
-    // for store: convert rev to winner
-    newObjectForStore.id = row.id
-    delete newObjectForStore.zaehlung_id
-    // optimistically update store
-    await db.write(async () => {
-      await row.update((row) => {
-        Object.entries(newObjectForStore).forEach(([key, value]) => {
-          if (!isEqual(value, row[key])) {
-            row[key] = value
-          }
+    const dataArray = useMemo(
+      () => createDataArray({ row, revRow, store }),
+      [revRow, row, store],
+    )
+    const onClickWiderspruchUebernehmen = useCallback(async () => {
+      // need to attach to the winner, that is row
+      // otherwise risk to still have lower depth and thus loosing
+      const newDepth = row._depth + 1
+      const newObject = {
+        zaehlung_id: revRow.zaehlung_id,
+        kultur_id: revRow.kultur_id,
+        datum: revRow.datum,
+        prognose: revRow.prognose,
+        bemerkungen: revRow.bemerkungen,
+        _parent_rev: row._rev,
+        _depth: newDepth,
+        _deleted: revRow._deleted,
+      }
+      const rev = `${newDepth}-${md5(JSON.stringify(newObject))}`
+      newObject._rev = rev
+      newObject.id = uuidv1()
+      newObject.changed = new window.Date().toISOString()
+      newObject.changed_by = user.email
+      newObject._revisions = toPgArray([rev, ...row._revisions])
+      const newObjectForStore = { ...newObject }
+      const response = await gqlClient
+        .query(mutations.mutateInsert_zaehlung_rev_one, {
+          object: newObject,
+          on_conflict: {
+            constraint: 'zaehlung_rev_pkey',
+            update_columns: ['id'],
+          },
+        })
+        .toPromise()
+      if (response.error) {
+        checkForOnlineError({ error: response.error, store })
+        return addNotification({
+          message: response.error.message,
+        })
+      }
+      historyTakeoverCallback()
+      // do not stringify revisions for store
+      // as _that_ is a real array
+      newObjectForStore._revisions =
+        row._revisions ? [rev, ...row._revisions] : [rev]
+      // TODO: is this a good idea?
+      newObjectForStore._conflicts = row._conflicts
+      // for store: convert rev to winner
+      newObjectForStore.id = row.id
+      delete newObjectForStore.zaehlung_id
+      // optimistically update store
+      await db.write(async () => {
+        await row.update((row) => {
+          Object.entries(newObjectForStore).forEach(([key, value]) => {
+            if (!isEqual(value, row[key])) {
+              row[key] = value
+            }
+          })
         })
       })
-    })
-  }, [
-    row,
-    revRow.zaehlung_id,
-    revRow.kultur_id,
-    revRow.datum,
-    revRow.prognose,
-    revRow.bemerkungen,
-    revRow._deleted,
-    user.email,
-    gqlClient,
-    historyTakeoverCallback,
-    db,
-    store,
-    addNotification,
-  ])
+    }, [
+      row,
+      revRow.zaehlung_id,
+      revRow.kultur_id,
+      revRow.datum,
+      revRow.prognose,
+      revRow.bemerkungen,
+      revRow._deleted,
+      user.email,
+      gqlClient,
+      historyTakeoverCallback,
+      db,
+      store,
+      addNotification,
+    ])
 
-  return (
-    <History
-      rev={revRow._rev}
-      dataArray={dataArray}
-      onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
-    />
-  )
-}
-
-export default observer(HistoryRow)
+    return (
+      <History
+        rev={revRow._rev}
+        dataArray={dataArray}
+        onClickWiderspruchUebernehmen={onClickWiderspruchUebernehmen}
+      />
+    )
+  },
+)
