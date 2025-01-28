7db99926ace1f8ef56b5f27ce5a5cc381c52e106
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-28 13:34:22 +0100

    work on 130

---
+++ src/components/Header/Vermehrung/Menu/Admin/index.jsx
@@ -0,0 +1,88 @@
+import React, { useContext, useCallback, useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import { FaChevronRight } from 'react-icons/fa'
+import styled from '@emotion/styled'
+
+import { MobxStoreContext } from '../../../../../mobxStoreContext.js'
+import { buildExceljsWorksheetsForLieferungenOfYear } from './buildExceljsWorksheetsForLieferungenOfYear.js'
+import { buildExceljsWorksheetsForKulturBedarfsplanung } from './buildExceljsWorksheetsForKulturBedarfsplanung.js'
+import { downloadExceljsWorkbook } from '../../../../../utils/downloadExceljsWorkbook.js'
+import { StyledMenuItem } from '../Menu.jsx'
+
+const FirstMenuItem = styled(MenuItem)`
+  margin-top: -5px !important;
+`
+
+export const HeaderAdminMenu = observer(
+  ({ setParentAnchorEl: setGrandParentAnchorEl }) => {
+    const store = useContext(MobxStoreContext)
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const onClickExporte = useCallback(
+      (event) => setAnchorEl(event.currentTarget),
+      [],
+    )
+
+    const onClickLieferungenDesJahrs = useCallback(
+      (event) => {
+        const year = event.target.value
+        if (year.length === 4) {
+          buildExceljsWorksheetsForLieferungenOfYear({ year, store })
+          setAnchorEl(null)
+          setGrandParentAnchorEl(null)
+        }
+      },
+      [setGrandParentAnchorEl, setAnchorEl, store],
+    )
+    const onClickKulturenFuerBedarfsplanung = useCallback(async () => {
+      const { Workbook } = await import('exceljs/dist/exceljs.min.js')
+      const workbook = new Workbook()
+      await buildExceljsWorksheetsForKulturBedarfsplanung({
+        store,
+        workbook,
+      })
+      downloadExceljsWorkbook({
+        store,
+        fileName: `kulturenFuerBedarfsplanung`,
+        workbook,
+      })
+      setAnchorEl(null)
+      setGrandParentAnchorEl(null)
+    }, [setGrandParentAnchorEl, setAnchorEl, store])
+
+    const onClose = useCallback(() => setAnchorEl(null), [setAnchorEl])
+
+    return (
+      <>
+        <StyledMenuItem
+          aria-owns={anchorEl ? 'menu' : null}
+          aria-haspopup="true"
+          onClick={onClickExporte}
+        >
+          Administration
+          <FaChevronRight />
+        </StyledMenuItem>
+        <Menu
+          id="menuExport"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={onClose}
+        >
+          <FirstMenuItem>
+            <TextField
+              label="Lieferungen des Jahrs:"
+              onChange={onClickLieferungenDesJahrs}
+              variant="standard"
+            />
+          </FirstMenuItem>
+          <MenuItem onClick={onClickKulturenFuerBedarfsplanung}>
+            aktueller Stand Kulturen für die Bedarfsplanung
+          </MenuItem>
+        </Menu>
+      </>
+    )
+  },
+)
