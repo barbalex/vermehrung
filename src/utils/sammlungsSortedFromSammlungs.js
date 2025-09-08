fc457cef0ccf8a748cb3931425d25e73e9606915
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-08 13:25:13 +0200

    sort sammlungs date desc

--- src/utils/sammlungsSortedFromSammlungs.js
+++ src/utils/sammlungsSortedFromSammlungs.js
@@ -1,47 +1,11 @@
 import sortBy from 'lodash/sortBy'
-import { first as first$ } from 'rxjs/operators'
 
-import { personFullname } from './personFullname.js'
+import { sammlungIdsSortedFromSammlungs } from './sammlungIdsSortedFromSammlungs.js'
 
 export const sammlungsSortedFromSammlungs = async (sammlungs) => {
-  const sammlungSorters = await Promise.all(
-    sammlungs.map(async (sammlung) => {
-      const datum = sammlung?.datum ?? ''
-      let herkunft
-      try {
-        herkunft = await sammlung?.herkunft?.fetch()
-      } catch {}
-      const herkunftNr = herkunft?.nr
-      const herkunftGemeinde = herkunft?.gemeinde
-      const herkunftLokalname = herkunft?.lokalname
-      let person
-      try {
-        person = await sammlung?.person?.fetch()
-      } catch {}
-      const fullname = personFullname(person)
-      let art
-      try {
-        art = await sammlung?.art?.fetch()
-      } catch {}
-      let artLabel = ''
-      try {
-        artLabel = await art?.label?.pipe(first$()).toPromise()
-      } catch {}
-      const sort = [
-        datum,
-        herkunftNr?.toString()?.toLowerCase(),
-        herkunftGemeinde?.toString()?.toLowerCase(),
-        herkunftLokalname?.toString()?.toLowerCase(),
-        fullname?.toString()?.toLowerCase(),
-        artLabel?.toString()?.toLowerCase(),
-      ]
+  const sammlungIdsSorted = await sammlungIdsSortedFromSammlungs(sammlungs)
 
-      return { id: sammlung.id, sort }
-    }),
+  return sortBy(sammlungs, (sammlung) =>
+    sammlungIdsSorted.findIndex((s) => s.id === sammlung.id),
   )
-  const sammlungsSorted = sortBy(
-    sammlungs,
-    (sammlung) => sammlungSorters.find((s) => s.id === sammlung.id).sort,
-  )
-  return sammlungsSorted
 }
