a53243eeaf99c6cd0ea39fc0edf30cab23a49d38
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-08 15:08:50 +0200

    sort zaehlungs date desc

--- src/utils/zaehlungSort.js
+++ src/utils/zaehlungSort.js
@@ -1,8 +1,8 @@
 export const zaehlungSort = (a, b) => {
-  const datumA = a?.datum ? new Date(a.datum) : ''
-  const datumB = b?.datum ? new Date(b.datum) : ''
-  if (datumA < datumB) return -1
-  if (datumA > datumB) return 1
+  const datumA = a?.datum ? new Date(a.datum) : new Date('1000-01-01')
+  const datumB = b?.datum ? new Date(b.datum) : new Date('1000-01-01')
+  if (datumA > datumB) return -1
+  if (datumA < datumB) return 1
 
   const anzahlPflanzenA = a?.anzahl_pflanzen ?? ''
   const anzahlPflanzenB = b?.anzahl_pflanzen ?? ''
