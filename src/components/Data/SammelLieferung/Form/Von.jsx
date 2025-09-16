f01791143dfe55159275e12bf5e3b635ea81c41c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-16 19:57:01 +0200

    uniqBy

--- src/components/Data/SammelLieferung/Form/Von.jsx
+++ src/components/Data/SammelLieferung/Form/Von.jsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled'
 import { Q } from '@nozbe/watermelondb'
 import { first as first$ } from 'rxjs/operators'
 import { combineLatest } from 'rxjs'
-import uniqBy from 'lodash/uniqBy'
+import { uniqBy } from 'es-toolkit'
 
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { Select } from '../../../shared/Select/index.jsx'
@@ -117,7 +117,7 @@ export const SammelLieferungVon = observer(
           } catch {}
           const kultursIncludingChoosen = uniqBy(
             [...kultursFiltered, ...(kultur && !showFilter ? [kultur] : [])],
-            'id',
+            (e) => e.id,
           )
           const kultursSorted = await kultursSortedFromKulturs(
             kultursIncludingChoosen,
@@ -143,7 +143,7 @@ export const SammelLieferungVon = observer(
           } catch {}
           const sammlungsIncludingChoosen = uniqBy(
             [...sammlungs, ...(sammlung && !showFilter ? [sammlung] : [])],
-            'id',
+            (e) => e.id,
           )
           const sammlungsSorted = await sammlungsSortedFromSammlungs(
             sammlungsIncludingChoosen,
