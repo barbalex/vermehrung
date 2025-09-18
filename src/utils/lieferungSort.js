8f6813aa9eb1c7dd03aae1786c6d680b0d4e1588
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-18 11:46:19 +0200

    correct

--- src/utils/lieferungSort.js
+++ src/utils/lieferungSort.js
@@ -2,7 +2,7 @@ export const lieferungSort = (a, b) => {
   const datumA = a?.datum ? new Date(a.datum) : new Date('1000-01-01')
   const datumB = b?.datum ? new Date(b.datum) : new Date('1000-01-01')
   if (datumA > datumB) return -1
-  if (datumA > datumB) return 1
+  if (datumA < datumB) return 1
 
   const anzahlPflanzenA = a?.anzahl_pflanzen ?? ''
   const anzahlPflanzenB = b?.anzahl_pflanzen ?? ''
