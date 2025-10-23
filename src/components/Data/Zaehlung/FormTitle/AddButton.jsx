7154221b7667a614c1bd0d9f4d53e9ad14f948fe
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-23 17:15:40 +0200

    de-memoize

--- src/components/Data/Zaehlung/FormTitle/AddButton.jsx
+++ src/components/Data/Zaehlung/FormTitle/AddButton.jsx
@@ -1,4 +1,4 @@
-import { useContext, useCallback } from 'react'
+import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { FaPlus } from 'react-icons/fa'
 import IconButton from '@mui/material/IconButton'
@@ -10,16 +10,12 @@ export const ZaehlungAddButton = observer(() => {
   const store = useContext(MobxStoreContext)
   const { insertZaehlungRev } = store
 
-  const add = useCallback(() => {
-    insertZaehlungRev()
-  }, [insertZaehlungRev])
-
   return (
     <ErrorBoundary>
       <IconButton
         aria-label="neue Zählung"
         title="neue Zählung"
-        onClick={add}
+        onClick={insertZaehlungRev}
         size="large"
       >
         <FaPlus />
