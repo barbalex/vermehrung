a2ea40c3f1e6f13fa436cc0a0dc48c3cc8f33a3a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-23 14:35:36 +0200

    de-memoize

--- src/components/Data/Event/FormTitle/Settings/Menu.jsx
+++ src/components/Data/Event/FormTitle/Settings/Menu.jsx
@@ -1,4 +1,4 @@
-import { useContext, useCallback, useState, useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
@@ -50,24 +50,22 @@ export const EventSettingsMenu = observer(
 
     const { ev_datum, tk, ev_geplant, ev_person_id } = kulturOption ?? {}
 
-    const saveToDb = useCallback(
-      async (event) => {
-        const field = event.target.name
-        const value = event.target.value === 'false'
-        kulturOption.edit({ field, value, store })
-      },
-      [kulturOption, store],
-    )
-    const openSettingsDocs = useCallback(() => {
+    const saveToDb = (event) => {
+      const field = event.target.name
+      const value = event.target.value === 'false'
+      kulturOption.edit({ field, value, store })
+    }
+
+    const openSettingsDocs = () => {
       setAnchorEl(null)
       const url = `${constants?.getAppUri()}/Dokumentation/felder-blenden`
       if (window.matchMedia('(display-mode: standalone)').matches) {
         return window.open(url, '_blank', 'toolbar=no')
       }
       window.open(url)
-    }, [setAnchorEl])
+    }
 
-    const onClose = useCallback(() => setAnchorEl(null), [setAnchorEl])
+    const onClose = () => setAnchorEl(null)
 
     return (
       <Menu
