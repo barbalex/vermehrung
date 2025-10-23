f989179f05bc45674cc5b374ab058ad37048c5a0
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-23 16:25:53 +0200

    de-memoize

--- src/components/Data/Teilkultur/FormTitle/Settings/index.jsx
+++ src/components/Data/Teilkultur/FormTitle/Settings/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
 import MenuItem from '@mui/material/MenuItem'
@@ -9,10 +9,7 @@ import { TeilkulturSettingsMenu as Menu } from './Menu.jsx'
 
 export const TeilkulturSettings = observer(({ asMenu, kulturId }) => {
   const [anchorEl, setAnchorEl] = useState(null)
-  const onClickConfig = useCallback(
-    (event) => setAnchorEl(event.currentTarget),
-    [],
-  )
+  const onClickConfig = (event) => setAnchorEl(event.currentTarget)
 
   if (asMenu) {
     return (
