10d979d0af1b1e54ada79f07ed472e6aa3d7c3de
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-23 15:36:59 +0200

    de-memoize

--- src/components/Data/Person/index.jsx
+++ src/components/Data/Person/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useCallback, useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from '@emotion/styled'
 import { Allotment } from 'allotment'
@@ -53,14 +53,8 @@ export const Person = observer(
     const { row, rawRow } = dataState
 
     const [activeConflict, setActiveConflict] = useState(null)
-    const conflictDisposalCallback = useCallback(
-      () => setActiveConflict(null),
-      [],
-    )
-    const conflictSelectionCallback = useCallback(
-      () => setActiveConflict(null),
-      [],
-    )
+    const conflictDisposalCallback = () => setActiveConflict(null)
+    const conflictSelectionCallback = () => setActiveConflict(null)
     // ensure that activeConflict is reset
     // when changing dataset
     useEffect(() => {
@@ -68,7 +62,7 @@ export const Person = observer(
     }, [id])
 
     const [showHistory, setShowHistory] = useState(false)
-    const historyTakeoverCallback = useCallback(() => setShowHistory(null), [])
+    const historyTakeoverCallback = () => setShowHistory(null)
 
     if (!row || !Object.keys(row ?? {})) return <Spinner />
     if (!showFilter && filter.show) return null
