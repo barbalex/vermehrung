c0e561ae06757eff99a1f14adf7704d27c30e0a6
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-28 11:28:44 +0100

    lazily import

--- src/components/Data/index.jsx
+++ src/components/Data/index.jsx
@@ -1,30 +1,76 @@
-import { useContext } from 'react'
+import { useContext, lazy } from 'react'
 import { observer } from 'mobx-react-lite'
 import isUuid from 'is-uuid'
 
-import { Art } from './Art/index.jsx'
-import { Arten } from './Arten/index.jsx'
-import { Event } from './Event/index.jsx'
-import { Events } from './Events/index.jsx'
-import { Herkunft } from './Herkunft/index.jsx'
-import { Herkuenfte } from './Herkuenfte/index.jsx'
-import { Sammlung } from './Sammlung/index.jsx'
-import { Sammlungen } from './Sammlungen/index.jsx'
-import { Garten } from './Garten/index.jsx'
-import { Gaerten } from './Gaerten/index.jsx'
-import { Kultur } from './Kultur/index.jsx'
-import { Kulturen } from './Kulturen/index.jsx'
-import { LieferungContainer as Lieferung } from './Lieferung/index.jsx'
-import { Lieferungen } from './Lieferungen/index.jsx'
-import { SammelLieferung } from './SammelLieferung/index.jsx'
-import { SammelLieferungen } from './SammelLieferungen/index.jsx'
-import { Person } from './Person/index.jsx'
-import { Personen } from './Personen/index.jsx'
-import { Zaehlung } from './Zaehlung/index.jsx'
-import { Zaehlungen } from './Zaehlungen/index.jsx'
-import { Teilkultur } from './Teilkultur/index.jsx'
-import { Teilkulturen } from './Teilkulturen/index.jsx'
-import { Root } from './Root/index.jsx'
+const Art = lazy(async () => ({
+  default: (await import('./Art/index.jsx')).Art,
+}))
+const Arten = lazy(async () => ({
+  default: (await import('./Arten/index.jsx')).Arten,
+}))
+const Event = lazy(async () => ({
+  default: (await import('./Event/index.jsx')).Event,
+}))
+const Events = lazy(async () => ({
+  default: (await import('./Events/index.jsx')).Events,
+}))
+const Herkunft = lazy(async () => ({
+  default: (await import('./Herkunft/index.jsx')).Herkunft,
+}))
+const Herkuenfte = lazy(async () => ({
+  default: (await import('./Herkuenfte/index.jsx')).Herkuenfte,
+}))
+const Sammlung = lazy(async () => ({
+  default: (await import('./Sammlung/index.jsx')).Sammlung,
+}))
+const Sammlungen = lazy(async () => ({
+  default: (await import('./Sammlungen/index.jsx')).Sammlungen,
+}))
+const Garten = lazy(async () => ({
+  default: (await import('./Garten/index.jsx')).Garten,
+}))
+const Gaerten = lazy(async () => ({
+  default: (await import('./Gaerten/index.jsx')).Gaerten,
+}))
+const Kultur = lazy(async () => ({
+  default: (await import('./Kultur/index.jsx')).Kultur,
+}))
+const Kulturen = lazy(async () => ({
+  default: (await import('./Kulturen/index.jsx')).Kulturen,
+}))
+const Lieferung = lazy(async () => ({
+  default: (await import('./Lieferung/index.jsx')).LieferungContainer,
+}))
+const Lieferungen = lazy(async () => ({
+  default: (await import('./Lieferungen/index.jsx')).Lieferungen,
+}))
+const SammelLieferung = lazy(async () => ({
+  default: (await import('./SammelLieferung/index.jsx')).SammelLieferung,
+}))
+const SammelLieferungen = lazy(async () => ({
+  default: (await import('./SammelLieferungen/index.jsx')).SammelLieferungen,
+}))
+const Person = lazy(async () => ({
+  default: (await import('./Person/index.jsx')).Person,
+}))
+const Personen = lazy(async () => ({
+  default: (await import('./Personen/index.jsx')).Personen,
+}))
+const Zaehlung = lazy(async () => ({
+  default: (await import('./Zaehlung/index.jsx')).Zaehlung,
+}))
+const Zaehlungen = lazy(async () => ({
+  default: (await import('./Zaehlungen/index.jsx')).Zaehlungen,
+}))
+const Teilkultur = lazy(async () => ({
+  default: (await import('./Teilkultur/index.jsx')).Teilkultur,
+}))
+const Teilkulturen = lazy(async () => ({
+  default: (await import('./Teilkulturen/index.jsx')).Teilkulturen,
+}))
+const Root = lazy(async () => ({
+  default: (await import('./Root/index.jsx')).Root,
+}))
 import { MobxStoreContext } from '../../mobxStoreContext.js'
 
 export const Data = observer(() => {
@@ -35,75 +81,52 @@ export const Data = observer(() => {
   const id = activeNodeArray.filter((e) => isUuid.v1(e)).at(-1)
 
   switch (activeForm) {
-    case 'root': {
+    case 'root':
       return <Root />
-    }
-    case 'art': {
+    case 'art':
       return <Art id={id} />
-    }
-    case 'arten': {
+    case 'arten':
       return <Arten />
-    }
-    case 'event': {
+    case 'event':
       return <Event id={id} />
-    }
-    case 'events': {
+    case 'events':
       return <Events />
-    }
-    case 'garten': {
+    case 'garten':
       return <Garten id={id} />
-    }
-    case 'gaerten': {
+    case 'gaerten':
       return <Gaerten />
-    }
-    case 'herkunft': {
+    case 'herkunft':
       return <Herkunft id={id} />
-    }
-    case 'herkuenfte': {
+    case 'herkuenfte':
       return <Herkuenfte />
-    }
-    case 'kultur': {
+    case 'kultur':
       return <Kultur id={id} />
-    }
-    case 'kulturen': {
+    case 'kulturen':
       return <Kulturen />
-    }
-    case 'sammel_lieferung': {
+    case 'sammel_lieferung':
       return <SammelLieferung id={id} />
-    }
-    case 'sammelLieferungen': {
+    case 'sammelLieferungen':
       return <SammelLieferungen />
-    }
-    case 'lieferung': {
+    case 'lieferung':
       return <Lieferung id={id} />
-    }
-    case 'lieferungen': {
+    case 'lieferungen':
       return <Lieferungen />
-    }
-    case 'person': {
+    case 'person':
       return <Person id={id} />
-    }
-    case 'personen': {
+    case 'personen':
       return <Personen />
-    }
-    case 'sammlung': {
+    case 'sammlung':
       return <Sammlung id={id} />
-    }
-    case 'sammlungen': {
+    case 'sammlungen':
       return <Sammlungen />
-    }
-    case 'teilkultur': {
+    case 'teilkultur':
       return <Teilkultur id={id} />
-    }
-    case 'teilkulturen': {
+    case 'teilkulturen':
       return <Teilkulturen />
-    }
-    case 'zaehlung': {
+    case 'zaehlung':
       return <Zaehlung id={id} />
-    }
-    case 'zaehlungen': {
+    case 'zaehlungen':
       return <Zaehlungen />
-    }
     default:
       return null
   }
