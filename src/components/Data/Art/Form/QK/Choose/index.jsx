fd8e8a37e82ac90ed26919ca7f62337e5b926c0b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-13 23:09:31 +0100

    use css modules

--- src/components/Data/Art/Form/QK/Choose/index.jsx
+++ src/components/Data/Art/Form/QK/Choose/index.jsx
@@ -1,32 +1,22 @@
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
 import { Row } from './Row.jsx'
 import { ErrorBoundary } from '../../../../../shared/ErrorBoundary.jsx'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const FieldsContainer = styled.div`
-  padding: 10px 0;
-`
-const Info = styled.div`
-  text-align: center;
-`
+import { container, fieldsContainer, info } from './index.module.css'
 
 export const Choose = observer(({ qks }) => (
   <ErrorBoundary>
-    <Container>
-      <Info>Diese Wahl gilt für alle Arten</Info>
-      <FieldsContainer>
+    <div className={container}>
+      <div className={info}>Diese Wahl gilt für alle Arten</div>
+      <div className={fieldsContainer}>
         {qks.map((row) => (
           <Row
             key={row.id}
             qk={row}
           />
         ))}
-      </FieldsContainer>
-    </Container>
+      </div>
+    </div>
   </ErrorBoundary>
 ))
