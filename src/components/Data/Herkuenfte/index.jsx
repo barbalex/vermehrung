6db17006e6402ae7c9102f6bf9ec7592963ac10a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 17:45:46 +0100

    use css modules

--- src/components/Data/Herkuenfte/index.jsx
+++ src/components/Data/Herkuenfte/index.jsx
@@ -21,7 +21,6 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
 `
 
 const TitleContainer = styled.div`
@@ -137,7 +136,7 @@ export const Herkuenfte = observer(({ filter: showFilter }) => {
 
   return (
     <ErrorBoundary>
-      <Container showfilter={showFilter}>
+      <Container style={{ backgroundColor: showFilter ? '#fff3e0' : 'unset' }}>
         {showFilter ?
           <FilterTitle
             title="Herkunft"
