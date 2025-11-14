fc35a2711acff772fcd8d9165b15c02e2bd8d24d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 16:52:43 +0100

    use css modules

--- src/components/Data/Event/FormTitle/Settings/Menu.jsx
+++ src/components/Data/Event/FormTitle/Settings/Menu.jsx
@@ -6,28 +6,11 @@ import MenuItem from '@mui/material/MenuItem'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 import { IoMdInformationCircleOutline } from 'react-icons/io'
-import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../../../mobxStoreContext.js'
 import { constants } from '../../../../../utils/constants.js'
 
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-right: 16px;
-  user-select: none;
-`
-const Title = styled.div`
-  padding: 12px 16px;
-  color: rgba(0, 0, 0, 0.6);
-  font-weight: 700;
-  user-select: none;
-`
-const Info = styled.div`
-  padding: 12px 16px;
-  color: rgba(0, 0, 0, 0.4);
-  user-select: none;
-`
+import { titleRow, title, info } from './Menu.module.css'
 
 export const EventSettingsMenu = observer(
   ({ anchorEl, setAnchorEl, kulturId }) => {
@@ -74,8 +57,8 @@ export const EventSettingsMenu = observer(
         open={Boolean(anchorEl)}
         onClose={onClose}
       >
-        <TitleRow>
-          <Title>Felder für Events wählen:</Title>
+        <div className={titleRow}>
+          <div className={title}>Felder für Events wählen:</div>
           <div>
             <IconButton
               aria-label="Anleitung öffnen"
@@ -86,7 +69,7 @@ export const EventSettingsMenu = observer(
               <IoMdInformationCircleOutline />
             </IconButton>
           </div>
-        </TitleRow>
+        </div>
         <MenuItem>
           <FormControlLabel
             value={tk === true ? 'true' : 'false'}
@@ -147,11 +130,11 @@ export const EventSettingsMenu = observer(
             labelPlacement="end"
           />
         </MenuItem>
-        <Info>
+        <div className={info}>
           Zwingende Felder sind nicht aufgelistet.
           <br />
           Die Wahl gilt (nur) für diese Kultur.
-        </Info>
+        </div>
       </Menu>
     )
   },
