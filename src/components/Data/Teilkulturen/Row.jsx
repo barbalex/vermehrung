50cd8613afd5f5bc740bf427dcd653ec5bdcfeb5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 18:15:25 +0100

    use css modules

--- src/components/Data/Teilkulturen/Row.jsx
+++ src/components/Data/Teilkulturen/Row.jsx
@@ -1,32 +1,10 @@
 import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
 import { teilkulturLabelFromTeilkultur } from '../../../utils/teilkulturLabelFromTeilkultur.js'
 
 import { container } from '../Arten/Row.module.css'
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 48px;
-  border-bottom: 1px solid rgba(74, 20, 140, 0.2);
-  border-collapse: collapse;
-  box-sizing: border-box;
-  margin: -1px 0;
-  padding: 10px;
-  cursor: pointer;
-  div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  &:hover {
-    background-color: rgba(74, 20, 140, 0.03);
-  }
-`
 
 export const TeilkulturRow = observer(({ style, index, rows }) => {
   const store = useContext(MobxStoreContext)
@@ -37,12 +15,13 @@ export const TeilkulturRow = observer(({ style, index, rows }) => {
   const onClickRow = () => setActiveNodeArray([...activeNodeArray, row.id])
 
   return (
-    <Row
+    <div
+      className={container}
       key={row.id}
       onClick={onClickRow}
       style={style}
     >
       <div>{teilkulturLabelFromTeilkultur({ teilkultur: row })}</div>
-    </Row>
+    </div>
   )
 })
