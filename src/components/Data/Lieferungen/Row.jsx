db184310661cbba9e2657bbc25280ae907a430ab
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 18:15:55 +0100

    use css modules

--- src/components/Data/Lieferungen/Row.jsx
+++ src/components/Data/Lieferungen/Row.jsx
@@ -1,33 +1,11 @@
 import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
 import { lieferungLabelFromLieferung } from '../../../utils/lieferungLabelFromLieferung.js'
 import { constants } from '../../../utils/constants.js'
 
 import { container } from '../Arten/Row.module.css'
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 48px;
-  border-bottom: 1px solid rgba(74, 20, 140, 0.2);
-  border-collapse: collapse;
-  box-sizing: border-box;
-  margin: -1px 0;
-  padding: 10px;
-  cursor: pointer;
-  div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  &:hover {
-    background-color: rgba(74, 20, 140, 0.03);
-  }
-`
 
 export const LieferungRow = observer(({ style, index, rows }) => {
   const store = useContext(MobxStoreContext)
@@ -38,12 +16,13 @@ export const LieferungRow = observer(({ style, index, rows }) => {
   const onClickRow = () => setActiveNodeArray([...activeNodeArray, row.id])
 
   return (
-    <Row
+    <div
+      className={container}
       key={row.id}
       onClick={onClickRow}
       style={style}
     >
       <div>{lieferungLabelFromLieferung({ lieferung: row })}</div>
-    </Row>
+    </div>
   )
 })
