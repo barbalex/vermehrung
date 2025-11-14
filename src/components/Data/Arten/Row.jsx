edc2cd8d7bd01fb5343ef5aa0625c3d0995010dd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 16:26:26 +0100

    use css modules

--- src/components/Data/Arten/Row.jsx
+++ src/components/Data/Arten/Row.jsx
@@ -1,31 +1,10 @@
 import { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 import { first as first$ } from 'rxjs/operators'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
-import { constants } from '../../../utils/constants.js'
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: ${constants.singleRowHeight};
-  border-bottom: 1px solid rgba(74, 20, 140, 0.2);
-  border-collapse: collapse;
-  box-sizing: border-box;
-  margin: -1px 0;
-  padding: 10px;
-  cursor: pointer;
-  div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  &:hover {
-    background-color: rgba(74, 20, 140, 0.03);
-  }
-`
+import { container } from './Row.module.css'
 
 export const ArtRow = observer(({ style, index, rows }) => {
   const store = useContext(MobxStoreContext)
@@ -52,12 +31,13 @@ export const ArtRow = observer(({ style, index, rows }) => {
   const onClickRow = () => setActiveNodeArray([...activeNodeArray, row.id])
 
   return (
-    <Row
+    <div
       key={row.id}
       onClick={onClickRow}
       style={style}
+      className={container}
     >
       <div>{label}</div>
-    </Row>
+    </div>
   )
 })
