f8d483a20b91a15e4c83582271afe52ecf7b3a02
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 17:45:29 +0100

    use css modules

--- src/components/Data/SammelLieferungen/index.jsx
+++ src/components/Data/SammelLieferungen/index.jsx
@@ -21,7 +21,6 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
 `
 
 const TitleContainer = styled.div`
@@ -122,7 +121,7 @@ export const SammelLieferungen = observer(({ filter: showFilter = false }) => {
 
   return (
     <ErrorBoundary>
-      <Container showfilter={showFilter}>
+      <Container style={{ backgroundColor: showFilter ? '#fff3e0' : 'unset' }}>
         {showFilter ?
           <FilterTitle
             title="Sammel-Lieferung"
