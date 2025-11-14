a8688c415c00b43d0a3182807bd6de12a9eecb3f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 19:59:24 +0100

    use css modules

--- src/components/Data/Sammlung/index.jsx
+++ src/components/Data/Sammlung/index.jsx
@@ -1,6 +1,5 @@
 import { useContext, useState, useEffect, useMemo } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 import { Allotment } from 'allotment'
 import { of as $of } from 'rxjs'
 
@@ -13,15 +12,7 @@ import { SammlungConflict as Conflict } from './Conflict.jsx'
 import { SammlungHistory as History } from './History/index.jsx'
 import { useObservable } from '../../../utils/useObservable.js'
 
-const Container = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-`
-const SplitPaneContainer = styled.div`
-  height: 100%;
-  position: relative;
-`
+import { container, splitPaneContainer } from '../Art/index.module.css'
 
 export const Sammlung = observer(
   ({
@@ -61,7 +52,8 @@ export const Sammlung = observer(
 
     return (
       <ErrorBoundary>
-        <Container
+        <div
+          className={container}
           style={{ backgroundColor: showFilter ? '#fff3e0' : 'unset' }}
         >
           <FormTitle
@@ -70,7 +62,7 @@ export const Sammlung = observer(
             showHistory={showHistory}
             setShowHistory={setShowHistory}
           />
-          <SplitPaneContainer>
+          <div className={splitPaneContainer}>
             <Allotment key={`${activeConflict}/${showHistory}`}>
               <Form
                 showFilter={showFilter}
@@ -98,8 +90,8 @@ export const Sammlung = observer(
                 : null}
               </Allotment.Pane>
             </Allotment>
-          </SplitPaneContainer>
-        </Container>
+          </div>
+        </div>
       </ErrorBoundary>
     )
   },
