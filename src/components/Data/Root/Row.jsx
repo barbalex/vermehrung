811985589dd3024f506fd794a8fbc63658948320
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-14 18:15:39 +0100

    use css modules

--- src/components/Data/Root/Row.jsx
+++ src/components/Data/Root/Row.jsx
@@ -1,31 +1,10 @@
 import { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
 import { tableFilter } from '../../../utils/tableFilter.js'
 
 import { container } from '../Arten/Row.module.css'
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 48px;
-  border-bottom: 1px solid rgba(74, 20, 140, 0.1);
-  border-collapse: collapse;
-  margin: -1px 0;
-  padding: 10px;
-  cursor: pointer;
-  div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  &:hover {
-    background-color: rgba(74, 20, 140, 0.03);
-  }
-`
 
 export const RootRow = observer(({ style, row }) => {
   const store = useContext(MobxStoreContext)
@@ -54,12 +33,13 @@ export const RootRow = observer(({ style, row }) => {
   const onClickRow = () => setActiveNodeArray(row.url)
 
   return (
-    <Row
+    <div
+      className={container}
       key={row.id}
       onClick={onClickRow}
       style={style}
     >
       <div>{`${row.name} (${count})`}</div>
-    </Row>
+    </div>
   )
 })
