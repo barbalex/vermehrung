6c284d9fab118ed1220efca127db67fbbd51878f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-15 14:46:54 +0100

    use css modules

--- src/components/Data/SammelLieferung/FormTitle/Copy/index.jsx
+++ src/components/Data/SammelLieferung/FormTitle/Copy/index.jsx
@@ -4,7 +4,6 @@ import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import { FaRegCopy } from 'react-icons/fa'
-import styled from '@emotion/styled'
 
 import { MobxStoreContext } from '../../../../../mobxStoreContext.js'
 import { exists } from '../../../../../utils/exists.js'
@@ -12,18 +11,7 @@ import { updateSammelLieferung } from './updateLieferung.js'
 import { updateAllSammelLieferungen } from './updateAllLieferungen.js'
 import { ErrorBoundary } from '../../../../shared/ErrorBoundary.jsx'
 
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-right: 16px;
-  user-select: none;
-`
-const Title = styled.div`
-  padding: 12px 16px;
-  color: rgba(0, 0, 0, 0.6);
-  font-weight: 700;
-  user-select: none;
-`
+import { titleRow, title } from './index.module.css'
 
 const sammelLieferungFields = [
   'id',
@@ -115,9 +103,11 @@ export const SammelLieferungCopyMenu = observer(
           open={Boolean(anchorEl)}
           onClose={onClose}
         >
-          <TitleRow>
-            <Title>Die Daten der Sammel-Lieferung kopieren:</Title>
-          </TitleRow>
+          <div className={titleRow}>
+            <div className={title}>
+              Die Daten der Sammel-Lieferung kopieren:
+            </div>
+          </div>
           {lieferung && (
             <MenuItem onClick={onClickActiveLieferung}>
               in die links angezeigte Lieferung
