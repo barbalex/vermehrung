75fe2e63f062ec8ea7bf1296c35b7164ce1fe14c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-15 10:48:53 +0100

    use css modules

--- src/components/Data/Event/FormTitle/FormTitle.jsx
+++ src/components/Data/Event/FormTitle/FormTitle.jsx
@@ -13,7 +13,7 @@ import { HistoryButton } from '../../../shared/HistoryButton.jsx'
 import { Menu } from '../../../shared/Menu.jsx'
 import UpSvg from '../../../../svg/to_up.svg?react'
 
-import { container, title, symbols } from './FormTitle.module.css'
+import { container, title, symbols } from '../../Art/FormTitle/FormTitle.module.css'
 
 export const EventFormTitle = observer(
   ({ row, totalCount, filteredCount, showHistory, setShowHistory }) => {
