c074b620d586a3224f23fbe5390e7b49553a3762
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-15 15:08:03 +0100

    use css modules

--- src/components/Data/Teilkultur/Form/Events/Row.jsx
+++ src/components/Data/Teilkultur/Form/Events/Row.jsx
@@ -1,43 +1,8 @@
 import { useState, useEffect } from 'react'
-import styled from '@emotion/styled'
 import format from 'date-fns/format'
 
 import { personFullname } from '../../../../../utils/personFullname.js'
-
-const Row = styled.div`
-  border-bottom: 1px solid rgba(74, 20, 140, 0.2);
-  border-collapse: collapse;
-  padding: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  &:hover {
-    background-color: rgba(74, 20, 140, 0.03);
-  }
-`
-const Datum = styled.div`
-  flex-basis: 85px;
-  flex-shrink: 0;
-  flex-grow: 0;
-  margin-right: 10px;
-`
-const Name = styled.div`
-  flex-basis: 150px;
-  flex-shrink: 1;
-  flex-grow: 1;
-  margin-right: 10px;
-`
-const Beschreibung = styled.div`
-  flex-basis: 170px;
-  flex-shrink: 5;
-  flex-grow: 5;
-  margin-right: 10px;
-`
-const Geplant = styled.div`
-  flex-basis: 60px;
-  flex-shrink: 0;
-  flex-grow: 0;
-  margin-right: 10px;
-`
+import { row, datumClass, name, beschreibung, geplant } from './Row.module.css'
 
 export const TeilkulturEventRow = ({ event }) => {
   const [personName, setPersonName] = useState()
@@ -63,11 +28,14 @@ export const TeilkulturEventRow = ({ event }) => {
     event.datum ? format(new Date(event.datum), 'yyyy.MM.dd') : 'Kein Datum'
 
   return (
-    <Row key={event.id}>
-      <Datum>{datum}</Datum>
-      <Geplant>{event?.geplant ? 'geplant' : ' '}</Geplant>
-      <Name>{personName ?? ' '}</Name>
-      <Beschreibung>{event?.beschreibung ?? ' '}</Beschreibung>
-    </Row>
+    <div
+      className={row}
+      key={event.id}
+    >
+      <div className={datumClass}>{datum}</div>
+      <div className={geplant}>{event?.geplant ? 'geplant' : ' '}</div>
+      <div className={name}>{personName ?? ' '}</div>
+      <div className={beschreibung}>{event?.beschreibung ?? ' '}</div>
+    </div>
   )
 }
