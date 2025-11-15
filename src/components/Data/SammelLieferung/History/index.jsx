f948ee4d12df4b7c9fa13eb66f3ad536aa299283
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-15 10:58:57 +0100

    use css modules

--- src/components/Data/SammelLieferung/History/index.jsx
+++ src/components/Data/SammelLieferung/History/index.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import gql from 'graphql-tag'
-import styled from '@emotion/styled'
 import Slider from 'react-slick'
 import { useQuery } from 'urql'
 
@@ -9,6 +8,8 @@ import { checkForOnlineError } from '../../../../utils/checkForOnlineError.js'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { SammelLieferungHistoryRow as Row } from './Row.jsx'
+
+import { container, errorContainer } from '../../Art/History/index.module.css'
 
 const sammelLieferungRevQuery = gql`
   query sammelLieferungRevForHistoryQuery($rev: [String!]) {
@@ -41,16 +42,6 @@ const sammelLieferungRevQuery = gql`
   }
 `
 
-const Container = styled.div`
-  overflow-y: auto;
-  scrollbar-width: thin;
-  padding: 0 25px;
-  height: 100%;
-`
-const ErrorContainer = styled.div`
-  padding: 25px;
-`
-
 const sliderSettings = {
   dots: false,
   infinite: false,
@@ -77,11 +68,11 @@ export const SammelLieferungHistory = observer(
     }
 
     if (error) {
-      return <ErrorContainer>{error.message}</ErrorContainer>
+      return <div className={errorContainer}>{error.message}</div>
     }
 
     return (
-      <Container>
+      <div className={container}>
         <Slider {...sliderSettings}>
           {revRows.map((r) => (
             <Row
@@ -93,7 +84,7 @@ export const SammelLieferungHistory = observer(
             />
           ))}
         </Slider>
-      </Container>
+      </div>
     )
   },
 )
