1af988c15ac6d30ef87bca4107c114f8f9f15aca
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-15 12:38:26 +0100

    use css modules

--- src/components/Data/Lieferung/Lieferung/Form/Wann.jsx
+++ src/components/Data/Lieferung/Lieferung/Form/Wann.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
 import IconButton from '@mui/material/IconButton'
 import { IoMdInformationCircleOutline } from 'react-icons/io'
 
@@ -10,37 +9,7 @@ import { Checkbox2States } from '../../../../shared/Checkbox2States.jsx'
 import { JesNo } from '../../../../shared/JesNo.jsx'
 import { constants } from '../../../../../utils/constants.js'
 
-const Title = styled.div`
-  font-weight: bold;
-  margin-top: auto;
-  margin-bottom: auto;
-`
-const TitleRow = styled.div`
-  background-color: ${(props) =>
-    props['data-filter'] ? '#ffe0b2' : 'rgba(248, 243, 254, 1)'};
-  flex-shrink: 0;
-  display: flex;
-  height: 40px;
-  justify-content: space-between;
-  margin-left: -10px;
-  margin-right: -10px;
-  margin-bottom: 10px;
-  padding: 0 10px;
-  position: sticky;
-  top: 0;
-  user-select: none;
-  z-index: 1;
-`
-const FieldRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  > div:not(:last-of-type) {
-    padding-right: 8px;
-  }
-  > div > button {
-    margin-top: 8px;
-  }
-`
+import { title, titleRow, fieldRow } from './Wann.module.css'
 
 export const LieferungWann = observer(
   ({ showFilter, row, saveToDb, ifNeeded }) => {
@@ -58,9 +27,14 @@ export const LieferungWann = observer(
 
     return (
       <>
-        <TitleRow data-filter={showFilter}>
-          <Title>wann</Title>
-        </TitleRow>
+        <div
+          className={titleRow}
+          style={{
+            backgroundColor: showFilter ? '#ffe0b2' : 'rgba(248, 243, 254, 1)',
+          }}
+        >
+          <div className={title}>wann</div>
+        </div>
         {ifNeeded('datum') && (
           <Date
             key={`${row.id}datum`}
@@ -72,7 +46,7 @@ export const LieferungWann = observer(
           />
         )}
         {ifNeeded('geplant') && (
-          <FieldRow>
+          <div className={fieldRow}>
             {showFilter ?
               <JesNo
                 key={`${row.id}geplant`}
@@ -101,7 +75,7 @@ export const LieferungWann = observer(
                 <IoMdInformationCircleOutline />
               </IconButton>
             </div>
-          </FieldRow>
+          </div>
         )}
       </>
     )
