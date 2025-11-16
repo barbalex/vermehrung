d522f5b8fc858edb5d6387f9348d4ef3952e71bd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 13:51:58 +0100

    use css modules

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,13 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import styled from '@emotion/styled'
 import { useLocation } from 'react-router'
 
 import { ErrorBoundary } from '../shared/ErrorBoundary.jsx'
-
 import { HeaderHome as Home } from './Home.jsx'
 import { HeaderDoku as Doku } from './Doku/index.jsx'
 import { HeaderVermehrung as Vermehrung } from './Vermehrung/index.jsx'
 
-const StyledAppBar = styled(AppBar)`
-  min-height: var(--app-bar-height) !important;
-  .MuiToolbar-root {
-    min-height: var(--app-bar-height) !important;
-    padding-left: 0 !important;
-    padding-right: 10px !important;
-  }
-  @media print {
-    display: none !important;
-  }
-`
+import { appBar } from './index.module.css'
 
 const Header = () => {
   const { pathname } = useLocation()
@@ -28,7 +16,10 @@ const Header = () => {
 
   return (
     <ErrorBoundary>
-      <StyledAppBar position="static">
+      <AppBar
+        position="static"
+        className={appBar}
+      >
         <Toolbar>
           {isHome ?
             <Home location={location} />
@@ -36,7 +27,7 @@ const Header = () => {
             <Vermehrung />
           : <Doku />}
         </Toolbar>
-      </StyledAppBar>
+      </AppBar>
     </ErrorBoundary>
   )
 }
