a8e5c8ab417900d35332731ffa4292f2c0a0ec7d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 14:12:02 +0100

    use css modules

--- src/components/Header/Doku/Filter/index.jsx
+++ src/components/Header/Doku/Filter/index.jsx
@@ -2,38 +2,12 @@ import { useContext } from 'react'
 import Input from '@mui/material/Input'
 import InputAdornment from '@mui/material/InputAdornment'
 import { FaTimes, FaFilter } from 'react-icons/fa'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
 import { MobxStoreContext } from '../../../../mobxStoreContext.js'
 import { FilterNumbers as Numbers } from './Numbers.jsx'
 
-const Container = styled.div`
-  height: 100%;
-  padding: 3px 6px;
-  display: flex;
-  border-radius: 3px;
-  background-color: #6625b5 !important;
-`
-const StyledInput = styled(Input)`
-  color: white !important;
-  div hr {
-    width: calc(100% - 20px) !important;
-  }
-  &:before,
-  &:after {
-    border-bottom: none !important;
-  }
-`
-const StyledFilterIcon = styled(FaFilter)`
-  pointer-events: auto;
-  color: white;
-`
-const StyledDeleteFilterIcon = styled(FaTimes)`
-  cursor: pointer;
-  pointer-events: auto;
-  color: ${(props) => (props['data-active'] ? 'white' : '#4a148c')};
-`
+import { container, input, filterIcon } from './index.module.css'
 
 export const DokuFilter = observer(() => {
   const store = useContext(MobxStoreContext)
@@ -42,8 +16,8 @@ export const DokuFilter = observer(() => {
   const onClickEmptyFilter = () => setDocFilter('')
 
   return (
-    <Container>
-      <StyledInput
+    <div className={container}>
+      <Input
         id="filterInput"
         value={docFilter}
         onChange={onChange}
@@ -55,23 +29,33 @@ export const DokuFilter = observer(() => {
         variant="outlined"
         startAdornment={
           <InputAdornment position="start">
-            <StyledFilterIcon />
+            <FaFilter className={filterIcon} />
           </InputAdornment>
         }
         endAdornment={
           <InputAdornment
             position="end"
             onClick={onClickEmptyFilter}
-            title="Filter entfernen"
+            title={
+              docFilter ? 'Filter entfernen' : (
+                'Filter entfernen (keiner gesetzt)'
+              )
+            }
           >
-            <StyledDeleteFilterIcon data-active={!!docFilter} />
+            <FaTimes
+              style={{
+                color: docFilter ? 'white' : '#4a148c',
+                cursor: docFilter ? 'pointer' : 'default',
+              }}
+            />
           </InputAdornment>
         }
+        className={input}
       />
       <Numbers
         totalCount={docsCount}
         filteredCount={docsFilteredCount}
       />
-    </Container>
+    </div>
   )
 })
