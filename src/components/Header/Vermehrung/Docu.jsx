2926a32efc0682bc03ee59d0efdbd5d0aa1d3d52
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 14:22:13 +0100

    use css modules

--- src/components/Header/Vermehrung/Docu.jsx
+++ src/components/Header/Vermehrung/Docu.jsx
@@ -3,22 +3,11 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuItem from '@mui/material/MenuItem'
 import { FaBook } from 'react-icons/fa'
-import styled from '@emotion/styled'
 import { Link } from 'react-router'
 import { observer } from 'mobx-react-lite'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
-
-const StyledButton = styled(Button)`
-  color: white !important;
-  border-color: rgba(255, 255, 255, 0.5) !important;
-  border-width: 0 !important;
-  text-transform: none !important;
-  margin-right: 5px !important;
-  &:hover {
-    border-width: 1px !important;
-  }
-`
+import { button } from './Docu.module.css'
 
 export const HeaderDocu = observer(({ asMenu }) => {
   const store = useContext(MobxStoreContext)
@@ -51,12 +40,13 @@ export const HeaderDocu = observer(({ asMenu }) => {
   }
 
   return (
-    <StyledButton
+    <Button
       variant="outlined"
       component={Link}
       to="/Dokumentation/"
+      className={button}
     >
       Dokumentation
-    </StyledButton>
+    </Button>
   )
 })
