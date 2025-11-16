fe6e697241407e51980d12bfb045b1f27f2a88bc
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 14:15:30 +0100

    use css modules

--- src/components/Header/Doku/Filter/Numbers.jsx
+++ src/components/Header/Doku/Filter/Numbers.jsx
@@ -1,32 +1,25 @@
-import styled from '@emotion/styled'
+import {
+  container,
+  innerContainer,
+  filtered,
+  total,
+} from './Numbers.module.css'
 
-const OuterContainer = styled.div`
-  padding: 0 2px 0 8px;
-  display: flex;
-  justify-content: center;
-`
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 0.7em;
-  font-weight: 400;
-  justify-content: center;
-  cursor: default;
-  user-select: none;
-  color: white;
-`
-const Filtered = styled.div`
-  text-align: center;
-`
-const Total = styled.div`
-  border-top: 1px solid white;
-  text-align: center;
-`
 export const FilterNumbers = ({ filteredCount, totalCount }) => (
-  <OuterContainer>
-    <Container>
-      <Filtered title="gefilterte Anzahl">{filteredCount}</Filtered>
-      <Total title="totale Anzahl">{totalCount}</Total>
-    </Container>
-  </OuterContainer>
+  <div className={container}>
+    <div className={innerContainer}>
+      <div
+        className={filtered}
+        title="gefilterte Anzahl"
+      >
+        {filteredCount}
+      </div>
+      <div
+        className={total}
+        title="totale Anzahl"
+      >
+        {totalCount}
+      </div>
+    </div>
+  </div>
 )
