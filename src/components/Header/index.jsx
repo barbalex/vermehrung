1197e54076eac644b5c4ebab4ede4ccbd5e4bca4
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 11:53:58 +0100

    use css variables

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,16 +4,15 @@ import styled from '@emotion/styled'
 import { useLocation } from 'react-router'
 
 import { ErrorBoundary } from '../shared/ErrorBoundary.jsx'
-import { constants } from '../../utils/constants.js'
 
 import { HeaderHome as Home } from './Home.jsx'
 import { HeaderDoku as Doku } from './Doku/index.jsx'
 import { HeaderVermehrung as Vermehrung } from './Vermehrung/index.jsx'
 
 const StyledAppBar = styled(AppBar)`
-  min-height: ${constants.appBarHeight}px !important;
+  min-height: var(--app-bar-height) !important;
   .MuiToolbar-root {
-    min-height: ${constants.appBarHeight}px !important;
+    min-height: var(--app-bar-height) !important;
     padding-left: 0 !important;
     padding-right: 10px !important;
   }
