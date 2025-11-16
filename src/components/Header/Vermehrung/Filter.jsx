f37c934f9dfd4ae378130880b52897cb84de1d00
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-16 18:35:33 +0100

    use css modules

--- src/components/Header/Vermehrung/Filter.jsx
+++ src/components/Header/Vermehrung/Filter.jsx
@@ -3,42 +3,18 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import { RiFilterFill, RiFilterLine } from 'react-icons/ri'
 import { MdDeleteSweep as DeleteFilterIcon } from 'react-icons/md'
-import styled from '@emotion/styled'
+import { styled } from '@mui/material/styles'
 import { observer } from 'mobx-react-lite'
 
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
+
+import { iconDiv } from './Filter.module.css'
 
 // tried to convert this to css modules
 // but mui somehow prevents the border of the button / iconButton from showing when showFilter is true
 // worked neither as style props nor as classes
 // cost me an hour of fighting against dragons, so I gave up
 
-const StyledButton = styled(Button)`
-  color: white !important;
-  border-color: rgba(255, 255, 255, 0.5) !important;
-  border-width: 0 !important;
-  text-transform: none !important;
-  margin-right: 5px !important;
-  height: 34px !important;
-  &:hover {
-    border-width: 1px !important;
-  }
-`
-const FilterButton = styled(StyledButton)`
-  border-width: ${(props) =>
-    props['data-active'] ? '1px !important' : '0 !important'};
-  height: 34px !important;
-`
-const StyledIconButton = styled(IconButton)`
-  ${(props) => props['data-active'] && 'border: 1px solid #9762d9 !important;'}
-  font-size: 2rem !important;
-`
-const IconDiv = styled.div`
-  svg {
-    height: 34px;
-    vertical-align: middle;
-  }
-`
 const StyledDeleteFilterIcon = styled(DeleteFilterIcon)`
   cursor: pointer;
   pointer-events: auto;
@@ -54,14 +30,24 @@ export const HeaderFilter = observer(() => {
 
   const onClickFilter = () => setShowFilter(!showFilter)
 
+  console.log('Filter', { singleColumnView, showFilter })
+
   if (singleColumnView) {
+    const StyledIconButton = styled(IconButton)({
+      border:
+        showFilter ?
+          '1px solid rgba(255, 255, 255, 0.5)'
+        : '0 solid rgba(255, 255, 255, 0.5)',
+      fontSize: '2rem',
+      '&:hover': { borderWidth: 1 },
+    })
+
     return (
       <StyledIconButton
         color="inherit"
         aria-label="Filter"
         onClick={onClickFilter}
         title="Filter"
-        data-active={showFilter}
       >
         {filtered ?
           <RiFilterFill />
@@ -70,15 +56,25 @@ export const HeaderFilter = observer(() => {
     )
   }
 
+  const StyledButton = styled(Button)({
+    color: 'white',
+    borderColor: 'rgba(255, 255, 255, 0.5)',
+    borderWidth: showFilter ? 1 : 0,
+    textTransform: 'none',
+    marginRight: 34,
+    height: 34,
+    '&:hover': { borderWidth: 1 },
+  })
+
   return (
-    <FilterButton
+    <StyledButton
       variant="outlined"
       onClick={onClickFilter}
-      data-active={showFilter}
     >
       Filter
       {filtered && (
-        <IconDiv
+        <div
+          className={iconDiv}
           aria-label="Alle Filter entfernen"
           title="Alle Filter entfernen"
           onClick={(e) => {
@@ -87,8 +83,8 @@ export const HeaderFilter = observer(() => {
           }}
         >
           <StyledDeleteFilterIcon />
-        </IconDiv>
+        </div>
       )}
-    </FilterButton>
+    </StyledButton>
   )
 })
