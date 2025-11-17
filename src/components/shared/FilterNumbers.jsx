940dac8ebec34676148e783b3641b062cc7a2ac9
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-17 20:51:02 +0100

    use css modules

--- src/components/shared/FilterNumbers.jsx
+++ src/components/shared/FilterNumbers.jsx
@@ -1,48 +1,42 @@
-import styled from '@emotion/styled'
 import MenuItem from '@mui/material/MenuItem'
 
-const OuterContainer = styled.div`
-  min-width: var(--title-row-height);
-  display: flex;
-  justify-content: center;
-`
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 0.7em;
-  font-weight: 500;
-  min-height: var(--title-row-height);
-  justify-content: center;
-  cursor: default;
-  user-select: none;
-`
-const Filtered = styled.div`
-  text-align: center;
-`
-const Total = styled.div`
-  border-top: 1px solid rgba(0, 0, 0, 0.54);
-  text-align: center;
-`
-const StyledMenuItem = styled(MenuItem)`
-  color: rgba(0, 0, 0, 0.54) !important;
-`
+import {
+  outerContainer,
+  container,
+  filtered,
+  total,
+  menuItem,
+} from './FilterNumbers.module.css'
 
 export const FilterNumbers = ({ filteredCount, totalCount, asMenu }) => {
   if (asMenu) {
     return (
-      <StyledMenuItem dense>
+      <MenuItem
+        dense
+        className={menuItem}
+      >
         <span title="gefilterte Anzahl">{filteredCount}</span>/
         <span title="totale Anzahl">{totalCount}</span>
-      </StyledMenuItem>
+      </MenuItem>
     )
   }
 
   return (
-    <OuterContainer>
-      <Container>
-        <Filtered title="gefilterte Anzahl">{filteredCount}</Filtered>
-        <Total title="totale Anzahl">{totalCount}</Total>
-      </Container>
-    </OuterContainer>
+    <div className={outerContainer}>
+      <div className={container}>
+        <div
+          className={filtered}
+          title="gefilterte Anzahl"
+        >
+          {filteredCount}
+        </div>
+        <div
+          className={total}
+          title="totale Anzahl"
+        >
+          {totalCount}
+        </div>
+      </div>
+    </div>
   )
 }
