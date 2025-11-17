8a763514af289887b2d905ba4a509892313b8868
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-17 11:39:47 +0100

    use css modules

--- src/components/QueuedQueries/QueuedQuery.jsx
+++ src/components/QueuedQueries/QueuedQuery.jsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import styled from '@emotion/styled'
 import { DateTime } from 'luxon'
 import { observer } from 'mobx-react-lite'
 import { FaUndoAlt } from 'react-icons/fa'
@@ -7,49 +6,7 @@ import IconButton from '@mui/material/IconButton'
 
 import { MobxStoreContext } from '../../mobxStoreContext.js'
 
-// to hover and style row, see: https://stackoverflow.com/a/48109479/712005
-const Value = styled.div`
-  padding: 5px 0;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: -1000%;
-    left: -1000%;
-    z-index: 1;
-    ${(props) => props.bt && 'border-top: 1px solid rgba(74,20,140,0.1);'}
-    border-bottom: 1px solid rgba(74,20,140,0.1);
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: -1px;
-    width: 1px;
-    z-index: 2;
-  }
-  &:nth-child(5n + 5)::after {
-    bottom: -1px;
-    right: 0;
-    left: -1000%;
-    height: 1px;
-    z-index: 3;
-    width: auto;
-    top: auto;
-  }
-  &:hover::before {
-    background-color: rgba(0, 0, 0, 0.05);
-  }
-`
-const Icon = styled.div`
-  justify-self: center;
-  padding: 5px 0;
-`
-const RevertButton = styled(IconButton)`
-  z-index: 4;
-`
+import { value, icon, revertButton } from './QueuedQuery.module.css'
 
 const valFromValue = (value) => {
   if (value === true) return 'wahr'
@@ -90,40 +47,71 @@ export const QueuedQuery = observer(({ qq, index }) => {
     removeQueuedQueryById(id)
   }
 
+  const valueStyle =
+    index === 0 ?
+      {
+        borderTop: '1px solid rgba(74,20,140,0.1)',
+      }
+    : {}
+
   return (
     <>
-      <Value bt={index === 0}>{`${DateTime.fromMillis(time).toFormat(
-        'yyyy.LL.dd HH.mm.ss',
-      )}`}</Value>
-      <Value bt={index === 0}>{revertTable}</Value>
-      <Value bt={index === 0}>{revertId}</Value>
-      <Value bt={index === 0}>
+      <div
+        className={value}
+        style={valueStyle}
+      >{`${DateTime.fromMillis(time).toFormat('yyyy.LL.dd HH.mm.ss')}`}</div>
+      <div
+        className={value}
+        style={valueStyle}
+      >
+        {revertTable}
+      </div>
+      <div
+        className={value}
+        style={valueStyle}
+      >
+        {revertId}
+      </div>
+      <div
+        className={value}
+        style={valueStyle}
+      >
         {isInsert ? 'neuer Datensatz' : revertField}
-      </Value>
-      <Value bt={index === 0}>
+      </div>
+      <div
+        className={value}
+        style={valueStyle}
+      >
         {isInsert ?
           ''
         : revertField ?
           valFromValue(revertValue)
         : JSON.parse(revertValues)}
-      </Value>
-      <Value bt={index === 0}>
+      </div>
+      <div
+        className={value}
+        style={valueStyle}
+      >
         {isInsert ?
           ''
         : revertField ?
           valFromValue(newValue)
         : JSON.parse(newValue)}
-      </Value>
-      <Icon bt={index === 0}>
-        <RevertButton
+      </div>
+      <div
+        className={icon}
+        style={valueStyle}
+      >
+        <IconButton
           title="widerrufen"
           aria-label="widerrufen"
           onClick={onClickRevert}
           size="small"
+          className={revertButton}
         >
           <FaUndoAlt />
-        </RevertButton>
-      </Icon>
+        </IconButton>
+      </div>
     </>
   )
 })
