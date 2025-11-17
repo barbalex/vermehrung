a0e4634e02d5f90149b573df1e940d8b2f38eef4
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-17 14:44:35 +0100

    use css modules

--- src/components/shared/Checkbox2StatesNonUpdatable.jsx
+++ src/components/shared/Checkbox2StatesNonUpdatable.jsx
@@ -1,31 +1,17 @@
+// not in use
 import { useState, useEffect } from 'react'
 import Checkbox from '@mui/material/Checkbox'
 import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
 import FormHelperText from '@mui/material/FormHelperText'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
-// without slight padding radio is slightly cut off!
-const Container = styled.div`
-  display: block;
-`
-const StyledFormControl = styled(FormControl)`
-  padding-left: 1px !important;
-  padding-bottom: 15px !important;
-`
-const StyledFormLabel = styled(FormLabel)`
-  padding-top: 10px !important;
-  padding-bottom: 8px !important;
-  font-size: 12px !important;
-  cursor: text;
-  user-select: none;
-  pointer-events: none;
-`
-const StyledCheckbox = styled(Checkbox)`
-  height: 2px !important;
-  width: 24px;
-`
+import {
+  container,
+  formControl,
+  formLabel,
+  checkbox,
+} from './Checkbox2States.module.css'
 
 export const Checkbox2StatesNonUpdatable = observer(
   ({
@@ -49,27 +35,34 @@ export const Checkbox2StatesNonUpdatable = observer(
     const checked = stateValue === true
 
     return (
-      <Container>
-        <StyledFormControl
+      <div className={container}>
+        <FormControl
           component="fieldset"
           error={!!message}
           aria-describedby={`${label}MessageText`}
           variant="standard"
+          className={formControl}
         >
-          <StyledFormLabel component="legend">{label}</StyledFormLabel>
-          <StyledCheckbox
+          <FormLabel
+            component="legend"
+            className={formLabel}
+          >
+            {label}
+          </FormLabel>
+          <Checkbox
             data-id={name}
             onClick={onClickButton}
             color="primary"
             checked={checked}
+            className={checkbox}
           />
           {!!message && (
             <FormHelperText id={`${label}MessageText`}>
               {message}
             </FormHelperText>
           )}
-        </StyledFormControl>
-      </Container>
+        </FormControl>
+      </div>
     )
   },
 )
