44d63975e3f9ed0b1a868eda2a73f9f1d7484df2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-17 21:47:27 +0100

    use css modules

--- src/components/shared/FormTitle.jsx
+++ src/components/shared/FormTitle.jsx
@@ -1,64 +1,37 @@
+// seems not used
 import { useContext } from 'react'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
 import { MobxStoreContext } from '../../mobxStoreContext.js'
 
-const Container = styled.div`
-  background-color: rgba(74, 20, 140, 0.1);
-  flex-shrink: 0;
-  display: flex;
-  @media print {
-    display: none !important;
-  }
-  height: var(--title-row-height);
-  justify-content: space-between;
-`
-const Title = styled.div`
-  font-weight: bold;
-  margin-top: auto;
-  margin-bottom: auto;
-`
-const Symbols = styled.div`
-  display: flex;
-  margin-top: auto;
-  margin-bottom: auto;
-`
-const FilterNumbers = styled.div`
-  padding-right: 8px;
-  cursor: default;
-  user-select: none;
-  padding-right: 5px;
-  margin-top: auto;
-  margin-bottom: auto;
-  min-width: var(--title-row-height);
-  text-align: center;
-`
+import {
+  container,
+  titleClass,
+  symbols,
+  filterNumbers,
+} from './FormTitle.module.css'
 
-const FormTitle = ({
-  title,
-  table,
-  rowsLength,
-  rowsFilteredLength,
-  filter,
-}) => {
-  const store = useContext(StoreContext)
-  const { filtered, show } = store.filter
+export const FormTitle = observer(
+  ({ title, table, rowsLength, rowsFilteredLength, filter }) => {
+    const store = useContext(StoreContext)
+    const { filtered, show } = store.filter
 
-  const titleText = filter ? `${title} Filter` : title
+    const titleText = filter ? `${title} Filter` : title
 
-  return (
-    <Container>
-      <Title>{titleText}</Title>
-      {table && (
-        <Symbols>
-          {(show || filtered) && (
-            <FilterNumbers title="Anzahl gefiltert/total">{`${rowsFilteredLength}/${rowsLength}`}</FilterNumbers>
-          )}
-        </Symbols>
-      )}
-    </Container>
-  )
-}
-
-export default observer(FormTitle)
+    return (
+      <div className={container}>
+        <div className={titleClass}>{titleText}</div>
+        {table && (
+          <div className={symbols}>
+            {(show || filtered) && (
+              <div
+                className={filterNumbers}
+                title="Anzahl gefiltert/total"
+              >{`${rowsFilteredLength}/${rowsLength}`}</div>
+            )}
+          </div>
+        )}
+      </div>
+    )
+  },
+)
