768dd291674234be49dfc90512de800da51cf24c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 11:37:01 +0100

    use css modules

--- src/components/shared/RadioButtonGroup.jsx
+++ src/components/shared/RadioButtonGroup.jsx
@@ -1,3 +1,4 @@
+// seems not in use
 import { useState, useEffect } from 'react'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
@@ -5,123 +6,112 @@ import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
 import FormHelperText from '@mui/material/FormHelperText'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import styled from '@emotion/styled'
-import { observer } from 'mobx-react-lite'
 
 import { toStringIfPossible } from '../../utils/toStringIfPossible.js'
 
-// without slight padding radio is slightly cut off!
-const StyledFormControl = styled(FormControl)`
-  padding-left: 1px !important;
-  padding-bottom: 19px !important;
-  break-inside: avoid;
-`
-const StyledFormLabel = styled(FormLabel)`
-  padding-top: 1px !important;
-  font-size: 12px !important;
-  cursor: text;
-  user-select: none;
-  pointer-events: none;
-  padding-bottom: 8px !important;
-`
-const StyledRadio = styled(Radio)`
-  height: 2px !important;
-`
+import { formControl, formLabel, radio } from './RadioButtonGroup.module.css'
 
-export const RadioButtonGroup = observer(
-  ({
-    value: valuePassed,
-    label,
-    name,
-    error,
-    helperText = '',
-    dataSource = [],
-    saveToDb,
-  }) => {
-    const [stateValue, setStateValue] = useState(valuePassed)
-    useEffect(() => {
-      setStateValue(valuePassed)
-    }, [valuePassed])
+export const RadioButtonGroup = ({
+  value: valuePassed,
+  label,
+  name,
+  error,
+  helperText = '',
+  dataSource = [],
+  saveToDb,
+}) => {
+  const [stateValue, setStateValue] = useState(valuePassed)
+  useEffect(() => {
+    setStateValue(valuePassed)
+  }, [valuePassed])
 
-    const onClickButton = (event) => {
-      /**
-       * if clicked element is active value: set null
-       * Problem: does not work on change event on RadioGroup
-       * because that only fires on changes
-       * Solution: do this in click event of button
-       */
-      const targetValue = event.target.value
-      // eslint-disable-next-line eqeqeq
-      if (targetValue !== undefined && targetValue == stateValue) {
-        // an already active option was clicked
-        // set value null
-        setStateValue(null)
-        const fakeEvent = {
-          target: {
-            value: null,
-            name,
-          },
-        }
-        return saveToDb(fakeEvent)
-      }
-    }
-
-    const onChangeGroup = (event) => {
-      // group only changes if value changes
-      const targetValue = event.target.value
-      // values are passed as strings > need to convert
-      const newValue =
-        targetValue === 'true' ? true
-        : targetValue === 'false' ? false
-        : isNaN(targetValue) ? targetValue
-        : +targetValue
-      setStateValue(newValue)
+  const onClickButton = (event) => {
+    /**
+     * if clicked element is active value: set null
+     * Problem: does not work on change event on RadioGroup
+     * because that only fires on changes
+     * Solution: do this in click event of button
+     */
+    const targetValue = event.target.value
+    // eslint-disable-next-line eqeqeq
+    if (targetValue !== undefined && targetValue == stateValue) {
+      // an already active option was clicked
+      // set value null
+      setStateValue(null)
       const fakeEvent = {
         target: {
-          value: newValue,
+          value: null,
           name,
         },
       }
-      saveToDb(fakeEvent)
+      return saveToDb(fakeEvent)
     }
+  }
 
-    const valueSelected =
-      stateValue !== null && stateValue !== undefined ?
-        toStringIfPossible(stateValue)
-      : ''
+  const onChangeGroup = (event) => {
+    // group only changes if value changes
+    const targetValue = event.target.value
+    // values are passed as strings > need to convert
+    const newValue =
+      targetValue === 'true' ? true
+      : targetValue === 'false' ? false
+      : isNaN(targetValue) ? targetValue
+      : +targetValue
+    setStateValue(newValue)
+    const fakeEvent = {
+      target: {
+        value: newValue,
+        name,
+      },
+    }
+    saveToDb(fakeEvent)
+  }
 
-    return (
-      <StyledFormControl
-        component="fieldset"
-        error={!!error}
-        aria-describedby={`${label}ErrorText`}
-        variant="standard"
+  const valueSelected =
+    stateValue !== null && stateValue !== undefined ?
+      toStringIfPossible(stateValue)
+    : ''
+
+  return (
+    <FormControl
+      component="fieldset"
+      error={!!error}
+      aria-describedby={`${label}ErrorText`}
+      variant="standard"
+      className={formControl}
+    >
+      <FormLabel
+        component="legend"
+        className={formLabel}
       >
-        <StyledFormLabel component="legend">{label}</StyledFormLabel>
-        <RadioGroup
-          aria-label={label}
-          value={valueSelected}
-          onChange={onChangeGroup}
-        >
-          {dataSource.map((e, index) => (
-            <FormControlLabel
-              key={index}
-              value={toStringIfPossible(e.value)}
-              control={<StyledRadio color="primary" />}
-              label={e.label}
-              onClick={onClickButton}
-            />
-          ))}
-        </RadioGroup>
-        {!!error && (
-          <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
-        )}
-        {!!helperText && (
-          <FormHelperText id={`${label}HelperText`}>
-            {helperText}
-          </FormHelperText>
-        )}
-      </StyledFormControl>
-    )
-  },
-)
+        {label}
+      </FormLabel>
+      <RadioGroup
+        aria-label={label}
+        value={valueSelected}
+        onChange={onChangeGroup}
+      >
+        {dataSource.map((e, index) => (
+          <FormControlLabel
+            key={index}
+            value={toStringIfPossible(e.value)}
+            control={
+              <Radio
+                color="primary"
+                className={radio}
+              />
+            }
+            label={e.label}
+            onClick={onClickButton}
+          />
+        ))}
+      </RadioGroup>
+      {!!error && (
+        <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
+      )}
+      {!!helperText && (
+        <FormHelperText id={`${label}HelperText`}>{helperText}</FormHelperText>
+      )}
+    </FormControl>
+  )
+}
