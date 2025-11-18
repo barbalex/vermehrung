85dfbf08960d5be129be1e757ae72eaf95fe85cd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 18:36:20 +0100

    clean

--- src/components/Data/Garten/FormTitle/Settings/index.jsx
+++ src/components/Data/Garten/FormTitle/Settings/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
 import MenuItem from '@mui/material/MenuItem'
 import { FaCog } from 'react-icons/fa'
@@ -7,7 +6,7 @@ import { FaCog } from 'react-icons/fa'
 import { ErrorBoundary } from '../../../../shared/ErrorBoundary.jsx'
 import { GartenSettingsMenu as Menu } from './Menu.jsx'
 
-export const GartenSettings = observer(({ asMenu }) => {
+export const GartenSettings = ({ asMenu }) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const onClickConfig = (event) => setAnchorEl(event.currentTarget)
 
@@ -47,4 +46,4 @@ export const GartenSettings = observer(({ asMenu }) => {
       />
     </ErrorBoundary>
   )
-})
+}
