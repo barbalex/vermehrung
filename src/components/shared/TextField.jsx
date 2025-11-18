93b6bd20b8063cf9346d938928d76a7d1dd2fa3c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 12:20:06 +0100

    use css modules

--- src/components/shared/TextField.jsx
+++ src/components/shared/TextField.jsx
@@ -3,87 +3,74 @@ import Input from '@mui/material/Input'
 import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
 import FormHelperText from '@mui/material/FormHelperText'
-import styled from '@emotion/styled'
-import { observer } from 'mobx-react-lite'
 
-const StyledFormControl = styled(FormControl)`
-  padding-bottom: 19px !important;
-  > div:before {
-    border-bottom-color: rgba(0, 0, 0, 0.1) !important;
-  }
-`
-const StyledInputLabel = styled(InputLabel)`
-  font-weight: ${(props) => props['data-weight']} !important;
-`
+import { formControl } from './TextField.module.css'
 
-export const TextField = observer(
-  ({
-    value,
-    label,
-    labelWeight = 400,
-    name,
-    type = 'text',
-    multiLine = false,
-    disabled = false,
-    hintText = '',
-    helperText = '',
-    error,
-    saveToDb,
-    schrinkLabel = true,
-  }) => {
-    const [stateValue, setStateValue] = useState(
-      value || value === 0 ? value : '',
-    )
-    useEffect(() => {
-      setStateValue(value || value === 0 ? value : '')
-    }, [value])
-    const onChange = (event) => setStateValue(event.target.value)
+export const TextField = ({
+  value,
+  label,
+  labelWeight = 400,
+  name,
+  type = 'text',
+  multiLine = false,
+  disabled = false,
+  hintText = '',
+  helperText = '',
+  error,
+  saveToDb,
+  schrinkLabel = true,
+}) => {
+  const [stateValue, setStateValue] = useState(
+    value || value === 0 ? value : '',
+  )
+  useEffect(() => {
+    setStateValue(value || value === 0 ? value : '')
+  }, [value])
+  const onChange = (event) => setStateValue(event.target.value)
 
-    const onKeyPress = (event) => {
-      if (event.key === 'Enter') {
-        saveToDb(event)
-      }
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      saveToDb(event)
     }
+  }
 
-    // once shrink is set, need to manually control ist
-    // shrink if value exists or shrinkLabel was passed
-    const schrink = schrinkLabel || !!value || value === 0
+  // once shrink is set, need to manually control ist
+  // shrink if value exists or shrinkLabel was passed
+  const schrink = schrinkLabel || !!value || value === 0
 
-    return (
-      <StyledFormControl
-        fullWidth
-        disabled={disabled}
-        error={!!error}
-        aria-describedby={`${label}ErrorText`}
-        variant="standard"
+  return (
+    <FormControl
+      fullWidth
+      disabled={disabled}
+      error={!!error}
+      aria-describedby={`${label}ErrorText`}
+      variant="standard"
+      className={formControl}
+    >
+      <InputLabel
+        htmlFor={label}
+        shrink={schrink}
+        style={{ fontWeight: labelWeight }}
       >
-        <StyledInputLabel
-          htmlFor={label}
-          shrink={schrink}
-          data-weight={labelWeight}
-        >
-          {label}
-        </StyledInputLabel>
-        <Input
-          id={label}
-          name={name}
-          value={stateValue}
-          type={type}
-          multiline={multiLine}
-          onChange={onChange}
-          onBlur={saveToDb}
-          onKeyPress={onKeyPress}
-          placeholder={hintText}
-        />
-        {!!error && (
-          <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
-        )}
-        {!!helperText && (
-          <FormHelperText id={`${label}HelperText`}>
-            {helperText}
-          </FormHelperText>
-        )}
-      </StyledFormControl>
-    )
-  },
-)
+        {label}
+      </InputLabel>
+      <Input
+        id={label}
+        name={name}
+        value={stateValue}
+        type={type}
+        multiline={multiLine}
+        onChange={onChange}
+        onBlur={saveToDb}
+        onKeyPress={onKeyPress}
+        placeholder={hintText}
+      />
+      {!!error && (
+        <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
+      )}
+      {!!helperText && (
+        <FormHelperText id={`${label}HelperText`}>{helperText}</FormHelperText>
+      )}
+    </FormControl>
+  )
+}
