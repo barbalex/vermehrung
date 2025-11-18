03c134692cf60c3a9a6305d77d0323d7e17196c5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 16:14:01 +0100

    use css modules

--- src/components/shared/History/index.jsx
+++ src/components/shared/History/index.jsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import styled from '@emotion/styled'
 import Button from '@mui/material/Button'
 import { observer } from 'mobx-react-lite'
 import { FaUndoAlt, FaArrowsAltH } from 'react-icons/fa'
@@ -11,38 +10,15 @@ import { Data } from '../Conflict/Data.jsx'
 import { MobxStoreContext } from '../../../mobxStoreContext.js'
 import { constants } from '../../../utils/constants.js'
 
-const Container = styled.div`
-  padding: 10px;
-`
-const Title = styled.h4`
-  margin-bottom: 0;
-`
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`
-const StyledIconButton = styled(IconButton)`
-  height: 18px;
-`
-const Rev = styled.span`
-  font-weight: normal;
-  padding-left: 7px;
-  color: rgba(0, 0, 0, 0.4);
-  font-size: 0.8em;
-`
-const ButtonRow = styled.div`
-  padding: 15px 0;
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-`
-const StyledButton = styled(Button)`
-  margin: 5px !important;
-  > span {
-    text-transform: none;
-  }
-`
+import {
+  container,
+  title,
+  titleRow,
+  iconButton,
+  revClass,
+  buttonRow,
+  button,
+} from './index.module.css'
 
 export const History = observer(
   ({ rev, dataArray, onClickWiderspruchUebernehmen }) => {
@@ -60,23 +36,25 @@ export const History = observer(
     }
 
     return (
-      <Container>
-        <TitleRow>
-          <Title>
-            Historische Version<Rev>{rev}</Rev>
-          </Title>
-          <StyledIconButton
+      <div className={container}>
+        <div className={titleRow}>
+          <h4 className={title}>
+            Historische Version<span className={revClass}>{rev}</span>
+          </h4>
+          <IconButton
             aria-label="Anleitung öffnen"
             title="Anleitung öffnen"
             onClick={openDocs}
             size="small"
+            className={iconButton}
           >
             <IoMdInformationCircleOutline />
-          </StyledIconButton>
-        </TitleRow>
+          </IconButton>
+        </div>
         <Data dataArray={dataArray} />
-        <ButtonRow>
-          <StyledButton
+        <div className={buttonRow}>
+          <Button
+            className={button}
             onClick={onClickWiderspruchUebernehmen}
             variant="outlined"
             title="Diese Version wiederherstellen"
@@ -84,8 +62,9 @@ export const History = observer(
             color="inherit"
           >
             {diffConflict ? 'grüne Werte wiederherstellen' : 'wiederherstellen'}
-          </StyledButton>
-          <StyledButton
+          </Button>
+          <Button
+            className={button}
             onClick={onClickToggleDiff}
             variant="outlined"
             title={
@@ -97,9 +76,9 @@ export const History = observer(
             color="inherit"
           >
             {diffConflict ? 'nicht vergleichen' : 'vergleichen'}
-          </StyledButton>
-        </ButtonRow>
-      </Container>
+          </Button>
+        </div>
+      </div>
     )
   },
 )
