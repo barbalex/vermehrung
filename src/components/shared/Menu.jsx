b08fc7ca3a1f44347d00fdfe7a42584be2dc4e86
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 09:43:00 +0100

    use css modules

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -2,29 +2,27 @@ import { useState } from 'react'
 import IconButton from '@mui/material/IconButton'
 import MuiMenu from '@mui/material/Menu'
 import { FaBars } from 'react-icons/fa'
-import styled from '@emotion/styled'
-
-const MenuButton = styled(IconButton)`
-  ${(props) => props['data-white'] && 'color: white !important;'}
-`
 
 export const Menu = ({ children, title = 'Menu', white = true }) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const closeMenu = () => setAnchorEl(null)
   const onClickButton = (event) => setAnchorEl(event.currentTarget)
 
+  const menuButtonStyle = white ? { color: 'white' } : {}
+
   return (
     <>
-      <MenuButton
+      <IconButton
         aria-controls="menu"
         aria-haspopup="true"
         aria-label={title}
         title={title}
         onClick={onClickButton}
         data-white={white}
+        style={menuButtonStyle}
       >
         <FaBars />
-      </MenuButton>
+      </IconButton>
       <MuiMenu
         id="menu"
         anchorEl={anchorEl}
