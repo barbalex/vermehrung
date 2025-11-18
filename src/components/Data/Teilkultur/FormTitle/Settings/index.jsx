ecb00bceb5252e7a5da3a2fee9575596deafe40d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 18:41:38 +0100

    clean

--- src/components/Data/Teilkultur/FormTitle/Settings/index.jsx
+++ src/components/Data/Teilkultur/FormTitle/Settings/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
 import MenuItem from '@mui/material/MenuItem'
 import { FaCog } from 'react-icons/fa'
@@ -7,7 +6,7 @@ import { FaCog } from 'react-icons/fa'
 import { ErrorBoundary } from '../../../../shared/ErrorBoundary.jsx'
 import { TeilkulturSettingsMenu as Menu } from './Menu.jsx'
 
-export const TeilkulturSettings = observer(({ asMenu, kulturId }) => {
+export const TeilkulturSettings = ({ asMenu, kulturId }) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const onClickConfig = (event) => setAnchorEl(event.currentTarget)
 
@@ -49,4 +48,4 @@ export const TeilkulturSettings = observer(({ asMenu, kulturId }) => {
       />
     </ErrorBoundary>
   )
-})
+}
--- src/components/shared/Checkbox2StatesNonUpdatable.jsx
+++ src/components/shared/Checkbox2StatesNonUpdatable.jsx
@@ -4,59 +4,54 @@ import Checkbox from '@mui/material/Checkbox'
 import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
 import FormHelperText from '@mui/material/FormHelperText'
-import { observer } from 'mobx-react-lite'
 
 import { container, formLabel, checkbox } from './Checkbox2States.module.css'
 
-export const Checkbox2StatesNonUpdatable = observer(
-  ({
-    label,
-    name,
-    value: valuePassed,
-    message: messagePassed = 'Dieser Wert ist nicht veränderbar',
-  }) => {
-    const [stateValue, setStateValue] = useState(valuePassed)
-    useEffect(() => {
-      setStateValue(valuePassed)
-    }, [valuePassed])
+export const Checkbox2StatesNonUpdatable = ({
+  label,
+  name,
+  value: valuePassed,
+  message: messagePassed = 'Dieser Wert ist nicht veränderbar',
+}) => {
+  const [stateValue, setStateValue] = useState(valuePassed)
+  useEffect(() => {
+    setStateValue(valuePassed)
+  }, [valuePassed])
 
-    const [message, setMessage] = useState(messagePassed)
-    const onClickButton = () => {
-      setMessage(message)
-      // can fire after component was unmounted...
-      setTimeout(() => setMessage(null), 10000)
-    }
+  const [message, setMessage] = useState(messagePassed)
+  const onClickButton = () => {
+    setMessage(message)
+    // can fire after component was unmounted...
+    setTimeout(() => setMessage(null), 10000)
+  }
 
-    const checked = stateValue === true
+  const checked = stateValue === true
 
-    return (
-      <div className={container}>
-        <FormControl
-          component="fieldset"
-          error={!!message}
-          aria-describedby={`${label}MessageText`}
-          variant="standard"
+  return (
+    <div className={container}>
+      <FormControl
+        component="fieldset"
+        error={!!message}
+        aria-describedby={`${label}MessageText`}
+        variant="standard"
+      >
+        <FormLabel
+          component="legend"
+          className={formLabel}
         >
-          <FormLabel
-            component="legend"
-            className={formLabel}
-          >
-            {label}
-          </FormLabel>
-          <Checkbox
-            data-id={name}
-            onClick={onClickButton}
-            color="primary"
-            checked={checked}
-            className={checkbox}
-          />
-          {!!message && (
-            <FormHelperText id={`${label}MessageText`}>
-              {message}
-            </FormHelperText>
-          )}
-        </FormControl>
-      </div>
-    )
-  },
-)
+          {label}
+        </FormLabel>
+        <Checkbox
+          data-id={name}
+          onClick={onClickButton}
+          color="primary"
+          checked={checked}
+          className={checkbox}
+        />
+        {!!message && (
+          <FormHelperText id={`${label}MessageText`}>{message}</FormHelperText>
+        )}
+      </FormControl>
+    </div>
+  )
+}
