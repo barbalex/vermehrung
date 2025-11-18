1a8c734fd103816e124de200a71980f832bb0a33
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-18 16:06:55 +0100

    use css modules

--- src/components/shared/ConflictList/index.jsx
+++ src/components/shared/ConflictList/index.jsx
@@ -1,23 +1,20 @@
-import { observer } from 'mobx-react-lite'
-import styled from '@emotion/styled'
-
 import { Conflict } from './Conflict.jsx'
 
-const Konflikte = styled.div`
-  margin-bottom: 15px;
-`
+import { container } from './index.module.css'
 
-export const ConflictList = observer(
-  ({ conflicts, activeConflict, setActiveConflict }) => (
-    <Konflikte>
-      {[...conflicts].sort().map((conflict) => (
-        <Conflict
-          key={conflict}
-          conflict={conflict}
-          activeConflict={activeConflict}
-          setActiveConflict={setActiveConflict}
-        />
-      ))}
-    </Konflikte>
-  ),
+export const ConflictList = ({
+  conflicts,
+  activeConflict,
+  setActiveConflict,
+}) => (
+  <div className={container}>
+    {[...conflicts].sort().map((conflict) => (
+      <Conflict
+        key={conflict}
+        conflict={conflict}
+        activeConflict={activeConflict}
+        setActiveConflict={setActiveConflict}
+      />
+    ))}
+  </div>
 )
