62e6195aa100d9565b03439c66617eec8fae5bd6
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-18 15:10:49 +0100

    import css modules un-destructured

--- src/components/Data/Kultur/Form/Timeline/Tooltip.jsx
+++ src/components/Data/Kultur/Form/Timeline/Tooltip.jsx
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 
 import { exists } from '../../../../../utils/exists.js'
-import { popup, pRow, pTitle, ereignisClass } from './Tooltip.module.css'
+import styles from './Tooltip.module.css'
 
 // somehow payload sometimes arrives as undefined, so set to []
 export const KulturTooltip = ({
@@ -27,10 +27,10 @@ export const KulturTooltip = ({
     const title = DateTime.fromMillis(label).toFormat('yyyy.LL.dd')
 
     return (
-      <div className={popup}>
-        <div className={pTitle}>
+      <div className={styles.popup}>
+        <div className={styles.pTitle}>
           {title}
-          <div className={ereignisClass}>{ereignis}</div>
+          <div className={styles.ereignisClass}>{ereignis}</div>
         </div>
         {payload?.map((o, i) => {
           // do not repeat Zählung/Lieferung all over the place
@@ -42,36 +42,36 @@ export const KulturTooltip = ({
             return (
               <div key={`${o.dataKey}0`}>
                 <div
-                  className={pRow}
+                  className={styles.pRow}
                   key={o.dataKey}
                 >{`${label}: ${o.value}`}</div>
                 {exists(o.payload['Lieferung Gramm Samen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}1`}
                   >{`Gramm Samen: ${o.payload['Lieferung Gramm Samen']}`}</div>
                 )}
                 {exists(o.payload['Bedarf']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}10`}
                   >{`Bedarf: ${o.payload['Bedarf']}`}</div>
                 )}
                 {exists(o.payload['Zählung andere Mengen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}2`}
                   >{`andere Mengen: ${o.payload['Zählung andere Mengen']}`}</div>
                 )}
                 {exists(o.payload['Lieferung andere Mengen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}3`}
                   >{`andere Mengen: ${o.payload['Lieferung andere Mengen']}`}</div>
                 )}
                 {exists(o.payload['Lieferung von Anzahl Individuen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}4`}
                   >{`von Anzahl Individuen: ${o.payload['Lieferung von Anzahl Individuen']}`}</div>
                 )}
@@ -79,7 +79,7 @@ export const KulturTooltip = ({
                   o.payload['Zählung Beschreibung auspflanzbereite Pflanzen'],
                 ) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}5`}
                   >{`Beschreibung auspflanzbereite Pflanzen: ${o.payload['Zählung Beschreibung auspflanzbereite Pflanzen']}`}</div>
                 )}
@@ -87,19 +87,19 @@ export const KulturTooltip = ({
                   o.payload['Lieferung Beschreibung auspflanzbereite Pflanzen'],
                 ) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}6`}
                   >{`Beschreibung auspflanzbereite Pflanzen: ${o.payload['Lieferung Beschreibung auspflanzbereite Pflanzen']}`}</div>
                 )}
                 {exists(o.payload['Zählung Bemerkungen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}7`}
                   >{`Bemerkungen: ${o.payload['Zählung Bemerkungen']}`}</div>
                 )}
                 {exists(o.payload['Lieferung Bemerkungen']) && (
                   <div
-                    className={pRow}
+                    className={styles.pRow}
                     key={`${o.dataKey}8`}
                   >{`Bemerkungen: ${o.payload['Lieferung Bemerkungen']}`}</div>
                 )}
@@ -108,7 +108,7 @@ export const KulturTooltip = ({
           }
           return (
             <div
-              className={pRow}
+              className={styles.pRow}
               key={o.dataKey}
             >{`${label}: ${o.value}`}</div>
           )
--- src/components/Data/Lieferung/Lieferung/Form/Von.jsx
+++ src/components/Data/Lieferung/Lieferung/Form/Von.jsx
@@ -12,11 +12,8 @@ import { kultursSortedFromKulturs } from '../../../../../utils/kultursSortedFrom
 import { sammlungsSortedFromSammlungs } from '../../../../../utils/sammlungsSortedFromSammlungs.js'
 import { herkunftLabelFromHerkunft } from '../../../../../utils/herkunftLabelFromHerkunft.js'
 
-import {
-  herkunftClass,
-  herkunftLabelClass,
-} from './Von.module.css'
-import { title, titleRow,  } from './Wann.module.css'
+import vonStyles from './Von.module.css'
+import wannStyles from './Wann.module.css'
 
 export const LieferungVon = observer(
   ({ showFilter, row, saveToDb, ifNeeded, herkunft, herkunftQuelle }) => {
@@ -158,12 +155,12 @@ export const LieferungVon = observer(
     return (
       <>
         <div
-          className={titleRow}
+          className={wannStyles.titleRow}
           style={{
             backgroundColor: showFilter ? '#ffe0b2' : 'rgba(248, 243, 254, 1)',
           }}
         >
-          <div className={title}>von</div>
+          <div className={wannStyles.title}>von</div>
         </div>
         {ifNeeded('von_sammlung_id') && (
           <Select
@@ -194,10 +191,10 @@ export const LieferungVon = observer(
           />
         )}
         <div
-          className={herkunftClass}
+          className={vonStyles.herkunftClass}
           style={{ ...(herkunft ? { color: '#c1c1c1' } : {}) }}
         >
-          <div className={herkunftLabelClass}>
+          <div className={vonStyles.herkunftLabelClass}>
             {herkunft ? `Herkunft (aus ${herkunftQuelle})` : 'Herkunft'}
           </div>
           {herkunftLabel}
