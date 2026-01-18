212f74637d25aba6b20028b7ff29d48e87942b29
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-18 15:45:28 +0100

    import css modules un-destructured

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,89 +6,83 @@ import image from '../images/puls_vulg_2500.webp'
 import placeholderSrc from '../images/puls_vulg_500.webp' // TODO: build small placeholder
 import { ProgressiveImg } from '../components/shared/ProgressiveImg.tsx'
 
-import {
-  outerContainer,
-  scrollContainer,
-  container,
-  cardContainer,
-  card,
-  pageTitle,
-  cardTitle,
-} from './index.module.css'
+import styles from './index.module.css'
 
 export const Home = () => (
   <>
     <ErrorBoundary>
-      <div className={outerContainer}>
+      <div className={styles.outerContainer}>
         <ProgressiveImg
           src={image}
           placeholderSrc={placeholderSrc}
         />
-        <div className={scrollContainer}>
-          <div className={container}>
+        <div className={styles.scrollContainer}>
+          <div className={styles.container}>
             <Typography
               align="center"
               variant="h6"
               color="inherit"
-              className={pageTitle}
+              className={styles.pageTitle}
             >
               Bedrohte Pflanzen vermehren
             </Typography>
-            <div className={cardContainer}>
-              <Card className={card}>
-                <h3 className={cardTitle}>Arten</h3>
+            <div className={styles.cardContainer}>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Arten</h3>
                 Vom Aussterben bedrohte Pflanzen-Arten werden gesammelt,
                 vermehrt und wieder ausgesetzt.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Herkünfte</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Herkünfte</h3>
                 Hier gibt es noch ursprüngliche Bestände der Arten.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Sammlungen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Sammlungen</h3>
                 ...beschreiben das Sammeln an Herkunfts-Orten.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Gärten</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Gärten</h3>
                 In Gärten und Gärtnereien werden die Arten in Kultur vermehrt
                 und auf das Aussetzen vorbereitet.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Kulturen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Kulturen</h3>
                 Pflanzen einer Art und einer Herkunft in einem Garten bilden
                 eine Kultur.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Teil-Kulturen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Teil-Kulturen</h3>
                 {`Gärtnereien organisieren ihre Kulturen oft in Untereinheiten, wie zum Beispiel: Beete, Kästen, Reihen.`}
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Lieferungen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Lieferungen</h3>
                 <p>Sammlungen werden an Kulturen geliefert.</p>
                 <p>
                   Aus Kulturen werden Pflanzen an andere Kulturen geliefert.
                   Oder in geeigneten Lebensräumen ausgesetzt.
                 </p>
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Zählungen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Zählungen</h3>
                 <p>
                   Regelmässig wird der Bestand der Kulturen erfasst und die
                   Vermehrung geplant.
                 </p>
                 <p>So können Sammlungen und Aussetzungen geplant werden.</p>
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Events und Aufgaben</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Events und Aufgaben</h3>
                 ...erleichtern Verwaltung und Dokumentation des Projekts.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Personen</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Personen</h3>
                 Die am Projekt beteiligten. Zum Beispiel Freiwillige, die in
                 ihren Gärten Kulturen pflegen.
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Wer organisiert das Projekt?</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>
+                  Wer organisiert das Projekt?
+                </h3>
                 <a
                   href="//toposmm.ch"
                   target="_blank"
@@ -106,8 +100,8 @@ export const Home = () => (
                 </a>
                 .
               </Card>
-              <Card className={card}>
-                <h3 className={cardTitle}>Interessiert?</h3>
+              <Card className={styles.card}>
+                <h3 className={styles.cardTitle}>Interessiert?</h3>
                 <p>
                   Sie haben einen Garten und möchten bedrohte Pflanzen
                   vermehren? Fragen Sie&nbsp;
