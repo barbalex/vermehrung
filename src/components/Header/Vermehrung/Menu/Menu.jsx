fb3cfbea7a4594b9997114306cfb69d0194c94d5
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-28 17:39:46 +0100

    up version

--- src/components/Header/Vermehrung/Menu/Menu.jsx
+++ src/components/Header/Vermehrung/Menu/Menu.jsx
@@ -118,7 +118,7 @@ export const HeaderHamburgerMenu = observer(
             labelPlacement="end"
           />
         </MenuItem>
-        <div className={styles.version}>Version: 1.21.31 vom 28.1.2026</div>
+        <div className={styles.version}>Version: 1.21.32 vom 28.1.2026</div>
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von vermehrung.ch
         </MenuItem>
