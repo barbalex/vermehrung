41ed5b5c80535b3153ecfc499080a283eb82a916
Alexander Gabriel <alex.barbalex@gmail.com>
2026-06-28 14:25:51 +0200

    correct

--- src/components/Header/Vermehrung/NavTree.jsx
+++ src/components/Header/Vermehrung/NavTree.jsx
@@ -39,29 +39,29 @@ export const HeaderNavTree = observer(() => {
         <path
           fill="none"
           stroke="#fff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="29.322"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="29.322"
           d="M236.17 198h241.81"
         />
         <g fill="#fff">
           <path
             stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="35.157"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="35.157"
             d="M131.36 432h347.63"
           />
-          <g stroke-width=".374">
+          <g strokeWidth=".374">
             <path d="m199.46 202.39-56.47 45.729c-3.903 3.16-10.215 3.16-14.076 0l-9.384-7.6c-3.903-3.16-3.903-8.27 0-11.398l40.028-32.414-40.028-32.414c-3.903-3.16-3.903-8.272 0-11.4l9.342-7.666c3.904-3.16 10.215-3.16 14.076 0l56.47 45.73c3.945 3.16 3.945 8.271.042 11.431zM308.89 326.76l-56.47 45.729c-3.903 3.16-10.215 3.16-14.076 0l-9.384-7.6c-3.903-3.16-3.903-8.27 0-11.398l40.028-32.414-40.028-32.414c-3.903-3.16-3.903-8.272 0-11.4l9.343-7.666c3.903-3.16 10.215-3.16 14.076 0l56.47 45.73c3.944 3.16 3.944 8.271.041 11.431zM91.574 439.05l-56.47 45.729c-3.903 3.16-10.215 3.16-14.076 0l-9.384-7.6c-3.903-3.16-3.903-8.27 0-11.398l40.028-32.414-40.028-32.414c-3.903-3.16-3.903-8.272 0-11.4l9.342-7.666c3.903-3.16 10.215-3.16 14.076 0l56.47 45.73c3.945 3.16 3.945 8.271.042 11.431z" />
           </g>
         </g>
         <path
           fill="none"
           stroke="#fff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="35.176"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="35.176"
           d="M131.01 73.556h348.01"
         />
         <path
@@ -71,9 +71,9 @@ export const HeaderNavTree = observer(() => {
         <path
           fill="#fff"
           stroke="#fff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="22.888"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="22.888"
           d="M334.39 319.59h147.34"
         />
       </svg>
